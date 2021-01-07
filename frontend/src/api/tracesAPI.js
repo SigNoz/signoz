61b3164d28cbce1ce046e99d0eeb788888f5c8bc
Ankit Nayan <ankit@signoz.io>
2021-01-07 18:04:41 +0530

    frontend working with LB

--- frontend/src/api/tracesAPI.js
+++ frontend/src/api/tracesAPI.js
@@ -5,7 +5,7 @@ export default axios.create({
   // baseURL: 'http://104.211.113.204:8080/api/v1/'
   // baseURL: process.env.QUERY_SERVICE_URL,
   // console.log('in traces API', process.env.QUERY_SERVICE_URL)
-  baseURL: process.env.REACT_APP_QUERY_SERVICE_URL + '/api/v1',
+  baseURL: 'api/v1/',
 
 });
 
