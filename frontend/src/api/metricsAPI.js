19dc319381d78caa08ab4d9f67d548d68f02c29a
Pranay Prateek <pranay.iitm@gmail.com>
2021-01-07 20:14:40 +0530

    updated metric API URL

--- frontend/src/api/metricsAPI.js
+++ frontend/src/api/metricsAPI.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export default axios.create({
-     baseURL: 'http://104.211.113.204:8080/api/v1/',
+    //  baseURL: 'http://104.211.113.204:8080/api/v1/',
     
-    // baseURL: 'http://localhost:3000/api/v1/',
+    baseURL: 'http://localhost:3000/api/v1/',
     
 }
 );
