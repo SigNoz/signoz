c708b296571dd7326fe65be48d2c6f7635089ce1
Himanshu DIxit <himanshu@signoz.io>
2021-02-23 20:15:54 +0530

    Remove unused var and change baseURl

--- frontend/src/constants/env.ts
+++ frontend/src/constants/env.ts
@@ -1,6 +1,3 @@
-import {IS_PRODUCTION} from "../../../../sass/frontend/src/constants/env";
-
 export const ENVIRONMENT = {
-    // baseURL: "",
-    baseURL:  "https://api.signoz.io",
+    baseURL: "",
 };
