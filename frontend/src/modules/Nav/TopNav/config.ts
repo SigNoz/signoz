de4adeded599b2dd3c8bed1574d24737ab18112f
dhrubesh <dhrubesh97@gmail.com>
2021-05-13 20:06:44 +0530

    creates 2 diff config for datepicker

---
+++ frontend/src/modules/Nav/TopNav/config.ts
@@ -0,0 +1,14 @@
+export const DefaultOptions = [
+	{ value: "custom", label: "Custom" },
+	{ value: "15min", label: "Last 15 min" },
+	{ value: "30min", label: "Last 30 min" },
+	{ value: "1hr", label: "Last 1 hour" },
+	{ value: "6hr", label: "Last 6 hour" },
+	{ value: "1day", label: "Last 1 day" },
+	{ value: "1week", label: "Last 1 week" },
+];
+
+export const ServiceMapOptions = [
+	{ value: "1min", label: "Last 1 min" },
+	{ value: "1day", label: "Last 1 day" },
+];
