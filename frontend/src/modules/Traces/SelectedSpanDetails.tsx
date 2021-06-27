421a102291cfb1fc1fbbc0fb31d8268e2fd5f0e9
Nidhi Tandon <nidhitandon08@gmail.com>
2021-06-27 08:47:46 +0530

    refactor: add ts support to selectedSpanDetails and TraceGanttChartHelpers, change folder structure

--- frontend/src/modules/Traces/SelectedSpanDetails.tsx
+++ frontend/src/modules/Traces/SelectedSpanDetails.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Card, Space, Tabs, Typography } from "antd";
 import styled from "styled-components";
-import { pushDStree } from "../../store/actions";
+import { pushDStree } from "Src/store/actions";
 
 const { TabPane } = Tabs;
 
 const { Text } = Typography;
 
 interface SelectedSpanDetailsProps {
-	data: pushDStree
+	data: pushDStree;
 }
 
 const Title = styled(Text)`
@@ -17,79 +17,82 @@ const Title = styled(Text)`
 `;
 
 const SelectedSpanDetails = (props: SelectedSpanDetailsProps) => {
-
-	let spanTags = props.data.tags;
+	let spanTags = props.data?.tags;
 	let service = props.data?.name?.split(":")[0];
 	let operation = props.data?.name?.split(":")[1];
 
 	return (
-		<Card style={{ border: "none", background: "transparent", padding: 0 }} bodyStyle={{ padding: 0 }}>
+		<Card
+			style={{ border: "none", background: "transparent", padding: 0 }}
+			bodyStyle={{ padding: 0 }}
+		>
 			<Space direction="vertical">
-
 				<strong> Details for selected Span </strong>
 				<Space direction="vertical" size={2}>
-					<Text style={{ marginTop: "18px" }}>
-						Service
-					</Text>
-					<Title style={{ color: "#2D9CDB", fontSize: "12px" }}>
-						{service}
-					</Title>
+					<Text style={{ marginTop: "18px" }}>Service</Text>
+					<Title style={{ color: "#2D9CDB", fontSize: "12px" }}>{service}</Title>
 				</Space>
 				<Space direction="vertical" size={2}>
-					<Text>
-						Operation
-					</Text>
-					<Text style={{ color: "#2D9CDB", fontSize: "12px" }}>
-						{operation}
-					</Text>
+					<Text>Operation</Text>
+					<Text style={{ color: "#2D9CDB", fontSize: "12px" }}>{operation}</Text>
 				</Space>
 			</Space>
 			<Tabs defaultActiveKey="1">
 				<TabPane tab="Tags" key="1">
-					{spanTags && spanTags.map((tags, index) => {
-						return (
-							<>
-								{tags.value && (
-									<>
-										<Text style={{ color: "#BDBDBD", fontSize: "12px", marginBottom: "8px" }}>
-											{tags.key}
-										</Text>
-										<div style={{
+					{spanTags &&
+						spanTags.map((tags, index) => {
+							return (
+								<>
+									{tags.value && (
+										<>
+											<Text
+												style={{ color: "#BDBDBD", fontSize: "12px", marginBottom: "8px" }}
+											>
+												{tags.key}
+											</Text>
+											<div
+												style={{
+													background: "#4F4F4F",
+													color: "#2D9CDB",
+													fontSize: "12px",
+													padding: "6px 8px",
+													wordBreak: "break-all",
+													marginBottom: "16px",
+												}}
+											>
+												{tags.key === "error" ? "true" : tags.value}
+											</div>
+										</>
+									)}
+								</>
+							);
+						})}
+				</TabPane>
+				<TabPane tab="Errors" key="2">
+					{spanTags &&
+						spanTags
+							.filter((tags) => tags.key === "error")
+							.map((error) => (
+								<>
+									<Text
+										style={{ color: "#BDBDBD", fontSize: "12px", marginBottom: "8px" }}
+									>
+										{error.key}
+									</Text>
+									<div
+										style={{
 											background: "#4F4F4F",
 											color: "#2D9CDB",
 											fontSize: "12px",
 											padding: "6px 8px",
 											wordBreak: "break-all",
 											marginBottom: "16px",
-										}}>
-											{tags.key === "error" ? "true" : tags.value}
-										</div>
-									</>
-								)}
-							</>
-						);
-					})}
-				</TabPane>
-				<TabPane tab="Errors" key="2">
-					{spanTags && spanTags
-						.filter((tags) => tags.key === "error")
-						.map((error) => (
-							<>
-								<Text style={{ color: "#BDBDBD", fontSize: "12px", marginBottom: "8px" }}>
-									{error.key}
-								</Text>
-								<div style={{
-									background: "#4F4F4F",
-									color: "#2D9CDB",
-									fontSize: "12px",
-									padding: "6px 8px",
-									wordBreak: "break-all",
-									marginBottom: "16px",
-								}}>
-									true
-								</div>
-							</>
-						))}
+										}}
+									>
+										true
+									</div>
+								</>
+							))}
 				</TabPane>
 			</Tabs>
 		</Card>
