0e11435ac1ea1d15eefc8062c727636f65e4e1bb
Nidhi Tandon <nidhitandon08@gmail.com>
2021-07-06 10:06:35 +0530

    feat(FE:TraceGanttChart): scroll to selected scroll on page load

--- frontend/src/modules/Traces/TraceGanttChart/TraceGanttChart.tsx
+++ frontend/src/modules/Traces/TraceGanttChart/TraceGanttChart.tsx
@@ -229,24 +229,26 @@ const TraceGanttChart = ({
 	};
 
 	const handleResetFocus = () => {
-		let rows = document.querySelectorAll("#collapsable table tbody tr");
+		const rows = document.querySelectorAll('#collapsable table tbody tr');
 		Array.from(rows).map((row) => {
-			row.classList.remove("hide");
+			row.classList.remove('hide');
 		});
 
 		resetZoom(true);
 	};
 
-	const handleScroll = (id) => {
-		let rows = document.querySelectorAll("#collapsable table tbody tr");
-		const table = document.querySelectorAll("#collapsable table");
-		Array.from(rows).map((row) => {
-			let attribKey = row.getAttribute("data-row-key");
-			if (id === attribKey) {
-				let scrollValue = row.offsetTop;
-				table[1].scrollTop = scrollValue;
-			}
-		});
+	const handleScroll = (id: string): void => {
+		if (!isEmpty(id)) {
+			const rows = document.querySelectorAll<HTMLElement>('#collapsable table tbody tr');
+			const table = document.querySelectorAll<HTMLElement>('#collapsable .ant-table-body');
+			Array.from(rows).map((row) => {
+				const attribKey = row.getAttribute('data-row-key');
+				if (id === attribKey) {
+					const scrollValue = row.offsetTop;
+					table[0].scrollTo(0, scrollValue);
+				}
+			});
+		}
 	};
 
 	const rowSelection = {
