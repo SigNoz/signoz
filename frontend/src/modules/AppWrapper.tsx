ee04e3601de8c3f19728424fb46d90b8d70342c0
Vimalraj571 <8166149+Vimalraj571@users.noreply.github.com>
2021-07-25 23:24:48 +0530

    fix(FE):React-Error-Boundary

--- frontend/src/modules/AppWrapper.tsx
+++ frontend/src/modules/AppWrapper.tsx
@@ -1,9 +1,10 @@
 import React, { Suspense } from "react";
-import { Spin } from "antd";
+import { Spin, Alert, Row, Col } from "antd";
 import { useThemeSwitcher } from "react-css-theme-switcher";
 import ROUTES from "Src/constants/routes";
 import { IS_LOGGED_IN } from "Src/constants/auth";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { ErrorBoundary } from 'react-error-boundary';
 
 import BaseLayout from "./BaseLayout";
 import {
@@ -19,60 +20,75 @@ import {
 } from "Src/pages";
 import { RouteProvider } from "./RouteProvider";
 
+function ErrorFallback({ error }) {
+	return (
+		<Row justify="space-around" align="middle" style={{ margin: 200 }}>
+			<Col span={12}>
+				<Alert
+					message="Error Something Went Wrong!"
+					showIcon
+					description="Hi Sorry Due to Some technical difficulty this error Happened We are Working hard fix this! Please Reload the Page."
+					type="error"
+				/>
+			</Col>
+		</Row>
+	)
+}
+
 const App = () => {
 	const { status } = useThemeSwitcher();
-
 	if (status === "loading") {
 		return null;
 	}
 
 	return (
 		<BrowserRouter>
-			<Suspense fallback={<Spin size="large" />}>
-				<Route path={"/"}>
-					<Switch>
-						<RouteProvider>
-							<BaseLayout>
-								<Suspense fallback={<Spin size="large" />}>
-									<Route path={ROUTES.SIGN_UP} exact component={Signup} />
-									<Route path={ROUTES.APPLICATION} exact component={ServicesTable} />
-									<Route
-										path={ROUTES.SERVICE_METRICS}
-										exact
-										component={ServiceMetrics}
-									/>
-									<Route path={ROUTES.SERVICE_MAP} exact component={ServiceMap} />
-									<Route path={ROUTES.TRACES} exact component={TraceDetail} />
-									<Route path={ROUTES.TRACE_GRAPH} exact component={TraceGraph} />
-									<Route path={ROUTES.SETTINGS} exact component={SettingsPage} />
-									<Route
-										path={ROUTES.INSTRUMENTATION}
-										exact
-										component={IntstrumentationPage}
-									/>
-									<Route
-										path={ROUTES.USAGE_EXPLORER}
-										exactexact
-										component={UsageExplorer}
-									/>
-									<Route
-										path="/"
-										exact
-										render={() => {
-											return localStorage.getItem(IS_LOGGED_IN) === "yes" ? (
-												<Redirect to={ROUTES.APPLICATION} />
-											) : (
-												<Redirect to={ROUTES.SIGN_UP} />
-											);
-										}}
-									/>
-								</Suspense>
-
-							</BaseLayout>
-						</RouteProvider>
-					</Switch>
-				</Route>
-			</Suspense>
+			<ErrorBoundary FallbackComponent={ErrorFallback}>
+				<Suspense fallback={<Spin size="large" />}>
+					<Route path={"/"}>
+						<Switch>
+							<RouteProvider>
+								<BaseLayout>
+									<Suspense fallback={<Spin size="large" />}>
+										<Route path={ROUTES.SIGN_UP} exact component={Signup} />
+										<Route path={ROUTES.APPLICATION} exact component={ServicesTable} />
+										<Route
+											path={ROUTES.SERVICE_METRICS}
+											exact
+											component={ServiceMetrics}
+										/>
+										<Route path={ROUTES.SERVICE_MAP} exact component={ServiceMap} />
+										<Route path={ROUTES.TRACES} exact component={TraceDetail} />
+										<Route path={ROUTES.TRACE_GRAPH} exact component={TraceGraph} />
+										<Route path={ROUTES.SETTINGS} exact component={SettingsPage} />
+										<Route
+											path={ROUTES.INSTRUMENTATION}
+											exact
+											component={IntstrumentationPage}
+										/>
+										<Route
+											path={ROUTES.USAGE_EXPLORER}
+											exactexact
+											component={UsageExplorer}
+										/>
+										<Route
+											path="/"
+											exact
+											render={() => {
+												return localStorage.getItem(IS_LOGGED_IN) === "yes" ? (
+													<Redirect to={ROUTES.APPLICATION} />
+												) : (
+													<Redirect to={ROUTES.SIGN_UP} />
+												);
+											}}
+										/>
+									</Suspense>
+								</BaseLayout>
+							</RouteProvider>
+						</Switch>
+					</Route>
+				</Suspense>
+			</ErrorBoundary>
 		</BrowserRouter>
 	);
 };
