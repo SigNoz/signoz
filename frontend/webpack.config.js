3acef9c86acad1a4b1e78daa35f34b4e879ccf8b
Rahul Rana <3rahul4@gmail.com>
2021-07-31 12:43:00 +0530

    chore: use a new port if existing port is in use (#199)
    * BugFix:Open a new port on local dev server if existing port is being used.
    
    * Update Read me and dev dependcies
    
    * Fix yarn package and add npmrc file

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,6 +1,7 @@
 // shared config (dev and prod)
 const { resolve } = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const portFinderSync = require('portfinder-sync')
 const dotenv = require("dotenv");
 const webpack = require("webpack");
 
@@ -22,7 +23,9 @@ module.exports = {
 		hot: true,
 		liveReload: false,
 		inline: true,
-		port: 3000,
+		// This is being used because if the port 3000 is being used
+		// then it will try to find another open port availble.
+		port: portFinderSync.getPort(3000),
 	},
 	output: {
 		filename: "js/bundle.[chunkhash].min.js",
