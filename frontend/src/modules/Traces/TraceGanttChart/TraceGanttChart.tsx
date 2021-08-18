ed8455592f70568c5e377a291d58d75637e529da
FIPalash Gupta <palash@signoz.io>
2021-08-18 16:48:43 +0530

    updated the dependency array

--- frontend/src/modules/Traces/TraceGanttChart/TraceGanttChart.tsx
+++ frontend/src/modules/Traces/TraceGanttChart/TraceGanttChart.tsx
@@ -105,7 +105,7 @@ const TraceGanttChart = ({
 			setDefaultExpandedRows([treeData?.[0]?.id]);
 			// /.setSpanTagsInfo({data: treeData?.[0]})
 		}
-	}, [selectedSpan, treeData, setDefaultExpandedRows, setSelectedRows]);
+	}, [selectedSpan, treeData, clickedSpan]);
 
 	const getMaxEndTime = (treeData) => {
 		if (treeData.length > 0) {
