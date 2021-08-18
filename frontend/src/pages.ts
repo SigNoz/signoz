6072f1a4d4dd677d8e818978b09471464c02a5eb
FIPalash Gupta <palash@signoz.io>
2021-08-18 15:38:59 +0530

    routes are imported is Loadable fashion with chunkName

--- frontend/src/pages.ts
+++ frontend/src/pages.ts
@@ -1,28 +1,61 @@
-import React from "react";
+import Loadable from "./components/Loadable";
 
-export const ServiceMetrics = React.lazy(
-	() => import("Src/modules/Metrics/ServiceMetricsDef"),
-);
-export const ServiceMap = React.lazy(
-	() => import("Src/modules/Servicemap/ServiceMap"),
-);
-export const TraceDetail = React.lazy(
-	() => import("Src/modules/Traces/TraceDetail"),
-);
-export const TraceGraph = React.lazy(
-	() => import("Src/modules/Traces/TraceGraphDef"),
-);
-export const UsageExplorer = React.lazy(
-	() => import("Src/modules/Usage/UsageExplorerDef"),
-);
-export const ServicesTable = React.lazy(
-	() => import("Src/modules/Metrics/ServicesTableDef"),
-);
-export const Signup = React.lazy(() => import("Src/modules/Auth/Signup"));
-export const SettingsPage = React.lazy(
-	() => import("Src/modules/Settings/settingsPage"),
+export const ServiceMetricsPage = Loadable(
+	() =>
+		import(
+			/* webpackChunkName: "ServiceMetricsPage" */ "Src/modules/Metrics/ServiceMetricsDef"
+		),
 );
 
-export const InstrumentationPage = React.lazy(
-	() => import("Src/modules/add-instrumentation/instrumentationPage"),
+export const ServiceMapPage = Loadable(
+	() =>
+		import(
+			/* webpackChunkName: "ServiceMapPage" */ "Src/modules/Servicemap/ServiceMap"
+		),
+);
+
+export const TraceDetailPage = Loadable(
+	() =>
+		import(
+			/* webpackChunkName: "TraceDetailPage" */ "Src/modules/Traces/TraceDetail"
+		),
+);
+
+export const TraceGraphPage = Loadable(
+	() =>
+		import(
+			/* webpackChunkName: "TraceGraphPage" */ "Src/modules/Traces/TraceGraphDef"
+		),
+);
+
+export const UsageExplorerPage = Loadable(
+	() =>
+		import(
+			/* webpackChunkName: "UsageExplorerPage" */ "Src/modules/Usage/UsageExplorerDef"
+		),
+);
+
+export const ServicesTablePage = Loadable(
+	() =>
+		import(
+			/* webpackChunkName: "ServicesTablePage" */ "Src/modules/Metrics/ServicesTableDef"
+		),
+);
+
+export const SignupPage = Loadable(
+	() => import(/* webpackChunkName: "SignupPage" */ "Src/modules/Auth/Signup"),
+);
+
+export const SettingsPage = Loadable(
+	() =>
+		import(
+			/* webpackChunkName: "SettingsPage" */ "Src/modules/Settings/settingsPage"
+		),
+);
+
+export const InstrumentationPage = Loadable(
+	() =>
+		import(
+			/* webpackChunkName: "InstrumentationPage" */ "Src/modules/add-instrumentation/instrumentationPage"
+		),
 );
