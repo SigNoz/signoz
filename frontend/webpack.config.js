7c70aac2300c534cbf86d6ac3d00bd5ac4832901
Jacoberson <c.jacob.moore92@gmail.com>
2021-08-24 20:13:31 -0500

    fixing hot reload issue

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -14,6 +14,7 @@ module.exports = {
 	mode: "development",
 	devtool: "source-map",
 	entry: resolve(__dirname, "./src/index.tsx"),
+	target: "web",
 	devServer: {
 		historyApiFallback: true,
 		publicPath: "/",
