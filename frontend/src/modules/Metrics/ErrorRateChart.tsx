3ea696db43bc8d0ca866239e2a645afa0255c35d
FIPalash Gupta <palash@signoz.io>
2021-09-23 15:38:45 +0530

    fix: ErrorChart is fixed

--- frontend/src/modules/Metrics/ErrorRateChart.tsx
+++ frontend/src/modules/Metrics/ErrorRateChart.tsx
@@ -1,9 +1,6 @@
-import { ActiveElement, Chart, ChartData, ChartEvent } from 'chart.js';
+import { ActiveElement, Chart, ChartEvent } from 'chart.js';
 import Graph from 'components/Graph';
-import ROUTES from 'constants/routes';
-import React, { memo, useMemo } from 'react';
-import { withRouter } from 'react-router';
-import { RouteComponentProps } from 'react-router-dom';
+import React, { useMemo, useState } from 'react';
 import { metricItem } from 'store/actions/MetricsActions';
 import styled from 'styled-components';
 
@@ -33,105 +30,89 @@ const PopUpElements = styled.p`
 	}
 `;
 
-interface ErrorRateChartProps extends RouteComponentProps<any> {
+interface ErrorRateChartProps {
 	data: metricItem[];
-	onTracePopupClick: Function;
+	onTracePopupClick: (props: number) => void;
 }
 
-interface ErrorRateChart {
-	chartRef: any;
-}
-
-class ErrorRateChart extends React.Component<ErrorRateChartProps> {
-	constructor(props: ErrorRateChartProps) {
-		super(props);
-		this.chartRef = React.createRef();
-	}
-
-	state = {
-		// data: props.data,
+const ErrorRateChart = ({
+	data,
+	onTracePopupClick,
+}: ErrorRateChartProps): JSX.Element => {
+	const [state, setState] = useState({
 		xcoordinate: 0,
 		ycoordinate: 0,
 		showpopUp: false,
 		firstpoint_ts: 0,
-		// graphInfo:{}
+	});
+
+	const gotoTracesHandler = (): void => {
+		onTracePopupClick(state.firstpoint_ts);
 	};
 
-	onClickhandler = (
+	const data_chartJS: Chart['data'] = useMemo(() => {
+		return {
+			labels: data.map((s) => new Date(s.timestamp / 1000000)),
+			datasets: [
+				{
+					label: 'Error Percentage (%)',
+					data: data.map((s: { errorRate: any }) => s.errorRate),
+					pointRadius: 0.5,
+					borderColor: 'rgba(227, 74, 51,1)', // Can also add transparency in border color
+					borderWidth: 2,
+				},
+			],
+		};
+	}, [data]);
+
+	const onClickhandler = (
 		event: ChartEvent,
 		elements: ActiveElement[],
 		chart: Chart,
 	): void => {
-		if (event.native) {
-			const points = chart.getElementsAtEventForMode(
-				event.native,
-				'nearest',
-				{ intersect: true },
-				true,
-			);
-
-			if (points.length) {
-				const firstPoint = points[0];
-
-				this.setState({
-					xcoordinate: firstPoint.element.x,
-					ycoordinate: firstPoint.element.y,
-					showpopUp: true,
-					firstpoint_ts: this.props.data[firstPoint.index].timestamp,
-				});
+		{
+			if (event.native) {
+				const points = chart.getElementsAtEventForMode(
+					event.native,
+					'nearest',
+					{ intersect: true },
+					true,
+				);
+				if (points.length) {
+					const firstPoint = points[0];
+					setState({
+						xcoordinate: firstPoint.element.x,
+						ycoordinate: firstPoint.element.y,
+						showpopUp: true,
+						firstpoint_ts: data[firstPoint.index].timestamp,
+					});
+				}
 			}
 		}
 	};
 
-	gotoTracesHandler = () => {
-		this.props.onTracePopupClick(this.state.firstpoint_ts);
-	};
+	return (
+		<div>
+			{state.showpopUp && (
+				<ChartPopUpUnique
+					xcoordinate={state.xcoordinate}
+					ycoordinate={state.ycoordinate}
+				>
+					<PopUpElements onClick={gotoTracesHandler}>View Traces</PopUpElements>
+				</ChartPopUpUnique>
+			)}
+			<div style={{ textAlign: 'center' }}>Error Percentage (%)</div>
 
-	render(): JSX.Element {
-		const ndata = this.props.data;
+			<GraphContainer>
+				<Graph
+					onClickHandler={onClickhandler}
+					xAxisType="timeseries"
+					type="line"
+					data={data_chartJS}
+				/>
+			</GraphContainer>
+		</div>
+	);
+};
 
-		const data_chartJS = (): ChartData => {
-			return {
-				labels: ndata.map((s) => new Date(s.timestamp / 1000000)), // converting from nano second to mili second
-				datasets: [
-					{
-						label: 'Error Percentage (%)',
-						data: ndata.map((s) => s.errorRate),
-						pointRadius: 0.5,
-						borderColor: 'rgba(227, 74, 51,1)', // Can also add transparency in border color
-						borderWidth: 2,
-					},
-				],
-			};
-		};
-
-		const data = data_chartJS();
-
-		return (
-			<div>
-				{this.state.showpopUp && (
-					<ChartPopUpUnique
-						xcoordinate={this.state.xcoordinate}
-						ycoordinate={this.state.ycoordinate}
-					>
-						<PopUpElements onClick={this.gotoTracesHandler}>
-							View Traces
-						</PopUpElements>
-					</ChartPopUpUnique>
-				)}
-				<div style={{ textAlign: 'center' }}>Error Percentage (%)</div>
-
-				<GraphContainer>
-					<Graph
-						onClickHandler={this.onClickhandler}
-						xAxisType="timeseries"
-						type="line"
-						data={data}
-					/>
-				</GraphContainer>
-			</div>
-		);
-	}
-}
-
-export default withRouter(ErrorRateChart);
+export default ErrorRateChart;
