e18fb3f03f4247cf2164f06f59eaefa906ff215e
Yash Joshi <jyash97@gmail.com>
2021-10-18 00:38:08 +0530

    a11y(signup-page): autofocus first input element (email)

--- frontend/src/pages/SignUp/index.tsx
+++ frontend/src/pages/SignUp/index.tsx
@@ -78,6 +78,7 @@ const Signup = (): JSX.Element => {
 						<Input
 							placeholder="mike@netflix.com"
 							type="email"
+							autoFocus
 							value={formState.email.value}
 							onChange={(e): void => updateForm('email', e.target)}
 							required
@@ -89,7 +90,6 @@ const Signup = (): JSX.Element => {
 						<label htmlFor="signupFirstName">First Name</label>
 						<Input
 							placeholder="Mike"
-							autoFocus
 							value={formState.firstName.value}
 							onChange={(e): void => updateForm('firstName', e.target)}
 							required
