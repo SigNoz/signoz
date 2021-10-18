14e7331e26f45c9613627a3ad6afa92cc1d957c1
FIPalash Gupta <palash@signoz.io>
2021-10-18 16:54:05 +0530

    fix: onboarding modal condition is fixed and new calling api every 50000 ms to fetch the data

--- frontend/src/pages/Metrics/index.tsx
+++ frontend/src/pages/Metrics/index.tsx
@@ -1,4 +1,5 @@
 import Spinner from 'components/Spinner';
+import { SKIP_ONBOARDING } from 'constants/onboarding';
 import MetricTable from 'container/MetricsTable';
 import React, { useEffect } from 'react';
 import { connect, useSelector } from 'react-redux';
@@ -8,11 +9,17 @@ import { GetService, GetServiceProps } from 'store/actions';
 import { AppState } from 'store/reducers';
 import AppActions from 'types/actions';
 import { GlobalReducer } from 'types/reducer/globalTime';
+import MetricReducer from 'types/reducer/metrics';
 
 const Metrics = ({ getService }: MetricsProps): JSX.Element => {
 	const { minTime, maxTime, loading } = useSelector<AppState, GlobalReducer>(
 		(state) => state.globalTime,
 	);
+	const { services } = useSelector<AppState, MetricReducer>(
+		(state) => state.metrics,
+	);
+
+	const isSkipped = localStorage.getItem(SKIP_ONBOARDING) === 'true';
 
 	useEffect(() => {
 		if (loading === false) {
@@ -22,6 +29,23 @@ const Metrics = ({ getService }: MetricsProps): JSX.Element => {
 			});
 		}
 	}, [getService, maxTime, minTime, loading]);
+
+	useEffect(() => {
+		let timeInterval: NodeJS.Timeout;
+
+		if (loading === false && !isSkipped && services.length === 0) {
+			timeInterval = setInterval(() => {
+				getService({
+					start: minTime,
+					end: maxTime,
+				});
+			}, 50000);
+		}
+
+		return (): void => {
+			clearInterval(timeInterval);
+		};
+	}, [getService, isSkipped, loading, maxTime, minTime, services]);
 
 	if (loading) {
 		return <Spinner tip="Loading..." />;
