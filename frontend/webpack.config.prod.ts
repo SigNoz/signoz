bed40ae417c171dbcbe17451894e699177356b2b
Ankit Nayan <ankit@signoz.io>
2021-11-22 12:58:04 +0530

    Revert "Bug(FE): Removed  un wanted package  from frontend and other enchancements (#386)"
    This reverts commit 20879dcf2e997fd04d4e19a6f62d9466332a0c76.

--- frontend/webpack.config.prod.ts
+++ frontend/webpack.config.prod.ts
@@ -29,13 +29,17 @@ const config: webpack.Configuration = {
 	module: {
 		rules: [
 			{
-				test: [/\.tsx?$/],
+				test: [/\.jsx?$/, /\.tsx?$/],
 				use: ['babel-loader'],
 				exclude: /node_modules/,
 			},
 			{
 				test: /\.css$/,
 				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.(scss|sass)$/,
+				use: ['style-loader', 'css-loader', 'sass-loader'],
 			},
 			{
 				test: /\.(jpe?g|png|gif|svg)$/i,
