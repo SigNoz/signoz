c33f171d6dd8e5184924076a4565494b3a240bf4
FIPalash Gupta <palash@signoz.io>
2021-11-22 10:32:33 +0530

    chore(UI): placeholder for the triggered alerts is updated

--- frontend/src/container/TriggeredAlerts/Filter.tsx
+++ frontend/src/container/TriggeredAlerts/Filter.tsx
@@ -54,7 +54,7 @@ const Filter = ({
 				onChange={onChangeSelectedFilterHandler}
 				mode="tags"
 				value={selectedFilter.map((e) => e.value)}
-				placeholder="Filter by tags status or severity or any other tag"
+				placeholder="severity:warning, alertname:Sample Alert"
 				tagRender={(props): JSX.Element => {
 					const { label, closable, onClose } = props;
 					return (
--- frontend/src/container/TriggeredAlerts/styles.ts
+++ frontend/src/container/TriggeredAlerts/styles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 
 export const Select = styled(SelectComponent)`
 	&&& {
-		min-width: 300px;
+		min-width: 350px;
 	}
 `;
 
