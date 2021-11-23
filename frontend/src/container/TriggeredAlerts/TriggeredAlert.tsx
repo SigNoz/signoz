26d3e6274aea12dbafa78f8cae84b786bfbb73a6
FIPalash Gupta <palash@signoz.io>
2021-11-23 21:32:39 +0530

    feat(UI): alert rules and grouped alerts are fetched automatically within in 30sec

--- frontend/src/container/TriggeredAlerts/TriggeredAlert.tsx
+++ frontend/src/container/TriggeredAlerts/TriggeredAlert.tsx
@@ -1,3 +1,5 @@
+import getGroupApi from 'api/alerts/getGroup';
+import useInterval from 'hooks/useInterval';
 import React, { useState } from 'react';
 import { Alerts } from 'types/api/alerts/getAll';
 
@@ -8,7 +10,27 @@ import NoFilterTable from './NoFilterTable';
 import { NoTableContainer } from './styles';
 
 const TriggeredAlerts = ({ allAlerts }: TriggeredAlertsProps): JSX.Element => {
-	const allInitialAlerts = allAlerts || [];
+	const [allInitialAlerts, setInitialAlerts] = useState(allAlerts || []);
+
+	useInterval(() => {
+		(async (): Promise<void> => {
+			const response = await getGroupApi({
+				active: true,
+				inhibited: true,
+				silenced: false,
+			});
+
+			if (response.statusCode === 200 && response.payload !== null) {
+				const initialAlerts: Alerts[] = [];
+
+				const allAlerts: Alerts[] = response.payload.reduce((acc, cur) => {
+					return [...acc, ...cur.alerts];
+				}, initialAlerts);
+
+				setInitialAlerts(allAlerts);
+			}
+		})();
+	}, 30000);
 
 	const [selectedGroup, setSelectedGroup] = useState<Value[]>([]);
 	const [selectedFilter, setSelectedFilter] = useState<Value[]>([]);
