a06144716bc1ced25235f653d58ab9942891090f
Vishal Sharma <makeavish786@gmail.com>
2021-12-01 16:34:13 +0530

    update command to bring down SigNoz docker containers

--- deploy/install.sh
+++ deploy/install.sh
@@ -496,7 +496,7 @@ else
     echo ""
 
     if [ $setup_type == 'clickhouse' ]; then
-        echo "ℹ️  To bring down SigNoz and clean volumes : sudo docker-compose -f docker/clickhouse-setup/docker-compose.yaml down -v"
+        echo "ℹ️  To bring down SigNoz and clean volumes : sudo docker-compose --env-file ./docker/clickhouse-setup/env/arm64.env -f docker/clickhouse-setup/docker-compose.yaml down -v"
     else
         echo "ℹ️  To bring down SigNoz and clean volumes : sudo docker-compose -f docker/druid-kafka-setup/docker-compose-tiny.yaml down -v"
     fi
