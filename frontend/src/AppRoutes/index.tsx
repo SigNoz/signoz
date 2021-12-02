447700326a05b688467e43564f8d5cf438252833
pal-sig <88981777+pal-sig@users.noreply.github.com>
2021-12-02 18:53:05 +0530

    fix(BUG): not found is fixed (#405)
    Co-authored-by: Ankit Nayan <ankit@signoz.io>

--- frontend/src/AppRoutes/index.tsx
+++ frontend/src/AppRoutes/index.tsx
@@ -4,24 +4,43 @@ import ROUTES from 'constants/routes';
 import AppLayout from 'container/AppLayout';
 import history from 'lib/history';
 import React, { Suspense } from 'react';
+import { useSelector } from 'react-redux';
 import { Redirect, Route, Router, Switch } from 'react-router-dom';
+import { AppState } from 'store/reducers';
+import AppReducer from 'types/reducer/app';
 
 import routes from './routes';
 
-const App = (): JSX.Element => (
-	<Router history={history}>
-		<AppLayout>
-			<Suspense fallback={<Spinner size="large" tip="Loading..." />}>
-				<Switch>
-					{routes.map(({ path, component, exact }) => (
-						<Route key={`${path}`} exact={exact} path={path} component={component} />
-					))}
-					<Redirect from="/" to={ROUTES.APPLICATION} />
-					<Route component={NotFound} />
-				</Switch>
-			</Suspense>
-		</AppLayout>
-	</Router>
-);
+
+const App = (): JSX.Element => {
+	const { isLoggedIn } = useSelector<AppState, AppReducer>((state) => state.app);
+
+	return (
+		<Router history={history}>
+			<AppLayout>
+				<Suspense fallback={<Spinner size="large" tip="Loading..." />}>
+					<Switch>
+						{routes.map(({ path, component, exact }, index) => (
+							<Route key={index} exact={exact} path={path} component={component} />
+						))}
+						<Route
+							path="/"
+							exact
+							render={(): JSX.Element =>
+								isLoggedIn ? (
+									<Redirect to={ROUTES.APPLICATION} />
+								) : (
+									<Redirect to={ROUTES.SIGN_UP} />
+								)
+							}
+						/>
+						<Route path="*" component={NotFound} />
+					</Switch>
+				</Suspense>
+			</AppLayout>
+		</Router>
+	);
+};
+
 
 export default App;
