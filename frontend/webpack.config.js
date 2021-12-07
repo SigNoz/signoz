SigNoz/signoz

commit 3e0f5a866d963a65738f0fe2d6c747fdfca05a65

fix(BUG): experimental changes are removed (#436)
* fix(BUG): experimental changes are removed

* fix(BUG): experimental changes are removed

frontend/webpack.config.js
@@ -1,27 +1,36 @@
 // shared config (dev and prod)
-import CompressionPlugin from 'compression-webpack-plugin';
-import CopyPlugin from 'copy-webpack-plugin';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import { resolve } from 'path';
-import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
-import webpack, { WebpackPluginInstance } from 'webpack';
+const { resolve } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const portFinderSync = require('portfinder-sync');
+const dotenv = require('dotenv');
+const webpack = require('webpack');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
-const __dirname = resolve();
+dotenv.config();
 
-const config: webpack.Configuration = {
-	mode: 'production',
+console.log(resolve(__dirname, './src/'));
+
+const config = {
+	mode: 'development',
 	devtool: 'source-map',
 	entry: resolve(__dirname, './src/index.tsx'),
-	output: {
-		filename: ({ chunk }): string => {
-			const hash = chunk?.hash;
-			const name = chunk?.name;
-			return `js/${name}-${hash}.js`;
+	devServer: {
+		historyApiFallback: true,
+		open: true,
+		hot: true,
+		liveReload: true,
+		port: portFinderSync.getPort(3000),
+		static: {
+			directory: resolve(__dirname, 'public'),
+			publicPath: '/',
+			watch: true,
 		},
+	},
+	target: 'web',
+	output: {
 		path: resolve(__dirname, './build'),
 		publicPath: '/',
 	},
-
 	resolve: {
 		extensions: ['.ts', '.tsx', '.js', '.jsx'],
 		plugins: [new TsconfigPathsPlugin({})],
@@ -35,11 +44,7 @@ const config: webpack.Configuration = {
 			},
 			{
 				test: /\.css$/,
-				use: ['style-loader', 'css-loader'],
-			},
-			{
-				test: /\.(scss|sass)$/,
-				use: ['style-loader', 'css-loader', 'sass-loader'],
+				use: ['css-loader'],
 			},
 			{
 				test: /\.(jpe?g|png|gif|svg)$/i,
@@ -56,12 +61,6 @@ const config: webpack.Configuration = {
 	},
 	plugins: [
 		new HtmlWebpackPlugin({ template: 'src/index.html.ejs' }),
-		(new CompressionPlugin({
-			exclude: /.map$/,
-		}) as unknown) as WebpackPluginInstance,
-		(new CopyPlugin({
-			patterns: [{ from: resolve(__dirname, 'public/'), to: '.' }],
-		}) as unknown) as WebpackPluginInstance,
 		new webpack.ProvidePlugin({
 			process: 'process/browser',
 		}),
@@ -74,4 +73,4 @@ const config: webpack.Configuration = {
 	},
 };
 
-export default config;
+module.exports = config;

.gitignore
@@ -14,6 +14,8 @@ frontend/coverage
 frontend/build
 frontend/.vscode
 frontend/.yarnclean
+frontend/.temp_cache
+
 # misc
 .DS_Store
 .env.local

frontend/webpack.config.prod.js
@@ -1,0 +1,110 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+// shared config (dev and prod)
+const { resolve } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+const webpack = require('webpack');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const config = {
+	mode: 'production',
+	entry: resolve(__dirname, './src/index.tsx'),
+	output: {
+		path: resolve(__dirname, './build'),
+		publicPath: '/',
+	},
+	resolve: {
+		extensions: ['.ts', '.tsx', '.js', '.jsx'],
+		plugins: [new TsconfigPathsPlugin({})],
+	},
+	cache: {
+		type: 'filesystem',
+		allowCollectingMemory: true,
+		cacheDirectory: resolve(__dirname, '.temp_cache'),
+		buildDependencies: {
+			// This makes all dependencies of this file - build dependencies
+			config: [__filename],
+			// By default webpack and loaders are build dependencies
+		},
+	},
+
+	module: {
+		rules: [
+			{
+				test: [/\.jsx?$/, /\.tsx?$/],
+				use: ['babel-loader'],
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.css$/,
+				use: [MiniCssExtractPlugin.loader, 'css-loader'],
+			},
+			{
+				test: /\.(jpe?g|png|gif|svg)$/i,
+				use: [
+					'file-loader?hash=sha512&digest=hex&name=img/[chunkhash].[ext]',
+					'image-webpack-loader?bypassOnDebug&optipng.optimizationLevel=7&gifsicle.interlaced=false',
+				],
+			},
+			{
+				test: /\.(ttf|eot|woff|woff2)$/,
+				use: ['file-loader'],
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({ template: 'src/index.html.ejs' }),
+		new CompressionPlugin({
+			exclude: /.map$/,
+		}),
+		new CopyPlugin({
+			patterns: [{ from: resolve(__dirname, 'public/'), to: '.' }],
+		}),
+		new webpack.ProvidePlugin({
+			process: 'process/browser',
+		}),
+		new webpack.DefinePlugin({
+			'process.env': JSON.stringify(process.env),
+		}),
+		new MiniCssExtractPlugin(),
+	],
+	optimization: {
+		chunkIds: 'named',
+		concatenateModules: true,
+		emitOnErrors: true,
+		flagIncludedChunks: true,
+		innerGraph: true, //tells webpack whether to conduct inner graph analysis for unused exports.
+		mangleWasmImports: true,
+		mergeDuplicateChunks: true,
+		minimize: true,
+		nodeEnv: 'production',
+		runtimeChunk: {
+			name: (entrypoint) => `runtime~${entrypoint.name}`,
+		},
+		minimizer: [
+			new TerserPlugin({
+				parallel: true,
+				terserOptions: {
+					compress: true,
+					keep_classnames: true,
+					keep_fnames: false,
+					sourceMap: false,
+					safari10: true,
+					parse: {
+						html5_comments: false,
+					},
+				},
+			}),
+			new CssMinimizerPlugin(),
+		],
+	},
+	performance: {
+		hints: 'warning',
+	},
+};
+
+module.exports = config;

frontend/webpack.config.ts
@@ -1,92 +1,0 @@
-// shared config (dev and prod)
-import dotenv from 'dotenv';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import { resolve } from 'path';
-//@ts-ignore
-import portFinderSync from 'portfinder-sync';
-import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
-import webpack from 'webpack';
-import { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server';
-
-dotenv.config();
-
-const __dirname = resolve();
-console.log(resolve(__dirname, './src/'));
-
-interface Configuration extends webpack.Configuration {
-	devServer?: WebpackDevServerConfiguration;
-}
-
-const config: Configuration = {
-	mode: 'development',
-	devtool: 'source-map',
-	entry: resolve(__dirname, './src/index.tsx'),
-	devServer: {
-		historyApiFallback: true,
-		open: true,
-		hot: true,
-		liveReload: true,
-		port: portFinderSync.getPort(3000),
-		static: {
-			directory: resolve(__dirname, 'public'),
-			publicPath: '/',
-			watch: true,
-		},
-	},
-	target: 'web',
-	output: {
-		filename: ({ chunk }) => {
-			const hash = chunk?.hash;
-			const name = chunk?.name;
-			return `js/${name}-${hash}.js`;
-		},
-		path: resolve(__dirname, './build'),
-		publicPath: '/',
-	},
-	resolve: {
-		extensions: ['.ts', '.tsx', '.js', '.jsx'],
-		plugins: [new TsconfigPathsPlugin({})],
-	},
-	module: {
-		rules: [
-			{
-				test: [/\.jsx?$/, /\.tsx?$/],
-				use: ['babel-loader'],
-				exclude: /node_modules/,
-			},
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader'],
-			},
-			{
-				test: /\.(scss|sass)$/,
-				use: ['style-loader', 'css-loader', 'sass-loader'],
-			},
-			{
-				test: /\.(jpe?g|png|gif|svg)$/i,
-				use: [
-					'file-loader?hash=sha512&digest=hex&name=img/[chunkhash].[ext]',
-					'image-webpack-loader?bypassOnDebug&optipng.optimizationLevel=7&gifsicle.interlaced=false',
-				],
-			},
-			{
-				test: /\.(ttf|eot|woff|woff2)$/,
-				use: ['file-loader'],
-			},
-		],
-	},
-	plugins: [
-		new HtmlWebpackPlugin({ template: 'src/index.html.ejs' }),
-		new webpack.ProvidePlugin({
-			process: 'process/browser',
-		}),
-		new webpack.DefinePlugin({
-			'process.env': JSON.stringify(process.env),
-		}),
-	],
-	performance: {
-		hints: false,
-	},
-};
-
-export default config;

commit 664853cc38774fb3a67511498976c3135f4820e2

fix(BUG): Allow webpack to link CSS with bundled HTML file.
While porting the webpack config file from typescript to
javascript, commit 3e0f5a866d963a65738f0fe2d6c747fdfca05a65 accidentally removed
style-loader from config setting. Due to this, the CSS file
wasn't being attached with the bundled html file.

Fixed this by adding the style-loader back to config settings
in webpack.

frontend/webpack.config.js
@@ -44,7 +44,7 @@ const config = {
 			},
 			{
 				test: /\.css$/,
-				use: ['css-loader'],
+				use: ['style-loader', 'css-loader'],
 			},
 			{
 				test: /\.(jpe?g|png|gif|svg)$/i,