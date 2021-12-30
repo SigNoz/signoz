ebf884af7c168f50811a1df942180073b57f27cc
Ankit Nayan <ankit@signoz.io>
2021-12-31 02:54:30 +0530

    chore: removed changes to main

--- pkg/query-service/main.go
+++ pkg/query-service/main.go
@@ -1,14 +1,10 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
 
-	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/client"
 	"go.signoz.io/query-service/app"
 	"go.signoz.io/query-service/constants"
 
@@ -33,20 +29,6 @@ func main() {
 
 	logger := loggerMgr.Sugar()
 	logger.Debug("START!")
-
-	cli, err := client.NewClientWithOpts(client.FromEnv)
-	if err != nil {
-		panic(err)
-	}
-
-	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
-	if err != nil {
-		panic(err)
-	}
-
-	for _, container := range containers {
-		fmt.Printf("%s %s\n", container.ID[:10], container.Image)
-	}
 
 	serverOptions := &app.ServerOptions{
 		// HTTPHostPort:   v.GetString(app.HTTPHostPort),
