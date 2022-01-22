68c97b7b244b9334d88afa0e54b7ed2f0a59d51b
Palash gupta <palash@signoz.io>
2022-01-22 16:46:53 +0530

    chore: duration is updated and panel body loading is updated

--- frontend/src/container/Trace/Filters/Panel/PanelBody/index.tsx
+++ frontend/src/container/Trace/Filters/Panel/PanelBody/index.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
-import { TraceFilterEnum } from 'types/reducer/trace';
+import { TraceFilterEnum, TraceReducer } from 'types/reducer/trace';
 import { Card } from 'antd';
 
 import Duration from './Duration';
 import CommonCheckBox from './CommonCheckBox';
+import { useSelector } from 'react-redux';
+import { AppState } from 'store/reducers';
+import Spinner from 'components/Spinner';
 
 const PanelBody = (props: PanelBodyProps): JSX.Element => {
 	const { type } = props;
+
+	const { filterLoading } = useSelector<AppState, TraceReducer>(
+		(state) => state.traces,
+	);
+
+	if (filterLoading) {
+		return (
+			<Card bordered={false}>
+				<Spinner height="10vh" tip="Loading.." />
+			</Card>
+		);
+	}
 
 	return (
 		<Card bordered={false}>
