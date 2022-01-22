3e270ca03770a7242477e55b7fdf1dc8d8a73731
Palash gupta <palash@signoz.io>
2022-01-22 18:06:05 +0530

    chore: Select filter is updated

--- frontend/src/container/Trace/TraceGraphFilter/index.tsx
+++ frontend/src/container/Trace/TraceGraphFilter/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Menu, Dropdown, Button, MenuItemProps, Space } from 'antd';
+import { Space, SelectProps } from 'antd';
 import { functions, groupBy } from './config';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppState } from 'store/reducers';
@@ -10,6 +10,10 @@ import {
 	UPDATE_SELECTED_GROUP_BY,
 } from 'types/actions/trace';
 import { Dispatch } from 'redux';
+import { SelectComponent } from './styles';
+import { SelectValue } from 'antd/lib/select';
+
+const { Option } = SelectComponent;
 
 const TraceGraphFilter = () => {
 	const { selectedFunction, selectedGroupBy } = useSelector<
@@ -18,63 +22,63 @@ const TraceGraphFilter = () => {
 	>((state) => state.traces);
 	const dispatch = useDispatch<Dispatch<AppActions>>();
 
-	const onClickSelectedFunctionHandler: MenuItemProps['onClick'] = (ev) => {
-		const selected = functions.find((e) => e.key === ev.key);
-
-		if (selected) {
-			dispatch({
-				type: UPDATE_SELECTED_FUNCTION,
-				payload: {
-					selectedFunction: selected.key,
-				},
-			});
+	const onClickSelectedFunctionHandler: SelectProps<SelectValue>['onChange'] = (
+		ev,
+	) => {
+		if (ev) {
+			const selected = functions.find((e) => e.key === ev);
+			if (selected) {
+				dispatch({
+					type: UPDATE_SELECTED_FUNCTION,
+					payload: {
+						selectedFunction: selected.key,
+					},
+				});
+			}
 		}
 	};
 
-	const onClickSelectedGroupByHandler: MenuItemProps['onClick'] = (ev) => {
-		const selected = groupBy.find((e) => e.key === ev.key);
-
-		if (selected) {
-			dispatch({
-				type: UPDATE_SELECTED_GROUP_BY,
-				payload: {
-					selectedGroupBy: selected.key,
-				},
-			});
+	const onClickSelectedGroupByHandler: SelectProps<SelectValue>['onChange'] = (
+		ev,
+	) => {
+		if (ev) {
+			const selected = groupBy.find((e) => e.key === ev);
+			if (selected) {
+				dispatch({
+					type: UPDATE_SELECTED_GROUP_BY,
+					payload: {
+						selectedGroupBy: selected.key,
+					},
+				});
+			}
 		}
 	};
-
-	const functionMenu = (
-		<Menu onClick={onClickSelectedFunctionHandler}>
-			{functions.map(({ displayValue, key }) => (
-				<Menu.Item key={key}>{displayValue}</Menu.Item>
-			))}
-		</Menu>
-	);
-
-	const groupByMenu = (
-		<Menu onClick={onClickSelectedGroupByHandler}>
-			{groupBy.map(({ displayValue, key }) => (
-				<Menu.Item key={key}>{displayValue}</Menu.Item>
-			))}
-		</Menu>
-	);
 
 	return (
 		<Space>
-			<Dropdown overlay={functionMenu} trigger={['click']}>
-				<Button>
-					{functions.find((e) => selectedFunction === e.key)?.displayValue ||
-						'Select Function'}
-				</Button>
-			</Dropdown>
+			<SelectComponent
+				dropdownMatchSelectWidth
+				value={functions.find((e) => selectedFunction === e.key)?.displayValue}
+				onChange={onClickSelectedFunctionHandler}
+			>
+				{functions.map((value) => (
+					<Option value={value.key} key={value.key}>
+						{value.displayValue}
+					</Option>
+				))}
+			</SelectComponent>
 
-			<Dropdown overlay={groupByMenu} trigger={['click']}>
-				<Button>
-					{groupBy.find((e) => selectedGroupBy === e.key)?.displayValue ||
-						'Select Group By'}
-				</Button>
-			</Dropdown>
+			<SelectComponent
+				dropdownMatchSelectWidth
+				value={groupBy.find((e) => selectedGroupBy === e.key)?.displayValue}
+				onChange={onClickSelectedGroupByHandler}
+			>
+				{groupBy.map((value) => (
+					<Option value={value.key} key={value.key}>
+						{value.displayValue}
+					</Option>
+				))}
+			</SelectComponent>
 		</Space>
 	);
 };
