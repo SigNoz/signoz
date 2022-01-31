08cc8b611e41640b97cf58d10dbc87f6aa17aff8
Palash gupta <palash@signoz.io>
2022-01-31 15:18:28 +0530

    chore: expand panel edge case is updated

--- frontend/src/store/actions/trace/expandPanel.ts
+++ frontend/src/store/actions/trace/expandPanel.ts
@@ -3,6 +3,8 @@ import { AppState } from 'store/reducers';
 import AppActions from 'types/actions';
 import { TraceFilterEnum, TraceReducer } from 'types/reducer/trace';
 import { updateURL } from './util';
+import getFilters from 'api/trace/getFilters';
+import { UPDATE_TRACE_FILTER_LOADING } from 'types/actions/trace';
 
 export const ExpandPanel = (
 	props: TraceFilterEnum,
@@ -11,22 +13,46 @@ export const ExpandPanel = (
 	dispatch: Dispatch<AppActions>,
 	getState: Store<AppState>['getState'],
 ) => void) => {
-	return (_, getState): void => {
-		const { traces } = getState();
+	return async (dispatch, getState): Promise<void> => {
+		const { traces, globalTime } = getState();
 
 		const { filterToFetchData } = traces;
+		const updatedSelectedFilter = new Map<TraceFilterEnum, string[]>(
+			traces.selectedFilter,
+		);
+
+		dispatch({
+			type: UPDATE_TRACE_FILTER_LOADING,
+			payload: {
+				filterLoading: true,
+			},
+		});
 
 		let updatedFilterToFetchTheData: TraceReducer['filterToFetchData'] = [];
 
 		if (isOpen) {
 			updatedFilterToFetchTheData = [...new Set([...filterToFetchData, props])];
+
+			const response = await getFilters({
+				start: String(globalTime.minTime),
+				end: String(globalTime.maxTime),
+				getFilters: [props],
+				other: {},
+			});
+
+			if (response.statusCode === 200 && response.payload) {
+				updatedSelectedFilter.set(
+					props,
+					Object.keys(response.payload[props]).map((e) => e),
+				);
+			}
 		} else {
 			updatedFilterToFetchTheData = filterToFetchData.filter((e) => e !== props);
 			traces.selectedFilter.delete(props);
 		}
 
 		updateURL(
-			traces.selectedFilter,
+			updatedSelectedFilter,
 			updatedFilterToFetchTheData,
 			traces.spansAggregate.currentPage,
 			traces.selectedTags,
