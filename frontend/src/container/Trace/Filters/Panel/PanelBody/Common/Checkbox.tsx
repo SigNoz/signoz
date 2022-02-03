361d2e1f8ee4c3ef55a92e4266af4852ed465587
Palash gupta <palash@signoz.io>
2022-02-03 15:23:34 +0530

    chore: checkbox component is updated

--- frontend/src/container/Trace/Filters/Panel/PanelBody/Common/Checkbox.tsx
+++ frontend/src/container/Trace/Filters/Panel/PanelBody/Common/Checkbox.tsx
@@ -76,6 +76,12 @@ const CheckBoxComponent = (props: CheckBoxProps): JSX.Element => {
 			if (response.statusCode === 200) {
 				const updatedFilter = getFilter(response.payload);
 
+				updatedFilter.set(props.name, {
+					[`${props.keyValue}`]: '-1',
+					...(filter.get(props.name) || {}),
+					...(updatedFilter.get(props.name) || {}),
+				});
+
 				dispatch({
 					type: UPDATE_ALL_FILTERS,
 					payload: {
