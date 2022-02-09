03eac8963f7ad21579f99cd834220a7d4a0f9fd6
palash-signoz <palash@signoz.io>
2022-02-09 17:22:21 +0530

    chore: Env fix (#693)
    * bug(UI): frontend build is fixed
    
    * chore; build is fixed
    
    * chore: build is fixed

--- frontend/src/constants/env.ts
+++ frontend/src/constants/env.ts
@@ -1,7 +1,6 @@
 export const ENVIRONMENT = {
 	baseURL:
 		process?.env?.FRONTEND_API_ENDPOINT ||
-		process.env?.GITPOD_WORKSPACE_URL?.replace('://', '://8080-') ||
-		'http://localhost:8080' ||
+		process?.env?.GITPOD_WORKSPACE_URL?.replace('://', '://8080-') ||
 		'',
 };
