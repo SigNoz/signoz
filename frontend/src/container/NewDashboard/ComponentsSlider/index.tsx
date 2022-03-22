c592b242ff2abab880fd0bc7d0b9a3cf19f03881
Palash gupta <palash@signoz.io>
2022-03-22 17:21:55 +0530

    chore: new dashboard is updated

--- frontend/src/container/NewDashboard/ComponentsSlider/index.tsx
+++ frontend/src/container/NewDashboard/ComponentsSlider/index.tsx
@@ -1,7 +1,8 @@
-import history from 'lib/history';
+/* eslint-disable @typescript-eslint/naming-convention */
+import { notification } from 'antd';
+import { updateDashboard } from 'container/GridGraphLayout/utils';
 import React, { useCallback } from 'react';
 import { useSelector } from 'react-redux';
-import { useLocation } from 'react-router-dom';
 import { AppState } from 'store/reducers';
 import AppReducer from 'types/reducer/app';
 import DashboardReducer from 'types/reducer/dashboards';
@@ -24,16 +25,43 @@ function DashboardGraphSlider(): JSX.Element {
 		},
 		[],
 	);
-	const { pathname } = useLocation();
 
 	const onClickHandler = useCallback(
-		(name: ITEMS) => {
-			const generateWidgetId = v4();
-			history.push(
-				`${pathname}/new?graphType=${name}&widgetId=${generateWidgetId}`,
-			);
+		async (name: ITEMS) => {
+			try {
+				const generateWidgetId = v4();
+
+				const getX = (): number => {
+					if (data.layout && data.layout?.length > 0) {
+						const lastIndexX = data.layout[(data.layout?.length || 0) - 1];
+						return (lastIndexX.w + lastIndexX.x) % 12;
+					}
+					return 0;
+				};
+
+				await updateDashboard({
+					data,
+					generateWidgetId,
+					graphType: name,
+					layout: [
+						...(data.layout || []),
+						{
+							h: 2,
+							i: (((data.layout || [])?.length || 0) + 1).toString(),
+							w: 6,
+							x: getX(),
+							y: 0,
+						},
+					],
+					selectedDashboard,
+				});
+			} catch (error) {
+				notification.error({
+					message: 'Something went wrong',
+				});
+			}
 		},
-		[pathname],
+		[data, selectedDashboard],
 	);
 	const { isDarkMode } = useSelector<AppState, AppReducer>((state) => state.app);
 	const fillColor: React.CSSProperties['color'] = isDarkMode ? 'white' : 'black';
@@ -42,7 +70,7 @@ function DashboardGraphSlider(): JSX.Element {
 		<Container>
 			{menuItems.map(({ name, Icon, display }) => (
 				<Card
-					onClick={(): void => onClickHandler(name)}
+					onClick={(): Promise<void> => onClickHandler(name)}
 					id={name}
 					onDragStart={onDragStartHandler}
 					key={name}
