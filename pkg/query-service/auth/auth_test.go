1d53b04dd256696ca99ffc2f0e39e3f0ce16a242
Ahsan Barkati <ahsanbarkati@gmail.com>
2022-03-26 22:09:50 +0530

    refactor

--- pkg/query-service/auth/auth_test.go
+++ pkg/query-service/auth/auth_test.go
@@ -7,6 +7,16 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestInvite(t *testing.T) {
+	resp, err := Invite(&InviteRequest{"ahsan@signoz.io"})
+	require.NoError(t, err)
+
+	claims, err := ParseJWT(resp.InviteToken)
+	require.NoError(t, err)
+
+	require.Equal(t, "ahsan@signoz.io", claims["email"].(string))
+}
+
 func TestLogin(t *testing.T) {
 	resp, err := Login(context.Background(), &LoginRequest{"ahsan", "pass"})
 	require.NoError(t, err)
