15eb9cd5a72e8677cf17b7bf0784debe8e91f5cc
Ahsan Barkati <ahsanbarkati@gmail.com>
2022-03-26 22:09:50 +0530

    refactor

--- pkg/query-service/auth/login.go
+++ pkg/query-service/auth/login.go
@@ -18,8 +18,8 @@ type User struct {
 	Groups   []Group
 }
 
+// Login method returns access and refresh tokens on successful login, else it errors out.
 func Login(ctx context.Context, request *LoginRequest) (*LoginResponse, error) {
-
 	user, err := authenticateLogin(ctx, request)
 	if err != nil {
 		return nil, err
@@ -29,7 +29,6 @@ func Login(ctx context.Context, request *LoginRequest) (*LoginResponse, error) {
 	if err != nil {
 		return nil, err
 	}
-
 	refreshJwt, err := generateRefreshJwt(user.UserID)
 	if err != nil {
 		return nil, err
@@ -38,6 +37,7 @@ func Login(ctx context.Context, request *LoginRequest) (*LoginResponse, error) {
 	return &LoginResponse{accessJwt: accessJwt, refrestJwt: refreshJwt}, nil
 }
 
+// authenticateLogin is responsible for querying the DB and validating the credentials.
 func authenticateLogin(ctx context.Context, request *LoginRequest) (*User, error) {
 	return &User{
 		UserID:   request.UserID,
