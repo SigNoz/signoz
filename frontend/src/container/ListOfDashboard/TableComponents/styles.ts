7939902f03c6674614594727e8caddda642139ab
Pranshu Chittora <pranshu@signoz.io>
2022-03-29 12:24:03 +0530

    fix: dashboard table element overflow (#930)

---
+++ frontend/src/container/ListOfDashboard/TableComponents/styles.ts
@@ -0,0 +1,8 @@
+import { blue } from '@ant-design/colors';
+import { Typography } from 'antd';
+import styled from 'styled-components';
+
+export const TableLinkText = styled(Typography.Text)`
+	color: ${blue.primary} !important;
+	cursor: pointer;
+`;
