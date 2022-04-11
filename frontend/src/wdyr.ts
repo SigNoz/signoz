ac70822b2e906d8cb28e5dbccf9e78eb5c7127e1
Palash gupta <palash@signoz.io>
2022-04-11 15:56:23 +0530

    wdyr is updated

--- frontend/src/wdyr.ts
+++ frontend/src/wdyr.ts
@@ -1,17 +1,17 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable global-require */
 /// <reference types="@welldone-software/why-did-you-render" />
 // ^ https://github.com/welldone-software/why-did-you-render/issues/161
 import React from 'react';
 
 if (process.env.NODE_ENV === 'development') {
-	import('@welldone-software/why-did-you-render').then((whyDidYouRender) => {
-		whyDidYouRender.default(React, {
-			trackAllPureComponents: true,
-			trackHooks: true,
-			// https://github.com/welldone-software/why-did-you-render/issues/85#issuecomment-596682587
-			trackExtraHooks: [require('react-redux/lib'), 'useSelector'],
-			include: [/^ConnectFunction/],
-			logOnDifferentValues: true,
-		});
+	const whyDidYouRender = require('@welldone-software/why-did-you-render');
+	whyDidYouRender(React, {
+		trackAllPureComponents: false,
+		trackExtraHooks: [[require('react-redux/lib'), 'useSelector']],
+		include: [/^ConnectFunction/],
+		logOnDifferentValues: true,
 	});
 }
+
+export default '';
