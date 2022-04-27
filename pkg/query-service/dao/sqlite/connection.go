07a6c1e08b8a79695fcd67d698529678edc318e5
Ahsan Barkati <ahsanbarkati@gmail.com>
2022-04-27 18:36:46 +0530

    Max connection to 1

--- pkg/query-service/dao/sqlite/connection.go
+++ pkg/query-service/dao/sqlite/connection.go
@@ -24,7 +24,7 @@ func InitDB(dataSourceName string) (*ModelDaoSqlite, error) {
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to Open sqlite3 DB")
 	}
-	db.SetMaxOpenConns(10)
+	db.SetMaxOpenConns(1)
 
 	table_schema := `
 		PRAGMA foreign_keys = ON;
