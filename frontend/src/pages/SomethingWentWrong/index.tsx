c81b0b2a8b2d3c33ea67a1f4e6958f52e6a9e203
palash-signoz <palash@signoz.io>
2022-05-03 16:01:41 +0530

    Auth Wrapper is updated (#1028)

--- frontend/src/pages/SomethingWentWrong/index.tsx
+++ frontend/src/pages/SomethingWentWrong/index.tsx
@@ -1,20 +1,11 @@
 import { Button, Typography } from 'antd';
-import getLocalStorageKey from 'api/browser/localstorage/get';
 import SomethingWentWrongAsset from 'assets/SomethingWentWrong';
 import { Container } from 'components/NotFound/styles';
-import { LOCALSTORAGE } from 'constants/localStorage';
 import ROUTES from 'constants/routes';
 import history from 'lib/history';
 import React from 'react';
-import { useDispatch } from 'react-redux';
-import { Dispatch } from 'redux';
-import AppActions from 'types/actions';
-import { LOGGED_IN } from 'types/actions/app';
 
 function SomethingWentWrong(): JSX.Element {
-	const dispatch = useDispatch<Dispatch<AppActions>>();
-	const isLoggedIn = getLocalStorageKey(LOCALSTORAGE.IS_LOGGED_IN);
-
 	return (
 		<Container>
 			<SomethingWentWrongAsset />
@@ -22,15 +13,6 @@ function SomethingWentWrong(): JSX.Element {
 			<Button
 				type="primary"
 				onClick={(): void => {
-					if (isLoggedIn) {
-						dispatch({
-							type: LOGGED_IN,
-							payload: {
-								isLoggedIn: true,
-							},
-						});
-					}
-
 					history.push(ROUTES.APPLICATION);
 				}}
 			>
