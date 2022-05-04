bad33fda48e8d115068cab54499eabbbe969b3f9
Pranshu Chittora <pranshu@signoz.io>
2022-05-04 19:27:37 +0530

    fix: fixed graph axis and tooltip

--- frontend/src/components/Graph/yAxisConfig.ts
+++ frontend/src/components/Graph/yAxisConfig.ts
@@ -1,12 +1,43 @@
 import { formattedValueToString, getValueFormat } from '@grafana/data';
 
 export const getYAxisFormattedValue = (
-	value: number,
+	value: string,
 	format: string,
 ): string => {
+	let numberValue: number = parseInt(value, 10);
+	let decimalPrecision: number | undefined;
+	try {
+		const decimalSplitted = value.split('.');
+		if (decimalSplitted.length === 1) {
+			decimalPrecision = 0;
+		} else {
+			const decimalDigits = decimalSplitted[1].split('');
+			decimalPrecision = decimalDigits.length;
+			let nonZeroCtr = 0;
+			for (let idx = 0; idx < decimalDigits.length; idx += 1) {
+				if (decimalDigits[idx] !== '0') {
+					nonZeroCtr += 1;
+					if (nonZeroCtr >= 2) {
+						decimalPrecision = idx + 1;
+						break;
+					}
+				}
+			}
+			numberValue = parseFloat(value);
+		}
+		return formattedValueToString(
+			getValueFormat(format)(numberValue, decimalPrecision, undefined, undefined),
+		);
+	} catch (error) {
+		console.error(error);
+	}
+	return `${numberValue}`;
+};
+
+export const getToolTipValue = (value: string, format: string): string => {
 	try {
 		return formattedValueToString(
-			getValueFormat(format)(value, 1, undefined, undefined),
+			getValueFormat(format)(parseFloat(value), undefined, undefined, undefined),
 		);
 	} catch (error) {
 		console.error(error);
