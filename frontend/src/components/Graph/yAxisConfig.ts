1a2ef4fde61203cf8b9dc85d50aa4b9ef240a730
Pranshu Chittora <pranshu@signoz.io>
2022-05-04 22:46:33 +0530

    fix: y-axis time unit (#1081)

--- frontend/src/components/Graph/yAxisConfig.ts
+++ frontend/src/components/Graph/yAxisConfig.ts
@@ -4,34 +4,39 @@ export const getYAxisFormattedValue = (
 	value: string,
 	format: string,
 ): string => {
-	let numberValue: number = parseInt(value, 10);
 	let decimalPrecision: number | undefined;
+	const parsedValue = getValueFormat(format)(
+		parseFloat(value),
+		undefined,
+		undefined,
+		undefined,
+	);
 	try {
-		const decimalSplitted = value.split('.');
+		const decimalSplitted = parsedValue.text.split('.');
 		if (decimalSplitted.length === 1) {
 			decimalPrecision = 0;
 		} else {
 			const decimalDigits = decimalSplitted[1].split('');
 			decimalPrecision = decimalDigits.length;
-			let nonZeroCtr = 0;
 			for (let idx = 0; idx < decimalDigits.length; idx += 1) {
 				if (decimalDigits[idx] !== '0') {
-					nonZeroCtr += 1;
-					if (nonZeroCtr >= 2) {
-						decimalPrecision = idx + 1;
-						break;
-					}
+					decimalPrecision = idx + 1;
+					break;
 				}
 			}
-			numberValue = parseFloat(value);
 		}
 		return formattedValueToString(
-			getValueFormat(format)(numberValue, decimalPrecision, undefined, undefined),
+			getValueFormat(format)(
+				parseFloat(value),
+				decimalPrecision,
+				undefined,
+				undefined,
+			),
 		);
 	} catch (error) {
 		console.error(error);
 	}
-	return `${numberValue}`;
+	return `${parseFloat(value)}`;
 };
 
 export const getToolTipValue = (value: string, format: string): string => {
