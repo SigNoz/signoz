8fc09c8d96c1624e2e1a62b4c007cbe4d8943162
Pranshu Chittora <pranshu@signoz.io>
2022-05-05 12:12:03 +0530

    fix: y-axis generic time unit conversion

--- frontend/src/components/Graph/yAxisConfig.ts
+++ frontend/src/components/Graph/yAxisConfig.ts
@@ -18,13 +18,22 @@ export const getYAxisFormattedValue = (
 		} else {
 			const decimalDigits = decimalSplitted[1].split('');
 			decimalPrecision = decimalDigits.length;
+			let nonZeroCtr = 0;
 			for (let idx = 0; idx < decimalDigits.length; idx += 1) {
 				if (decimalDigits[idx] !== '0') {
-					decimalPrecision = idx + 1;
+					nonZeroCtr += 1;
+					if (nonZeroCtr >= 2) {
+						decimalPrecision = idx + 1;
+					}
+				} else if (nonZeroCtr) {
+					decimalPrecision = idx;
 					break;
 				}
 			}
+			// decimalPrecision = idx + 2;
+			// break;
 		}
+
 		return formattedValueToString(
 			getValueFormat(format)(
 				parseFloat(value),
