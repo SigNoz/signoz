3c8eaf9792210244e3a5a9523ec1ad84cb243612
Srikanth Chekuri <srikanth.chekuri92@gmail.com>
2022-05-09 08:38:24 +0530

    Revert temp env change

--- frontend/src/constants/env.ts
+++ frontend/src/constants/env.ts
@@ -1,3 +1,6 @@
 export const ENVIRONMENT = {
-	baseURL: "http://localhost:8080",
+	baseURL:
+		process?.env?.FRONTEND_API_ENDPOINT ||
+		process?.env?.GITPOD_WORKSPACE_URL?.replace('://', '://8080-') ||
+		'',
 };
