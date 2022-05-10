b4833eeb0efbb44569d1a0da9665e98ce72560b9
Palash gupta <palash@signoz.io>
2022-05-10 17:27:04 +0530

    fix: list alerts rules is handled

--- frontend/src/container/ListAlertRules/index.tsx
+++ frontend/src/container/ListAlertRules/index.tsx
@@ -8,15 +8,22 @@ import ListAlert from './ListAlert';
 
 function ListAlertRules(): JSX.Element {
 	const { t } = useTranslation('common');
-	const { data, isError, isLoading, refetch } = useQuery('allAlerts', {
+	const { data, isError, isLoading, refetch, status } = useQuery('allAlerts', {
 		queryFn: getAll,
 		cacheTime: 0,
 	});
 
+	// api failed to load the data
 	if (isError) {
 		return <div>{data?.error || t('something_went_wrong')}</div>;
 	}
 
+	// api is successful but error is present
+	if (status === 'success' && data.statusCode !== 200) {
+		return <div>{data?.error || t('something_went_wrong')}</div>;
+	}
+
+	// in case of loading
 	if (isLoading || !data?.payload) {
 		return <Spinner height="75vh" tip="Loading Rules..." />;
 	}
