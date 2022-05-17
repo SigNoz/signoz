a2d67f1222f2c09bf808497146834ac9d61b4bca
Palash gupta <palash@signoz.io>
2022-05-17 11:55:30 +0530

    fix: isProduction is removed

--- frontend/src/constants/env.ts
+++ frontend/src/constants/env.ts
@@ -3,6 +3,5 @@ export const ENVIRONMENT = {
 		process?.env?.FRONTEND_API_ENDPOINT ||
 		process?.env?.GITPOD_WORKSPACE_URL?.replace('://', '://8080-') ||
 		'',
-	isProduction: process?.env?.NODE_ENV === 'production',
 	NODE_ENV: process?.env?.NODE_ENV,
 };
