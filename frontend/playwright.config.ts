b2fc4776b76ce2d5321ce3feaa52d3cf89def9e9
Palash gupta <palash@signoz.io>
2022-05-18 00:08:36 +0530

    feat: playwright is configured

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -1,4 +1,4 @@
-import { devices, PlaywrightTestConfig } from '@playwright/test';
+import { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
 	forbidOnly: !!process.env.CI,
@@ -7,24 +7,12 @@ const config: PlaywrightTestConfig = {
 	name: 'Signoz',
 	testDir: './tests',
 	use: {
-		trace: 'on-first-retry',
+		trace: 'retain-on-failure',
+		baseURL: process.env.FRONTEND_API_ENDPOINT,
 	},
 	updateSnapshots: 'all',
 	fullyParallel: false,
 	quiet: true,
-	projects: [
-		{
-			name: 'chromium',
-			use: { ...devices['Desktop Chrome'] },
-		},
-		{
-			name: 'firefox',
-			use: { ...devices['Desktop Firefox'] },
-		},
-		{
-			name: 'webkit',
-			use: { ...devices['Desktop Safari'] },
-		},
-	],
 };
+
 export default config;
