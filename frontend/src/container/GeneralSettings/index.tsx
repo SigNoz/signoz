db4f9ada0b25a8d1980cc6329a861abab362cd87
Pranshu Chittora <pranshu@signoz.io>
2022-05-24 18:47:34 +0530

    feat: ttl async with polling (#1195)

--- frontend/src/container/GeneralSettings/index.tsx
+++ frontend/src/container/GeneralSettings/index.tsx
@@ -5,15 +5,30 @@ import Spinner from 'components/Spinner';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useQueries } from 'react-query';
+import { ErrorResponse, SuccessResponse } from 'types/api';
+import { PayloadProps as GetRetentionPeriodAPIPayloadProps } from 'types/api/settings/getRetention';
 
 import GeneralSettingsContainer from './GeneralSettings';
 
 function GeneralSettings(): JSX.Element {
 	const { t } = useTranslation('common');
-	const [getRetentionPeriodApiResponse, getDisksResponse] = useQueries([
+	const [
+		getRetentionPeriodMetricsApiResponse,
+		getRetentionPeriodTracesApiResponse,
+		getDisksResponse,
+	] = useQueries([
 		{
-			queryFn: getRetentionPeriodApi,
-			queryKey: 'getRetentionPeriodApi',
+			queryFn: (): Promise<
+				| SuccessResponse<GetRetentionPeriodAPIPayloadProps<'metrics'>>
+				| ErrorResponse
+			> => getRetentionPeriodApi('metrics'),
+			queryKey: 'getRetentionPeriodApiMetrics',
+		},
+		{
+			queryFn: (): Promise<
+				SuccessResponse<GetRetentionPeriodAPIPayloadProps<'traces'>> | ErrorResponse
+			> => getRetentionPeriodApi('traces'),
+			queryKey: 'getRetentionPeriodApiTraces',
 		},
 		{
 			queryFn: getDisks,
@@ -21,10 +36,20 @@ function GeneralSettings(): JSX.Element {
 		},
 	]);
 
-	if (getRetentionPeriodApiResponse.isError || getDisksResponse.isError) {
+	if (getRetentionPeriodMetricsApiResponse.isError || getDisksResponse.isError) {
 		return (
 			<Typography>
-				{getRetentionPeriodApiResponse.data?.error ||
+				{getRetentionPeriodMetricsApiResponse.data?.error ||
+					getDisksResponse.data?.error ||
+					t('something_went_wrong')}
+			</Typography>
+		);
+	}
+
+	if (getRetentionPeriodTracesApiResponse.isError || getDisksResponse.isError) {
+		return (
+			<Typography>
+				{getRetentionPeriodTracesApiResponse.data?.error ||
 					getDisksResponse.data?.error ||
 					t('something_went_wrong')}
 			</Typography>
@@ -32,10 +57,14 @@ function GeneralSettings(): JSX.Element {
 	}
 
 	if (
-		getRetentionPeriodApiResponse.isLoading ||
+		getRetentionPeriodMetricsApiResponse.isLoading ||
 		getDisksResponse.isLoading ||
 		!getDisksResponse.data?.payload ||
-		!getRetentionPeriodApiResponse.data?.payload
+		!getRetentionPeriodMetricsApiResponse.data?.payload ||
+		getRetentionPeriodTracesApiResponse.isLoading ||
+		getDisksResponse.isLoading ||
+		!getDisksResponse.data?.payload ||
+		!getRetentionPeriodTracesApiResponse.data?.payload
 	) {
 		return <Spinner tip="Loading.." height="70vh" />;
 	}
@@ -44,7 +73,10 @@ function GeneralSettings(): JSX.Element {
 		<GeneralSettingsContainer
 			{...{
 				getAvailableDiskPayload: getDisksResponse.data?.payload,
-				ttlValuesPayload: getRetentionPeriodApiResponse.data?.payload,
+				metricsTtlValuesPayload: getRetentionPeriodMetricsApiResponse.data?.payload,
+				metricsTtlValuesRefetch: getRetentionPeriodMetricsApiResponse.refetch,
+				tracesTtlValuesPayload: getRetentionPeriodTracesApiResponse.data?.payload,
+				tracesTtlValuesRefetch: getRetentionPeriodTracesApiResponse.refetch,
 			}}
 		/>
 	);
