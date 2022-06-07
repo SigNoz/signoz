1c90e621898e6a0294ce59d5e04e52e4a65afcac
palash-signoz <88981777+palash-signoz@users.noreply.github.com>
2022-06-07 16:14:49 +0530

    feat: dashboard layout is updated (#1221)
    * feat: dashboard layout is updated
    
    * feat: onClick is made fixed
    
    * feat: layout is updated
    
    * feat: layout is updated
    
    * feat: layout is updated
    
    * fix: memo is removed and grid layout component is refactored to use use query
    
    * fix: saveDashboard is updated
    
    * feat: layout is fixed
    
    * fix: tsc error are fixed
    
    * fix: delete widgets is updated
    
    * fix: useMount once is added
    
    * fix: useMount once is removed
    
    * chore: removed the commented code
    
    Co-authored-by: Ankit Nayan <ankit@signoz.io>

---
+++ frontend/src/container/GridGraphLayout/GraphLayout.tsx
@@ -0,0 +1,101 @@
+import { PlusOutlined, SaveFilled } from '@ant-design/icons';
+import useComponentPermission from 'hooks/useComponentPermission';
+import React from 'react';
+import { Layout } from 'react-grid-layout';
+import { useSelector } from 'react-redux';
+import { AppState } from 'store/reducers';
+import { Widgets } from 'types/api/dashboard/getAll';
+import AppReducer from 'types/reducer/app';
+
+import { LayoutProps, State } from '.';
+import {
+	Button,
+	ButtonContainer,
+	Card,
+	CardContainer,
+	ReactGridLayout,
+} from './styles';
+
+function GraphLayout({
+	layouts,
+	saveLayoutState,
+	onLayoutSaveHandler,
+	addPanelLoading,
+	onAddPanelHandler,
+	onLayoutChangeHandler,
+	widgets,
+	setLayout,
+}: GraphLayoutProps): JSX.Element {
+	const { role } = useSelector<AppState, AppReducer>((state) => state.app);
+	const { isDarkMode } = useSelector<AppState, AppReducer>((state) => state.app);
+
+	const [saveLayout] = useComponentPermission(['save_layout'], role);
+
+	return (
+		<>
+			<ButtonContainer>
+				{saveLayout && (
+					<Button
+						loading={saveLayoutState.loading}
+						onClick={(): Promise<void> => onLayoutSaveHandler(layouts)}
+						icon={<SaveFilled />}
+						danger={saveLayoutState.error}
+					>
+						Save Layout
+					</Button>
+				)}
+
+				<Button
+					loading={addPanelLoading}
+					disabled={addPanelLoading}
+					onClick={onAddPanelHandler}
+					icon={<PlusOutlined />}
+				>
+					Add Panel
+				</Button>
+			</ButtonContainer>
+
+			<ReactGridLayout
+				isResizable
+				cols={12}
+				rowHeight={100}
+				autoSize
+				width={100}
+				isDraggable
+				isDroppable
+				useCSSTransforms
+				allowOverlap={false}
+				onLayoutChange={onLayoutChangeHandler}
+			>
+				{layouts.map(({ Component, ...rest }) => {
+					const currentWidget = (widgets || [])?.find((e) => e.id === rest.i);
+
+					return (
+						<CardContainer
+							isDarkMode={isDarkMode}
+							key={currentWidget?.id || 'empty'} // don't change this key
+							data-grid={rest}
+						>
+							<Card>
+								<Component setLayout={setLayout} />
+							</Card>
+						</CardContainer>
+					);
+				})}
+			</ReactGridLayout>
+		</>
+	);
+}
+
+interface GraphLayoutProps {
+	layouts: LayoutProps[];
+	saveLayoutState: State;
+	onLayoutSaveHandler: (layout: Layout[]) => Promise<void>;
+	addPanelLoading: boolean;
+	onAddPanelHandler: VoidFunction;
+	onLayoutChangeHandler: (layout: Layout[]) => Promise<void>;
+	widgets: Widgets[] | undefined;
+	setLayout: React.Dispatch<React.SetStateAction<LayoutProps[]>>;
+}
+
+export default GraphLayout;
