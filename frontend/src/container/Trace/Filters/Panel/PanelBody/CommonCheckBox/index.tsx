bef83d30cce8e3d86dc39c8e312c4759b44ef5f5
Palash <palashgdev@gmail.com>
2022-06-23 19:12:43 +0530

    feat: duration filter is updated (#1272)
    * feat: duration filter is updated
    
    * feat: search filter is added in the trace filter
    
    Co-authored-by: Palash gupta <palash@signoz.io>

--- frontend/src/container/Trace/Filters/Panel/PanelBody/CommonCheckBox/index.tsx
+++ frontend/src/container/Trace/Filters/Panel/PanelBody/CommonCheckBox/index.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import { Button, Input } from 'antd';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Dispatch } from 'redux';
 import { AppState } from 'store/reducers';
+import { INITIAL_FILTER_VALUE } from 'store/reducers/trace';
+import AppActions from 'types/actions';
+import { UPDATE_SPAN_UPDATE_FILTER_DISPLAY_VALUE } from 'types/actions/trace';
 import { TraceFilterEnum, TraceReducer } from 'types/reducer/trace';
 
 import CheckBoxComponent from '../Common/Checkbox';
 
+const { Search } = Input;
+
 function CommonCheckBox(props: CommonCheckBoxProps): JSX.Element {
-	const { filter } = useSelector<AppState, TraceReducer>(
+	const { filter, filterDisplayValue } = useSelector<AppState, TraceReducer>(
 		(state) => state.traces,
 	);
 
@@ -15,9 +22,34 @@ function CommonCheckBox(props: CommonCheckBoxProps): JSX.Element {
 	const status = filter.get(name) || {};
 
 	const statusObj = Object.keys(status);
+	const numberOfFilters = filterDisplayValue.get(name) || 0;
+	const dispatch = useDispatch<Dispatch<AppActions>>();
+	const [searchFilter, setSearchFilter] = useState<string>('');
+
+	const onClickMoreHandler = (): void => {
+		const newFilterDisplayValue = new Map(filterDisplayValue);
+		const preValue =
+			(newFilterDisplayValue.get(name) || 0) + INITIAL_FILTER_VALUE;
+
+		newFilterDisplayValue.set(name, preValue);
+
+		dispatch({
+			type: UPDATE_SPAN_UPDATE_FILTER_DISPLAY_VALUE,
+			payload: newFilterDisplayValue,
+		});
+	};
 
 	return (
 		<>
+			<Search
+				value={searchFilter}
+				onChange={(e): void => setSearchFilter(e.target.value)}
+				style={{
+					padding: '0 3%',
+				}}
+				placeholder="Filter Values"
+			/>
+
 			{statusObj
 				.sort((a, b) => {
 					const countA = +status[a];
@@ -27,6 +59,13 @@ function CommonCheckBox(props: CommonCheckBoxProps): JSX.Element {
 						return a.length - b.length;
 					}
 					return countA - countB;
+				})
+				.filter((_, index) => index < numberOfFilters)
+				.filter((filter) => {
+					if (searchFilter.length === 0) {
+						return true;
+					}
+					return filter.includes(searchFilter);
 				})
 				.map((e) => (
 					<CheckBoxComponent
@@ -38,6 +77,12 @@ function CommonCheckBox(props: CommonCheckBoxProps): JSX.Element {
 						}}
 					/>
 				))}
+
+			{numberOfFilters && statusObj.length > numberOfFilters && (
+				<Button onClick={onClickMoreHandler} type="link">
+					More
+				</Button>
+			)}
 		</>
 	);
 }
