1e980c3886701dc12c3b59e3ff29ceb44944da60
Palash <palashgdev@gmail.com>
2022-06-23 12:37:42 +0530

    feat: condition is updated

--- frontend/src/container/Trace/Filters/Panel/PanelBody/CommonCheckBox/index.tsx
+++ frontend/src/container/Trace/Filters/Panel/PanelBody/CommonCheckBox/index.tsx
@@ -39,6 +39,10 @@ function CommonCheckBox(props: CommonCheckBoxProps): JSX.Element {
 		});
 	};
 
+	const isMoreButtonAvilable = Boolean(
+		numberOfFilters && statusObj.length > numberOfFilters,
+	);
+
 	return (
 		<>
 			{statusObj.length > 0 && (
@@ -62,13 +66,15 @@ function CommonCheckBox(props: CommonCheckBoxProps): JSX.Element {
 					}
 					return countA - countB;
 				})
-				.filter((_, index) => index < numberOfFilters)
 				.filter((filter) => {
 					if (searchFilter.length === 0) {
 						return true;
 					}
-					return filter.includes(searchFilter);
+					return filter
+						.toLocaleLowerCase()
+						.includes(searchFilter.toLocaleLowerCase());
 				})
+				.filter((_, index) => index < numberOfFilters)
 				.map((e) => (
 					<CheckBoxComponent
 						key={e}
@@ -80,7 +86,7 @@ function CommonCheckBox(props: CommonCheckBoxProps): JSX.Element {
 					/>
 				))}
 
-			{numberOfFilters && statusObj.length > numberOfFilters && (
+			{isMoreButtonAvilable && (
 				<Button onClick={onClickMoreHandler} type="link">
 					More
 				</Button>
