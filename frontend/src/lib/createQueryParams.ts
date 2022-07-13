5554cce379347b9c4b6adae4e5c6850a5b6400c1
Palash <palashgdev@gmail.com>
2022-07-13 19:49:27 +0530

    feat: exception page is updated (#1376)
    * chore: all error utils is added
    
    * chore: error page list is added with total page and other handlings
    
    * test: unit test case for order is added

--- frontend/src/lib/createQueryParams.ts
+++ frontend/src/lib/createQueryParams.ts
@@ -1,6 +1,6 @@
-const createQueryParams = (params: { [x: string]: string }): string =>
+const createQueryParams = (params: { [x: string]: string | number }): string =>
 	Object.keys(params)
-		.map((k) => `${k}=${encodeURI(params[k])}`)
+		.map((k) => `${k}=${encodeURI(String(params[k]))}`)
 		.join('&');
 
 export default createQueryParams;
