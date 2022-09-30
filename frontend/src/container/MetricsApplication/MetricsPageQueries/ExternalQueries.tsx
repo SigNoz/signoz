364858f57446c90f533ff965fa745e927af3b716
Priyanka Chakraborty <chakraborty.priyanka@gmail.com>
2022-09-30 12:26:22 +0530

    refactor: remove explicit return

--- frontend/src/container/MetricsApplication/MetricsPageQueries/ExternalQueries.tsx
+++ frontend/src/container/MetricsApplication/MetricsPageQueries/ExternalQueries.tsx
@@ -4,234 +4,236 @@ import {
 	IQueryBuilderTagFilterItems,
 } from 'types/api/dashboard/getAll';
 
-export const externalCallErrorPercent = (
-	servicename: string | undefined,
-	legend: '{{address}}',
-	temp: IQueryBuilderTagFilterItems[] | [],
-): {
+export const externalCallErrorPercent = ({
+	servicename,
+	legend,
+	tagFilterItems,
+}: ExternalCallDurationByAddressProps): {
 	formulas: IMetricsBuilderFormula[];
 	queryBuilder: IMetricsBuilderQuery[];
-} => {
-	return {
-		formulas: [
-			{
-				name: 'F1',
-				expression: 'A*100/B',
-				disabled: false,
-				legend: 'External Call Error Percentage',
-			},
-		],
-		queryBuilder: [
-			{
-				name: 'A',
-				aggregateOperator: 18,
-				metricName: 'signoz_external_call_latency_count',
-				tagFilters: {
-					items: [
-						{
-							id: '',
-							key: 'service_name',
-							op: 'IN',
-							value: [`${servicename}`],
-						},
-						{
-							id: '',
-							key: 'status_code',
-							op: 'IN',
-							value: ['STATUS_CODE_ERROR'],
-						},
-						...temp,
-					],
+} => ({
+	formulas: [
+		{
+			name: 'F1',
+			expression: 'A*100/B',
+			disabled: false,
+			legend: 'External Call Error Percentage',
+		},
+	],
+	queryBuilder: [
+		{
+			name: 'A',
+			aggregateOperator: 18,
+			metricName: 'signoz_external_call_latency_count',
+			tagFilters: {
+				items: [
+					{
+						id: '',
+						key: 'service_name',
+						op: 'IN',
+						value: [`${servicename}`],
+					},
+					{
+						id: '',
+						key: 'status_code',
+						op: 'IN',
+						value: ['STATUS_CODE_ERROR'],
+					},
+					...tagFilterItems,
+				],
 
-					op: 'AND',
-				},
-				groupBy: ['address'],
-				legend,
-				disabled: false,
+				op: 'AND',
 			},
-			{
-				name: 'B',
-				aggregateOperator: 18,
-				metricName: 'signoz_external_call_latency_count',
-				tagFilters: {
-					items: [
-						{
-							id: '',
-							key: 'service_name',
-							op: 'IN',
-							value: [`${servicename}`],
-						},
-						...temp,
-					],
-					op: 'AND',
-				},
-				groupBy: ['address'],
-				legend,
-				disabled: false,
+			groupBy: ['address'],
+			legend,
+			disabled: false,
+		},
+		{
+			name: 'B',
+			aggregateOperator: 18,
+			metricName: 'signoz_external_call_latency_count',
+			tagFilters: {
+				items: [
+					{
+						id: '',
+						key: 'service_name',
+						op: 'IN',
+						value: [`${servicename}`],
+					},
+					...tagFilterItems,
+				],
+				op: 'AND',
 			},
-		],
-	};
-};
+			groupBy: ['address'],
+			legend,
+			disabled: false,
+		},
+	],
+});
 
 export const externalCallDuration = (
 	servicename: string | undefined,
-	temp: IQueryBuilderTagFilterItems[] | [],
+	tagFilterItems: IQueryBuilderTagFilterItems[] | [],
 ): {
 	formulas: IMetricsBuilderFormula[];
 	queryBuilder: IMetricsBuilderQuery[];
-} => {
-	return {
-		formulas: [
-			{
-				disabled: false,
-				expression: 'A/B',
-				name: 'F1',
-				legend: 'Average Duration',
+} => ({
+	formulas: [
+		{
+			disabled: false,
+			expression: 'A/B',
+			name: 'F1',
+			legend: 'Average Duration',
+		},
+	],
+	queryBuilder: [
+		{
+			aggregateOperator: 18,
+			disabled: true,
+			groupBy: [],
+			legend: '',
+			metricName: 'signoz_external_call_latency_sum',
+			name: 'A',
+			reduceTo: 1,
+			tagFilters: {
+				items: [
+					{
+						id: '',
+						key: 'service_name',
+						op: 'IN',
+						value: [`${servicename}`],
+					},
+					...tagFilterItems,
+				],
+				op: 'AND',
 			},
-		],
-		queryBuilder: [
-			{
-				aggregateOperator: 18,
-				disabled: true,
-				groupBy: [],
-				legend: '',
-				metricName: 'signoz_external_call_latency_sum',
-				name: 'A',
-				reduceTo: 1,
-				tagFilters: {
-					items: [
-						{
-							id: '',
-							key: 'service_name',
-							op: 'IN',
-							value: [`${servicename}`],
-						},
-						...temp,
-					],
-					op: 'AND',
-				},
+		},
+		{
+			aggregateOperator: 18,
+			disabled: true,
+			groupBy: [],
+			legend: '',
+			metricName: 'signoz_external_call_latency_count',
+			name: 'B',
+			reduceTo: 1,
+			tagFilters: {
+				items: [
+					{
+						id: '',
+						key: 'service_name',
+						op: 'IN',
+						value: [`${servicename}`],
+					},
+					...tagFilterItems,
+				],
+				op: 'AND',
 			},
-			{
-				aggregateOperator: 18,
-				disabled: true,
-				groupBy: [],
-				legend: '',
-				metricName: 'signoz_external_call_latency_count',
-				name: 'B',
-				reduceTo: 1,
-				tagFilters: {
-					items: [
-						{
-							id: '',
-							key: 'service_name',
-							op: 'IN',
-							value: [`${servicename}`],
-						},
-						...temp,
-					],
-					op: 'AND',
-				},
-			},
-		],
-	};
-};
+		},
+	],
+});
 
 export const externalCallRpsByAddress = (
 	servicename: string | undefined,
 	legend: '{{address}}',
-	temp: IQueryBuilderTagFilterItems[] | [],
+	tagFilterItems: IQueryBuilderTagFilterItems[] | [],
 ): {
 	formulas: IMetricsBuilderFormula[];
 	queryBuilder: IMetricsBuilderQuery[];
-} => {
-	return {
-		formulas: [],
-		queryBuilder: [
-			{
-				aggregateOperator: 18,
-				disabled: false,
-				groupBy: ['address'],
-				legend,
-				metricName: 'signoz_external_call_latency_count',
-				name: 'A',
-				reduceTo: 1,
-				tagFilters: {
-					items: [
-						{
-							id: '',
-							key: 'service_name',
-							op: 'IN',
-							value: [`${servicename}`],
-						},
-						...temp,
-					],
-					op: 'AND',
-				},
+} => ({
+	formulas: [],
+	queryBuilder: [
+		{
+			aggregateOperator: 18,
+			disabled: false,
+			groupBy: ['address'],
+			legend,
+			metricName: 'signoz_external_call_latency_count',
+			name: 'A',
+			reduceTo: 1,
+			tagFilters: {
+				items: [
+					{
+						id: '',
+						key: 'service_name',
+						op: 'IN',
+						value: [`${servicename}`],
+					},
+					...tagFilterItems,
+				],
+				op: 'AND',
 			},
-		],
-	};
-};
+		},
+	],
+});
 
 export const externalCallDurationByAddress = (
 	servicename: string | undefined,
+	// ExternalCallDurationByAddressProps,
 	legend: '{{address}}',
-	temp: IQueryBuilderTagFilterItems[] | [],
+	tagFilterItems: IQueryBuilderTagFilterItems[] | [],
 ): {
 	formulas: IMetricsBuilderFormula[];
 	queryBuilder: IMetricsBuilderQuery[];
-} => {
-	return {
-		formulas: [
-			{
-				disabled: false,
-				expression: 'A/B',
-				name: 'F1',
-				legend,
+} => ({
+	formulas: [
+		{
+			disabled: false,
+			expression: 'A/B',
+			name: 'F1',
+			legend,
+		},
+	],
+	queryBuilder: [
+		{
+			aggregateOperator: 18,
+			disabled: false,
+			groupBy: ['address'],
+			legend,
+			metricName: 'signoz_external_call_latency_sum',
+			name: 'A',
+			reduceTo: 1,
+			tagFilters: {
+				items: [
+					{
+						id: '',
+						key: 'service_name',
+						op: 'IN',
+						value: [`${servicename}`],
+					},
+					...tagFilterItems,
+				],
+				op: 'AND',
 			},
-		],
-		queryBuilder: [
-			{
-				aggregateOperator: 18,
-				disabled: false,
-				groupBy: ['address'],
-				legend,
-				metricName: 'signoz_external_call_latency_sum',
-				name: 'A',
-				reduceTo: 1,
-				tagFilters: {
-					items: [
-						{
-							id: '',
-							key: 'service_name',
-							op: 'IN',
-							value: [`${servicename}`],
-						},
-						...temp,
-					],
-					op: 'AND',
-				},
+		},
+		{
+			aggregateOperator: 18,
+			disabled: false,
+			groupBy: ['address'],
+			legend,
+			metricName: 'signoz_external_call_latency_count',
+			name: 'B',
+			reduceTo: 1,
+			tagFilters: {
+				items: [
+					{
+						id: '',
+						key: 'service_name',
+						op: 'IN',
+						value: [`${servicename}`],
+					},
+					...tagFilterItems,
+				],
+				op: 'AND',
 			},
-			{
-				aggregateOperator: 18,
-				disabled: false,
-				groupBy: ['address'],
-				legend,
-				metricName: 'signoz_external_call_latency_count',
-				name: 'B',
-				reduceTo: 1,
-				tagFilters: {
-					items: [
-						{
-							id: '',
-							key: 'service_name',
-							op: 'IN',
-							value: [`${servicename}`],
-						},
-						...temp,
-					],
-					op: 'AND',
-				},
-			},
-		],
-	};
-};
+		},
+	],
+});
+
+interface ExternalCallDurationByAddressProps extends ExternalCallProps {
+	legend?: '{{address}}';
+}
+
+interface ExternalCallProps {
+	servicename: string | undefined;
+	tagFilterItems: IQueryBuilderTagFilterItems[] | [];
+}
