6feaa4abc7d6506e7bc75eac1294259af4cf4446
Priyanka Chakraborty <chakraborty.priyanka@gmail.com>
2022-10-16 12:00:25 +0530

    refactor: factory-and-helper

---
+++ frontend/src/container/MetricsApplication/MetricsPageQueries/DBCallQueries.ts
@@ -0,0 +1,72 @@
+import {
+	IMetricsBuilderFormula,
+	IMetricsBuilderQuery,
+	IQueryBuilderTagFilterItems,
+} from 'types/api/dashboard/getAll';
+
+import {
+	getQueryBuilderQueries,
+	getQueryBuilderQuerieswithAdditionalItems,
+} from './MetricsPageQueriesFactory';
+
+export const databaseCallsRPS = ({
+	servicename,
+	legend,
+	tagFilterItems,
+}: DatabaseCallsRPSProps): {
+	formulas: IMetricsBuilderFormula[];
+	queryBuilder: IMetricsBuilderQuery[];
+} => {
+	const metricName = 'Database Calls RPS';
+	const groupBy = ['address'];
+
+	return getQueryBuilderQueries({
+		metricName,
+		legend,
+		groupBy,
+		servicename,
+		tagFilterItems,
+	});
+};
+
+export const databaseCallsAvgDuration = ({
+	servicename,
+	tagFilterItems,
+}: DatabaseCallProps): {
+	formulas: IMetricsBuilderFormula[];
+	queryBuilder: IMetricsBuilderQuery[];
+} => {
+	const metricNameA = 'signoz_db_latency_sum';
+	const metricNameB = 'signoz_db_latency_count';
+	const additionalItems = {
+		id: '',
+		key: 'status_code',
+		op: 'IN',
+		value: ['STATUS_CODE_ERROR'],
+	};
+	const expression = 'A/B';
+	const legendFormula = '';
+	const legend = '';
+	const disabled = true;
+
+	return getQueryBuilderQuerieswithAdditionalItems({
+		servicename,
+		legend,
+		disabled,
+		tagFilterItems,
+		metricNameA,
+		metricNameB,
+		additionalItems,
+		expression,
+		legendFormula,
+	});
+};
+
+interface DatabaseCallsRPSProps extends DatabaseCallProps {
+	legend: '{{db_system}}';
+}
+
+interface DatabaseCallProps {
+	servicename: string | undefined;
+	tagFilterItems: IQueryBuilderTagFilterItems[] | [];
+}
