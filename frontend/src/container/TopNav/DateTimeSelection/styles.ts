65af8c1b98d85469da6fdb40584df24457c9dbb4
Palash Gupta <palashgdev@gmail.com>
2022-11-10 20:48:40 +0530

    801 dropdown is added in the dashboard page (#1669)
    * chore: update the import from constant rather than static string
    
    * chore: removed redundant div
    
    * feat: added auto refresh component
    
    * refactor: top nav is refactored

--- frontend/src/container/TopNav/DateTimeSelection/styles.ts
+++ frontend/src/container/TopNav/DateTimeSelection/styles.ts
@@ -1,10 +1,6 @@
 import { Form as FormComponent, Typography as TypographyComponent } from 'antd';
 import styled from 'styled-components';
 
-export const Container = styled.div`
-	justify-content: flex-end;
-`;
-
 export const Form = styled(FormComponent)`
 	&&& {
 		justify-content: flex-end;
@@ -23,6 +19,12 @@ export const FormItem = styled(Form.Item)`
 	}
 `;
 
-export const RefreshTextContainer = styled.div`
+interface Props {
+	refreshButtonHidden: boolean;
+}
+
+export const RefreshTextContainer = styled.div<Props>`
 	min-height: 2rem;
+	visibility: ${({ refreshButtonHidden }): string =>
+		refreshButtonHidden ? 'hidden' : 'visible'};
 `;
