b55587c9445afd3c2cb5392f65ec53a1e6a9749f
Palash Gupta <palashgdev@gmail.com>
2023-01-04 12:48:12 +0530

    feat: antdv5 is updated (#1880)
    * feat: v5 is in progress
    
    * feat: antdv5 is updated
    
    * fix: build is fixed
    
    * fix: default config is over written by custom one
    
    * chore: onchange handler is updated
    
    * chore: overflow is hidden in the layout
    
    * Update index.tsx
    
    * fix: import is fixed
    
    * chore: un used import is fixed
    
    * fix: dark mode is updated in service map
    
    * fix: config dropdown is updated
    
    * fix: logs types is updated
    
    * fix: copy clipboard notification is updated
    
    Co-authored-by: Pranay Prateek <pranay@signoz.io>

--- frontend/src/components/Logs/CopyClipboardHOC.tsx
+++ frontend/src/components/Logs/CopyClipboardHOC.tsx
@@ -1,29 +1,27 @@
-import { Popover } from 'antd';
-import React from 'react';
+import { notification, Popover } from 'antd';
+import React, { useCallback, useEffect } from 'react';
 import { useCopyToClipboard } from 'react-use';
 
-interface CopyClipboardHOCProps {
-	textToCopy: string;
-	children: React.ReactNode;
-}
 function CopyClipboardHOC({
 	textToCopy,
 	children,
 }: CopyClipboardHOCProps): JSX.Element {
-	const [, setCopy] = useCopyToClipboard();
+	const [value, setCopy] = useCopyToClipboard();
+
+	useEffect(() => {
+		if (value.value) {
+			notification.success({
+				message: 'Copied to clipboard',
+			});
+		}
+	}, [value]);
+
+	const onClick = useCallback((): void => {
+		setCopy(textToCopy);
+	}, [setCopy, textToCopy]);
 
 	return (
-		<span
-			style={{
-				margin: 0,
-				padding: 0,
-				cursor: 'pointer',
-			}}
-			onClick={(): void => setCopy(textToCopy)}
-			onKeyDown={(): void => setCopy(textToCopy)}
-			role="button"
-			tabIndex={0}
-		>
+		<span onClick={onClick} onKeyDown={onClick} role="button" tabIndex={0}>
 			<Popover
 				placement="top"
 				content={<span style={{ fontSize: '0.9rem' }}>Copy to clipboard</span>}
@@ -32,6 +30,11 @@ function CopyClipboardHOC({
 			</Popover>
 		</span>
 	);
+}
+
+interface CopyClipboardHOCProps {
+	textToCopy: string;
+	children: React.ReactNode;
 }
 
 export default CopyClipboardHOC;
---
+++ frontend/src/container/ConfigDropdown/Config/styles.ts
@@ -0,0 +1,10 @@
+import { Menu } from 'antd';
+import styled from 'styled-components';
+
+export const MenuItem = styled(Menu.Item)`
+	&&& {
+		height: 1.75rem;
+		display: flex;
+		align-items: center;
+	}
+`;
