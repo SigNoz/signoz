5c63dfedf5bf62a1671a76f5cce9db680a2f3817
Palash <palashgdev@gmail.com>
2023-01-10 23:44:40 +0530

    chore: removed un used styles

--- frontend/src/container/GridGraphLayout/Graph/FullView/styles.ts
+++ frontend/src/container/GridGraphLayout/Graph/FullView/styles.ts
@@ -1,14 +1,5 @@
 import styled from 'styled-components';
 
-export const GraphContainer = styled.div`
-	min-height: 70vh;
-	align-items: center;
-	justify-content: center;
-	display: flex;
-	flex-direction: column;
-	width: 100%;
-`;
-
 export const NotFoundContainer = styled.div`
 	display: flex;
 	justify-content: center;
