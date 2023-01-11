ae650cd6b288a7a70ab4db773b6110c52893d070
Chintan Sudani <csudani7@gmail.com>
2023-01-11 21:37:34 +0530

    fix: Added theme file to maintain color easy

--- frontend/src/lib/getRandomColor.ts
+++ frontend/src/lib/getRandomColor.ts
@@ -1,47 +1,7 @@
 import { Span } from 'types/api/trace/getTraceItem';
+import { themeColors } from './theme';
 
-export const colors = [
-	'#2F80ED',
-	'#BB6BD9',
-	'#F2994A',
-	'#219653',
-	'#56CCF2',
-	'#F2C94C',
-	'#BDBDBD',
-	'#FF6633',
-	'#FFB399',
-	'#FF33FF',
-	'#FFFF99',
-	'#00B3E6',
-	'#E6B333',
-	'#3366E6',
-	'#999966',
-	'#99FF99',
-	'#B34D4D',
-	'#80B300',
-	'#809900',
-	'#E6B3B3',
-	'#6680B3',
-	'#66991A',
-	'#FF99E6',
-	'#CCFF1A',
-	'#FF1A66',
-	'#E6331A',
-	'#33FFCC',
-	'#66994D',
-	'#B366CC',
-	'#4D8000',
-	'#B33300',
-	'#CC80CC',
-	'#66664D',
-	'#991AFF',
-	'#E666FF',
-	'#4DB3FF',
-	'#1AB399',
-	'#E666B3',
-];
-
-export const errorColor = '#d32f2f';
+export const colors = Object.values(themeColors.chartcolors);
 
 export function getRandomNumber(min: number, max: number): number {
 	return Math.random() * (max - min) + min;
