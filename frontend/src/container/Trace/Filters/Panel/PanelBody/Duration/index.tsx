5ed7c9a46e159f1123a226fc76b9a0e268364567
Palash Gupta <palashgdev@gmail.com>
2023-01-13 12:01:46 +0530

    feat: react is updated to v18 (#2030)

--- frontend/src/container/Trace/Filters/Panel/PanelBody/Duration/index.tsx
+++ frontend/src/container/Trace/Filters/Panel/PanelBody/Duration/index.tsx
@@ -171,7 +171,7 @@ function Duration(): JSX.Element {
 		updatedUrl(min, max);
 	};
 
-	const TipComponent = useCallback((value) => {
+	const TipComponent = useCallback((value: undefined | number) => {
 		if (value === undefined) {
 			return <div />;
 		}
