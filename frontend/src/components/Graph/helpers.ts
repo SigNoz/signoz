a07af59bd3571dea94cad3dbf3067c05e055f7d3
Palash Gupta <palashgdev@gmail.com>
2023-01-18 19:30:57 +0530

    chore: helpers is updated

--- frontend/src/components/Graph/helpers.ts
+++ frontend/src/components/Graph/helpers.ts
@@ -1,12 +1,8 @@
 import { themeColors } from 'constants/theme';
 
 export const getAxisLabelColor = (currentTheme: string): string => {
-	switch (currentTheme) {
-		case 'dark':
-			return themeColors.whiteCream;
-		case 'light':
-			return themeColors.black;
-		default:
-			return themeColors.whiteCream;
+	if (currentTheme === 'light') {
+		return themeColors.black;
 	}
+	return themeColors.whiteCream;
 };
