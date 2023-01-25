3dc56427230d12042da738d173dd46bde2d97b91
makeavish <makeavish786@gmail.com>
2023-01-25 11:34:59 +0530

    feat: tag filtering frontend changes

--- frontend/src/container/GridGraphLayout/Graph/index.tsx
+++ frontend/src/container/GridGraphLayout/Graph/index.tsx
@@ -21,7 +21,6 @@ import { GlobalTime } from 'types/actions/globalTime';
 import { ErrorResponse, SuccessResponse } from 'types/api';
 import { Widgets } from 'types/api/dashboard/getAll';
 import { MetricRangePayloadProps } from 'types/api/metrics/getQueryRange';
-import DashboardReducer from 'types/reducer/dashboards';
 import { GlobalReducer } from 'types/reducer/globalTime';
 
 import { LayoutProps } from '..';
@@ -50,25 +49,13 @@ function GridCardGraph({
 		AppState,
 		GlobalReducer
 	>((state) => state.globalTime);
-	const { dashboards } = useSelector<AppState, DashboardReducer>(
-		(state) => state.dashboards,
-	);
-	const [selectedDashboard] = dashboards;
-	const selectedData = selectedDashboard?.data;
-	const { variables } = selectedData;
 
 	const response = useQuery<
 		SuccessResponse<MetricRangePayloadProps> | ErrorResponse
 	>(
 		[
 			`GetMetricsQueryRange-${widget.timePreferance}-${globalSelectedInterval}-${widget.id}`,
-			{
-				widget,
-				maxTime,
-				minTime,
-				globalSelectedInterval,
-				variables,
-			},
+			{ widget, maxTime, minTime, globalSelectedInterval },
 		],
 		() =>
 			GetMetricQueryRange({
