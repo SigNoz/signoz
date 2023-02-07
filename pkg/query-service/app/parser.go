538172869c88bd40d2e244ee788e557c2145153f
makeavish <makeavish786@gmail.com>
2023-02-07 10:38:39 +0530

    chore: handle none groupby in frontend

--- pkg/query-service/app/parser.go
+++ pkg/query-service/app/parser.go
@@ -355,11 +355,6 @@ func parseFilteredSpanAggregatesRequest(r *http.Request) (*model.GetFilteredSpan
 		}
 	}
 
-	groupBy := postData.GroupBy
-	if groupBy == "none" {
-		postData.GroupBy = ""
-	}
-
 	var dimension, aggregationOption string
 
 	switch function {
