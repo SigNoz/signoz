48a6513e6f3f9c1f6591453e5cb070515b7ae8a2
mindhash <mindhash@mindhashs-MacBook-Pro.local>
2023-02-09 16:03:24 +0530

    fix: changed ask admin message

--- pkg/query-service/app/clickhouseReader/reader.go
+++ pkg/query-service/app/clickhouseReader/reader.go
@@ -165,7 +165,7 @@ func (r *ClickHouseReader) Start(readerReady chan bool) {
 	// 	Format: &allowedFormat,
 	// }
 
-	logger := promlog.New(logLevel)
+	logger := promlog.New(&promlog.Config{Level: &logLevel})
 
 	startTime := func() (int64, error) {
 		return int64(promModel.Latest), nil
