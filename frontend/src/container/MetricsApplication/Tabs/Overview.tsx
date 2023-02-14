8fb5529165b5102cb973d49610634c12efc71f17
Anton <kolesiaaa@gmail.com>
2023-02-14 10:32:27 +0200

    fix: add condition for end and start stamps

--- frontend/src/container/MetricsApplication/Tabs/Overview.tsx
+++ frontend/src/container/MetricsApplication/Tabs/Overview.tsx
@@ -113,7 +113,9 @@ function Application({ getWidgetQueryBuilder }: DashboardProps): JSX.Element {
 			const startTimestamp = Math.trunc(start);
 			const endTimestamp = Math.trunc(end);
 
-			dispatch(UpdateTimeInterval('custom', [startTimestamp, endTimestamp]));
+			if (startTimestamp !== endTimestamp) {
+				dispatch(UpdateTimeInterval('custom', [startTimestamp, endTimestamp]));
+			}
 		},
 		[dispatch],
 	);
