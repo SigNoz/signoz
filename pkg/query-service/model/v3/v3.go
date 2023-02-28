c642e9a05beb706a38f5653160e333b39c095003
Srikanth Chekuri <srikanth.chekuri92@gmail.com>
2023-02-28 20:08:57 +0530

    chore: add query range v3 API request/response payload types (#2262)

--- pkg/query-service/model/v3/v3.go
+++ pkg/query-service/model/v3/v3.go
@@ -1,5 +1,7 @@
 package v3
 
+import "time"
+
 type DataSource string
 
 const (
@@ -41,6 +43,33 @@ const (
 	AggregateOperatorHistQuant90   AggregateOperator = "hist_quantile_90"
 	AggregateOperatorHistQuant95   AggregateOperator = "hist_quantile_95"
 	AggregateOperatorHistQuant99   AggregateOperator = "hist_quantile_99"
+)
+
+type ReduceToOperator string
+
+const (
+	ReduceToOperatorLast ReduceToOperator = "last"
+	ReduceToOperatorSum  ReduceToOperator = "sum"
+	ReduceToOperatorAvg  ReduceToOperator = "avg"
+	ReduceToOperatorMin  ReduceToOperator = "min"
+	ReduceToOperatorMax  ReduceToOperator = "max"
+)
+
+type QueryType string
+
+const (
+	QueryTypeBuilder       QueryType = "builder"
+	QueryTypeClickHouseSQL QueryType = "clickhouse_sql"
+	QueryTypePromQL        QueryType = "promql"
+)
+
+type PanelType string
+
+const (
+	PanelTypeValue PanelType = "value"
+	PanelTypeGraph PanelType = "graph"
+	PanelTypeTable PanelType = "table"
+	PanelTypeList  PanelType = "list"
 )
 
 // AggregateAttributeRequest is a request to fetch possible attribute keys
@@ -96,4 +125,97 @@ type FilterAttributeValueResponse struct {
 	StringAttributeValues []string      `json:"stringAttributeValues"`
 	NumberAttributeValues []interface{} `json:"numberAttributeValues"`
 	BoolAttributeValues   []bool        `json:"boolAttributeValues"`
+}
+
+type QueryRangeParamsV3 struct {
+	Start          int64                  `json:"start"`
+	End            int64                  `json:"end"`
+	Step           int64                  `json:"step"`
+	CompositeQuery *CompositeQuery        `json:"compositeQuery"`
+	Variables      map[string]interface{} `json:"variables,omitempty"`
+}
+
+type PromQuery struct {
+	Query    string `json:"query"`
+	Stats    string `json:"stats,omitempty"`
+	Disabled bool   `json:"disabled"`
+}
+
+type ClickHouseQuery struct {
+	Query    string `json:"query"`
+	Disabled bool   `json:"disabled"`
+}
+
+type CompositeQuery struct {
+	BuilderQueries    map[string]*BuilderQuery    `json:"builderQueries,omitempty"`
+	ClickHouseQueries map[string]*ClickHouseQuery `json:"chQueries,omitempty"`
+	PromQueries       map[string]*PromQuery       `json:"promQueries,omitempty"`
+	PanelType         PanelType                   `json:"panelType"`
+	QueryType         QueryType                   `json:"queryType"`
+}
+
+type BuilderQuery struct {
+	QueryName          string            `json:"queryName"`
+	DataSource         DataSource        `json:"dataSource"`
+	AggregateOperator  AggregateOperator `json:"aggregateOperator"`
+	AggregateAttribute string            `json:"aggregateAttribute,omitempty"`
+	Filters            *FilterSet        `json:"filters,omitempty"`
+	GroupBy            []string          `json:"groupBy,omitempty"`
+	Expression         string            `json:"expression"`
+	Disabled           bool              `json:"disabled"`
+	Having             []Having          `json:"having,omitempty"`
+	Limit              uint64            `json:"limit"`
+	Offset             uint64            `json:"offset"`
+	PageSize           uint64            `json:"pageSize"`
+	OrderBy            []OrderBy         `json:"orderBy,omitempty"`
+	ReduceTo           ReduceToOperator  `json:"reduceTo,omitempty"`
+	SelectColumns      []string          `json:"selectColumns,omitempty"`
+}
+
+type FilterSet struct {
+	Operator string       `json:"op,omitempty"`
+	Items    []FilterItem `json:"items"`
+}
+
+type FilterItem struct {
+	Key      string      `json:"key"`
+	Value    interface{} `json:"value"`
+	Operator string      `json:"op"`
+}
+
+type OrderBy struct {
+	ColumnName string `json:"columnName"`
+	Order      string `json:"order"`
+}
+
+type Having struct {
+	ColumnName string      `json:"columnName"`
+	Operator   string      `json:"operator"`
+	Value      interface{} `json:"value"`
+}
+
+type QueryRangeResponse struct {
+	ResultType string    `json:"resultType"`
+	Result     []*Result `json:"result"`
+}
+
+type Result struct {
+	QueryName string  `json:"queryName"`
+	Series    *Series `json:"series"`
+	List      []*Row  `json:"list"`
+}
+
+type Series struct {
+	Labels map[string]string `json:"labels"`
+	Points []Point           `json:"values"`
+}
+
+type Row struct {
+	Timestamp time.Time         `json:"timestamp"`
+	Data      map[string]string `json:"data"`
+}
+
+type Point struct {
+	Timestamp int64   `json:"timestamp"`
+	Value     float64 `json:"value"`
 }
