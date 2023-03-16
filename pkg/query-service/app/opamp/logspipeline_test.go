bac717e9e6891715d980252135d922e0ce76b7e9
nityanandagohain <nityanandagohain@gmail.com>
2023-03-16 10:20:57 +0530

    fix: use structs instead of interface

--- pkg/query-service/app/opamp/logspipeline_test.go
+++ pkg/query-service/app/opamp/logspipeline_test.go
@@ -22,11 +22,11 @@ var buildProcessorTestData = []struct {
 			},
 		},
 		pipelineProcessor: map[string]interface{}{
-			constants.LogsPPLPfx + "_b": struct{}{},
+			constants.LogsPPLPfx + "b": struct{}{},
 		},
 		outputConf: map[string]interface{}{
 			"processors": map[string]interface{}{
-				constants.LogsPPLPfx + "_b": struct{}{},
+				constants.LogsPPLPfx + "b": struct{}{},
 				"batch":                     struct{}{},
 			},
 		},
@@ -35,7 +35,7 @@ var buildProcessorTestData = []struct {
 		Name: "Remove",
 		agentConf: map[string]interface{}{
 			"processors": map[string]interface{}{
-				constants.LogsPPLPfx + "_b": struct{}{},
+				constants.LogsPPLPfx + "b": struct{}{},
 				"batch":                     struct{}{},
 			},
 		},
@@ -50,17 +50,17 @@ var buildProcessorTestData = []struct {
 		Name: "remove and upsert 1",
 		agentConf: map[string]interface{}{
 			"processors": map[string]interface{}{
-				constants.LogsPPLPfx + "_a": struct{}{},
-				constants.LogsPPLPfx + "_b": struct{}{},
+				constants.LogsPPLPfx + "a": struct{}{},
+				constants.LogsPPLPfx + "b": struct{}{},
 				"batch":                     struct{}{},
 			},
 		},
 		pipelineProcessor: map[string]interface{}{
-			constants.LogsPPLPfx + "_b": struct{}{},
+			constants.LogsPPLPfx + "b": struct{}{},
 		},
 		outputConf: map[string]interface{}{
 			"processors": map[string]interface{}{
-				constants.LogsPPLPfx + "_b": struct{}{},
+				constants.LogsPPLPfx + "b": struct{}{},
 				"batch":                     struct{}{},
 			},
 		},
@@ -69,19 +69,19 @@ var buildProcessorTestData = []struct {
 		Name: "remove and upsert 2",
 		agentConf: map[string]interface{}{
 			"processors": map[string]interface{}{
-				"memory_limiter":            struct{}{},
-				constants.LogsPPLPfx + "_a": struct{}{},
-				constants.LogsPPLPfx + "_b": struct{}{},
+				"memorylimiter":            struct{}{},
+				constants.LogsPPLPfx + "a": struct{}{},
+				constants.LogsPPLPfx + "b": struct{}{},
 				"batch":                     struct{}{},
 			},
 		},
 		pipelineProcessor: map[string]interface{}{
-			constants.LogsPPLPfx + "_b": struct{}{},
+			constants.LogsPPLPfx + "b": struct{}{},
 		},
 		outputConf: map[string]interface{}{
 			"processors": map[string]interface{}{
-				"memory_limiter":            struct{}{},
-				constants.LogsPPLPfx + "_b": struct{}{},
+				"memorylimiter":            struct{}{},
+				constants.LogsPPLPfx + "b": struct{}{},
 				"batch":                     struct{}{},
 			},
 		},
@@ -89,7 +89,7 @@ var buildProcessorTestData = []struct {
 }
 
 func TestBuildLogParsingProcessors(t *testing.T) {
-	for _, test := range buildProcessorTestData {
+	for , test := range buildProcessorTestData {
 		Convey(test.Name, t, func() {
 			err := buildLogParsingProcessors(test.agentConf, test.pipelineProcessor)
 			So(err, ShouldBeNil)
@@ -101,56 +101,56 @@ func TestBuildLogParsingProcessors(t *testing.T) {
 
 var BuildLogsPipelineTestData = []struct {
 	Name             string
-	currentPipeline  []interface{}
-	logsPipeline     []interface{}
-	expectedPipeline []interface{}
+	currentPipeline  []string
+	logsPipeline     []string
+	expectedPipeline []string
 }{
 	{
 		Name:             "Add new pipelines",
-		currentPipeline:  []interface{}{"processor1", "processor2"},
-		logsPipeline:     []interface{}{constants.LogsPPLPfx + "_a", constants.LogsPPLPfx + "_b"},
-		expectedPipeline: []interface{}{constants.LogsPPLPfx + "_a", constants.LogsPPLPfx + "_b", "processor1", "processor2"},
+		currentPipeline:  []string{"processor1", "processor2"},
+		logsPipeline:     []string{constants.LogsPPLPfx + "a", constants.LogsPPLPfx + "b"},
+		expectedPipeline: []string{constants.LogsPPLPfx + "a", constants.LogsPPLPfx + "b", "processor1", "processor2"},
 	},
 	{
 		Name:             "Add new pipeline and respect custom processors",
-		currentPipeline:  []interface{}{constants.LogsPPLPfx + "_a", "processor1", constants.LogsPPLPfx + "_b", "processor2"},
-		logsPipeline:     []interface{}{constants.LogsPPLPfx + "_a", constants.LogsPPLPfx + "_b", constants.LogsPPLPfx + "_c"},
-		expectedPipeline: []interface{}{constants.LogsPPLPfx + "_a", "processor1", constants.LogsPPLPfx + "_b", constants.LogsPPLPfx + "_c", "processor2"},
+		currentPipeline:  []string{constants.LogsPPLPfx + "a", "processor1", constants.LogsPPLPfx + "b", "processor2"},
+		logsPipeline:     []string{constants.LogsPPLPfx + "a", constants.LogsPPLPfx + "b", constants.LogsPPLPfx + "c"},
+		expectedPipeline: []string{constants.LogsPPLPfx + "a", "processor1", constants.LogsPPLPfx + "b", constants.LogsPPLPfx + "c", "processor2"},
 	},
 	{
 		Name:             "Add new pipeline and respect custom processors in the beginning and middle",
-		currentPipeline:  []interface{}{"processor1", constants.LogsPPLPfx + "_a", "processor2", constants.LogsPPLPfx + "_b", "batch"},
-		logsPipeline:     []interface{}{constants.LogsPPLPfx + "_a", constants.LogsPPLPfx + "_b", constants.LogsPPLPfx + "_c"},
-		expectedPipeline: []interface{}{"processor1", constants.LogsPPLPfx + "_a", "processor2", constants.LogsPPLPfx + "_b", constants.LogsPPLPfx + "_c", "batch"},
+		currentPipeline:  []string{"processor1", constants.LogsPPLPfx + "a", "processor2", constants.LogsPPLPfx + "b", "batch"},
+		logsPipeline:     []string{constants.LogsPPLPfx + "a", constants.LogsPPLPfx + "b", constants.LogsPPLPfx + "c"},
+		expectedPipeline: []string{"processor1", constants.LogsPPLPfx + "a", "processor2", constants.LogsPPLPfx + "b", constants.LogsPPLPfx + "c", "batch"},
 	},
 	{
 		Name:             "Remove old pipeline add add new",
-		currentPipeline:  []interface{}{constants.LogsPPLPfx + "_a", constants.LogsPPLPfx + "_b", "processor1", "processor2"},
-		logsPipeline:     []interface{}{constants.LogsPPLPfx + "_a"},
-		expectedPipeline: []interface{}{constants.LogsPPLPfx + "_a", "processor1", "processor2"},
+		currentPipeline:  []string{constants.LogsPPLPfx + "a", constants.LogsPPLPfx + "b", "processor1", "processor2"},
+		logsPipeline:     []string{constants.LogsPPLPfx + "a"},
+		expectedPipeline: []string{constants.LogsPPLPfx + "a", "processor1", "processor2"},
 	},
 	{
 		Name:             "Remove old pipeline from middle",
-		currentPipeline:  []interface{}{"processor1", "processor2", constants.LogsPPLPfx + "_a", "processor3", constants.LogsPPLPfx + "_b", "batch"},
-		logsPipeline:     []interface{}{constants.LogsPPLPfx + "_a"},
-		expectedPipeline: []interface{}{"processor1", "processor2", constants.LogsPPLPfx + "_a", "processor3", "batch"},
+		currentPipeline:  []string{"processor1", "processor2", constants.LogsPPLPfx + "a", "processor3", constants.LogsPPLPfx + "b", "batch"},
+		logsPipeline:     []string{constants.LogsPPLPfx + "a"},
+		expectedPipeline: []string{"processor1", "processor2", constants.LogsPPLPfx + "a", "processor3", "batch"},
 	},
 	{
 		Name:             "Remove old pipeline from middle and add new pipeline",
-		currentPipeline:  []interface{}{"processor1", "processor2", constants.LogsPPLPfx + "_a", "processor3", constants.LogsPPLPfx + "_b", "batch"},
-		logsPipeline:     []interface{}{constants.LogsPPLPfx + "_a", constants.LogsPPLPfx + "_c"},
-		expectedPipeline: []interface{}{"processor1", "processor2", constants.LogsPPLPfx + "_a", constants.LogsPPLPfx + "_c", "processor3", "batch"},
+		currentPipeline:  []string{"processor1", "processor2", constants.LogsPPLPfx + "a", "processor3", constants.LogsPPLPfx + "b", "batch"},
+		logsPipeline:     []string{constants.LogsPPLPfx + "a", constants.LogsPPLPfx + "c"},
+		expectedPipeline: []string{"processor1", "processor2", constants.LogsPPLPfx + "a", constants.LogsPPLPfx + "c", "processor3", "batch"},
 	},
 	{
 		Name:             "Remove multiple old pipelines from middle and add multiple new ones",
-		currentPipeline:  []interface{}{"processor1", constants.LogsPPLPfx + "_a", "processor2", constants.LogsPPLPfx + "_b", "processor3", constants.LogsPPLPfx + "_c", "processor4", constants.LogsPPLPfx + "_d", "processor5", "batch"},
-		logsPipeline:     []interface{}{constants.LogsPPLPfx + "_a", constants.LogsPPLPfx + "_a1", constants.LogsPPLPfx + "_c", constants.LogsPPLPfx + "_c1"},
-		expectedPipeline: []interface{}{"processor1", constants.LogsPPLPfx + "_a", constants.LogsPPLPfx + "_a1", "processor2", "processor3", constants.LogsPPLPfx + "_c", constants.LogsPPLPfx + "_c1", "processor4", "processor5", "batch"},
+		currentPipeline:  []string{"processor1", constants.LogsPPLPfx + "a", "processor2", constants.LogsPPLPfx + "b", "processor3", constants.LogsPPLPfx + "c", "processor4", constants.LogsPPLPfx + "d", "processor5", "batch"},
+		logsPipeline:     []string{constants.LogsPPLPfx + "a", constants.LogsPPLPfx + "a1", constants.LogsPPLPfx + "c", constants.LogsPPLPfx + "c1"},
+		expectedPipeline: []string{"processor1", constants.LogsPPLPfx + "a", constants.LogsPPLPfx + "a1", "processor2", "processor3", constants.LogsPPLPfx + "c", constants.LogsPPLPfx + "c1", "processor4", "processor5", "batch"},
 	},
 }
 
 func TestBuildLogsPipeline(t *testing.T) {
-	for _, test := range BuildLogsPipelineTestData {
+	for , test := range BuildLogsPipelineTestData {
 		Convey(test.Name, t, func() {
 			v, err := buildLogsProcessors(test.currentPipeline, test.logsPipeline)
 			So(err, ShouldBeNil)
