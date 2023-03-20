5f7b8eba74e7651297c31da6f52b3b47458c3251
Chintan Sudani <csudani7@gmail.com>
2023-03-20 12:40:58 +0530

    fix: scalable code of component of column

--- frontend/src/container/PipelinePage/PipelineListsView/TableComponents/index.tsx
+++ frontend/src/container/PipelinePage/PipelineListsView/TableComponents/index.tsx
@@ -2,29 +2,32 @@ import dayjs from 'dayjs';
 import React from 'react';
 import { PipelineData, ProcessorData } from 'types/api/pipeline/def';
 
+import { PipelineIndexIcon } from '../AddNewProcessor/styles';
 import { ListDataStyle, ProcessorIndexIcon } from '../styles';
-import PipelineSequence from './PipelineSequence';
+
+const componentMap: ComponentMap = {
+	orderId: ({ record }) => <PipelineIndexIcon>{record}</PipelineIndexIcon>,
+	createdAt: ({ record }) => (
+		<span>{dayjs(record).locale('en').format('MMMM DD, YYYY hh:mm A')}</span>
+	),
+	id: ({ record }) => <ProcessorIndexIcon>{record}</ProcessorIndexIcon>,
+	name: ({ record }) => <ListDataStyle>{record}</ListDataStyle>,
+};
 
 function TableComponents({
 	columnKey,
 	record,
 }: TableComponentsProps): JSX.Element {
-	if (columnKey === 'orderId') {
-		return <PipelineSequence value={record} />;
-	}
-	if (columnKey === 'createdAt') {
-		return (
-			<span>{dayjs(record).locale('en').format('MMMM DD, YYYY hh:mm A')}</span>
-		);
-	}
-	if (columnKey === 'id') {
-		return <ProcessorIndexIcon>{record}</ProcessorIndexIcon>;
-	}
-	if (columnKey === 'name') {
-		return <ListDataStyle>{record}</ListDataStyle>;
-	}
-	return <span>{record}</span>;
+	const Component =
+		componentMap[columnKey] ??
+		(({ record }): JSX.Element => <span>{record}</span>);
+
+	return <Component record={record} />;
 }
+
+type ComponentMap = {
+	[key: string]: React.FC<{ record: Record }>;
+};
 
 export type Record = PipelineData['orderId'] & ProcessorData;
 
