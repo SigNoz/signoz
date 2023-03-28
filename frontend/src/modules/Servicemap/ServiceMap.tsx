cc84d28872b68352634426969ea81b4d6af5a5e3
Palash Gupta <palashgdev@gmail.com>
2023-03-28 11:46:25 +0530

    feat: service map is updated

--- frontend/src/modules/Servicemap/ServiceMap.tsx
+++ frontend/src/modules/Servicemap/ServiceMap.tsx
@@ -4,11 +4,9 @@
 import { Card } from 'antd';
 import Spinner from 'components/Spinner';
 import ResourceAttributesFilter from 'container/ResourceAttributesFilter';
-import { useIsDarkMode } from 'hooks/useDarkMode';
 import useResourceAttribute from 'hooks/useResourceAttribute';
 import { IResourceAttribute } from 'hooks/useResourceAttribute/types';
 import React, { useEffect, useRef } from 'react';
-import { ForceGraph2D } from 'react-force-graph';
 import { connect } from 'react-redux';
 import { RouteComponentProps, withRouter } from 'react-router-dom';
 import { getDetailedServiceMapItems, ServiceMapStore } from 'store/actions';
@@ -16,7 +14,7 @@ import { AppState } from 'store/reducers';
 import styled from 'styled-components';
 import { GlobalTime } from 'types/actions/globalTime';
 
-import { getGraphData, getTooltip, getZoomPx, transformLabel } from './utils';
+import Map from './Map';
 
 const Container = styled.div`
 	.force-graph-container {
@@ -66,8 +64,6 @@ export interface graphDataType {
 function ServiceMap(props: ServiceMapProps): JSX.Element {
 	const fgRef = useRef();
 
-	const isDarkMode = useIsDarkMode();
-
 	const { getDetailedServiceMapItems, globalTime, serviceMap } = props;
 
 	const { queries } = useResourceAttribute();
@@ -96,48 +92,10 @@ function ServiceMap(props: ServiceMapProps): JSX.Element {
 			</Container>
 		);
 	}
-
-	const { nodes, links } = getGraphData(serviceMap, isDarkMode);
-	const graphData = { nodes, links };
 	return (
 		<Container>
 			<ResourceAttributesFilter />
-			<ForceGraph2D
-				ref={fgRef}
-				cooldownTicks={100}
-				graphData={graphData}
-				linkLabel={getTooltip}
-				linkAutoColorBy={(d) => d.target}
-				linkDirectionalParticles="value"
-				linkDirectionalParticleSpeed={(d) => d.value}
-				nodeCanvasObject={(node, ctx) => {
-					const label = transformLabel(node.id);
-					const { fontSize } = node;
-					ctx.font = `${fontSize}px Roboto`;
-					const { width } = node;
-
-					ctx.fillStyle = node.color;
-					ctx.beginPath();
-					ctx.arc(node.x, node.y, width, 0, 2 * Math.PI, false);
-					ctx.fill();
-					ctx.textAlign = 'center';
-					ctx.textBaseline = 'middle';
-					ctx.fillStyle = isDarkMode ? '#ffffff' : '#000000';
-					ctx.fillText(label, node.x, node.y);
-				}}
-				onLinkHover={(node) => {
-					const tooltip = document.querySelector('.graph-tooltip');
-					if (tooltip && node) {
-						tooltip.innerHTML = getTooltip(node);
-					}
-				}}
-				nodePointerAreaPaint={(node, color, ctx) => {
-					ctx.fillStyle = color;
-					ctx.beginPath();
-					ctx.arc(node.x, node.y, 5, 0, 2 * Math.PI, false);
-					ctx.fill();
-				}}
-			/>
+			<Map fgRef={fgRef} serviceMap={serviceMap} />
 		</Container>
 	);
 }
