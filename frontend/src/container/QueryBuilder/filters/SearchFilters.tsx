420ae875f619bc9b4b85fca77995246a4be1207f
Chintan Sudani <csudani7@gmail.com>
2023-03-29 15:01:53 +0530

    feat: poc of search bar

--- frontend/src/container/QueryBuilder/filters/SearchFilters.tsx
+++ frontend/src/container/QueryBuilder/filters/SearchFilters.tsx
@@ -1,7 +1,66 @@
-import React from 'react';
+import { Select } from 'antd';
+import { IOption } from 'container/MetricsApplication/ResourceAttributesFilter/types';
+import React, { useCallback, useEffect, useState } from 'react';
 
 function SearchFilters(): JSX.Element {
-	return <div>SearchFilters</div>;
+	const [currentState, setCurrentState] = useState<string>('tagKey');
+	const [optionsData, setOptionsData] = useState<IOption[]>([]);
+
+	useEffect(() => {
+		if (currentState === 'tagKey') {
+			setOptionsData([
+				{ value: 'jack', label: 'Jack' },
+				{ value: 'lucy', label: 'Lucy' },
+				{ value: 'Yiminghe', label: 'yiminghe' },
+				{ value: 'disabled', label: 'Disabled' },
+			]);
+		}
+		if (currentState === 'tagOperator') {
+			setOptionsData([
+				{ value: '=', label: '=' },
+				{ value: '!=', label: '!=' },
+				{ value: '>', label: '>' },
+				{ value: '<', label: '<' },
+			]);
+		}
+		if (currentState === 'tagValue') {
+			setOptionsData([
+				{ value: '12', label: '12' },
+				{ value: '34', label: '34' },
+				{ value: '10', label: '10' },
+				{ value: '14', label: '14' },
+			]);
+		}
+	}, [currentState]);
+
+	const handleChange = useCallback(
+		(value: string[]): void => {
+			console.log(`selected ${value}`);
+			if (currentState === 'tagKey') {
+				setCurrentState('tagOperator');
+			}
+			if (currentState === 'tagOperator') {
+				setCurrentState('tagValue');
+			}
+			if (currentState === 'tagValues') {
+				setCurrentState('');
+			}
+		},
+		[currentState],
+	);
+
+	return (
+		<Select
+			allowClear
+			placeholder="Type your Query"
+			onChange={handleChange}
+			style={{ width: '100%' }}
+			mode="tags"
+			showArrow={false}
+			optionLabelProp="label"
+			options={optionsData}
+		/>
+	);
 }
 
 export default SearchFilters;
