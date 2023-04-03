f138c649b003287a369aa7fe17032562f3d5639a
Chintan Sudani <csudani7@gmail.com>
2023-04-03 10:40:39 +0530

    chore: conflict resolve

---
+++ frontend/src/container/QueryBuilder/components/Query/config.ts
@@ -0,0 +1,4 @@
+export const queryLabelStyle = { marginRight: '0.5rem' };
+export const operatorsSelectStyle = { minWidth: 104, marginRight: '0.75rem' };
+export const dropdownStyle = { display: 'none', marginLeft: '0.5rem' };
+export const selectAreaStyle = { marginRight: '0.5rem', marginLeft: '0.5rem' };
