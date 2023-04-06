f176bfadb4356f28d5379c80fa94c9165202b0be
Chintan Sudani <csudani7@gmail.com>
2023-04-06 14:57:40 +0530

    fix: suggested changes

--- frontend/src/container/QueryBuilder/components/Query/config.ts
+++
@@ -1,2 +0,0 @@
-export const queryLabelStyle = { marginRight: '0.5rem', marginLeft: '0.5rem' };
-export const dropdownStyle = { display: 'none', marginLeft: '0.5rem' };
---
+++ frontend/src/container/QueryBuilder/components/Query/styles.ts
@@ -0,0 +1,7 @@
+import { Select } from 'antd';
+import styled from 'styled-components';
+
+export const StyledSelect = styled(Select)`
+	margin-right: 0.5rem;
+	margin-left: 0.5rem;
+`;
