d25c36828eb551d2c616607dee03afa0ce549c4e
Chintan Sudani <csudani7@gmail.com>
2023-04-06 14:57:40 +0530

    fix: suggested changes

--- frontend/src/container/QueryBuilder/components/Query/config.ts
+++
@@ -1,3 +0,0 @@
-export const queryLabelStyle = { marginRight: '0.5rem', marginLeft: '0.5rem' };
-export const dropdownStyle = { display: 'none', marginLeft: '0.5rem' };
-export const selectStyle = { width: '100%' };
