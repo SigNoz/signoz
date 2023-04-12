813eeb6d5a0ca1d5d62818948cc410369b6e1d80
Palash Gupta <palashgdev@gmail.com>
2023-04-12 13:30:50 +0530

    test: metrics table test case is added (#2554)

--- frontend/jest.setup.ts
+++ frontend/jest.setup.ts
@@ -1,5 +1,20 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable object-shorthand */
+/* eslint-disable func-names */
+
 /**
  * Adds custom matchers from the react testing library to all tests
  */
 import '@testing-library/jest-dom';
 import 'jest-styled-components';
+
+// Mock window.matchMedia
+window.matchMedia =
+	window.matchMedia ||
+	function (): any {
+		return {
+			matches: false,
+			addListener: function () {},
+			removeListener: function () {},
+		};
+	};
