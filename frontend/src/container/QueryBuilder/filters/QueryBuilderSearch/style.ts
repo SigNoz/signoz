51f1d0fd05a057cd347b049f5d39b8b836f491a3
Chintan Sudani <46838508+techchintan@users.noreply.github.com>
2023-04-18 11:44:28 +0530

    feat: added aggregate every filter (#2581)
    * fix: remove frontend code owner
    
    * chore: set Cache-Control for auto complete requests (#2504)
    
    * feat(filter): add group by filter (#2538)
    
    * feat: added aggregate every filter
    
    ---------
    
    Co-authored-by: Srikanth Chekuri <srikanth.chekuri92@gmail.com>
    Co-authored-by: Yevhen Shevchenko <90138953+yeshev@users.noreply.github.com>

--- frontend/src/container/QueryBuilder/filters/QueryBuilderSearch/style.ts
+++ frontend/src/container/QueryBuilder/filters/QueryBuilderSearch/style.ts
@@ -2,8 +2,8 @@ import { CheckOutlined } from '@ant-design/icons';
 import { Typography } from 'antd';
 import styled from 'styled-components';
 
-export const TypographyText = styled(Typography.Text)<{ isInNin: boolean }>`
-	width: ${({ isInNin }): string => (isInNin ? '10rem' : 'auto')};
+export const TypographyText = styled(Typography.Text)<{ $isInNin: boolean }>`
+	width: ${({ $isInNin }): string => ($isInNin ? '10rem' : 'auto')};
 `;
 
 export const StyledCheckOutlined = styled(CheckOutlined)`
