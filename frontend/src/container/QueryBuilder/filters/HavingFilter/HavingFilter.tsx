4c583932ac6a2b357342ae6f7cb7daf6fd57f90f
Yevhen Shevchenko <y.shevchenko@seedium.io>
2023-04-18 15:17:52 +0300

    feat(builder): add having filter

---
+++ frontend/src/container/QueryBuilder/filters/HavingFilter/HavingFilter.tsx
@@ -0,0 +1,186 @@
+import { Select } from 'antd';
+// ** Constants
+import { QUERY_BUILDER_OPERATORS_BY_TYPES } from 'constants/queryBuilder';
+import {
+	transformFromStringToHaving,
+	transformHavingToStringValue,
+} from 'lib/query/transformQueryBuilderData';
+// ** Helpers
+import { transformStringWithPrefix } from 'lib/query/transformStringWithPrefix';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import { Having } from 'types/api/queryBuilder/queryBuilderData';
+import { SelectOption } from 'types/common/select';
+
+// ** Types
+import { HavingFilterProps } from './HavingFilter.interfaces';
+
+const { Option } = Select;
+
+const initialFormValues: Having = {
+	columnName: '',
+	op: '',
+	value: '',
+};
+
+export function HavingFilter({
+	query,
+	onChange,
+}: HavingFilterProps): JSX.Element {
+	const { having } = query;
+	const [searchText, setSearchText] = useState<string>('');
+	const [options, setOptions] = useState<SelectOption<string, string>[]>([]);
+	const [localValues, setLocalValues] = useState<string[]>([]);
+	const [currentFormValue, setCurrentFormValue] = useState<Having>(
+		initialFormValues,
+	);
+
+	const numberOperators = QUERY_BUILDER_OPERATORS_BY_TYPES.number;
+
+	const aggregatorAttribute = useMemo(
+		() =>
+			transformStringWithPrefix({
+				str: query.aggregateAttribute.key,
+				prefix: query.aggregateAttribute.type || '',
+				condition: !query.aggregateAttribute.isColumn,
+			}),
+		[query],
+	);
+
+	const columnName = useMemo(
+		() => `${query.aggregateOperator.toUpperCase()}(${aggregatorAttribute})`,
+		[query, aggregatorAttribute],
+	);
+
+	const aggregatorOptions: SelectOption<string, string>[] = useMemo(
+		() => [{ label: columnName, value: columnName }],
+		[columnName],
+	);
+
+	const generateOptions = useCallback(
+		(search: string): void => {
+			const [aggregator = '', op = '', value = ''] = search.split(' ');
+			let newOptions: SelectOption<string, string>[] = [];
+
+			const isAggregatorExist = columnName
+				.toLowerCase()
+				.includes(search.toLowerCase());
+
+			const isAggregatorChosen = aggregator === columnName;
+
+			if (isAggregatorExist || aggregator === '') {
+				newOptions = aggregatorOptions;
+			}
+
+			if ((isAggregatorChosen && op === '') || op) {
+				const filteredOperators = numberOperators.filter((num) =>
+					num.toLowerCase().includes(op.toLowerCase()),
+				);
+
+				newOptions = filteredOperators.map((opt) => ({
+					label: `${columnName} ${opt} ${value}`,
+					value: `${columnName} ${opt} ${value}`,
+				}));
+			}
+
+			setOptions(newOptions);
+		},
+		[columnName, numberOperators, aggregatorOptions],
+	);
+
+	const isValidHavingValue = (search: string): boolean => {
+		const arr = search.split(' ');
+		if (arr.length === 3 && arr.at(-1)) {
+			const havingValue = arr[2];
+
+			const numRegexp = /^-?\d*\.?\d*$/;
+
+			return numRegexp.test(havingValue);
+		}
+
+		return true;
+	};
+
+	const handleSearch = (search: string): void => {
+		const currentSearch = search.split(' ').slice(0, 3).join(' ');
+		const isValidSearch = isValidHavingValue(search);
+
+		if (isValidSearch) {
+			setSearchText(currentSearch.trimStart());
+		}
+	};
+
+	const resetChanges = (): void => {
+		handleSearch('');
+		setCurrentFormValue(initialFormValues);
+		setOptions(aggregatorOptions);
+	};
+
+	const handleChange = (values: string[]): void => {
+		const having: Having[] = values.map(transformFromStringToHaving);
+
+		const isSelectable: boolean = Object.values(currentFormValue).every(
+			(value) => !!value,
+		);
+
+		if (isSelectable) {
+			onChange(having);
+			resetChanges();
+		}
+	};
+
+	const handleSelect = (currentValue: string): void => {
+		const arr = currentValue.split(' ');
+		const isCompleted = arr.every((item) => !!item);
+
+		const isClearSearch = isCompleted && arr.length === 3;
+
+		handleSearch(isClearSearch ? '' : currentValue);
+	};
+
+	const parseSearchText = useCallback(
+		(text: string) => {
+			const [columnName = '', op = '', value = ''] = text.split(' ');
+			setCurrentFormValue({ columnName, op, value });
+
+			generateOptions(text);
+		},
+		[generateOptions],
+	);
+
+	const handleDeselect = (value: string): void => {
+		const result = localValues.filter((item) => item !== value);
+		setLocalValues(result);
+	};
+
+	useEffect(() => {
+		parseSearchText(searchText);
+	}, [searchText, parseSearchText]);
+
+	useEffect(() => {
+		setLocalValues(transformHavingToStringValue(having));
+	}, [having]);
+
+	return (
+		<Select
+			mode="multiple"
+			onSearch={handleSearch}
+			searchValue={searchText}
+			value={localValues}
+			data-testid="havingSelect"
+			disabled={!query.aggregateAttribute.key}
+			style={{ width: '100%' }}
+			notFoundContent={!currentFormValue.value ? undefined : null}
+			placeholder="Count(operation) > 5"
+			onDeselect={handleDeselect}
+			onBlur={resetChanges}
+			onChange={handleChange}
+			onSelect={handleSelect}
+		>
+			{options.map((opt) => (
+				<Option key={opt.value} value={opt.value} title="havingOption">
+					{opt.label}
+				</Option>
+			))}
+		</Select>
+	);
+}
---
+++ frontend/src/container/QueryBuilder/filters/HavingFilter/__tests__/utils.test.tsx
@@ -0,0 +1,155 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+// Constants
+import { initialQueryBuilderFormValues } from 'constants/queryBuilder';
+import { QUERY_BUILDER_OPERATORS_BY_TYPES } from 'constants/queryBuilder';
+import { transformFromStringToHaving } from 'lib/query/transformQueryBuilderData';
+import React from 'react';
+// ** Types
+import { IBuilderQueryForm } from 'types/api/queryBuilder/queryBuilderData';
+import { DataSource } from 'types/common/queryBuilder';
+
+// ** Components
+import { HavingFilter } from '../HavingFilter';
+
+const valueWithAttributeAndOperator: IBuilderQueryForm = {
+	...initialQueryBuilderFormValues,
+	dataSource: DataSource.LOGS,
+	aggregateOperator: 'SUM',
+	aggregateAttribute: {
+		isColumn: false,
+		key: 'bytes',
+		type: 'tag',
+		dataType: 'float64',
+	},
+};
+
+describe('Having filter behaviour', () => {
+	test('Having filter render is rendered', () => {
+		const mockFn = jest.fn();
+		const { unmount } = render(
+			<HavingFilter query={initialQueryBuilderFormValues} onChange={mockFn} />,
+		);
+
+		const selectId = 'havingSelect';
+
+		const select = screen.getByTestId(selectId);
+
+		expect(select).toBeInTheDocument();
+
+		unmount();
+	});
+
+	test('Having render is disabled initially', () => {
+		const mockFn = jest.fn();
+		const { unmount } = render(
+			<HavingFilter query={initialQueryBuilderFormValues} onChange={mockFn} />,
+		);
+
+		const input = screen.getByRole('combobox');
+
+		expect(input).toBeDisabled();
+
+		unmount();
+	});
+
+	test('Is having filter is enable', () => {
+		const mockFn = jest.fn();
+		const { unmount } = render(
+			<HavingFilter query={valueWithAttributeAndOperator} onChange={mockFn} />,
+		);
+
+		const input = screen.getByRole('combobox');
+
+		expect(input).toBeEnabled();
+
+		unmount();
+	});
+
+	test('Autocomplete in the having filter', async () => {
+		const onChange = jest.fn();
+		const user = userEvent.setup();
+
+		const constructedAttribute = 'SUM(tag_bytes)';
+		const optionTestTitle = 'havingOption';
+
+		const { unmount } = render(
+			<HavingFilter query={valueWithAttributeAndOperator} onChange={onChange} />,
+		);
+
+		// get input
+		const input = screen.getByRole('combobox');
+
+		// click on the select
+		await user.click(input);
+
+		// show predefined options for operator with attribute SUM(tag_bytes)
+		const option = screen.getByTitle(optionTestTitle);
+
+		expect(option).toBeInTheDocument();
+
+		await user.click(option);
+
+		// autocomplete input
+		expect(input).toHaveValue(constructedAttribute);
+
+		// clear value from input and write from keyboard
+		await user.clear(input);
+
+		await user.type(input, 'bytes');
+
+		// show again predefined options for operator with attribute SUM(tag_bytes)
+		const sameAttributeOption = screen.getByTitle(optionTestTitle);
+
+		expect(sameAttributeOption).toBeInTheDocument();
+
+		await user.click(sameAttributeOption);
+
+		expect(input).toHaveValue(constructedAttribute);
+
+		// show operators after SUM(tag_bytes)
+		const operatorsOptions = screen.getAllByTitle(optionTestTitle);
+
+		expect(operatorsOptions.length).toEqual(
+			QUERY_BUILDER_OPERATORS_BY_TYPES.number.length,
+		);
+
+		// show operators after SUM(tag_bytes) when type from keyboard
+		await user.clear(input);
+
+		await user.type(input, `${constructedAttribute} !=`);
+
+		// get filtered operators
+		const filteredOperators = screen.getAllByTitle(optionTestTitle);
+
+		expect(filteredOperators.length).toEqual(1);
+
+		// clear and show again all operators
+		await user.clear(input);
+		await user.type(input, constructedAttribute);
+
+		const returnedOptions = screen.getAllByTitle(optionTestTitle);
+
+		expect(returnedOptions.length).toEqual(
+			QUERY_BUILDER_OPERATORS_BY_TYPES.number.length,
+		);
+
+		// check write value after operator
+		await user.clear(input);
+		await user.type(input, `${constructedAttribute} != 123`);
+
+		expect(input).toHaveValue(`${constructedAttribute} != 123`);
+
+		const optionWithValue = screen.getByTitle(optionTestTitle);
+
+		// onChange after complete writting in the input or autocomplete
+		await user.click(optionWithValue);
+
+		expect(onChange).toHaveBeenCalledTimes(1);
+		expect(onChange).toHaveBeenCalledWith([
+			transformFromStringToHaving(`${constructedAttribute} != 123`),
+		]);
+
+		unmount();
+	});
+});
