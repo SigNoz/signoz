a8253ade01926132ecd1a53829f32e126a42c6a1
makeavish <makeavish786@gmail.com>
2023-04-21 11:23:05 +0530

    chore: validateAndCastValueData

--- pkg/query-service/utils/format.go
+++ pkg/query-service/utils/format.go
@@ -3,10 +3,59 @@ package utils
 import (
 	"fmt"
 	"reflect"
+	"strconv"
 	"strings"
 
+	v3 "go.signoz.io/signoz/pkg/query-service/model/v3"
 	"go.uber.org/zap"
 )
+
+func ValidateAndCastValue(v interface{}, dataType v3.AttributeKeyDataType) (interface{}, error) {
+	switch dataType {
+	case v3.AttributeKeyDataTypeString:
+		switch x := v.(type) {
+		case string, int, float32, float64, bool:
+			return fmt.Sprintf("%v", x), nil
+		case []interface{}:
+			return v, nil
+		default:
+			return nil, fmt.Errorf("invalid data type")
+		}
+	case v3.AttributeKeyDataTypeBool:
+		switch x := v.(type) {
+		case bool:
+			return v, nil
+		case string:
+			return strconv.ParseBool(x)
+		default:
+			return nil, fmt.Errorf("invalid data type")
+		}
+	case v3.AttributeKeyDataTypeInt64:
+		switch x := v.(type) {
+		case int, int64:
+			return x, nil
+		case string:
+			return strconv.ParseInt(x, 10, 64)
+		default:
+			return nil, fmt.Errorf("invalid data type")
+		}
+	case v3.AttributeKeyDataTypeFloat64:
+		switch x := v.(type) {
+		case float32, float64:
+			return v, nil
+		case string:
+			return strconv.ParseFloat(x, 64)
+		case int:
+			return float64(x), nil
+		case int64:
+			return float64(x), nil
+		default:
+			return nil, fmt.Errorf("invalid data type")
+		}
+	default:
+		return nil, fmt.Errorf("invalid data type")
+	}
+}
 
 // ClickHouseFormattedValue formats the value to be used in clickhouse query
 func ClickHouseFormattedValue(v interface{}) string {
---
+++ pkg/query-service/utils/format_test.go
@@ -0,0 +1,212 @@
+package utils
+
+import (
+	"reflect"
+	"testing"
+
+	v3 "go.signoz.io/signoz/pkg/query-service/model/v3"
+)
+
+type args struct {
+	v        interface{}
+	dataType v3.AttributeKeyDataType
+}
+
+var testValidateAndCastValueData = []struct {
+	name    string
+	args    args
+	want    interface{}
+	wantErr bool
+}{
+	// Test cases for v3.AttributeKeyDataTypeString
+	{
+		name: "v3.AttributeKeyDataTypeString: Valid string",
+		args: args{
+			v:        "test",
+			dataType: v3.AttributeKeyDataTypeString,
+		},
+		want:    "test",
+		wantErr: false,
+	},
+	{
+		name: "v3.AttributeKeyDataTypeString: Valid int",
+		args: args{
+			v:        1,
+			dataType: v3.AttributeKeyDataTypeString,
+		},
+		want:    "1",
+		wantErr: false,
+	},
+	{
+		name: "v3.AttributeKeyDataTypeString: Valid float32",
+		args: args{
+			v:        float32(1.1),
+			dataType: v3.AttributeKeyDataTypeString,
+		},
+		want:    "1.1",
+		wantErr: false,
+	},
+	{
+		name: "v3.AttributeKeyDataTypeString: Valid float64",
+		args: args{
+			v:        float64(1.1),
+			dataType: v3.AttributeKeyDataTypeString,
+		},
+		want:    "1.1",
+		wantErr: false,
+	},
+	{
+		name: "v3.AttributeKeyDataTypeString: Valid bool",
+		args: args{
+			v:        true,
+			dataType: v3.AttributeKeyDataTypeString,
+		},
+		want:    "true",
+		wantErr: false,
+	},
+	{
+		name: "v3.AttributeKeyDataTypeString: Valid []interface{}",
+		args: args{
+			v:        []interface{}{"test"},
+			dataType: v3.AttributeKeyDataTypeString,
+		},
+		want:    []interface{}{"test"},
+		wantErr: false,
+	},
+	{
+		name: "v3.AttributeKeyDataTypeString: Invalid type",
+		args: args{
+			v:        map[string]interface{}{"test": "test"},
+			dataType: v3.AttributeKeyDataTypeString,
+		},
+		want:    nil,
+		wantErr: true,
+	},
+	// Test cases for v3.AttributeKeyDataTypeBool
+	{
+		name: "v3.AttributeKeyDataTypeBool: Valid bool",
+		args: args{
+			v:        true,
+			dataType: v3.AttributeKeyDataTypeBool,
+		},
+		want:    true,
+		wantErr: false,
+	},
+	{
+		name: "v3.AttributeKeyDataTypeBool: Valid string",
+		args: args{
+			v:        "true",
+			dataType: v3.AttributeKeyDataTypeBool,
+		},
+		want:    true,
+		wantErr: false,
+	},
+	{
+		name: "v3.AttributeKeyDataTypeBool: Invalid type",
+		args: args{
+			v:        1,
+			dataType: v3.AttributeKeyDataTypeBool,
+		},
+		want:    nil,
+		wantErr: true,
+	},
+	// Test cases for v3.AttributeKeyDataTypeInt64
+	{
+		name: "v3.AttributeKeyDataTypeInt64: Valid int",
+		args: args{
+			v:        1,
+			dataType: v3.AttributeKeyDataTypeInt64,
+		},
+		want:    1,
+		wantErr: false,
+	},
+	{
+		name: "v3.AttributeKeyDataTypeInt64: Valid int64",
+		args: args{
+			v:        int64(1),
+			dataType: v3.AttributeKeyDataTypeInt64,
+		},
+		want:    int64(1),
+		wantErr: false,
+	},
+	{
+		name: "v3.AttributeKeyDataTypeInt64: Valid string",
+		args: args{
+			v:        "1",
+			dataType: v3.AttributeKeyDataTypeInt64,
+		},
+		want:    int64(1),
+		wantErr: false,
+	},
+	{
+		name: "v3.AttributeKeyDataTypeInt64: Invalid type",
+		args: args{
+			v:        true,
+			dataType: v3.AttributeKeyDataTypeInt64,
+		},
+		want:    nil,
+		wantErr: true,
+	},
+	// Test cases for v3.AttributeKeyDataTypeFloat64
+	{
+		name: "v3.AttributeKeyDataTypeFloat64: Valid float32",
+		args: args{
+			v:        float32(1.1),
+			dataType: v3.AttributeKeyDataTypeFloat64,
+		},
+		want:    float32(1.1),
+		wantErr: false,
+	},
+	{
+		name: "v3.AttributeKeyDataTypeFloat64: Valid float64",
+		args: args{
+			v:        float64(1.1),
+			dataType: v3.AttributeKeyDataTypeFloat64,
+		},
+		want:    float64(1.1),
+		wantErr: false,
+	},
+	{
+		name: "v3.AttributeKeyDataTypeFloat64: Valid int",
+		args: args{
+			v:        1,
+			dataType: v3.AttributeKeyDataTypeFloat64,
+		},
+		want:    float64(1),
+		wantErr: false,
+	},
+	{
+		name: "v3.AttributeKeyDataTypeFloat64: Valid string",
+		args: args{
+			v:        "1.1",
+			dataType: v3.AttributeKeyDataTypeFloat64,
+		},
+		want:    float64(1.1),
+		wantErr: false,
+	},
+	{
+		name: "v3.AttributeKeyDataTypeFloat64: Invalid type",
+		args: args{
+			v:        true,
+			dataType: v3.AttributeKeyDataTypeFloat64,
+		},
+		want:    nil,
+		wantErr: true,
+	},
+}
+
+// Test cases for ValidateAndCastValue function in pkg/query-service/utils/format.go
+func TestValidateAndCastValue(t *testing.T) {
+	for _, tt := range testValidateAndCastValueData {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ValidateAndCastValue(tt.args.v, tt.args.dataType)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ValidateAndCastValue() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) && !reflect.DeepEqual(err, tt.wantErr) {
+				t.Errorf("ValidateAndCastValue() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
