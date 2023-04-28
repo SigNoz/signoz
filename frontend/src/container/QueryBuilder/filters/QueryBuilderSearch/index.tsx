8aec1562168d777ff536a8cec052d820c3e0cefc
Chintan Sudani <csudani7@gmail.com>
2023-04-28 09:03:51 +0530

    fix: chip & iscolumn issue

--- frontend/src/container/QueryBuilder/filters/QueryBuilderSearch/index.tsx
+++ frontend/src/container/QueryBuilder/filters/QueryBuilderSearch/index.tsx
@@ -92,10 +92,6 @@ function QueryBuilderSearch({
 		return tags;
 	}, [isMatricsDataSource, query.aggregateAttribute.key, tags]);
 
-	const isMatricsDataSource = useMemo(
-		() => query.dataSource === DataSource.METRICS,
-		[query.dataSource],
-	);
 
 	useEffect(() => {
 		const initialTagFilters: TagFilter = { items: [], op: 'AND' };
