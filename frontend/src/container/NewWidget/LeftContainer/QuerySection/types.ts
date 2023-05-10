a56c5533790619de6684b78f3fb8f74d32f54f68
Yevhen Shevchenko <y.shevchenko@seedium.io>
2023-05-10 17:10:05 +0300

    fix: date for graph

--- frontend/src/container/NewWidget/LeftContainer/QuerySection/types.ts
+++ frontend/src/container/NewWidget/LeftContainer/QuerySection/types.ts
@@ -1,11 +1,5 @@
 import { Query } from 'types/api/dashboard/getAll';
 
-export enum EQueryCategories {
-	query_builder = 0,
-	clickhouse_query,
-	promql,
-}
-
 export interface IHandleUpdatedQuery {
 	updatedQuery: Query;
 }
