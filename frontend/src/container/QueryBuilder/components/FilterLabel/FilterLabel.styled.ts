f491adb76d3a9a2599945b77dff885c5b1bbdd64
Palash Gupta <palashgdev@gmail.com>
2023-05-10 23:48:34 +0530

    fix: query builder filter label is adopted for dark mode

--- frontend/src/container/QueryBuilder/components/FilterLabel/FilterLabel.styled.ts
+++ frontend/src/container/QueryBuilder/components/FilterLabel/FilterLabel.styled.ts
@@ -1,6 +1,10 @@
+import { themeColors } from 'constants/theme';
 import styled from 'styled-components';
 
-export const StyledLabel = styled.div`
+interface Props {
+	isDarkMode: boolean;
+}
+export const StyledLabel = styled.div<Props>`
 	padding: 0 0.6875rem;
 	min-height: 2rem;
 	min-width: 5.625rem;
@@ -8,6 +12,8 @@ export const StyledLabel = styled.div`
 	white-space: nowrap;
 	align-items: center;
 	border-radius: 0.125rem;
-	border: 0.0625rem solid #434343;
-	background-color: #141414;
+	border: ${({ isDarkMode }): string =>
+		`1px solid ${
+			isDarkMode ? themeColors.borderDarkGrey : themeColors.borderLightGrey
+		}`};
 `;
