dc71f179d6c69e4d227297bf7b48856400725038
Yevhen Shevchenko <y.shevchenko@seedium.io>
2023-05-11 16:46:01 +0300

    refactor(query_builder): remove old codebase

--- frontend/src/types/api/alerts/compositeQuery.ts
+++ frontend/src/types/api/alerts/compositeQuery.ts
@@ -1,12 +1,6 @@
 import { GRAPH_TYPES } from 'container/NewDashboard/ComponentsSlider';
-import {
-	IClickHouseQuery,
-	IMetricsBuilderFormula,
-	IMetricsBuilderQuery,
-	IPromQLQuery,
-	IQueryBuilderTagFilters,
-} from 'types/api/dashboard/getAll';
-import { EAggregateOperator, EQueryType } from 'types/common/dashboard';
+import { IClickHouseQuery, IPromQLQuery } from 'types/api/dashboard/getAll';
+import { EQueryType } from 'types/common/dashboard';
 import { QueryDataResourse } from 'types/common/queryBuilderMappers.types';
 
 export interface ICompositeMetricQuery {
@@ -31,45 +25,4 @@ export interface IPromQuery extends IPromQLQuery {
 
 export interface IPromQueries {
 	[key: string]: IPromQuery;
-}
-export interface IBuilderQueries {
-	[key: string]: IBuilderQuery;
-}
-
-// IBuilderQuery combines IMetricQuery and IFormulaQuery
-// for api calls
-export interface IBuilderQuery
-	extends Omit<
-		IMetricQuery,
-		'aggregateOperator' | 'legend' | 'metricName' | 'tagFilters'
-	> {
-	aggregateOperator: EAggregateOperator | undefined;
-	disabled: boolean;
-	name: string;
-	legend?: string;
-	metricName: string | null;
-	groupBy?: string[];
-	expression?: string;
-	tagFilters?: IQueryBuilderTagFilters;
-	toggleDisable?: boolean;
-	toggleDelete?: boolean;
-}
-
-export interface IFormulaQueries {
-	[key: string]: IFormulaQuery;
-}
-
-export interface IFormulaQuery extends IMetricsBuilderFormula {
-	formulaOnly: boolean;
-	queryName: string;
-}
-
-export interface IMetricQueries {
-	[key: string]: IMetricQuery;
-}
-
-export interface IMetricQuery extends IMetricsBuilderQuery {
-	formulaOnly: boolean;
-	expression?: string;
-	queryName: string;
 }
