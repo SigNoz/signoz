5112b2abc049471b7aaeab006810ce1a7a3cafbb
Palash Gupta <palashgdev@gmail.com>
2023-05-15 15:53:26 +0530

    feat: add signoz cloud button in header

--- frontend/src/container/Header/index.tsx
+++ frontend/src/container/Header/index.tsx
@@ -3,8 +3,7 @@ import {
 	CaretUpFilled,
 	LogoutOutlined,
 } from '@ant-design/icons';
-import type { MenuProps } from 'antd';
-import { Divider, Dropdown, Space, Typography } from 'antd';
+import { Button, Divider, Dropdown, MenuProps, Space, Typography } from 'antd';
 import { Logout } from 'api/utils';
 import ROUTES from 'constants/routes';
 import Config from 'container/ConfigDropdown';
@@ -91,6 +90,13 @@ function HeaderContainer(): JSX.Element {
 		[onToggleHandler, onLogoutKeyDown],
 	);
 
+	const onClickSignozCloud = (): void => {
+		window.open(
+			'https://signoz.io/pricing/?utm_source=product_navbar&utm_medium=frontend',
+			'_blank',
+		);
+	};
+
 	return (
 		<Header>
 			<Container>
@@ -107,6 +113,10 @@ function HeaderContainer(): JSX.Element {
 				</NavLink>
 
 				<Space style={{ height: '100%' }} align="center">
+					<Button onClick={onClickSignozCloud} type="primary">
+						Try Signoz Cloud
+					</Button>
+
 					<Config frontendId="tooltip" />
 
 					<ToggleButton
