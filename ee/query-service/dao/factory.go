c4110084f897e354aa77d7f1869c541b849e0347
makeavish <makeavish786@gmail.com>
2023-05-17 14:13:50 +0530

    chore: cleanup obsolete code

--- ee/query-service/dao/factory.go
+++ ee/query-service/dao/factory.go
@@ -6,24 +6,13 @@ import (
 	"go.signoz.io/signoz/ee/query-service/dao/sqlite"
 )
 
-var db ModelDao
-
 func InitDao(engine, path string) (ModelDao, error) {
 
 	switch engine {
 	case "sqlite":
-		db, err:= sqlite.InitDB(path)
-		return db, err
+		return sqlite.InitDB(path)
 	default:
 		return nil, fmt.Errorf("qsdb type: %s is not supported in query service", engine)
 	}
 
-}
-
-func DB() ModelDao {
-	if db == nil {
-		// Should never reach here
-		panic("GetDB called before initialization")
-	}
-	return db
 }
