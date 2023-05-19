4ca2e6a2b0e4ee2d1d8745edd568f57190fec66b
Palash Gupta <palashgdev@gmail.com>
2023-05-19 17:55:38 +0530

    fix: file is updated with typescript

---
+++ frontend/i18-generate-hash.ts
@@ -0,0 +1,22 @@
+import crypto, { BinaryToTextEncoding } from 'crypto';
+import fs from 'fs';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import glob from 'glob';
+
+function generateChecksum(
+	str: string,
+	algorithm = 'md5',
+	encoding: BinaryToTextEncoding = 'hex',
+): string {
+	return crypto.createHash(algorithm).update(str, 'utf8').digest(encoding);
+}
+
+const result: { [key: string]: string } = {};
+
+glob.sync(`public/locales/**/*.json`).forEach((path: string) => {
+	const [, lang] = path.split('public/locales');
+	const content = fs.readFileSync(path, { encoding: 'utf-8' });
+	result[lang.replace('.json', '')] = generateChecksum(content);
+});
+
+fs.writeFileSync('./i18n-translations-hash.json', JSON.stringify(result));
