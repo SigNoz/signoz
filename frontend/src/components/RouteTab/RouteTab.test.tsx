41e93cc678303a93c9959054672896775c517934
Palash Gupta <palashgdev@gmail.com>
2023-05-24 15:12:38 +0530

    chore: tsc is fixed

--- frontend/src/components/RouteTab/RouteTab.test.tsx
+++ frontend/src/components/RouteTab/RouteTab.test.tsx
@@ -1,6 +1,5 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import { createMemoryHistory } from 'history';
-import React from 'react';
 import { Router } from 'react-router-dom';
 
 import RouteTab from './index';
