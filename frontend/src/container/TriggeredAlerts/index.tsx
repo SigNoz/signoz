def12dc8aa12eb6d79a0cd76b92218e7710bc7de
Palash <palashgdev@gmail.com>
2023-06-03 18:23:32 +0530

    feat: alerts is migrated to useQuery

--- frontend/src/container/TriggeredAlerts/index.tsx
+++ frontend/src/container/TriggeredAlerts/index.tsx
@@ -1,91 +1,47 @@
 import getTriggeredApi from 'api/alerts/getTriggered';
 import Spinner from 'components/Spinner';
-import { State } from 'hooks/useFetch';
 import { useNotifications } from 'hooks/useNotifications';
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
-import { PayloadProps } from 'types/api/alerts/getTriggered';
+import { useQuery } from 'react-query';
+import { useSelector } from 'react-redux';
+import { AppState } from 'store/reducers';
+import AppReducer from 'types/reducer/app';
 
 import TriggerComponent from './TriggeredAlert';
 
 function TriggeredAlerts(): JSX.Element {
-	const [groupState, setGroupState] = useState<State<PayloadProps>>({
-		error: false,
-		errorMessage: '',
-		loading: true,
-		success: false,
-		payload: [],
-	});
 	const { t } = useTranslation(['common']);
 	const { notifications } = useNotifications();
+	const { user } = useSelector<AppState, AppReducer>((state) => state.app);
 
-	const fetchData = useCallback(async () => {
-		try {
-			setGroupState((state) => ({
-				...state,
-				loading: true,
-			}));
-
-			const response = await getTriggeredApi({
+	const alertsResponse = useQuery(['triggeredAlerts', user?.userId], {
+		queryFn: () =>
+			getTriggeredApi({
 				active: true,
 				inhibited: true,
 				silenced: false,
-			});
-
-			if (response.statusCode === 200) {
-				setGroupState((state) => ({
-					...state,
-					loading: false,
-					payload: response.payload || [],
-				}));
-			} else {
-				setGroupState((state) => ({
-					...state,
-					loading: false,
-					error: true,
-					errorMessage: response.error || 'Something went wrong',
-				}));
-			}
-		} catch (error) {
-			setGroupState((state) => ({
-				...state,
-				error: true,
-				loading: false,
-				errorMessage: 'Something went wrong',
-			}));
-		}
-	}, []);
+			}),
+		refetchInterval: 30000,
+	});
 
 	useEffect(() => {
-		fetchData();
-	}, [fetchData]);
-
-	useEffect(() => {
-		if (groupState.error) {
+		if (alertsResponse?.data?.error) {
 			notifications.error({
-				message: groupState.errorMessage || t('something_went_wrong'),
+				message: alertsResponse.data?.error || t('something_went_wrong'),
 			});
 		}
-	}, [groupState.error, groupState.errorMessage, t, notifications]);
+	}, [t, notifications, alertsResponse.data?.error]);
 
-	if (groupState.error) {
+	if (alertsResponse.error) {
 		return <TriggerComponent allAlerts={[]} />;
 	}
 
-	if (groupState.loading || groupState.payload === undefined) {
+	if (alertsResponse.isFetching || alertsResponse?.data?.payload === undefined) {
 		return <Spinner height="75vh" tip="Loading Alerts..." />;
 	}
 
-	// commented the reduce() call as we no longer use /alerts/groups
-	// API from alert manager, which returns a group for each receiver
-
-	// const initialAlerts: Alerts[] = [];
-
-	// const allAlerts: Alerts[] = groupState.payload.reduce((acc, curr) => {
-	//	return [...acc, ...curr.alerts];
-	// }, initialAlerts);
-
-	return <TriggerComponent allAlerts={groupState.payload} />;
+	return <TriggerComponent allAlerts={alertsResponse?.data?.payload || []} />;
 }
 
 export default TriggeredAlerts;
