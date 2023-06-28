608136fdda19c5448df0a79386b1874cb8e95699
Rajat-Dabade <rajat@signoz.io>
2023-06-28 18:11:10 +0530

    refactor: reverted the unwanted changes in getDashboardVariable

--- frontend/src/lib/dashbaordVariables/getDashboardVariables.ts
+++ frontend/src/lib/dashbaordVariables/getDashboardVariables.ts
@@ -8,27 +8,24 @@ export const getDashboardVariables = (): Record<string, unknown> => {
 			dashboards: { dashboards },
 		} = store.getState();
 
-		if (dashboards.length > 0) {
-			const [selectedDashboard] = dashboards;
-			const {
-				data: { variables = {} },
-			} = selectedDashboard;
+		const [selectedDashboard] = dashboards;
+		const {
+			data: { variables = {} },
+		} = selectedDashboard;
 
-			const { start, end } = getStartEndRangeTime({
-				type: 'GLOBAL_TIME',
-				interval: globalTime.selectedTime,
-			});
+		const { start, end } = getStartEndRangeTime({
+			type: 'GLOBAL_TIME',
+			interval: globalTime.selectedTime,
+		});
 
-			const variablesTuple: Record<string, unknown> = {
-				SIGNOZ_START_TIME: parseInt(start, 10) * 1e3,
-				SIGNOZ_END_TIME: parseInt(end, 10) * 1e3,
-			};
-			Object.keys(variables).forEach((key) => {
-				variablesTuple[key] = variables[key].selectedValue;
-			});
-			return variablesTuple;
-		}
-		return {};
+		const variablesTuple: Record<string, unknown> = {
+			SIGNOZ_START_TIME: parseInt(start, 10) * 1e3,
+			SIGNOZ_END_TIME: parseInt(end, 10) * 1e3,
+		};
+		Object.keys(variables).forEach((key) => {
+			variablesTuple[key] = variables[key].selectedValue;
+		});
+		return variablesTuple;
 	} catch (e) {
 		console.error(e);
 	}
