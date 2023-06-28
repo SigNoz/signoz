03cbf6e588fd8cd4035334d8fff5fefbe0d7dc57
Palash Gupta <palashgdev@gmail.com>
2023-06-28 14:01:12 +0530

    chore: user input is overriden from global step

--- frontend/src/hooks/queryBuilder/useStepInterval.ts
+++ frontend/src/hooks/queryBuilder/useStepInterval.ts
@@ -22,10 +22,7 @@ export const updateStepInterval = (
 			queryData:
 				query?.builder?.queryData?.map((item) => ({
 					...item,
-					stepInterval:
-						item.stepInterval <= stepInterval
-							? stepInterval
-							: item.stepInterval || stepInterval,
+					stepInterval,
 				})) || [],
 		},
 	};
