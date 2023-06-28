1eabacbaf487c4b07eb9ffdd1364c7779debee2f
dnazarenkoo <134951516+dnazarenkoo@users.noreply.github.com>
2023-06-28 15:55:20 +0300

    feat: add the export panel to the traces explorer (#2983)
    * feat: add the export panel to the traces explorer
    
    * feat: onExport dashboard widget is updated
    
    * chore: made common hook useDashboard
    
    ---------
    
    Co-authored-by: Palash Gupta <palashgdev@gmail.com>

--- frontend/src/pages/TracesExplorer/index.tsx
+++ frontend/src/pages/TracesExplorer/index.tsx
@@ -1,27 +1,94 @@
 import { Tabs } from 'antd';
+import axios from 'axios';
+import { QueryParams } from 'constants/query';
 import { initialQueriesMap, PANEL_TYPES } from 'constants/queryBuilder';
-import { PANEL_TYPES_QUERY } from 'constants/queryBuilderQueryNames';
+import {
+	COMPOSITE_QUERY,
+	PANEL_TYPES_QUERY,
+} from 'constants/queryBuilderQueryNames';
+import ROUTES from 'constants/routes';
+import ExportPanel from 'container/ExportPanel';
 import { GRAPH_TYPES } from 'container/NewDashboard/ComponentsSlider';
 import QuerySection from 'container/TracesExplorer/QuerySection';
+import { useUpdateDashboard } from 'hooks/dashboard/useUpdateDashboard';
+import { addEmptyWidgetInDashboardJSONWithQuery } from 'hooks/dashboard/utils';
 import { useQueryBuilder } from 'hooks/queryBuilder/useQueryBuilder';
 import { useShareBuilderUrl } from 'hooks/queryBuilder/useShareBuilderUrl';
+import { useNotifications } from 'hooks/useNotifications';
+import history from 'lib/history';
 import { useCallback, useMemo } from 'react';
+import { generatePath } from 'react-router-dom';
+import { Dashboard } from 'types/api/dashboard/getAll';
 import { DataSource } from 'types/common/queryBuilder';
 
-import { Container } from './styles';
+import { ActionsWrapper, Container } from './styles';
 import { getTabsItems } from './utils';
 
 function TracesExplorer(): JSX.Element {
+	const { notifications } = useNotifications();
 	const {
+		currentQuery,
+		stagedQuery,
+		panelType,
 		updateAllQueriesOperators,
 		redirectWithQueryBuilderData,
-		currentQuery,
-		panelType,
 	} = useQueryBuilder();
 
 	const tabsItems = getTabsItems();
-
 	const currentTab = panelType || PANEL_TYPES.TIME_SERIES;
+
+	const defaultQuery = useMemo(
+		() =>
+			updateAllQueriesOperators(
+				initialQueriesMap.traces,
+				PANEL_TYPES.TIME_SERIES,
+				DataSource.TRACES,
+			),
+		[updateAllQueriesOperators],
+	);
+
+	const exportDefaultQuery = useMemo(
+		() =>
+			updateAllQueriesOperators(
+				stagedQuery || initialQueriesMap.traces,
+				PANEL_TYPES.TIME_SERIES,
+				DataSource.TRACES,
+			),
+		[stagedQuery, updateAllQueriesOperators],
+	);
+
+	const { mutate: updateDashboard, isLoading } = useUpdateDashboard();
+
+	const handleExport = useCallback(
+		(dashboard: Dashboard | null): void => {
+			if (!dashboard) return;
+
+			const updatedDashboard = addEmptyWidgetInDashboardJSONWithQuery(
+				dashboard,
+				exportDefaultQuery,
+			);
+
+			updateDashboard(updatedDashboard, {
+				onSuccess: (data) => {
+					const dashboardEditView = `${generatePath(ROUTES.DASHBOARD, {
+						dashboardId: data?.payload?.uuid,
+					})}/new?${QueryParams.graphType}=graph&${
+						QueryParams.widgetId
+					}=empty&${COMPOSITE_QUERY}=${JSON.stringify(exportDefaultQuery)}`;
+
+					history.push(dashboardEditView);
+				},
+				onError: (error) => {
+					if (axios.isAxiosError(error)) {
+						notifications.error({
+							message: error.message,
+						});
+					}
+				},
+			});
+		},
+		[exportDefaultQuery, notifications, updateDashboard],
+	);
 
 	const handleTabChange = useCallback(
 		(newPanelType: string): void => {
@@ -43,23 +110,17 @@ function TracesExplorer(): JSX.Element {
 		],
 	);
 
-	const defaultValue = useMemo(
-		() =>
-			updateAllQueriesOperators(
-				initialQueriesMap.traces,
-				PANEL_TYPES.TIME_SERIES,
-				DataSource.TRACES,
-			),
-		[updateAllQueriesOperators],
-	);
-
-	useShareBuilderUrl(defaultValue);
+	useShareBuilderUrl(defaultQuery);
 
 	return (
 		<>
 			<QuerySection />
 
 			<Container>
+				<ActionsWrapper>
+					<ExportPanel isLoading={isLoading} onExport={handleExport} />
+				</ActionsWrapper>
+
 				<Tabs
 					defaultActiveKey={currentTab}
 					activeKey={currentTab}
