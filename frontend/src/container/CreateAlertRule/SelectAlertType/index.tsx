cda37b99b435489a0217aa58ebc954b9f1765f35
Palash Gupta <palashgdev@gmail.com>
2023-06-30 11:18:12 +0530

    feat: create alerts is updated from trace explorer (#2995)

--- frontend/src/container/CreateAlertRule/SelectAlertType/index.tsx
+++ frontend/src/container/CreateAlertRule/SelectAlertType/index.tsx
@@ -1,61 +1,40 @@
 import { Row } from 'antd';
+import { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { AlertTypes } from 'types/api/alerts/alertTypes';
 
+import { getOptionList } from './config';
 import { AlertTypeCard, SelectTypeContainer } from './styles';
-
-interface OptionType {
-	title: string;
-	selection: AlertTypes;
-	description: string;
-}
+import { OptionType } from './types';
 
 function SelectAlertType({ onSelect }: SelectAlertTypeProps): JSX.Element {
 	const { t } = useTranslation(['alerts']);
 
-	const renderOptions = (): JSX.Element => {
-		const optionList: OptionType[] = [
-			{
-				title: t('metric_based_alert'),
-				selection: AlertTypes.METRICS_BASED_ALERT,
-				description: t('metric_based_alert_desc'),
-			},
-			{
-				title: t('log_based_alert'),
-				selection: AlertTypes.LOGS_BASED_ALERT,
-				description: t('log_based_alert_desc'),
-			},
-			{
-				title: t('traces_based_alert'),
-				selection: AlertTypes.TRACES_BASED_ALERT,
-				description: t('traces_based_alert_desc'),
-			},
-			{
-				title: t('exceptions_based_alert'),
-				selection: AlertTypes.EXCEPTIONS_BASED_ALERT,
-				description: t('exceptions_based_alert_desc'),
-			},
-		];
-		return (
+	const optionList = getOptionList(t);
+
+	const renderOptions = useMemo(
+		() => (
 			<>
-				{optionList.map((o: OptionType) => (
+				{optionList.map((option: OptionType) => (
 					<AlertTypeCard
-						key={o.selection}
-						title={o.title}
+						key={option.selection}
+						title={option.title}
 						onClick={(): void => {
-							onSelect(o.selection);
+							onSelect(option.selection);
 						}}
 					>
-						{o.description}
+						{option.description}
 					</AlertTypeCard>
 				))}
 			</>
-		);
-	};
+		),
+		[onSelect, optionList],
+	);
+
 	return (
 		<SelectTypeContainer>
 			<h3> {t('choose_alert_type')} </h3>
-			<Row>{renderOptions()}</Row>
+			<Row>{renderOptions}</Row>
 		</SelectTypeContainer>
 	);
 }
