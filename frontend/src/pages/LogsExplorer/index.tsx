5042c56b4cf18598b3a92463495f85779ec70673
Yevhen Shevchenko <90138953+yeshev@users.noreply.github.com>
2023-07-07 15:49:35 +0300

    fix: disable step interval for all views (#3070)
    * fix: disable step interval for all views
    
    * fix: disable interval for dashboard
    
    ---------
    
    Co-authored-by: Vishal Sharma <makeavish786@gmail.com>

--- frontend/src/pages/LogsExplorer/index.tsx
+++ frontend/src/pages/LogsExplorer/index.tsx
@@ -1,59 +1,16 @@
-import { Button, Col, Row } from 'antd';
-import { initialQueriesMap, PANEL_TYPES } from 'constants/queryBuilder';
+import { Col, Row } from 'antd';
+import LogExplorerQuerySection from 'container/LogExplorerQuerySection';
 import LogsExplorerViews from 'container/LogsExplorerViews';
-import { QueryBuilder } from 'container/QueryBuilder';
-import { QueryBuilderProps } from 'container/QueryBuilder/QueryBuilder.interfaces';
-import { useGetPanelTypesQueryParam } from 'hooks/queryBuilder/useGetPanelTypesQueryParam';
-import { useQueryBuilder } from 'hooks/queryBuilder/useQueryBuilder';
-import { useShareBuilderUrl } from 'hooks/queryBuilder/useShareBuilderUrl';
-import { useMemo } from 'react';
-import { DataSource } from 'types/common/queryBuilder';
 
 // ** Styles
-import { ButtonWrapperStyled, WrapperStyled } from './styles';
-import { prepareQueryWithDefaultTimestamp } from './utils';
+import { WrapperStyled } from './styles';
 
 function LogsExplorer(): JSX.Element {
-	const { handleRunQuery, updateAllQueriesOperators } = useQueryBuilder();
-	const panelTypes = useGetPanelTypesQueryParam(PANEL_TYPES.LIST);
-
-	const defaultValue = useMemo(() => {
-		const updatedQuery = updateAllQueriesOperators(
-			initialQueriesMap.logs,
-			PANEL_TYPES.LIST,
-			DataSource.LOGS,
-		);
-		return prepareQueryWithDefaultTimestamp(updatedQuery);
-	}, [updateAllQueriesOperators]);
-
-	useShareBuilderUrl(defaultValue);
-
-	const inactiveLogsFilters: QueryBuilderProps['inactiveFilters'] = useMemo(() => {
-		if (panelTypes === PANEL_TYPES.TABLE) {
-			const result: QueryBuilderProps['inactiveFilters'] = { stepInterval: true };
-
-			return result;
-		}
-
-		return {};
-	}, [panelTypes]);
-
 	return (
 		<WrapperStyled>
 			<Row gutter={[0, 28]}>
 				<Col xs={24}>
-					<QueryBuilder
-						panelType={panelTypes}
-						config={{ initialDataSource: DataSource.LOGS, queryVariant: 'static' }}
-						inactiveFilters={inactiveLogsFilters}
-						actions={
-							<ButtonWrapperStyled>
-								<Button type="primary" onClick={handleRunQuery}>
-									Run Query
-								</Button>
-							</ButtonWrapperStyled>
-						}
-					/>
+					<LogExplorerQuerySection />
 				</Col>
 				<Col xs={24}>
 					<LogsExplorerViews />
