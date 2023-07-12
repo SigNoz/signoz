d6a5ef4a16592fee6eb89c33501dcc61b8612bd1
Rajat-Dabade <rajat@signoz.io>
2023-07-12 11:16:16 +0530

    refactor: removed the dependency dashboard action

--- frontend/src/container/GridGraphLayout/Graph/GraphWithoutDashboard.tsx
+++ frontend/src/container/GridGraphLayout/Graph/GraphWithoutDashboard.tsx
@@ -5,7 +5,6 @@ import Spinner from 'components/Spinner';
 import GridGraphComponent from 'container/GridGraphComponent';
 import { useGetQueryRange } from 'hooks/queryBuilder/useGetQueryRange';
 import { useStepInterval } from 'hooks/queryBuilder/useStepInterval';
-import { useNotifications } from 'hooks/useNotifications';
 import usePreviousValue from 'hooks/usePreviousValue';
 import { getDashboardVariables } from 'lib/dashbaordVariables/getDashboardVariables';
 import getChartData from 'lib/getChartData';
@@ -18,23 +17,12 @@ import {
 	useMemo,
 	useState,
 } from 'react';
-import { Layout } from 'react-grid-layout';
-import { useTranslation } from 'react-i18next';
 import { useInView } from 'react-intersection-observer';
-import { connect, useSelector } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { ThunkDispatch } from 'redux-thunk';
-import {
-	DeleteWidget,
-	DeleteWidgetProps,
-} from 'store/actions/dashboard/deleteWidget';
+import { useSelector } from 'react-redux';
 import { AppState } from 'store/reducers';
-import AppActions from 'types/actions';
 import { Widgets } from 'types/api/dashboard/getAll';
-import AppReducer from 'types/reducer/app';
 import { GlobalReducer } from 'types/reducer/globalTime';
 
-import { LayoutProps } from '..';
 import EmptyWidget from '../EmptyWidget';
 import WidgetHeader from '../WidgetHeader';
 import FullView from './FullView/index.metricsBuilder';
@@ -42,11 +30,8 @@ import { FullViewContainer, Modal } from './styles';
 
 function GraphWithoutDashboard({
 	widget,
-	deleteWidget,
 	name,
 	yAxisUnit,
-	layout = [],
-	setLayout,
 	onDragSelect,
 	onClickHandler,
 }: GridCardGraphProps): JSX.Element {
@@ -55,10 +40,6 @@ function GraphWithoutDashboard({
 		triggerOnce: true,
 		initialInView: false,
 	});
-
-	const { notifications } = useNotifications();
-
-	const { t } = useTranslation(['common']);
 
 	const [errorMessage, setErrorMessage] = useState<string | undefined>('');
 	const [hovered, setHovered] = useState(false);
@@ -69,9 +50,6 @@ function GraphWithoutDashboard({
 		AppState,
 		GlobalReducer
 	>((state) => state.globalTime);
-	const { featureResponse } = useSelector<AppState, AppReducer>(
-		(state) => state.app,
-	);
 
 	const updatedQuery = useStepInterval(widget?.query);
 
@@ -127,32 +105,6 @@ function GraphWithoutDashboard({
 		[],
 	);
 
-	const onDeleteHandler = useCallback(() => {
-		const widgetId = isEmptyWidget ? layout[0].i : widget?.id;
-
-		featureResponse
-			.refetch()
-			.then(() => {
-				deleteWidget({ widgetId, setLayout });
-				onToggleModal(setDeleteModal);
-			})
-			.catch(() => {
-				notifications.error({
-					message: t('common:something_went_wrong'),
-				});
-			});
-	}, [
-		isEmptyWidget,
-		widget?.id,
-		layout,
-		featureResponse,
-		deleteWidget,
-		setLayout,
-		onToggleModal,
-		notifications,
-		t,
-	]);
-
 	const getModals = (): JSX.Element => (
 		<>
 			<Modal
@@ -161,7 +113,6 @@ function GraphWithoutDashboard({
 				open={deleteModal}
 				title="Delete"
 				height="10vh"
-				onOk={onDeleteHandler}
 				centered
 			>
 				<Typography>Are you sure you want to delete this widget</Typography>
@@ -319,20 +270,10 @@ function GraphWithoutDashboard({
 	);
 }
 
-interface DispatchProps {
-	deleteWidget: ({
-		widgetId,
-	}: DeleteWidgetProps) => (dispatch: Dispatch<AppActions>) => void;
-}
-
-interface GridCardGraphProps extends DispatchProps {
+interface GridCardGraphProps {
 	widget: Widgets;
 	name: string;
 	yAxisUnit: string | undefined;
-	// eslint-disable-next-line react/require-default-props
-	layout?: Layout[];
-	// eslint-disable-next-line react/require-default-props
-	setLayout?: Dispatch<SetStateAction<LayoutProps[]>>;
 	onDragSelect?: (start: number, end: number) => void;
 	onClickHandler?: GraphOnClickHandler;
 }
@@ -342,10 +283,4 @@ GraphWithoutDashboard.defaultProps = {
 	onClickHandler: undefined,
 };
 
-const mapDispatchToProps = (
-	dispatch: ThunkDispatch<unknown, unknown, AppActions>,
-): DispatchProps => ({
-	deleteWidget: bindActionCreators(DeleteWidget, dispatch),
-});
-
-export default connect(null, mapDispatchToProps)(memo(GraphWithoutDashboard));
+export default memo(GraphWithoutDashboard);
