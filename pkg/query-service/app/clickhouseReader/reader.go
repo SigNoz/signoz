db4ce422afe84d3e217c8fe9bdf36efe74df87f8
nityanandagohain <nityanandagohain@gmail.com>
2023-07-26 17:10:49 +0530

    fix: ignore autocomplete request for body

--- pkg/query-service/app/clickhouseReader/reader.go
+++ pkg/query-service/app/clickhouseReader/reader.go
@@ -4030,11 +4030,16 @@ func (r *ClickHouseReader) GetLogAttributeValues(ctx context.Context, req *v3.Fi
 	var attributeValues v3.FilterAttributeValueResponse
 
 	// if dataType or tagType is not present return empty response
-	if len(req.FilterAttributeKeyDataType) == 0 || len(req.TagType) == 0 || req.FilterAttributeKey == "body" {
+	if len(req.FilterAttributeKeyDataType) == 0 || len(req.TagType) == 0 {
 		// also check if it is not a top level key
 		if _, ok := constants.StaticFieldsLogsV3[req.FilterAttributeKey]; !ok {
 			return &v3.FilterAttributeValueResponse{}, nil
 		}
+	}
+
+	// ignore autocomplete request for body
+	if req.FilterAttributeKey == "body" {
+		return &v3.FilterAttributeValueResponse{}, nil
 	}
 
 	// if data type is bool, return true and false
