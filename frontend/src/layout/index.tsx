b973c48bcd90dad577d9c21ef9d6207bd3fe77ed
Mustajab Ikram <mustajabdelhi@gmail.com>
2023-08-01 00:47:22 +0530

    fix: add translation support for page titles

--- frontend/src/layout/index.tsx
+++ frontend/src/layout/index.tsx
@@ -1,27 +1,26 @@
 import ROUTES from 'constants/routes';
-import routeTitleMap from 'constants/routeTitleMap';
 import { Helmet } from 'react-helmet-async';
 import { useTranslation } from 'react-i18next';
 import { useLocation } from 'react-router-dom';
 
 function Head(): JSX.Element {
 	const location = useLocation();
-	const { t } = useTranslation();
+	const { t } = useTranslation('titles');
 
-	function getRouteKey(): string {
+	function getRouteKey(): keyof typeof ROUTES | 'DEFAULT' {
 		const [routeKey] = Object.entries(ROUTES).find(
 			([, value]) => value === location.pathname,
 		) || ['DEFAULT'];
 
-		return routeKey;
+		return routeKey as keyof typeof ROUTES | 'DEFAULT';
 	}
 
 	const routeKey = getRouteKey();
-	const pageTitle = routeTitleMap[routeKey];
+	const pageTitle = t(routeKey);
 
 	return (
 		<Helmet>
-			<title>{t(pageTitle)}</title>
+			<title>{pageTitle}</title>
 		</Helmet>
 	);
 }
