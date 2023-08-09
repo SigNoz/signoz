d58d53c733029656a05373d9f2ec7b5548f34ed0
Yevhen Shevchenko <y.shevchenko@seedium.io>
2023-08-09 17:14:25 +0300

    feat: add event source provider with hook

---
+++ frontend/src/providers/EventSource.tsx
@@ -0,0 +1,123 @@
+import { apiV3 } from 'api/apiV1';
+import getLocalStorageKey from 'api/browser/localstorage/get';
+import { ENVIRONMENT } from 'constants/env';
+import { LOCALSTORAGE } from 'constants/localStorage';
+import { EventListener, EventSourcePolyfill } from 'event-source-polyfill';
+import {
+	createContext,
+	PropsWithChildren,
+	useCallback,
+	useContext,
+	useMemo,
+	useRef,
+	useState,
+} from 'react';
+
+interface IEventSourceContext {
+	eventSourceInstance: EventSourcePolyfill | null;
+	isConnectionOpen: boolean;
+	isConnectionLoading: boolean;
+	isConnectionError: string;
+	handleStartOpenConnection: (url?: string) => void;
+	handleCloseConnection: () => void;
+}
+
+const EventSourceContext = createContext<IEventSourceContext>({
+	eventSourceInstance: null,
+	isConnectionOpen: false,
+	isConnectionLoading: false,
+	isConnectionError: '',
+	handleStartOpenConnection: () => {},
+	handleCloseConnection: () => {},
+});
+
+export function EventSourceProvider({
+	children,
+}: PropsWithChildren): JSX.Element {
+	const [isConnectionOpen, setIsConnectionOpen] = useState<boolean>(false);
+	const [isConnectionLoading, setIsConnectionLoading] = useState<boolean>(false);
+	const [isConnectionError, setIsConnectionError] = useState<string>('');
+
+	const eventSourceRef = useRef<EventSourcePolyfill | null>(null);
+
+	const handleCloseConnection = useCallback(() => {
+		if (!eventSourceRef.current) return;
+
+		eventSourceRef.current.close();
+		setIsConnectionOpen(false);
+		setIsConnectionLoading(false);
+	}, []);
+
+	const handleOpenConnection: EventListener = useCallback(() => {
+		setIsConnectionLoading(false);
+		setIsConnectionOpen(true);
+	}, []);
+
+	const handleErrorConnection: EventListener = useCallback(() => {
+		if (!eventSourceRef.current) return;
+
+		handleCloseConnection();
+
+		eventSourceRef.current.removeEventListener('error', handleErrorConnection);
+		eventSourceRef.current.removeEventListener('open', handleOpenConnection);
+	}, [handleCloseConnection, handleOpenConnection]);
+
+	const handleStartOpenConnection = useCallback(
+		(url?: string) => {
+			const eventSourceUrl = url || `${ENVIRONMENT.baseURL}${apiV3}logs/livetail`;
+
+			const TIMEOUT_IN_MS = 10 * 60 * 1000;
+
+			eventSourceRef.current = new EventSourcePolyfill(eventSourceUrl, {
+				headers: {
+					Authorization: `Bearer ${getLocalStorageKey(LOCALSTORAGE.AUTH_TOKEN)}`,
+				},
+				heartbeatTimeout: TIMEOUT_IN_MS,
+			});
+
+			setIsConnectionLoading(true);
+			setIsConnectionError('');
+
+			eventSourceRef.current.addEventListener('error', handleErrorConnection);
+
+			eventSourceRef.current.addEventListener('open', handleOpenConnection);
+		},
+		[handleErrorConnection, handleOpenConnection],
+	);
+
+	const contextValue = useMemo(
+		() => ({
+			eventSourceInstance: eventSourceRef.current,
+			isConnectionError,
+			isConnectionLoading,
+			isConnectionOpen,
+			handleStartOpenConnection,
+			handleCloseConnection,
+		}),
+		[
+			isConnectionError,
+			isConnectionLoading,
+			isConnectionOpen,
+			handleStartOpenConnection,
+			handleCloseConnection,
+		],
+	);
+
+	console.log({ contextValue });
+
+	return (
+		<EventSourceContext.Provider value={contextValue}>
+			{children}
+		</EventSourceContext.Provider>
+	);
+}
+
+export const useEventSource = (): IEventSourceContext => {
+	const context = useContext(EventSourceContext);
+
+	if (!context) {
+		throw new Error('Should be used inside the context');
+	}
+
+	return context;
+};
