30dececa2aa09e0f2a16070f302cf953ba4eb615
Rajat-Dabade <rajat@signoz.io>
2023-08-16 20:15:51 +0530

    fix: the precision data points

--- frontend/src/components/Graph/yAxisConfig.ts
+++ frontend/src/components/Graph/yAxisConfig.ts
@@ -5,14 +5,9 @@ export const getYAxisFormattedValue = (
 	format: string,
 ): string => {
 	let decimalPrecision: number | undefined;
-	const parsedValue = getValueFormat(format)(
-		parseFloat(value),
-		undefined,
-		undefined,
-		undefined,
-	);
+
 	try {
-		const decimalSplitted = parsedValue.text.split('.');
+		const decimalSplitted = value.split('.');
 		if (decimalSplitted.length === 1) {
 			decimalPrecision = 0;
 		} else {
@@ -48,8 +43,33 @@ export const getYAxisFormattedValue = (
 
 export const getToolTipValue = (value: string, format: string): string => {
 	try {
+		let decimalPrecision: number | undefined;
+		const decimalSplitted = value.split('.');
+		if (decimalSplitted.length === 1) {
+			decimalPrecision = 0;
+		} else {
+			const decimalDigits = decimalSplitted[1].split('');
+			decimalPrecision = decimalDigits.length;
+			let nonZeroCtr = 0;
+			for (let idx = 0; idx < decimalDigits.length; idx += 1) {
+				if (decimalDigits[idx] !== '0') {
+					nonZeroCtr += 1;
+					if (nonZeroCtr >= 2) {
+						decimalPrecision = idx + 1;
+					}
+				} else if (nonZeroCtr) {
+					decimalPrecision = idx;
+					break;
+				}
+			}
+		}
 		return formattedValueToString(
-			getValueFormat(format)(parseFloat(value), undefined, undefined, undefined),
+			getValueFormat(format)(
+				parseFloat(value),
+				decimalPrecision,
+				undefined,
+				undefined,
+			),
 		);
 	} catch (error) {
 		console.error(error);
