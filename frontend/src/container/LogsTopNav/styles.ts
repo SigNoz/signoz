3fa89a5492b1031198945121ccb2e16d23dc75a9
Yevhen Shevchenko <y.shevchenko@seedium.io>
2023-08-16 11:57:41 +0300

    fix: success button color

--- frontend/src/container/LogsTopNav/styles.ts
+++ frontend/src/container/LogsTopNav/styles.ts
@@ -1,19 +1,19 @@
+import { green } from '@ant-design/colors';
 import { Button, ButtonProps } from 'antd';
-import { themeColors } from 'constants/theme';
 import styled, { css, FlattenSimpleInterpolation } from 'styled-components';
 
 export const LiveButtonStyled = styled(Button)<ButtonProps>`
-	background-color: rgba(${themeColors.buttonSuccessRgb}, 0.9);
+	background-color: ${green[6]};
 
 	${({ danger }): FlattenSimpleInterpolation =>
 		!danger
 			? css`
 					&:hover {
-						background-color: rgba(${themeColors.buttonSuccessRgb}, 1) !important;
+						background-color: ${green[5]} !important;
 					}
 
 					&:active {
-						background-color: rgba(${themeColors.buttonSuccessRgb}, 0.7) !important;
+						background-color: ${green[7]} !important;
 					}
 			  `
 			: css``}
