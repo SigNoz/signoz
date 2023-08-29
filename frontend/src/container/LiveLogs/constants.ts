eb95d0496591f5340511ca4dd7b8d638f55a6a64
Yevhen Shevchenko <y.shevchenko@seedium.io>
2023-08-29 10:21:36 +0300

    feat: use prefetched query in live

--- frontend/src/container/LiveLogs/constants.ts
+++ frontend/src/container/LiveLogs/constants.ts
@@ -4,24 +4,43 @@ import {
 } from 'constants/queryBuilder';
 import { FILTERS } from 'container/QueryBuilder/filters/OrderByFilter/config';
 import { BaseAutocompleteData } from 'types/api/queryBuilder/queryAutocompleteResponse';
-import { Query } from 'types/api/queryBuilder/queryBuilderData';
+import { IBuilderQuery, Query } from 'types/api/queryBuilder/queryBuilderData';
 import { LogsAggregatorOperator } from 'types/common/queryBuilder';
 
-export const liveLogsCompositeQuery: Query = {
-	...initialQueriesMap.logs,
-	builder: {
-		...initialQueriesMap.logs.builder,
-		queryData: [
-			{
-				...initialQueryBuilderFormValuesMap.logs,
-				aggregateOperator: LogsAggregatorOperator.NOOP,
-				disabled: true,
-				pageSize: 10,
-				orderBy: [{ columnName: 'timestamp', order: FILTERS.DESC }],
-			},
-		],
-	},
+export const defaultLiveQueryDataConfig: Partial<IBuilderQuery> = {
+	aggregateOperator: LogsAggregatorOperator.NOOP,
+	disabled: true,
+	pageSize: 10,
+	orderBy: [{ columnName: 'timestamp', order: FILTERS.DESC }],
 };
+
+type GetDefaultCompositeQueryParams = {
+	query: Query;
+	initialQueryData: IBuilderQuery;
+	customQueryData?: Partial<IBuilderQuery>;
+};
+
+export const constructCompositeQuery = ({
+	query,
+	initialQueryData,
+	customQueryData,
+}: GetDefaultCompositeQueryParams): Query => ({
+	...query,
+	builder: {
+		...query.builder,
+		queryData: query.builder.queryData.map((item) => ({
+			...initialQueryData,
+			...item,
+			...customQueryData,
+		})),
+	},
+});
+
+export const liveLogsCompositeQuery = constructCompositeQuery({
+	query: initialQueriesMap.logs,
+	initialQueryData: initialQueryBuilderFormValuesMap.logs,
+	customQueryData: defaultLiveQueryDataConfig,
+});
 
 export const idObject: BaseAutocompleteData = {
 	key: 'id',
