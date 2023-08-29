361ba1381fa53f1558f898187206c73f9de3f5ca
Yevhen Shevchenko <90138953+yeshev@users.noreply.github.com>
2023-08-29 14:54:42 +0300

    feat: get live data (#3337)
    * feat: get live data
    
    * fix: change color, change number format
    
    * chore: useMemo is updated
    
    * feat: add live logs list (#3341)
    
    * feat: add live logs list
    
    * feat: hide view if error, clear logs
    
    * feat: add condition for disable initial loading
    
    * fix: double request
    
    * fix: render id in the where clause
    
    * fix: render where clause and live list
    
    * fix: last log padding
    
    * fix: list data loading
    
    * fix: no logs text
    
    * fix: logs list size
    
    * fix: small issues
    
    * fix: render view with memo
    
    ---------
    
    Co-authored-by: Palash Gupta <palashgdev@gmail.com>
    
    ---------
    
    Co-authored-by: Palash Gupta <palashgdev@gmail.com>

--- frontend/src/container/LiveLogs/constants.ts
+++ frontend/src/container/LiveLogs/constants.ts
@@ -3,6 +3,7 @@ import {
 	initialQueryBuilderFormValuesMap,
 } from 'constants/queryBuilder';
 import { FILTERS } from 'container/QueryBuilder/filters/OrderByFilter/config';
+import { BaseAutocompleteData } from 'types/api/queryBuilder/queryAutocompleteResponse';
 import { Query } from 'types/api/queryBuilder/queryBuilderData';
 import { LogsAggregatorOperator } from 'types/common/queryBuilder';
 
@@ -20,4 +21,11 @@ export const liveLogsCompositeQuery: Query = {
 			},
 		],
 	},
+};
+
+export const idObject: BaseAutocompleteData = {
+	key: 'id',
+	type: '',
+	dataType: 'string',
+	isColumn: true,
 };
--- frontend/src/container/LiveLogsTopNav/index.tsx
+++ frontend/src/container/LiveLogsTopNav/index.tsx
@@ -1,30 +1,64 @@
 import { PauseCircleFilled, PlayCircleFilled } from '@ant-design/icons';
+import { Space } from 'antd';
+import BackButton from 'container/LiveLogs/BackButton';
+import { getQueryWithoutFilterId } from 'container/LiveLogs/utils';
 import LocalTopNav from 'container/LocalTopNav';
+import { useQueryBuilder } from 'hooks/queryBuilder/useQueryBuilder';
 import { useEventSource } from 'providers/EventSource';
-import { useMemo } from 'react';
+import { memo, useCallback, useMemo } from 'react';
 
 import { LiveButtonStyled } from './styles';
 
 function LiveLogsTopNav(): JSX.Element {
-	const { isConnectionOpen, isConnectionLoading } = useEventSource();
-
-	const isPaused = useMemo(() => isConnectionOpen || isConnectionLoading, [
+	const {
 		isConnectionOpen,
 		isConnectionLoading,
+		initialLoading,
+		handleCloseConnection,
+		handleSetInitialLoading,
+	} = useEventSource();
+
+
+	const { redirectWithQueryBuilderData, currentQuery } = useQueryBuilder();
+
+	const isPlaying = isConnectionOpen || isConnectionLoading || initialLoading;
+
+	const onLiveButtonClick = useCallback(() => {
+		if (initialLoading) {
+			handleSetInitialLoading(false);
+		}
+
+		if ((!isConnectionOpen && isConnectionLoading) || isConnectionOpen) {
+			handleCloseConnection();
+		} else {
+			const preparedQuery = getQueryWithoutFilterId(currentQuery);
+			redirectWithQueryBuilderData(preparedQuery);
+		}
+	}, [
+		initialLoading,
+		isConnectionOpen,
+		isConnectionLoading,
+		currentQuery,
+		handleSetInitialLoading,
+		handleCloseConnection,
+		redirectWithQueryBuilderData,
 	]);
 
 	const liveButton = useMemo(
 		() => (
-			<LiveButtonStyled
-				loading={isConnectionLoading}
-				icon={isPaused ? <PauseCircleFilled /> : <PlayCircleFilled />}
-				danger={isPaused}
-				type="primary"
-			>
-				{isPaused ? 'Pause' : 'Resume'}
-			</LiveButtonStyled>
+			<Space size={16}>
+				<LiveButtonStyled
+					icon={isPlaying ? <PauseCircleFilled /> : <PlayCircleFilled />}
+					danger={isPlaying}
+					onClick={onLiveButtonClick}
+					type="primary"
+				>
+					{isPlaying ? 'Pause' : 'Resume'}
+				</LiveButtonStyled>
+				<BackButton />
+			</Space>
 		),
-		[isPaused, isConnectionLoading],
+		[isPlaying, onLiveButtonClick],
 	);
 
 	return (
@@ -35,4 +69,4 @@ function LiveLogsTopNav(): JSX.Element {
 	);
 }
 
-export default LiveLogsTopNav;
+export default memo(LiveLogsTopNav);
