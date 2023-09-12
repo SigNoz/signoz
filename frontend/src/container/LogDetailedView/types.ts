5fe92254c6886c0fb88e038ec4f0e1f52ab352b6
Palash Gupta <palashgdev@gmail.com>
2023-09-12 14:55:31 +0530

    chore: getData types is updated

---
+++ frontend/src/container/LogDetailedView/types.ts
@@ -0,0 +1,8 @@
+export enum DataTypes {
+	Int64 = 'int64',
+	String = 'string',
+	Float64 = 'float64',
+	ArrayFloat64 = 'Array(float64)',
+	ArrayInt64 = 'Array(int64)',
+	ArrayString = 'Array(string)',
+}
