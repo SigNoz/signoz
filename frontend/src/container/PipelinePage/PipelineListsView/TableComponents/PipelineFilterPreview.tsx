e47ece9e065d7f21c815b1fac1a6e8260859535d
Raj <rkssisodiya@gmail.com>
2023-09-19 20:09:23 +0530

    chore: use QueryBuilderSearch input for specifying log pipeline filter

--- frontend/src/container/PipelinePage/PipelineListsView/TableComponents/PipelineFilterPreview.tsx
+++ frontend/src/container/PipelinePage/PipelineListsView/TableComponents/PipelineFilterPreview.tsx
@@ -5,6 +5,8 @@ import { PipelineData } from 'types/api/pipeline/def';
 function PipelineFilterPreview({
 	filter,
 }: PipelineFilterPreviewProps): JSX.Element {
+	// TODO(Raj): Add container styling, overflow wrapping
+	// should maintain some space between rows.
 	return (
 		<div>
 			{queryFilterTags(filter).map((tag) => (
