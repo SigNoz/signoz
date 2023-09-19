513ed4d658fee1647dc574593a848227dd7420c8
Raj <rkssisodiya@gmail.com>
2023-09-19 21:34:35 +0530

    chore: styles for pipeline filter preview in pipeline list view

--- frontend/src/container/PipelinePage/PipelineListsView/TableComponents/PipelineFilterPreview.tsx
+++ frontend/src/container/PipelinePage/PipelineListsView/TableComponents/PipelineFilterPreview.tsx
@@ -1,18 +1,28 @@
-import { Tag } from 'antd';
 import { queryFilterTags } from 'hooks/queryBuilder/useTag';
+import styled from 'styled-components';
 import { PipelineData } from 'types/api/pipeline/def';
+
+const FilterCondition = styled.div`
+	display: inline;
+	padding: 0 0.25em;
+`;
+
+const FilterContainer = styled.div`
+	display: flex;
+	flex-wrap: wrap;
+	gap: 0.2rem;
+	font-size: 0.75rem;
+`;
 
 function PipelineFilterPreview({
 	filter,
 }: PipelineFilterPreviewProps): JSX.Element {
-	// TODO(Raj): Add container styling, overflow wrapping
-	// should maintain some space between rows.
 	return (
-		<div>
+		<FilterContainer>
 			{queryFilterTags(filter).map((tag) => (
-				<Tag key={tag}>{tag}</Tag>
+				<FilterCondition key={tag}>{tag}</FilterCondition>
 			))}
-		</div>
+		</FilterContainer>
 	);
 }
 
