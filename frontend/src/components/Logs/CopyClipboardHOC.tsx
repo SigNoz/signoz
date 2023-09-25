7d16a9c5a9ecb41f55cb838f69406202c3888a0a
Yash Joshi <jyash97@gmail.com>
2023-09-26 00:26:07 +0530

    chore: refactor copy clipboard hoc

--- frontend/src/components/Logs/CopyClipboardHOC.tsx
+++ frontend/src/components/Logs/CopyClipboardHOC.tsx
@@ -1,28 +1,27 @@
 import { Popover } from 'antd';
+import copy from 'copy-to-clipboard';
 import { useNotifications } from 'hooks/useNotifications';
-import { ReactNode, useCallback, useEffect } from 'react';
-import { useCopyToClipboard } from 'react-use';
+import { ReactNode, useCallback } from 'react';
 
 function CopyClipboardHOC({
 	textToCopy,
 	children,
 }: CopyClipboardHOCProps): JSX.Element {
-	const [value, setCopy] = useCopyToClipboard();
 	const { notifications } = useNotifications();
-	useEffect(() => {
-		if (value.value) {
+
+	const handleCopy = useCallback(() => {
+		try {
+			copy(textToCopy);
 			notifications.success({
 				message: 'Copied to clipboard',
 			});
+		} catch (e) {
+			//
 		}
-	}, [value, notifications]);
-
-	const onClick = useCallback((): void => {
-		setCopy(textToCopy);
-	}, [setCopy, textToCopy]);
+	}, [textToCopy, notifications]);
 
 	return (
-		<span onClick={onClick} role="presentation" tabIndex={-1}>
+		<span onClick={handleCopy} role="presentation" tabIndex={-1}>
 			<Popover
 				placement="top"
 				content={<span style={{ fontSize: '0.9rem' }}>Copy to clipboard</span>}
