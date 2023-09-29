c37985926213c4190c69f85c3af8537d452ec864
Rajat-Dabade <rajat@signoz.io>
2023-09-29 13:39:36 +0530

    chore: updated type names

--- frontend/src/components/Logs/CategoryHeading/CategoryHeading.tsx
+++ frontend/src/components/Logs/CategoryHeading/CategoryHeading.tsx
@@ -2,9 +2,9 @@ import './CategoryHeading.styles.scss';
 
 import { Typography } from 'antd';
 
-import { ICategoryHeadingProps } from './CategoryHeading.types';
+import { CategoryHeadingProps } from './CategoryHeading.types';
 
-function CategoryHeading({ children }: ICategoryHeadingProps): JSX.Element {
+function CategoryHeading({ children }: CategoryHeadingProps): JSX.Element {
 	return (
 		<Typography.Text type="secondary" className="container--text-size">
 			{children}
--- frontend/src/components/Logs/CategoryHeading/CategoryHeading.types.ts
+++ frontend/src/components/Logs/CategoryHeading/CategoryHeading.types.ts
@@ -1,5 +1,5 @@
 import { ReactNode } from 'react';
 
-export type ICategoryHeadingProps = {
+export type CategoryHeadingProps = {
 	children: ReactNode;
 };
