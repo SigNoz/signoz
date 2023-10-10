edfd1fdacc62ffce276993d0381a643ee78ebd4b
Raj <rkssisodiya@gmail.com>
2023-10-10 14:46:48 +0530

    chore: extract SampleLogsResponseDisplay for reuse

---
+++ frontend/src/container/PipelinePage/PipelineListsView/Preview/components/SampleLogs/index.tsx
@@ -0,0 +1,18 @@
+import './styles.scss';
+
+import useSampleLogs, { SampleLogsRequest } from '../../hooks/useSampleLogs';
+import SampleLogsResponseDisplay from './SampleLogsResponseDisplay';
+
+function SampleLogs(props: SampleLogsRequest): JSX.Element {
+	const sampleLogsResponse = useSampleLogs(props);
+
+	if ((props?.filter?.items?.length || 0) < 1) {
+		return (
+			<div className="sample-logs-notice-container">Please select a filter</div>
+		);
+	}
+
+	return <SampleLogsResponseDisplay response={sampleLogsResponse} />;
+}
+
+export default SampleLogs;
