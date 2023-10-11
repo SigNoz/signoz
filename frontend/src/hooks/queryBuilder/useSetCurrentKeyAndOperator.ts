f3c00e1a575716404f1ce186bfbf4dfe3e251509
Nityananda Gohain <nityanandagohain@gmail.com>
2023-10-11 17:35:53 +0530

    Revert "feat: lowercase operators support in the where clause is updated (#3657)" (#3697)
    This reverts commit 0e04b779a94740e741fc0e49800c3633b76e8030.

--- frontend/src/hooks/queryBuilder/useSetCurrentKeyAndOperator.ts
+++ frontend/src/hooks/queryBuilder/useSetCurrentKeyAndOperator.ts
@@ -1,24 +1,32 @@
-import { getTagToken } from 'container/QueryBuilder/filters/QueryBuilderSearch/utils';
-import { useMemo, useRef } from 'react';
+import {
+	getRemovePrefixFromKey,
+	getTagToken,
+} from 'container/QueryBuilder/filters/QueryBuilderSearch/utils';
+import { useMemo } from 'react';
+import { BaseAutocompleteData } from 'types/api/queryBuilder/queryAutocompleteResponse';
 
 type ICurrentKeyAndOperator = [string, string, string[]];
 
 export const useSetCurrentKeyAndOperator = (
 	value: string,
+	keys: BaseAutocompleteData[],
 ): ICurrentKeyAndOperator => {
-	const keyRef = useRef<string>('');
-	const operatorRef = useRef<string>('');
-
-	const result = useMemo(() => {
+	const [key, operator, result] = useMemo(() => {
+		let key = '';
+		let operator = '';
 		let result: string[] = [];
 		const { tagKey, tagOperator, tagValue } = getTagToken(value);
+		const isSuggestKey = keys?.some(
+			(el) => el?.key === getRemovePrefixFromKey(tagKey),
+		);
+		if (isSuggestKey || keys.length === 0) {
+			key = tagKey || '';
+			operator = tagOperator || '';
+			result = tagValue || [];
+		}
 
-		keyRef.current = tagKey || '';
-		operatorRef.current = tagOperator || '';
-		result = tagValue || [];
+		return [key, operator, result];
+	}, [value, keys]);
 
-		return result;
-	}, [value]);
-
-	return [keyRef.current, operatorRef.current, result];
+	return [key, operator, result];
 };
