f8feaa73f568e646a1e4869a19c0e417585a3542
Yunus A M <myounis.ar@live.com>
2023-10-26 18:13:52 +0530

    fix: update workspace-locked test cases

--- frontend/src/pages/WorkspaceLocked/WorkspaceLocked.test.tsx
+++ frontend/src/pages/WorkspaceLocked/WorkspaceLocked.test.tsx
@@ -1,46 +1,70 @@
+import { licensesSuccessWorkspaceLockedResponse } from 'mocks-server/__mockdata__/licenses';
+import { server } from 'mocks-server/server';
+import { rest } from 'msw';
 import { act, render, screen } from 'tests/test-utils';
 
 import WorkspaceLocked from '.';
 
 describe('WorkspaceLocked', () => {
+	const apiURL = 'http://localhost/api/v2/licenses';
+
 	test('Should render the component', async () => {
+		server.use(
+			rest.get(apiURL, (req, res, ctx) =>
+				res(ctx.status(200), ctx.json(licensesSuccessWorkspaceLockedResponse)),
+			),
+		);
+
 		act(() => {
 			render(<WorkspaceLocked />);
 		});
-		const workspaceLocked = screen.getByRole('heading', {
+
+		const workspaceLocked = await screen.findByRole('heading', {
 			name: /workspace locked/i,
 		});
 		expect(workspaceLocked).toBeInTheDocument();
 
-		const gotQuestionText = screen.getByText(/got question?/i);
+		const gotQuestionText = await screen.findByText(/got question?/i);
 		expect(gotQuestionText).toBeInTheDocument();
 
-		const contactUsLink = screen.getByRole('link', {
+		const contactUsLink = await screen.findByRole('link', {
 			name: /contact us/i,
 		});
 		expect(contactUsLink).toBeInTheDocument();
 	});
 
 	test('Render for Admin', async () => {
+		server.use(
+			rest.get(apiURL, (req, res, ctx) =>
+				res(ctx.status(200), ctx.json(licensesSuccessWorkspaceLockedResponse)),
+			),
+		);
+
 		render(<WorkspaceLocked />);
-		const contactAdminMessage = screen.queryByText(
+		const contactAdminMessage = await screen.queryByText(
 			/please contact your administrator for further help/i,
 		);
 		expect(contactAdminMessage).not.toBeInTheDocument();
-		const updateCreditCardBtn = screen.getByRole('button', {
+		const updateCreditCardBtn = await screen.findByRole('button', {
 			name: /update credit card/i,
 		});
 		expect(updateCreditCardBtn).toBeInTheDocument();
 	});
 
 	test('Render for non Admin', async () => {
+		server.use(
+			rest.get(apiURL, (req, res, ctx) =>
+				res(ctx.status(200), ctx.json(licensesSuccessWorkspaceLockedResponse)),
+			),
+		);
+
 		render(<WorkspaceLocked />, {}, 'VIEWER');
-		const updateCreditCardBtn = screen.queryByRole('button', {
+		const updateCreditCardBtn = await screen.queryByRole('button', {
 			name: /update credit card/i,
 		});
 		expect(updateCreditCardBtn).not.toBeInTheDocument();
 
-		const contactAdminMessage = screen.getByText(
+		const contactAdminMessage = await screen.findByText(
 			/please contact your administrator for further help/i,
 		);
 		expect(contactAdminMessage).toBeInTheDocument();
