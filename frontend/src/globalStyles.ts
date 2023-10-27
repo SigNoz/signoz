b1e2cc9cfea02c7ed2588f7e192b72dcfa74cb8c
Yunus A M <myounis.ar@live.com>
2023-10-27 18:44:56 +0530

    feat: create tooltip plugin for uplot charts

--- frontend/src/globalStyles.ts
+++ frontend/src/globalStyles.ts
@@ -14,9 +14,31 @@ body {
   box-sizing: border-box;
 }
 
- .u-legend {
-    max-height: 30px; // slicing the height of the widget Header height ;
-    overflow-y: auto;
+.u-legend {
+  max-height: 30px; // slicing the height of the widget Header height ;
+  overflow-y: auto;
+}
+
+#overlay {
+  font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande',
+    'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+  font-size: 12px;
+  position: absolute;
+  background: rgba(0, 0, 0, 0.9);
+  padding: 0.5rem;
+  margin: 0.5rem;
+  color: #fff;
+  z-index: 100;
+  pointer-events: none;
+  overflow: auto;
+  max-height: 300px;
+  border-radius: 5px;
+}
+
+.tooltip-content-row {
+  display: flex;
+  align-items: center;
+  gap: 4px;
 }
 `;
 
