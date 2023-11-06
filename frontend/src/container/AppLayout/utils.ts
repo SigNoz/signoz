e9eb649808274507fb9cc839079c005327ffa962
Rajat-Dabade <rajat@signoz.io>
2023-11-06 18:31:28 +0530

    refactor: done with title refactor

--- frontend/src/container/AppLayout/utils.ts
+++ frontend/src/container/AppLayout/utils.ts
@@ -1,9 +1,31 @@
-import ROUTES from 'constants/routes';
+const DEFAULT_TITLE = 'Open source Observability platform | SigNoz';
+const SIGNOZ = 'SigNoz';
+
+const SigleRouteMap: { [key: string]: string } = {
+	Services: 'services',
+};
+
+const convertFirstLetterToCapital = (inputString: string): string =>
+	inputString.charAt(0).toUpperCase() + inputString.slice(1);
+
+function getWordAfterFirstSlash(inputString: string): string | undefined {
+	const segments = inputString.split('/');
+	if (segments.length === 2) {
+		return segments[1];
+	}
+	if (segments.length >= 2) {
+		if (!SigleRouteMap[segments[1]]) {
+			return segments[2];
+		}
+		return segments[1];
+	}
+	return undefined;
+}
 
 export function getRouteKey(pathname: string): string {
-	const [routeKey] = Object.entries(ROUTES).find(
-		([, value]) => value === pathname,
-	) || ['DEFAULT'];
-
-	return routeKey;
+	const title = getWordAfterFirstSlash(pathname);
+	if (title === undefined) {
+		return DEFAULT_TITLE;
+	}
+	return `${convertFirstLetterToCapital(title)} | ${SIGNOZ}`;
 }
