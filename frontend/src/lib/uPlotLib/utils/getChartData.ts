4083970289d8d05ecae12a084b61e4c3b5968489
Yunus M <myounis.ar@live.com>
2023-11-20 10:56:33 +0530

    feat: update logic to generate x series for line series charts (#3993)
    Co-authored-by: Palash Gupta <palashgdev@gmail.com>

--- frontend/src/lib/uPlotLib/utils/getChartData.ts
+++ frontend/src/lib/uPlotLib/utils/getChartData.ts
@@ -7,13 +7,24 @@ export const getUPlotChartData = (
 	const seriesList = apiResponse?.data?.result || [];
 	const uPlotData: uPlot.AlignedData = [];
 
+	// this helps us identify the series with the max number of values and helps define the x axis - timestamps
+	const xSeries = seriesList.reduce(
+		(maxObj, currentObj) =>
+			currentObj.values.length > maxObj.values.length ? currentObj : maxObj,
+		seriesList[0],
+	);
+
 	// sort seriesList
 	for (let index = 0; index < seriesList.length; index += 1) {
 		seriesList[index]?.values?.sort((a, b) => a[0] - b[0]);
 	}
 
+	const timestampArr = xSeries?.values?.map((v) => v[0]);
+
+	const uplotDataFormatArr = new Float64Array(timestampArr);
+
 	// timestamp
-	uPlotData.push(new Float64Array(seriesList[0]?.values?.map((v) => v[0])));
+	uPlotData.push(uplotDataFormatArr);
 
 	const numberOfTimestamps = uPlotData[0].length;
 
