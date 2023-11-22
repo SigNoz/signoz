e9dde3bedbf07ce3e32e954c2fc64c6f927413b2
Vikrant Gupta <vikrant.thomso@gmail.com>
2023-11-23 02:25:50 +0530

    feat: added navigation checks

--- e2e/tests/navigation.spec.ts
+++ e2e/tests/navigation.spec.ts
@@ -1,8 +1,20 @@
-import { test } from "@playwright/test";
+import { test, expect } from "@playwright/test";
 import ROUTES from "../../frontend/src/constants/routes";
+import { SERVICE_TABLE_HEADERS } from "./contants";
 
 test("Basic Navigation Check across different resources", async ({ page }) => {
-  await page.goto(ROUTES.APPLICATION);
+  await Promise.all([
+    page.goto(ROUTES.APPLICATION),
+    page.waitForRequest("**/v1/services"),
+  ]);
+
+  const p99Latency = page.locator(
+    `th:has-text("${SERVICE_TABLE_HEADERS.P99LATENCY}")`
+  );
+
+  await expect(p99Latency).toBeVisible();
+
+  await page.goto(ROUTES.TRACES_EXPLORER);
 
   await page.pause();
 });
