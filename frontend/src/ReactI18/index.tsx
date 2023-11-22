4ad8a1f3ad46a1bf38955536c89f2a1ad41c02ab
Rajat-Dabade <rajat@signoz.io>
2023-11-22 12:07:03 +0530

    refactor: shifted i18n to original location

--- frontend/src/ReactI18/index.tsx
+++ frontend/src/ReactI18/index.tsx
@@ -1,8 +1,9 @@
-import cacheBursting from 'i18n-translations-hash.json';
 import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
+
+import cacheBursting from '../../i18n-translations-hash.json';
 
 i18n
 	// load translation using http -> see /public/locales
