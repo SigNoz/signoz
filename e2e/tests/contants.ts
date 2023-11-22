ea3b471112f8de4d1efb8863ac67ed3360bdb88a
Vikrant Gupta <vikrant.thomso@gmail.com>
2023-11-23 02:38:59 +0530

    feat: added navigation checks

--- e2e/tests/contants.ts
+++ e2e/tests/contants.ts
@@ -4,3 +4,7 @@ export const SERVICE_TABLE_HEADERS = {
   ERROR_RATE: "Error Rate (% of total)",
   OPS_PER_SECOND: "Operations Per Second",
 };
+
+export const DATA_TEST_IDS = {
+  NEW_DASHBOARD_BTN: "create-new-dashboard",
+};
