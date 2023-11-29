3ac4a524bd9f9e92b03690db7189bfffea42ced3
Raj <rkssisodiya@gmail.com>
2023-11-29 19:45:19 +0530

    chore: some more cleanup

--- frontend/src/container/PipelinePage/PipelineListsView/AddNewProcessor/ProcessorForm.tsx
+++ frontend/src/container/PipelinePage/PipelineListsView/AddNewProcessor/ProcessorForm.tsx
@@ -1,24 +1,60 @@
-import { Form } from 'antd';
+import { Form, Input, Select } from 'antd';
 import { useWatch } from 'antd/es/form/Form';
+import { ModalFooterTitle } from 'container/PipelinePage/styles';
+import { useTranslation } from 'react-i18next';
 
+import { formValidationRules } from '../config';
 import { processorFields, ProcessorFormField } from './config';
-import NameInput from './FormFields/NameInput';
-import SelectInput from './FormFields/SelectInput';
+import {
+	Container,
+	FormWrapper,
+	PipelineIndexIcon,
+	StyledSelect,
+} from './styles';
 
 function ProcessorFieldInput({
 	fieldData,
 }: ProcessorFieldInputProps): JSX.Element | null {
+	const { t } = useTranslation('pipeline');
 	const form = Form.useFormInstance();
-	useWatch([], form);
 
+	// Watch form values so we can evaluate shouldRender on
+	// conditional fields when form values are updated.
+	useWatch(fieldData?.dependencies || [], form);
 	if (fieldData.shouldRender && !fieldData.shouldRender(form)) {
 		return null;
 	}
 
-	if (fieldData?.options) {
-		return <SelectInput key={fieldData.id} fieldData={fieldData} />;
-	}
-	return <NameInput key={fieldData.id} fieldData={fieldData} />;
+	return (
+		<Container>
+			<PipelineIndexIcon size="small">
+				{Number(fieldData.id) + 1}
+			</PipelineIndexIcon>
+			<FormWrapper>
+				<Form.Item
+					required={false}
+					label={<ModalFooterTitle>{fieldData.fieldName}</ModalFooterTitle>}
+					key={fieldData.id}
+					name={fieldData.name}
+					initialValue={fieldData.initialValue}
+					rules={fieldData.rules ? fieldData.rules : formValidationRules}
+					dependencies={fieldData.dependencies || []}
+				>
+					{fieldData?.options ? (
+						<StyledSelect>
+							{fieldData.options.map(({ value, label }) => (
+								<Select.Option key={value + label} value={value}>
+									{label}
+								</Select.Option>
+							))}
+						</StyledSelect>
+					) : (
+						<Input placeholder={t(fieldData.placeholder)} />
+					)}
+				</Form.Item>
+			</FormWrapper>
+		</Container>
+	);
 }
 
 interface ProcessorFieldInputProps {
