22f644671d8ee358e37111b4b31772ff39efa2e6
Raj <rkssisodiya@gmail.com>
2023-11-29 10:10:13 +0530

    chore: some cleanup

--- pkg/query-service/app/logparsingpipeline/time_parser_test.go
+++ pkg/query-service/app/logparsingpipeline/time_parser_test.go
@@ -1,11 +1,17 @@
 package logparsingpipeline
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+	"strings"
 	"testing"
+	"time"
 
 	"github.com/antonmedv/expr"
 	"github.com/stretchr/testify/require"
+	"go.signoz.io/signoz/pkg/query-service/model"
+	v3 "go.signoz.io/signoz/pkg/query-service/model/v3"
 )
 
 func TestRegexForStrptimeLayout(t *testing.T) {
@@ -56,4 +62,77 @@ func TestRegexForStrptimeLayout(t *testing.T) {
 		require.Equal(output, test.shouldMatch, test.strptimeLayout)
 
 	}
+}
+
+func TestTimestampParsingProcessor(t *testing.T) {
+	require := require.New(t)
+
+	testPipelines := []Pipeline{
+		{
+			OrderId: 1,
+			Name:    "pipeline1",
+			Alias:   "pipeline1",
+			Enabled: true,
+			Filter: &v3.FilterSet{
+				Operator: "AND",
+				Items: []v3.FilterItem{
+					{
+						Key: v3.AttributeKey{
+							Key:      "method",
+							DataType: v3.AttributeKeyDataTypeString,
+							Type:     v3.AttributeKeyTypeTag,
+						},
+						Operator: "=",
+						Value:    "GET",
+					},
+				},
+			},
+			Config: []PipelineOperator{},
+		},
+	}
+
+	// Start with JSON serialized timestamp parser to validate deserialization too
+	// TODO(Raj): Is this needed?
+	var timestampParserOp PipelineOperator
+	err := json.Unmarshal([]byte(`
+		{
+			"orderId": 1,
+			"enabled": true,
+			"type": "time_parser",
+			"name": "Test timestamp parser",
+			"id": "test-timestamp-parser",
+			"parse_from": "attributes.test_timestamp",
+			"layout_type": "strptime",
+			"layout": "%Y-%m-%dT%H:%M:%S.%f%z"
+		}
+	`), &timestampParserOp)
+	require.Nil(err)
+	testPipelines[0].Config = append(testPipelines[0].Config, timestampParserOp)
+
+	testTimestampStr := "2023-11-27T12:03:28.239907+0530"
+	testLog := makeTestLogEntry(
+		"test log",
+		map[string]string{
+			"method":         "GET",
+			"test_timestamp": testTimestampStr,
+		},
+	)
+
+	result, collectorWarnAndErrorLogs, err := SimulatePipelinesProcessing(
+		context.Background(),
+		testPipelines,
+		[]model.SignozLog{
+			testLog,
+		},
+	)
+	require.Nil(err)
+	require.Equal(1, len(result))
+	require.Equal(0, len(collectorWarnAndErrorLogs), strings.Join(collectorWarnAndErrorLogs, "\n"))
+	processed := result[0]
+
+	expectedTimestamp, err := time.Parse("2006-01-02T15:04:05.999999-0700", testTimestampStr)
+	require.Nil(err)
+
+	require.Equal(uint64(expectedTimestamp.UnixNano()), processed.Timestamp)
+
 }
