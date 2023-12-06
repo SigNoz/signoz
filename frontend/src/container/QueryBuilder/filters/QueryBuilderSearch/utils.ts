6d31eda33c4aef52969b9940f608d0fc757eb974
Vikrant Gupta <vikrant.thomso@gmail.com>
2023-12-07 00:47:02 +0530

    feat: [GH-3645]: case insensitive operators for query builder

--- frontend/src/container/QueryBuilder/filters/QueryBuilderSearch/utils.ts
+++ frontend/src/container/QueryBuilder/filters/QueryBuilderSearch/utils.ts
@@ -4,7 +4,7 @@ import { parse } from 'papaparse';
 import { orderByValueDelimiter } from '../OrderByFilter/utils';
 
 // eslint-disable-next-line no-useless-escape
-export const tagRegexp = /^\s*(.*?)\s*(IN|NOT_IN|LIKE|NOT_LIKE|REGEX|NOT_REGEX|=|!=|EXISTS|NOT_EXISTS|CONTAINS|NOT_CONTAINS|>=|>|<=|<|HAS|NHAS)\s*(.*)$/g;
+export const tagRegexp = /^\s*(.*?)\s*(\bIN\b|\bNOT_IN\b|\bLIKE\b|\bNOT_LIKE\b|\bREGEX\b|\bNOT_REGEX\b|\b=\b|\b!=\b|\bEXISTS\b|\bNOT_EXISTS\b|\bCONTAINS\b|\bNOT_CONTAINS\b|\b>=\b|\b>\b|\b<=\b|\b<\b|\bHAS\b|\bNHAS\b)\s*(.*)$/gi;
 
 export function isInNInOperator(value: string): boolean {
 	return value === OPERATORS.IN || value === OPERATORS.NIN;
@@ -24,8 +24,8 @@ export function getTagToken(tag: string): ITagToken {
 		const [, matchTagKey, matchTagOperator, matchTagValue] = match;
 		return {
 			tagKey: matchTagKey,
-			tagOperator: matchTagOperator,
-			tagValue: isInNInOperator(matchTagOperator)
+			tagOperator: matchTagOperator.toUpperCase(),
+			tagValue: isInNInOperator(matchTagOperator.toUpperCase())
 				? parse(matchTagValue).data.flat()
 				: matchTagValue,
 		} as ITagToken;
