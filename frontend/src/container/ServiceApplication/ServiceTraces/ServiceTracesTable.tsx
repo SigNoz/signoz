da8a63f530be006cecfff618fbc4860fa81663e0
Yunus M <myounis.ar@live.com>
2023-12-21 13:55:47 +0530

    feat: show rate limit warning in services page when total rps > 100

--- frontend/src/container/ServiceApplication/ServiceTraces/ServiceTracesTable.tsx
+++ frontend/src/container/ServiceApplication/ServiceTraces/ServiceTracesTable.tsx
@@ -1,6 +1,11 @@
+import { WarningFilled } from '@ant-design/icons';
+import { Flex, Typography } from 'antd';
 import { ResizeTable } from 'components/ResizeTable';
-import { useMemo } from 'react';
+import { MAX_RPS_LIMIT } from 'constants/global';
+import { useEffect, useMemo, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import { useLocation } from 'react-router-dom';
+import { getTotalRPS } from 'utils/services';
 
 import { getColumns } from '../Columns/ServiceColumn';
 import ServiceTableProps from '../types';
@@ -10,16 +15,37 @@ function ServiceTraceTable({
 	loading,
 }: ServiceTableProps): JSX.Element {
 	const { search } = useLocation();
+	const [RPS, setRPS] = useState(0);
+	const { t: getText } = useTranslation(['services']);
 
 	const tableColumns = useMemo(() => getColumns(search, false), [search]);
 
+	useEffect(() => {
+		if (services.length > 0) {
+			const rps = getTotalRPS(services);
+			setRPS(rps);
+		} else {
+			setRPS(0);
+		}
+	}, [services]);
+
 	return (
-		<ResizeTable
-			columns={tableColumns}
-			loading={loading}
-			dataSource={services}
-			rowKey="serviceName"
-		/>
+		<>
+			{RPS > MAX_RPS_LIMIT && (
+				<Flex justify="center">
+					<Typography.Title level={5} type="warning" style={{ marginTop: 0 }}>
+						<WarningFilled /> {getText('rps_over_100')}
+					</Typography.Title>
+				</Flex>
+			)}
+
+			<ResizeTable
+				columns={tableColumns}
+				loading={loading}
+				dataSource={services}
+				rowKey="serviceName"
+			/>
+		</>
 	);
 }
 
