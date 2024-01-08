426cb41fc8063f46ba8eac05faafb75381aa7503
Vikrant Gupta <54737045+Vikrant2520@users.noreply.github.com>
2024-01-09 01:12:56 +0530

    feat: added new toolbar for logs explorer (#4336)

--- frontend/src/container/Toolbar/Toolbar.tsx
+++ frontend/src/container/Toolbar/Toolbar.tsx
@@ -1,112 +1,29 @@
 import './Toolbar.styles.scss';
 
-import { Button, Select, Switch, Typography } from 'antd';
-import { Atom, MousePointerSquare, Play, Terminal } from 'lucide-react';
+import DateTimeSelectionV2 from 'container/TopNav/DateTimeSelectionV2';
 
 interface ToolbarProps {
-	selectedView: string;
-	showHistogram: boolean;
-	onToggleHistrogramVisibility: () => void;
+	leftActions?: JSX.Element;
+	rightActions?: JSX.Element;
 }
 
 export default function Toolbar({
-	selectedView,
-	showHistogram,
-	onToggleHistrogramVisibility,
+	leftActions,
+	rightActions,
 }: ToolbarProps): JSX.Element {
-	const onChange = (value: string): void => {
-		console.log(`selected ${value}`);
-	};
-
-	const onSearch = (value: string): void => {
-		console.log('search:', value);
-	};
-
-	console.log(selectedView, showHistogram);
-
-	// Filter `option.label` match the user type `input`
-	const filterOption = (
-		input: string,
-		option?: { label: string; value: string },
-	): any => (option?.label ?? '').toLowerCase().includes(input.toLowerCase());
-
+	console.log(leftActions, rightActions);
 	return (
 		<div className="toolbar">
-			<div className="left-options">
-				<div className="query-builder-view-options">
-					<div
-						className="views-tabs"
-						// onChange={handleModeChange}
-					>
-						<Button
-							value="search"
-							// eslint-disable-next-line sonarjs/no-duplicate-string
-							className={selectedView === 'search' ? 'active-tab' : ''}
-						>
-							<MousePointerSquare size={14} />
-						</Button>
-						<Button
-							value="query-builder"
-							className={selectedView === 'query-builder' ? 'active-tab' : ''}
-						>
-							<Atom size={14} />
-						</Button>
-						<Button
-							value="clickhouse"
-							className={selectedView === 'clickhouse' ? 'active-tab' : ''}
-						>
-							<Terminal size={14} />
-						</Button>
-					</div>
-				</div>
-
-				<div className="histogram-view-controller">
-					<Typography>Histogram</Typography>
-					<Switch
-						size="small"
-						defaultChecked
-						onChange={onToggleHistrogramVisibility}
-					/>
-				</div>
+			<div className="leftActions">{leftActions}</div>
+			<div className="timeRange">
+				<DateTimeSelectionV2 />
 			</div>
-
-			<div className="right-options">
-				<div className="date-picker-container">
-					<div className="refresh-time">Refreshed 10 min ago</div>
-
-					<Select
-						showSearch
-						style={{
-							width: '160px',
-						}}
-						placeholder="Live"
-						optionFilterProp="children"
-						onChange={onChange}
-						onSearch={onSearch}
-						filterOption={filterOption}
-						options={[
-							{
-								value: 'jack',
-								label: 'Jack',
-							},
-							{
-								value: 'lucy',
-								label: 'Lucy',
-							},
-							{
-								value: 'tom',
-								label: 'Tom',
-							},
-						]}
-					/>
-				</div>
-
-				<div className="stage-run-query-container">
-					<Button type="primary" icon={<Play size={14} />}>
-						Stage & Run Query
-					</Button>
-				</div>
-			</div>
+			<div className="rightActions">{rightActions}</div>
 		</div>
 	);
 }
+
+Toolbar.defaultProps = {
+	leftActions: <div />,
+	rightActions: <div />,
+};
