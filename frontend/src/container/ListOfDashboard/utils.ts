350daedc232fdf6b8e12441bdf4281936edd9321
keshavg2 <keshavgupta9906@gmail.com>
2024-01-09 00:42:41 +0530

    fix: filter in dashboard data

--- frontend/src/container/ListOfDashboard/utils.ts
+++ frontend/src/container/ListOfDashboard/utils.ts
@@ -1,14 +1,16 @@
+import { Dashboard } from 'types/api/dashboard/getAll';
+
 export const filterDashboard = (
 	searchValue: string,
-	dashboardList: any,
+	dashboardList: Dashboard[],
 ): any[] => {
 	// Convert the searchValue to lowercase for case-insensitive search
 	const searchValueLowerCase = searchValue.toLowerCase();
 
 	// Use the filter method to find matching objects
-	return dashboardList.filter((item: any) => {
+	return dashboardList.filter((item: Dashboard) => {
 		// Convert each property value to lowercase for case-insensitive search
-		const itemValues = Object.values(item?.data).map((value: any) =>
+		const itemValues = Object.values(item?.data).map((value) =>
 			value.toString().toLowerCase(),
 		);
 
