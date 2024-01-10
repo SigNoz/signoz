9604e0368be215436b7399abbd9b787cc8b6d07a
Rajat-Dabade <rajat@signoz.io>
2024-01-10 11:30:36 +0530

    refactor: completed with tab

--- frontend/src/components/LogDetail/index.tsx
+++ frontend/src/components/LogDetail/index.tsx
@@ -1,8 +1,15 @@
-import { Drawer, Tabs } from 'antd';
+import './LogDetails.styles.scss';
+
+import { CloseOutlined, DownOutlined, UpOutlined } from '@ant-design/icons';
+import { Color } from '@signozhq/design-tokens';
+import { Divider, Drawer, Radio, Typography } from 'antd';
+import { RadioChangeEvent } from 'antd/lib';
 import JSONView from 'container/LogDetailedView/JsonView';
 import TableView from 'container/LogDetailedView/TableView';
-import { useMemo } from 'react';
+import { useIsDarkMode } from 'hooks/useDarkMode';
+import { useState } from 'react';
 
+import { VIEW_TYPES, VIEWS } from './constants';
 import { LogDetailProps } from './LogDetail.interfaces';
 
 function LogDetail({
@@ -11,40 +18,110 @@ function LogDetail({
 	onAddToQuery,
 	onClickActionItem,
 }: LogDetailProps): JSX.Element {
-	const items = useMemo(
-		() => [
-			{
-				label: 'Table',
-				key: '1',
-				children: log && (
-					<TableView
-						logData={log}
-						onAddToQuery={onAddToQuery}
-						onClickActionItem={onClickActionItem}
-					/>
-				),
-			},
-			{
-				label: 'JSON',
-				key: '2',
-				children: log && <JSONView logData={log} />,
-			},
-		],
-		[log, onAddToQuery, onClickActionItem],
-	);
+	console.log({ log });
+	const [selectedView, setSelectedView] = useState<VIEWS>(VIEW_TYPES.OVERVIEW);
+	// const items = useMemo(
+	// 	() => [
+	// 		{
+	// 			label: 'Table',
+	// 			key: '1',
+	// 			children: log && (
+	// 				<TableView
+	// 					logData={log}
+	// 					onAddToQuery={onAddToQuery}
+	// 					onClickActionItem={onClickActionItem}
+	// 				/>
+	// 			),
+	// 		},
+	// 		{
+	// 			label: 'JSON',
+	// 			key: '2',
+	// 			children: log && <JSONView logData={log} />,
+	// 		},
+	// 	],
+	// 	[log, onAddToQuery, onClickActionItem],
+	// );
+
+	const isDarkMode = useIsDarkMode();
+	const handleModeChange = (e: RadioChangeEvent): void => {
+		setSelectedView(e.target.value);
+	};
+
+	if (!log) {
+		return <>No log data</>;
+	}
 
 	return (
 		<Drawer
 			width="60%"
-			title="Log Details"
+			title={
+				<>
+					<Divider type="vertical" /> Log Detail
+				</>
+			}
 			placement="right"
 			closable
 			onClose={onClose}
 			open={log !== null}
-			style={{ overscrollBehavior: 'contain' }}
+			style={{ overscrollBehavior: 'contain', background: Color.BG_SLATE_500 }}
+			className="log-detail-drawer"
 			destroyOnClose
+			closeIcon={
+				<CloseOutlined
+					style={{
+						color: isDarkMode ? Color.BG_VANILLA_400 : Color.BG_SLATE_500,
+						fontSize: 16,
+					}}
+				/>
+			}
+			extra={
+				<>
+					<Radio.Button className="log-detail-drawer__radio-button">
+						<UpOutlined />
+					</Radio.Button>
+					<Radio.Button className="log-detail-drawer__radio-button">
+						<DownOutlined />
+					</Radio.Button>
+				</>
+			}
 		>
-			<Tabs defaultActiveKey="1" items={items} />
+			<div className="log-detail-drawer__log">
+				<Divider type="vertical" style={{ fontSize: '20px' }} />
+				<Typography.Text>{log?.body}</Typography.Text>
+			</div>
+
+			<Radio.Group
+				className="views-tabs"
+				onChange={handleModeChange}
+				value={selectedView}
+			>
+				<Radio.Button
+					className={selectedView === 'OVERVIEW' ? 'selected_view' : undefined}
+					value={VIEW_TYPES.OVERVIEW}
+				>
+					Overview
+				</Radio.Button>
+				<Radio.Button
+					className={selectedView === 'JSON' ? 'selected_view' : undefined}
+					value={VIEW_TYPES.JSON}
+				>
+					JSON
+				</Radio.Button>
+				<Radio.Button
+					className={selectedView === 'CONTENT' ? 'selected_view' : undefined}
+					value={VIEW_TYPES.CONTENT}
+				>
+					Content
+				</Radio.Button>
+			</Radio.Group>
+			{selectedView === VIEW_TYPES.OVERVIEW && (
+				<TableView
+					logData={log}
+					onAddToQuery={onAddToQuery}
+					onClickActionItem={onClickActionItem}
+				/>
+			)}
+			{selectedView === VIEW_TYPES.JSON && <JSONView logData={log} />}
 		</Drawer>
 	);
 }
