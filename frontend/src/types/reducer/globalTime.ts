fab3755a884ed6c530623d7d9ba766265d1f1ec9
Vikrant Gupta <vikrant.thomso@gmail.com>
2024-01-11 01:07:38 +0530

    feat: new table view for logs explorer list section  (#4353)
    * feat: table view changes for logs list
    
    * feat: code refactor to support log line actions
    
    * feat: code refactor to support log line actions
    
    * fix: the positioning of the btns
    
    * feat: fix the table onclick
    
    * fix: header issue
    
    * fix: on hover
    
    * fix: type issue

--- frontend/src/types/reducer/globalTime.ts
+++ frontend/src/types/reducer/globalTime.ts
@@ -1,11 +1,12 @@
 import { Time } from 'container/TopNav/DateTimeSelection/config';
+import { Time as TimeV2 } from 'container/TopNav/DateTimeSelectionV2/config';
 import { GlobalTime } from 'types/actions/globalTime';
 
 export interface GlobalReducer {
 	maxTime: GlobalTime['maxTime'];
 	minTime: GlobalTime['minTime'];
 	loading: boolean;
-	selectedTime: Time;
+	selectedTime: Time | TimeV2;
 	isAutoRefreshDisabled: boolean;
 	selectedAutoRefreshInterval: string;
 }
