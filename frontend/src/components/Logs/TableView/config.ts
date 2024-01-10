c1df7cb59481c6050b91cd8d6bc1a15a4d80ddfe
Vikrant Gupta <vikrant.thomso@gmail.com>
2024-01-11 01:07:38 +0530

    feat: new table view for logs explorer list section  (#4353)
    * feat: table view changes for logs list
    
    * feat: code refactor to support log line actions
    
    * feat: code refactor to support log line actions
    
    * fix: the positioning of the btns
    
    * feat: fix the table onclick
    
    * fix: header issue
    
    * fix: on hover
    
    * fix: type issue

--- frontend/src/components/Logs/TableView/config.ts
+++ frontend/src/components/Logs/TableView/config.ts
@@ -6,6 +6,13 @@ export const defaultCellStyle: CSSProperties = {
 	paddingBottom: 6,
 	paddingRight: 8,
 	paddingLeft: 8,
+	color: 'var(--bg-vanilla-400, #c0c1c3)',
+	fontSize: '14px',
+	fontStyle: 'normal',
+	fontWeight: 400,
+	lineHeight: '18px',
+	letterSpacing: '-0.07px',
+	marginBottom: '0px',
 };
 
 export const defaultTableStyle: CSSProperties = {
