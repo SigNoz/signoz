14333413bec28655e9b6a70a01a2467412df9230
Rajat-Dabade <rajat@signoz.io>
2024-01-11 11:26:10 +0530

    refactor: completed and added support for lightmode on body

--- frontend/src/components/LogDetail/index.tsx
+++ frontend/src/components/LogDetail/index.tsx
@@ -1,12 +1,20 @@
 import './LogDetails.styles.scss';
 
-import { CloseOutlined, DownOutlined, UpOutlined } from '@ant-design/icons';
 import { Color } from '@signozhq/design-tokens';
-import { Divider, Drawer, Radio, Typography } from 'antd';
+import { Button, Divider, Drawer, Input, Radio, Typography } from 'antd';
 import { RadioChangeEvent } from 'antd/lib';
 import JSONView from 'container/LogDetailedView/JsonView';
-import TableView from 'container/LogDetailedView/TableView';
+import Overview from 'container/LogDetailedView/Overview';
 import { useIsDarkMode } from 'hooks/useDarkMode';
+import {
+	Braces,
+	ChevronDown,
+	ChevronUp,
+	Search,
+	Table,
+	TextSelect,
+	X,
+} from 'lucide-react';
 import { useState } from 'react';
 
 import { VIEW_TYPES, VIEWS } from './constants';
@@ -18,33 +26,28 @@ function LogDetail({
 	onAddToQuery,
 	onClickActionItem,
 }: LogDetailProps): JSX.Element {
-	console.log({ log });
+	console.log({ onClickActionItem }); // TODO: remove, ketp for linter error
 	const [selectedView, setSelectedView] = useState<VIEWS>(VIEW_TYPES.OVERVIEW);
-	// const items = useMemo(
-	// 	() => [
-	// 		{
-	// 			label: 'Table',
-	// 			key: '1',
-	// 			children: log && (
-	// 				<TableView
-	// 					logData={log}
-	// 					onAddToQuery={onAddToQuery}
-	// 					onClickActionItem={onClickActionItem}
-	// 				/>
-	// 			),
-	// 		},
-	// 		{
-	// 			label: 'JSON',
-	// 			key: '2',
-	// 			children: log && <JSONView logData={log} />,
-	// 		},
-	// 	],
-	// 	[log, onAddToQuery, onClickActionItem],
-	// );
-
+	const [isSearchVisible, setIsSearchVisible] = useState<boolean>(false);
+	const [fieldSearchInput, setFieldSearchInput] = useState<string>('');
 	const isDarkMode = useIsDarkMode();
+
 	const handleModeChange = (e: RadioChangeEvent): void => {
 		setSelectedView(e.target.value);
+	};
+
+	const handleSearchVisible = (): void => {
+		setIsSearchVisible(!isSearchVisible);
+	};
+
+	const drawerCloseHandler = (
+		e: React.MouseEvent | React.KeyboardEvent,
+	): void => {
+		setFieldSearchInput('');
+		setIsSearchVisible(false);
+		if (onClose) {
+			onClose(e);
+		}
 	};
 
 	if (!log) {
@@ -56,69 +59,107 @@ function LogDetail({
 			width="60%"
 			title={
 				<>
-					<Divider type="vertical" /> Log Detail
+					<Divider
+						type="vertical"
+						style={{
+							border: isDarkMode ? undefined : `1px solid ${Color.BG_VANILLA_400}`,
+						}}
+					/>
+					<Typography.Text className="title">Log details</Typography.Text>
 				</>
 			}
 			placement="right"
 			closable
-			onClose={onClose}
+			onClose={drawerCloseHandler}
 			open={log !== null}
-			style={{ overscrollBehavior: 'contain', background: Color.BG_SLATE_500 }}
+			style={{
+				overscrollBehavior: 'contain',
+				background: isDarkMode ? Color.BG_SLATE_500 : Color.BG_VANILLA_100,
+			}}
 			className="log-detail-drawer"
 			destroyOnClose
-			closeIcon={
-				<CloseOutlined
-					style={{
-						color: isDarkMode ? Color.BG_VANILLA_400 : Color.BG_SLATE_500,
-						fontSize: 16,
-					}}
-				/>
-			}
+			closeIcon={<X />}
 			extra={
 				<>
-					<Radio.Button className="log-detail-drawer__radio-button">
-						<UpOutlined />
+					<Radio.Button className="radio-button">
+						<ChevronUp size={16} />
 					</Radio.Button>
-					<Radio.Button className="log-detail-drawer__radio-button">
-						<DownOutlined />
+					<Radio.Button className="radio-button">
+						<ChevronDown size={16} />
 					</Radio.Button>
 				</>
 			}
 		>
 			<div className="log-detail-drawer__log">
-				<Divider type="vertical" style={{ fontSize: '20px' }} />
-				<Typography.Text>{log?.body}</Typography.Text>
+				<Divider
+					type="vertical"
+					style={{
+						fontSize: '20px',
+						border: isDarkMode ? undefined : `1px solid ${Color.BG_VANILLA_400}`,
+					}}
+				/>
+				<Typography.Text className="log-body">{log?.body}</Typography.Text>
 			</div>
 
-			<Radio.Group
-				className="views-tabs"
-				onChange={handleModeChange}
-				value={selectedView}
-			>
-				<Radio.Button
-					className={selectedView === 'OVERVIEW' ? 'selected_view' : undefined}
-					value={VIEW_TYPES.OVERVIEW}
+			<div className="tabs-and-search">
+				<Radio.Group
+					className="views-tabs"
+					onChange={handleModeChange}
+					value={selectedView}
 				>
-					Overview
-				</Radio.Button>
-				<Radio.Button
-					className={selectedView === 'JSON' ? 'selected_view' : undefined}
-					value={VIEW_TYPES.JSON}
-				>
-					JSON
-				</Radio.Button>
-				<Radio.Button
-					className={selectedView === 'CONTENT' ? 'selected_view' : undefined}
-					value={VIEW_TYPES.CONTENT}
-				>
-					Content
-				</Radio.Button>
-			</Radio.Group>
+					<Radio.Button
+						className={
+							// eslint-disable-next-line sonarjs/no-duplicate-string
+							selectedView === 'OVERVIEW' ? 'selected_view' : undefined
+						}
+						value={VIEW_TYPES.OVERVIEW}
+					>
+						<div className="view-title">
+							<Table size={14} />
+							Overview
+						</div>
+					</Radio.Button>
+					<Radio.Button
+						className={selectedView === 'JSON' ? 'selected_view' : undefined}
+						value={VIEW_TYPES.JSON}
+					>
+						<div className="view-title">
+							<Braces size={14} />
+							JSON
+						</div>
+					</Radio.Button>
+					<Radio.Button
+						className={selectedView === 'CONTENT' ? 'selected_view' : undefined}
+						value={VIEW_TYPES.CONTENT}
+					>
+						<div className="view-title">
+							<TextSelect size={14} />
+							Content
+						</div>
+					</Radio.Button>
+				</Radio.Group>
+
+				<Button
+					className="btn-search"
+					icon={<Search size={14} />}
+					onClick={handleSearchVisible}
+				/>
+			</div>
+
+			{isSearchVisible && (
+				<Input
+					placeholder="Search..."
+					className="search-input"
+					value={fieldSearchInput}
+					onChange={(e): void => setFieldSearchInput(e.target.value)}
+				/>
+			)}
+
 			{selectedView === VIEW_TYPES.OVERVIEW && (
-				<TableView
+				<Overview
 					logData={log}
 					onAddToQuery={onAddToQuery}
-					onClickActionItem={onClickActionItem}
+					fieldSearchInput={fieldSearchInput}
 				/>
 			)}
 			{selectedView === VIEW_TYPES.JSON && <JSONView logData={log} />}
