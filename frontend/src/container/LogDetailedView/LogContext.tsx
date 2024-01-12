317c7941ebe694f0025dfacf88babe5d017d07bf
Rajat-Dabade <rajat@signoz.io>
2024-01-12 18:14:10 +0530

    chore: populated log context

--- frontend/src/container/LogDetailedView/LogContext.tsx
+++ frontend/src/container/LogDetailedView/LogContext.tsx
@@ -1,7 +1,45 @@
-function LogContext(): JSX.Element {
+import RawLogView from 'components/Logs/RawLogView';
+import LogsContextList from 'container/LogsContextList';
+import useInitialQuery from 'container/LogsExplorerContext/useInitialQuery';
+import { ORDERBY_FILTERS } from 'container/QueryBuilder/filters/OrderByFilter/config';
+import { useState } from 'react';
+import { ILog } from 'types/api/logs/log';
+import { Query, TagFilter } from 'types/api/queryBuilder/queryBuilderData';
+
+function LogContext({ log }: { log: ILog }): JSX.Element {
+	const initialContextQuery = useInitialQuery(log);
+
+	const [contextQuery, setContextQuery] = useState<Query>(initialContextQuery);
+	const [filters, setFilters] = useState<TagFilter | null>(null);
+	const [isEdit, setIsEdit] = useState<boolean>(false);
+
+	console.log({ setContextQuery, setFilters, setIsEdit });
+
 	return (
 		<div>
-			<h1>Log Context</h1>
+			<LogsContextList
+				order={ORDERBY_FILTERS.ASC}
+				filters={filters}
+				isEdit={isEdit}
+				log={log}
+				query={contextQuery}
+			/>
+			<div>
+				<RawLogView
+					isActiveLog
+					isReadOnly
+					isTextOverflowEllipsisDisabled
+					data={log}
+					linesPerRow={1}
+				/>
+			</div>
+			<LogsContextList
+				order={ORDERBY_FILTERS.DESC}
+				filters={filters}
+				isEdit={isEdit}
+				log={log}
+				query={contextQuery}
+			/>
 		</div>
 	);
 }
