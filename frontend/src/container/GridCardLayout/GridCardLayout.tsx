a73a856ecb72fb1a55161842dc594b8ab05a29e5
Yunus M <myounis.ar@live.com>
2024-01-17 15:09:54 +0530

    fix: autosave layout to layout change

--- frontend/src/container/GridCardLayout/GridCardLayout.tsx
+++ frontend/src/container/GridCardLayout/GridCardLayout.tsx
@@ -1,6 +1,6 @@
 import './GridCardLayout.styles.scss';
 
-import { PlusOutlined, SaveFilled } from '@ant-design/icons';
+import { PlusOutlined } from '@ant-design/icons';
 import { SOMETHING_WENT_WRONG } from 'constants/api';
 import { PANEL_TYPES } from 'constants/queryBuilder';
 import { themeColors } from 'constants/theme';
@@ -8,9 +8,12 @@ import { useUpdateDashboard } from 'hooks/dashboard/useUpdateDashboard';
 import useComponentPermission from 'hooks/useComponentPermission';
 import { useIsDarkMode } from 'hooks/useDarkMode';
 import { useNotifications } from 'hooks/useNotifications';
+import isEqual from 'lodash-es/isEqual';
 import { FullscreenIcon } from 'lucide-react';
 import { useDashboard } from 'providers/Dashboard/Dashboard';
+import { useEffect, useState } from 'react';
 import { FullScreen, useFullScreenHandle } from 'react-full-screen';
+import { Layout } from 'react-grid-layout';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 import { AppState } from 'store/reducers';
@@ -51,6 +54,8 @@ function GraphLayout({ onAddPanelHandler }: GraphLayoutProps): JSX.Element {
 
 	const isDarkMode = useIsDarkMode();
 
+	const [dashboardLayout, setDashboardLayout] = useState(layouts);
+
 	const updateDashboardMutation = useUpdateDashboard();
 
 	const { notifications } = useNotifications();
@@ -78,7 +83,7 @@ function GraphLayout({ onAddPanelHandler }: GraphLayoutProps): JSX.Element {
 			...selectedDashboard,
 			data: {
 				...selectedDashboard.data,
-				layout: layouts.filter((e) => e.i !== PANEL_TYPES.EMPTY_WIDGET),
+				layout: dashboardLayout.filter((e) => e.i !== PANEL_TYPES.EMPTY_WIDGET),
 			},
 			uuid: selectedDashboard.uuid,
 		};
@@ -90,9 +95,6 @@ function GraphLayout({ onAddPanelHandler }: GraphLayoutProps): JSX.Element {
 						setLayouts(updatedDashboard.payload.data.layout);
 					setSelectedDashboard(updatedDashboard.payload);
 				}
-				notifications.success({
-					message: t('dashboard:layout_saved_successfully'),
-				});
 
 				featureResponse.refetch();
 			},
@@ -108,6 +110,27 @@ function GraphLayout({ onAddPanelHandler }: GraphLayoutProps): JSX.Element {
 		? [...ViewMenuAction, ...EditMenuAction]
 		: [...ViewMenuAction];
 
+	const handleLayoutChange = (layout: Layout[]): void => {
+		if (!isEqual(layout, dashboardLayout)) {
+			setDashboardLayout(layout);
+		}
+	};
+
+	useEffect(() => {
+		if (
+			dashboardLayout &&
+			Array.isArray(dashboardLayout) &&
+			dashboardLayout.length > 0 &&
+			!isDashboardLocked &&
+			saveLayoutPermission &&
+			!updateDashboardMutation.isLoading
+		) {
+			onSaveHandler();
+		}
+
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [dashboardLayout]);
+
 	return (
 		<>
 			<ButtonContainer>
@@ -119,17 +142,6 @@ function GraphLayout({ onAddPanelHandler }: GraphLayoutProps): JSX.Element {
 				>
 					{t('dashboard:full_view')}
 				</Button>
-
-				{!isDashboardLocked && saveLayoutPermission && (
-					<Button
-						loading={updateDashboardMutation.isLoading}
-						onClick={onSaveHandler}
-						icon={<SaveFilled />}
-						disabled={updateDashboardMutation.isLoading}
-					>
-						{t('dashboard:save_layout')}
-					</Button>
-				)}
 
 				{!isDashboardLocked && addPanelPermission && (
 					<Button
@@ -153,12 +165,12 @@ function GraphLayout({ onAddPanelHandler }: GraphLayoutProps): JSX.Element {
 					isDroppable={!isDashboardLocked && addPanelPermission}
 					isResizable={!isDashboardLocked && addPanelPermission}
 					allowOverlap={false}
-					onLayoutChange={setLayouts}
+					onLayoutChange={handleLayoutChange}
 					draggableHandle=".drag-handle"
-					layout={layouts}
+					layout={dashboardLayout}
 					style={{ backgroundColor: isDarkMode ? '' : themeColors.snowWhite }}
 				>
-					{layouts.map((layout) => {
+					{dashboardLayout.map((layout) => {
 						const { i: id } = layout;
 						const currentWidget = (widgets || [])?.find((e) => e.id === id);
 
