a78a8b6291e5e8f3c2b441ac8f671b9569d3f111
Vikrant Gupta <vikrant.thomso@gmail.com>
2024-01-24 00:07:47 +0530

    fix: added old logs explorer CTA in new designs

--- frontend/src/container/TopNav/DateTimeSelectionV2/Refresh.tsx
+++ frontend/src/container/TopNav/DateTimeSelectionV2/Refresh.tsx
@@ -21,6 +21,11 @@ function RefreshText({
 		};
 	}, [onLastRefreshHandler, refreshText]);
 
+	if (refreshButtonHidden) {
+		// eslint-disable-next-line react/jsx-no-useless-fragment
+		return <></>;
+	}
+
 	return (
 		<RefreshTextContainer refreshButtonHidden={refreshButtonHidden}>
 			<Typography>{refreshText}</Typography>
