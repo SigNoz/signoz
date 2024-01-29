01a56522a36e26bebc5609cb57a06c10b7178b09
Raj <rkssisodiya@gmail.com>
2024-01-29 17:34:07 +0530

    chore: add explicit happy case test for regex parsing processor

---
+++ pkg/query-service/app/logparsingpipeline/processors_test.go
@@ -0,0 +1,81 @@
+package logparsingpipeline
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"go.signoz.io/signoz/pkg/query-service/model"
+	v3 "go.signoz.io/signoz/pkg/query-service/model/v3"
+)
+
+// Tests for pipeline processors other than the ones
+// covered in dedicated files.
+
+func TestRegexProcessor(t *testing.T) {
+	require := require.New(t)
+
+	testPipelines := []Pipeline{
+		{
+			OrderId: 1,
+			Name:    "pipeline1",
+			Alias:   "pipeline1",
+			Enabled: true,
+			Filter: &v3.FilterSet{
+				Operator: "AND",
+				Items: []v3.FilterItem{
+					{
+						Key: v3.AttributeKey{
+							Key:      "method",
+							DataType: v3.AttributeKeyDataTypeString,
+							Type:     v3.AttributeKeyTypeTag,
+						},
+						Operator: "=",
+						Value:    "GET",
+					},
+				},
+			},
+			Config: []PipelineOperator{},
+		},
+	}
+
+	var parserOp PipelineOperator
+	err := json.Unmarshal([]byte(`
+		{
+			"orderId": 1,
+			"enabled": true,
+			"type": "regex_parser",
+			"name": "Test regex parser",
+			"id": "test-regex-parser",
+			"parse_from": "body",
+			"parse_to": "attributes",
+			"regex": "PAN: (?P<pan>[a-zA-Z]{5}[0-9]{4}[a-zA-Z]{1}) "
+		}
+	`), &parserOp)
+	require.Nil(err)
+	testPipelines[0].Config = append(testPipelines[0].Config, parserOp)
+
+	testPan := "GDTSJ4756E"
+	testLog := makeTestSignozLog(
+		fmt.Sprintf("test string with PAN: %s and some more text", testPan),
+		map[string]interface{}{
+			"method": "GET",
+		},
+	)
+
+	result, collectorWarnAndErrorLogs, err := SimulatePipelinesProcessing(
+		context.Background(),
+		testPipelines,
+		[]model.SignozLog{
+			testLog,
+		},
+	)
+	require.Nil(err)
+	require.Equal(1, len(result))
+	require.Equal(0, len(collectorWarnAndErrorLogs), strings.Join(collectorWarnAndErrorLogs, "\n"))
+	processed := result[0]
+	require.Equal(testPan, processed.Attributes_string["pan"])
+}
