04dd8cee0e1bb48f1ea774c57588a638a531ecd2
Raj <rkssisodiya@gmail.com>
2024-01-29 17:49:25 +0530

    chore: log pipelines: move trace parser test to processors_test.go

--- pkg/query-service/app/logparsingpipeline/processors_test.go
+++ pkg/query-service/app/logparsingpipeline/processors_test.go
@@ -4,12 +4,15 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"strconv"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 	"go.signoz.io/signoz/pkg/query-service/model"
 	v3 "go.signoz.io/signoz/pkg/query-service/model/v3"
+	"go.signoz.io/signoz/pkg/query-service/utils"
 )
 
 // Tests for pipeline processors other than the ones
@@ -207,4 +210,111 @@ func TestJSONProcessor(t *testing.T) {
 	processed := result[0]
 	require.Equal("value", processed.Attributes_string["test_str"])
 	require.Equal(1.1, processed.Attributes_float64["test_float"])
+}
+
+func TestTraceParsingProcessor(t *testing.T) {
+	require := require.New(t)
+
+	testPipelines := []Pipeline{
+		{
+			OrderId: 1,
+			Name:    "pipeline1",
+			Alias:   "pipeline1",
+			Enabled: true,
+			Filter: &v3.FilterSet{
+				Operator: "AND",
+				Items: []v3.FilterItem{
+					{
+						Key: v3.AttributeKey{
+							Key:      "method",
+							DataType: v3.AttributeKeyDataTypeString,
+							Type:     v3.AttributeKeyTypeTag,
+						},
+						Operator: "=",
+						Value:    "GET",
+					},
+				},
+			},
+			Config: []PipelineOperator{},
+		},
+	}
+
+	// Start with JSON serialized trace parser to validate deserialization too
+	var traceParserOp PipelineOperator
+	err := json.Unmarshal([]byte(`
+		{
+			"orderId": 1,
+			"enabled": true,
+			"type": "trace_parser",
+			"name": "Test trace parser",
+			"id": "test-trace-parser",
+			"trace_id": {
+					"parse_from": "attributes.test_trace_id"
+			},
+			"span_id": {
+					"parse_from": "attributes.test_span_id"
+			},
+			"trace_flags": {
+					"parse_from": "attributes.test_trace_flags"
+			}
+		}
+	`), &traceParserOp)
+	require.Nil(err)
+	testPipelines[0].Config = append(testPipelines[0].Config, traceParserOp)
+
+	testTraceId, err := utils.RandomHex(16)
+	require.Nil(err)
+
+	testSpanId, err := utils.RandomHex(8)
+	require.Nil(err)
+
+	testTraceFlags, err := utils.RandomHex(1)
+	require.Nil(err)
+
+	testLog := model.SignozLog{
+		Timestamp: uint64(time.Now().UnixNano()),
+		Body:      "test log",
+		Attributes_string: map[string]string{
+			"method":           "GET",
+			"test_trace_id":    testTraceId,
+			"test_span_id":     testSpanId,
+			"test_trace_flags": testTraceFlags,
+		},
+		SpanID:     "",
+		TraceID:    "",
+		TraceFlags: 0,
+	}
+
+	result, collectorWarnAndErrorLogs, err := SimulatePipelinesProcessing(
+		context.Background(),
+		testPipelines,
+		[]model.SignozLog{
+			testLog,
+		},
+	)
+	require.Nil(err)
+	require.Equal(1, len(result))
+	require.Equal(0, len(collectorWarnAndErrorLogs))
+	processed := result[0]
+
+	require.Equal(testTraceId, processed.TraceID)
+	require.Equal(testSpanId, processed.SpanID)
+
+	expectedTraceFlags, err := strconv.ParseUint(testTraceFlags, 16, 16)
+	require.Nil(err)
+	require.Equal(uint32(expectedTraceFlags), processed.TraceFlags)
+
+	// trace parser should work even if parse_from value is empty
+	testPipelines[0].Config[0].SpanId.ParseFrom = ""
+	result, collectorWarnAndErrorLogs, err = SimulatePipelinesProcessing(
+		context.Background(),
+		testPipelines,
+		[]model.SignozLog{
+			testLog,
+		},
+	)
+	require.Nil(err)
+	require.Equal(1, len(result))
+	require.Equal(0, len(collectorWarnAndErrorLogs))
+	require.Equal("", result[0].SpanID)
 }
