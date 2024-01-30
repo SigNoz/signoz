0225548a785fca11b7df5d0055acbba8e9abc183
Rajat Dabade <rajat@signoz.io>
2024-01-30 16:45:43 +0530

    [Feat]: View in Traces (#4450)
    * refactor: datetime selector beside run query removed add to dashboard
    
    * refactor: added tab for traces view details page
    
    * refactor: done with the save view in traces
    
    * fix: the gittery effect when navigatigating from views
    
    * refactor: view tab view title light mode support
    
    * refactor: removed console
    
    * fix: gittery effect when switch view from views tabs
    
    * refactor: separate traces routes
    
    * refactor: remove query params

--- frontend/src/pages/LogsModulePage/LogsModulePage.tsx
+++ frontend/src/pages/LogsModulePage/LogsModulePage.tsx
@@ -1,56 +1,20 @@
 import './LogsModulePage.styles.scss';
 
 import RouteTab from 'components/RouteTab';
-import ROUTES from 'constants/routes';
+import { TabRoutes } from 'components/RouteTab/types';
 import history from 'lib/history';
-import { Compass, TowerControl, Workflow } from 'lucide-react';
-import LogsExplorer from 'pages/LogsExplorer';
-import Pipelines from 'pages/Pipelines';
-import SaveView from 'pages/SaveView';
 import { useLocation } from 'react-use';
-import { DataSource } from 'types/common/queryBuilder';
 
-export const logsExplorer = {
-	Component: LogsExplorer,
-	name: (
-		<div className="tab-item">
-			<Compass size={16} /> Explorer
-		</div>
-	),
-	route: ROUTES.LOGS,
-	key: ROUTES.LOGS,
-};
-
-export const logsPipelines = {
-	Component: Pipelines,
-	name: (
-		<div className="tab-item">
-			<Workflow size={16} /> Pipelines
-		</div>
-	),
-	route: ROUTES.LOGS_PIPELINES,
-	key: ROUTES.LOGS_PIPELINES,
-};
-
-export const logSaveView = {
-	Component: SaveView,
-	name: (
-		<div className="tab-item">
-			<TowerControl size={16} /> Views
-		</div>
-	),
-	route: `${ROUTES.SAVE_VIEWS}?sourcepage=${DataSource.LOGS}`,
-	key: ROUTES.SAVE_VIEWS,
-};
+import { logSaveView, logsExplorer, logsPipelines } from './constants';
 
 export default function LogsModulePage(): JSX.Element {
 	const { pathname } = useLocation();
 
-	const routes = [logsExplorer, logsPipelines, logSaveView];
+	const routes: TabRoutes[] = [logsExplorer, logsPipelines, logSaveView];
 
 	return (
 		<div className="logs-module-container">
-			<RouteTab routes={routes} activeKey={pathname} history={history} />
+			<RouteTab routes={routes} activeKey={pathname} history={history} />;
 		</div>
 	);
 }
