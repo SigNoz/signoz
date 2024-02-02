3b4fb3eb8098cc07d36ef35ede162fac5b418148
Yunus M <myounis.ar@live.com>
2024-02-02 14:52:28 +0530

    UI feedback updates (#4482)
    * feat: handle loading and fix ui issues
    
    * feat: ui updates

--- frontend/src/container/LogsExplorerList/LogsExplorerList.interfaces.ts
+++ frontend/src/container/LogsExplorerList/LogsExplorerList.interfaces.ts
@@ -4,6 +4,7 @@ import { IBuilderQuery } from 'types/api/queryBuilder/queryBuilderData';
 export type LogsExplorerListProps = {
 	isLoading: boolean;
 	isFetching: boolean;
+	isDataFetched: boolean;
 	currentStagedQueryData: IBuilderQuery | null;
 	logs: ILog[];
 	onEndReached: (index: number) => void;
