c797d28e06aef3791103fad3c9ec9d522a9d3a05
Vikrant Gupta <vikrant.thomso@gmail.com>
2024-02-05 14:28:38 +0530

    feat: setup the context for keyboard hotkeys

--- frontend/src/AppRoutes/index.tsx
+++ frontend/src/AppRoutes/index.tsx
@@ -8,6 +8,7 @@ import { LOCALSTORAGE } from 'constants/localStorage';
 import ROUTES from 'constants/routes';
 import AppLayout from 'container/AppLayout';
 import useAnalytics from 'hooks/analytics/useAnalytics';
+import { KeyboardHotkeysProvider } from 'hooks/hotkeys/useKeyboardHotkeys';
 import { useThemeConfig } from 'hooks/useDarkMode';
 import useGetFeatureFlag from 'hooks/useGetFeatureFlag';
 import useLicense, { LICENSE_PLAN_KEY } from 'hooks/useLicense';
@@ -177,22 +178,24 @@ function App(): JSX.Element {
 						<ResourceProvider>
 							<QueryBuilderProvider>
 								<DashboardProvider>
-									<AppLayout>
-										<Suspense fallback={<Spinner size="large" tip="Loading..." />}>
-											<Switch>
-												{routes.map(({ path, component, exact }) => (
-													<Route
-														key={`${path}`}
-														exact={exact}
-														path={path}
-														component={component}
-													/>
-												))}
+									<KeyboardHotkeysProvider>
+										<AppLayout>
+											<Suspense fallback={<Spinner size="large" tip="Loading..." />}>
+												<Switch>
+													{routes.map(({ path, component, exact }) => (
+														<Route
+															key={`${path}`}
+															exact={exact}
+															path={path}
+															component={component}
+														/>
+													))}
 
-												<Route path="*" component={NotFound} />
-											</Switch>
-										</Suspense>
-									</AppLayout>
+													<Route path="*" component={NotFound} />
+												</Switch>
+											</Suspense>
+										</AppLayout>
+									</KeyboardHotkeysProvider>
 								</DashboardProvider>
 							</QueryBuilderProvider>
 						</ResourceProvider>
---
+++ frontend/src/hooks/hotkeys/useKeyboardHotkeys.tsx
@@ -0,0 +1,99 @@
+import { noop } from 'lodash-es';
+import {
+	createContext,
+	useCallback,
+	useContext,
+	useEffect,
+	useMemo,
+	useRef,
+} from 'react';
+
+interface KeyboardHotkeysContextReturnValue {
+	registerShortcut: (keyCombination: string, callback: () => void) => void;
+	deregisterShortcut: (keyCombination: string) => void;
+}
+
+const KeyboardHotkeysContext = createContext<KeyboardHotkeysContextReturnValue>(
+	{
+		registerShortcut: noop,
+		deregisterShortcut: noop,
+	},
+);
+
+const IGNORE_INPUTS = ['input', 'textarea']; // Inputs in which hotkey events will be ignored
+
+const useKeyboardHotkeys = (): KeyboardHotkeysContextReturnValue => {
+	const context = useContext(KeyboardHotkeysContext);
+	if (!context) {
+		throw new Error(
+			'useKeyboardHotkeys must be used within a KeyboardHotkeysProvider',
+		);
+	}
+
+	return context;
+};
+
+function KeyboardHotkeysProvider({
+	children,
+}: {
+	children: JSX.Element;
+}): JSX.Element {
+	const shortcuts = useRef<Record<string, () => void>>({});
+
+	const handleKeyPress = (event: KeyboardEvent): void => {
+		const { key, ctrlKey, altKey, shiftKey, metaKey, target } = event;
+
+		if (IGNORE_INPUTS.includes((target as HTMLElement).tagName.toLowerCase())) {
+			return;
+		}
+		const modifiers = { ctrlKey, altKey, shiftKey, metaKey };
+		const shortcutKey = `${key}${modifiers.ctrlKey ? '+ctrl' : ''}${
+			modifiers.altKey ? '+alt' : ''
+		}${modifiers.shiftKey ? '+shift' : ''}${modifiers.metaKey ? '+meta' : ''}`;
+
+		if (shortcuts.current[shortcutKey]) {
+			shortcuts.current[shortcutKey]();
+		}
+	};
+
+	useEffect(() => {
+		document.addEventListener('keydown', handleKeyPress);
+		return (): void => {
+			document.removeEventListener('keydown', handleKeyPress);
+		};
+	}, []);
+
+	const registerShortcut = useCallback(
+		(keyCombination: string, callback: () => void): void => {
+			if (!shortcuts.current[keyCombination]) {
+				shortcuts.current[keyCombination] = callback;
+			}
+		},
+		[shortcuts],
+	);
+
+	const deregisterShortcut = useCallback(
+		(keyCombination: string): void => {
+			if (shortcuts.current[keyCombination]) {
+				delete shortcuts.current[keyCombination];
+			}
+		},
+		[shortcuts],
+	);
+
+	const contextValue = useMemo(
+		() => ({
+			registerShortcut,
+			deregisterShortcut,
+		}),
+		[registerShortcut, deregisterShortcut],
+	);
+
+	return (
+		<KeyboardHotkeysContext.Provider value={contextValue}>
+			{children}
+		</KeyboardHotkeysContext.Provider>
+	);
+}
+
+export { KeyboardHotkeysProvider, useKeyboardHotkeys };
