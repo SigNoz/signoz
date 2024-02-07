cac45dc8632e095c39bc6a5f6b4b7b365c9d6ccb
Vikrant Gupta <vikrant.thomso@gmail.com>
2024-02-07 12:19:10 +0530

    fix: cmd+enter should stage and run query

--- frontend/src/container/QueryBuilder/components/ToolbarActions/RightToolbarActions.tsx
+++ frontend/src/container/QueryBuilder/components/ToolbarActions/RightToolbarActions.tsx
@@ -22,7 +22,7 @@ export default function RightToolbarActions({
 			deregisterShortcut(LogsExplorerShortcuts.StageAndRunQuery);
 		};
 		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
+	}, [onStageRunQuery]);
 	return (
 		<div>
 			<Button
