294b6ae01c5452894765ae50c4dbccca34a5020e
Rajat-Dabade <rajat@signoz.io>
2024-02-12 12:23:52 +0530

    refactor: icons change for apdd panels

--- frontend/src/assets/Dashboard/List.tsx
+++ frontend/src/assets/Dashboard/List.tsx
@@ -2,8 +2,8 @@ function List(): JSX.Element {
 	return (
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
-			width="24"
-			height="24"
+			width="30"
+			height="30"
 			viewBox="0 0 24 24"
 			fill="none"
 			stroke="currentColor"
