5f02c966c4a5ce2a5af449948c6c839c5f76ca62
Vikrant Gupta <vikrant.thomso@gmail.com>
2024-02-14 11:58:51 +0530

    feat: support zoom in for bar chart logs explorer

--- frontend/src/container/LogsExplorerChart/index.tsx
+++ frontend/src/container/LogsExplorerChart/index.tsx
@@ -1,9 +1,16 @@
 import Graph from 'components/Graph';
 import Spinner from 'components/Spinner';
+import { QueryParams } from 'constants/query';
 import { themeColors } from 'constants/theme';
+import useUrlQuery from 'hooks/useUrlQuery';
 import getChartData, { GetChartDataProps } from 'lib/getChartData';
+import GetMinMax from 'lib/getMinMax';
 import { colors } from 'lib/getRandomColor';
+import history from 'lib/history';
 import { memo, useCallback, useMemo } from 'react';
+import { useDispatch } from 'react-redux';
+import { useLocation } from 'react-router-dom';
+import { UpdateTimeInterval } from 'store/actions';
 
 import { LogsExplorerChartProps } from './LogsExplorerChart.interfaces';
 import { CardStyled } from './LogsExplorerChart.styled';
@@ -14,6 +21,9 @@ function LogsExplorerChart({
 	isLabelEnabled = true,
 	className,
 }: LogsExplorerChartProps): JSX.Element {
+	const dispatch = useDispatch();
+	const urlQuery = useUrlQuery();
+	const location = useLocation();
 	const handleCreateDatasets: Required<GetChartDataProps>['createDataset'] = useCallback(
 		(element, index, allLabels) => ({
 			data: element,
@@ -26,6 +36,28 @@ function LogsExplorerChart({
 				: {}),
 		}),
 		[isLabelEnabled],
+	);
+
+	const onDragSelect = useCallback(
+		(start: number, end: number): void => {
+			const startTimestamp = Math.trunc(start);
+			const endTimestamp = Math.trunc(end);
+
+			if (startTimestamp !== endTimestamp) {
+				dispatch(UpdateTimeInterval('custom', [startTimestamp, endTimestamp]));
+			}
+
+			const { maxTime, minTime } = GetMinMax('custom', [
+				startTimestamp,
+				endTimestamp,
+			]);
+
+			urlQuery.set(QueryParams.startTime, minTime.toString());
+			urlQuery.set(QueryParams.endTime, maxTime.toString());
+			const generatedUrl = `${location.pathname}?${urlQuery.toString()}`;
+			history.push(generatedUrl);
+		},
+		[dispatch, location.pathname, urlQuery],
 	);
 
 	const graphData = useMemo(
@@ -46,7 +78,13 @@ function LogsExplorerChart({
 			{isLoading ? (
 				<Spinner size="default" height="100%" />
 			) : (
-				<Graph name="logsExplorerChart" data={graphData.data} type="bar" animate />
+				<Graph
+					name="logsExplorerChart"
+					data={graphData.data}
+					type="bar"
+					animate
+					onDragSelect={onDragSelect}
+				/>
 			)}
 		</CardStyled>
 	);
