3de05b9b213c30219137e5a58a6acd9300c80605
Vikrant Gupta <vikrant.thomso@gmail.com>
2024-02-15 23:51:11 +0530

    chore: added test for rendering of logs explorer page without API calls

--- frontend/src/pages/LogsExplorer/__tests__/LogsExplorer.test.tsx
+++ frontend/src/pages/LogsExplorer/__tests__/LogsExplorer.test.tsx
@@ -1,4 +1,5 @@
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { QueryBuilderProvider } from 'providers/QueryBuilder';
 import MockQueryClientProvider from 'providers/test/MockQueryClientProvider';
 import { I18nextProvider } from 'react-i18next';
@@ -9,12 +10,21 @@ import store from 'store';
 
 import LogsExplorer from '..';
 
+// mocking the graph components in this test as this should be handled separately
 jest.mock(
 	'container/TimeSeriesView/TimeSeriesView',
 	() =>
 		// eslint-disable-next-line func-names, @typescript-eslint/explicit-function-return-type, react/display-name
 		function () {
 			return <div>Time Series Chart</div>;
+		},
+);
+jest.mock(
+	'container/LogsExplorerChart',
+	() =>
+		// eslint-disable-next-line func-names, @typescript-eslint/explicit-function-return-type, react/display-name
+		function () {
+			return <div>Histogram Chart</div>;
 		},
 );
 
@@ -26,19 +36,16 @@ jest.mock('d3-interpolate', () => ({
 	interpolate: jest.fn(),
 }));
 
-jest.mock(
-	'container/LogsExplorerChart',
-	() =>
-		// eslint-disable-next-line func-names, @typescript-eslint/explicit-function-return-type, react/display-name
-		function () {
-			return <div>Histogram</div>;
-		},
-);
-
 describe('Logs Explorer Tests', () => {
-	test('Logs Explorer default view test', () => {
-		const { container } = render(
-			<MemoryRouter>
+	test('Logs Explorer default view test without data', async () => {
+		const {
+			getByText,
+			getByRole,
+			queryByText,
+			getByTestId,
+			queryByTestId,
+		} = render(
+			<MemoryRouter initialEntries={['/logs/logs-explorer']}>
 				<Provider store={store}>
 					<I18nextProvider i18n={i18n}>
 						<MockQueryClientProvider>
@@ -51,6 +58,32 @@ describe('Logs Explorer Tests', () => {
 			</MemoryRouter>,
 		);
 
-		expect(container).toMatchSnapshot();
+		// check the presence of histogram chart
+		expect(getByText('Histogram Chart')).toBeInTheDocument();
+
+		// toggle the chart and check it gets removed from the DOM
+		const histogramToggle = getByRole('switch');
+		await userEvent.click(histogramToggle);
+		expect(queryByText('Histogram Chart')).not.toBeInTheDocument();
+
+		// check the presence of search bar and query builder and absence of clickhouse
+		const searchView = getByTestId('search-view');
+		expect(searchView).toBeInTheDocument();
+		const queryBuilderView = getByTestId('query-builder-view');
+		expect(queryBuilderView).toBeInTheDocument();
+		const clickhouseView = queryByTestId('clickhouse-view');
+		expect(clickhouseView).not.toBeInTheDocument();
+
+		// check the presence of List View / Time Series View / Table View
+		const listView = getByTestId('logs-list-view');
+		const timeSeriesView = getByTestId('time-series-view');
+		const tableView = getByTestId('table-view');
+		expect(listView).toBeInTheDocument();
+		expect(timeSeriesView).toBeInTheDocument();
+		expect(tableView).toBeInTheDocument();
+
+		// check the presence of old logs explorer CTA
+		const oldLogsCTA = getByText('Switch to Old Logs Explorer');
+		expect(oldLogsCTA).toBeInTheDocument();
 	});
 });
