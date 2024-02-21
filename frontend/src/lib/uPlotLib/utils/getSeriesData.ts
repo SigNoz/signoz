b5e077604ccaa453ab794a205022ba56724c793d
Rajat-Dabade <rajat@signoz.io>
2024-02-21 10:53:48 +0530

    refactor: opacity of the fill color

--- frontend/src/lib/uPlotLib/utils/getSeriesData.ts
+++ frontend/src/lib/uPlotLib/utils/getSeriesData.ts
@@ -65,7 +65,7 @@ const getSeries = ({
 					: lineInterpolations.spline,
 			show: newGraphVisibilityStates ? newGraphVisibilityStates[i] : true,
 			label,
-			fill: panelType && panelType === PANEL_TYPES.BAR ? color : undefined,
+			fill: panelType && panelType === PANEL_TYPES.BAR ? `${color}40` : undefined,
 			stroke: color,
 			width: 2,
 			spanGaps: true,
