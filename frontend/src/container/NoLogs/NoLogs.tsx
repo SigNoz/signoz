f9eddc9b18f6dc7ba9aebfca690876688da2b6ab
Yunus M <myounis.ar@live.com>
2024-02-26 12:09:31 +0530

    fix: update no logs text and link based on the datasource (#4594)

--- frontend/src/container/NoLogs/NoLogs.tsx
+++ frontend/src/container/NoLogs/NoLogs.tsx
@@ -2,25 +2,31 @@ import './NoLogs.styles.scss';
 
 import { Typography } from 'antd';
 import { ArrowUpRight } from 'lucide-react';
+import { DataSource } from 'types/common/queryBuilder';
 
-export default function NoLogs(): JSX.Element {
+export default function NoLogs({
+	dataSource,
+}: {
+	dataSource: DataSource;
+}): JSX.Element {
 	return (
 		<div className="no-logs-container">
 			<div className="no-logs-container-content">
 				<img className="eyes-emoji" src="/Images/eyesEmoji.svg" alt="eyes emoji" />
 				<Typography className="no-logs-text">
-					No logs yet.
+					No {dataSource} yet.
 					<span className="sub-text">
-						When we receive logs, they would show up here
+						{' '}
+						When we receive {dataSource}, they would show up here
 					</span>
 				</Typography>
 
 				<Typography.Link
 					className="send-logs-link"
-					href="https://signoz.io/docs/userguide/logs/"
+					href={`https://signoz.io/docs/userguide/${dataSource}/`}
 					target="_blank"
 				>
-					Sending Logs to SigNoz <ArrowUpRight size={16} />
+					Sending {dataSource} to SigNoz <ArrowUpRight size={16} />
 				</Typography.Link>
 			</div>
 		</div>
