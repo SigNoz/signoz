eb4d37dc87d41ea2279d1608eab2663780bfab61
Vikrant Gupta <vikrant.thomso@gmail.com>
2024-02-28 01:42:33 +0530

    feat: added remove integration modal

--- frontend/src/pages/Integrations/IntegrationDetailPage/IntegrationDetailPage.tsx
+++ frontend/src/pages/Integrations/IntegrationDetailPage/IntegrationDetailPage.tsx
@@ -5,6 +5,7 @@ import { ArrowLeft } from 'lucide-react';
 
 import IntegrationDetailContent from './IntegrationDetailContent';
 import IntegrationDetailHeader from './IntegrationDetailHeader';
+import IntergrationsUninstallBar from './IntegrationsUninstallBar';
 
 interface IntegrationDetailPageProps {
 	selectedIntegration: string;
@@ -31,8 +32,9 @@ function IntegrationDetailPage(props: IntegrationDetailPageProps): JSX.Element {
 				description="Redis is an open source (BSD licensed), in-memory data structure store, used as a database, cache, and message broker."
 				icon="/Icons/redis-logo.svg"
 			/>
-
 			<IntegrationDetailContent />
+
+			<IntergrationsUninstallBar integrationTitle="Redis" />
 		</div>
 	);
 }
---
+++ frontend/src/pages/Integrations/IntegrationDetailPage/IntegrationsUninstallBar.tsx
@@ -0,0 +1,65 @@
+import './IntegrationDetailPage.styles.scss';
+
+import { Button, Modal, Typography } from 'antd';
+import { X } from 'lucide-react';
+import { useState } from 'react';
+
+interface IntergrationsUninstallBarProps {
+	integrationTitle: string;
+}
+function IntergrationsUninstallBar(
+	props: IntergrationsUninstallBarProps,
+): JSX.Element {
+	const { integrationTitle } = props;
+	const [isModalOpen, setIsModalOpen] = useState(false);
+
+	const showModal = (): void => {
+		setIsModalOpen(true);
+	};
+
+	const handleOk = (): void => {
+		setIsModalOpen(false);
+	};
+
+	const handleCancel = (): void => {
+		setIsModalOpen(false);
+	};
+	return (
+		<div className="uninstall-integration-bar">
+			<div className="unintall-integration-bar-text">
+				<Typography.Text className="heading">Remove Integration</Typography.Text>
+				<Typography.Text className="subtitle">
+					Removing the {integrationTitle} integration would make your workspace stop
+					listening for data from Redis instances.
+				</Typography.Text>
+			</div>
+			<Button
+				className="uninstall-integration-btn"
+				icon={<X size={14} />}
+				onClick={(): void => showModal()}
+			>
+				Remove from SigNoz
+			</Button>
+			<Modal
+				className="remove-integration-modal"
+				open={isModalOpen}
+				title="Remove integration"
+				onOk={handleOk}
+				onCancel={handleCancel}
+				okText="Remove Integration"
+				okButtonProps={{
+					danger: true,
+				}}
+			>
+				<Typography.Text>
+					Removing this integration makes SigNoz stop listening for data from
+					{integrationTitle}
+					instances. Youd still have to manually remove the configuration in your
+					code to stop sending data.
+				</Typography.Text>
+			</Modal>
+		</div>
+	);
+}
+
+export default IntergrationsUninstallBar;
