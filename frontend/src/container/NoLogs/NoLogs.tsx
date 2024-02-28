d399963e03c9b5083517d2a797d1480f252e9d5b
Vikrant Gupta <vikrant.thomso@gmail.com>
2024-02-28 17:43:47 +0530

    chore: handle cloud user

--- frontend/src/container/NoLogs/NoLogs.tsx
+++ frontend/src/container/NoLogs/NoLogs.tsx
@@ -5,22 +5,29 @@ import ROUTES from 'constants/routes';
 import history from 'lib/history';
 import { ArrowUpRight } from 'lucide-react';
 import { DataSource } from 'types/common/queryBuilder';
+import { isCloudUser } from 'utils/app';
 
 export default function NoLogs({
 	dataSource,
 }: {
 	dataSource: DataSource;
 }): JSX.Element {
+	const cloudUser = isCloudUser();
 	const handleLinkClick = (
 		e: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
 	): void => {
 		e.preventDefault();
 		e.stopPropagation();
-		history.push(
-			dataSource === 'traces'
-				? ROUTES.GET_STARTED_APPLICATION_MONITORING
-				: ROUTES.GET_STARTED_LOGS_MANAGEMENT,
-		);
+
+		if (cloudUser) {
+			history.push(
+				dataSource === 'traces'
+					? ROUTES.GET_STARTED_APPLICATION_MONITORING
+					: ROUTES.GET_STARTED_LOGS_MANAGEMENT,
+			);
+		} else {
+			window.open(`https://signoz.io/docs/userguide/${dataSource}/`, '_blank');
+		}
 	};
 	return (
 		<div className="no-logs-container">
@@ -34,12 +41,7 @@ export default function NoLogs({
 					</span>
 				</Typography>
 
-				<Typography.Link
-					className="send-logs-link"
-					href={`https://signoz.io/docs/userguide/${dataSource}/`}
-					target="_blank"
-					onClick={handleLinkClick}
-				>
+				<Typography.Link className="send-logs-link" onClick={handleLinkClick}>
 					Sending {dataSource} to SigNoz <ArrowUpRight size={16} />
 				</Typography.Link>
 			</div>
