a8e565073f5134fa0a770f71e76007ce64a83974
Vikrant Gupta <vikrant.thomso@gmail.com>
2024-02-28 11:42:13 +0530

    feat: added test connection bars

--- frontend/src/pages/Integrations/IntegrationDetailPage/IntegrationDetailHeader.tsx
+++ frontend/src/pages/Integrations/IntegrationDetailPage/IntegrationDetailHeader.tsx
@@ -3,6 +3,8 @@ import './IntegrationDetailPage.styles.scss';
 import { Button, Typography } from 'antd';
 import { ArrowLeftRight } from 'lucide-react';
 
+import TestConnection from './TestConnection';
+
 interface IntegrationDetailHeaderProps {
 	id: string;
 	title: string;
@@ -14,17 +16,20 @@ function IntegrationDetailHeader(
 ): JSX.Element {
 	const { id, title, icon, description } = props;
 	return (
-		<div className="integration-detail-header" key={id}>
-			<div className="image-container">
-				<img src={icon} alt={title} className="image" />
+		<div className="integration-connection-header">
+			<div className="integration-detail-header" key={id}>
+				<div className="image-container">
+					<img src={icon} alt={title} className="image" />
+				</div>
+				<div className="details">
+					<Typography.Text className="heading">{title}</Typography.Text>
+					<Typography.Text className="description">{description}</Typography.Text>
+				</div>
+				<Button className="configure-btn" icon={<ArrowLeftRight size={14} />}>
+					Connect {title}
+				</Button>
 			</div>
-			<div className="details">
-				<Typography.Text className="heading">{title}</Typography.Text>
-				<Typography.Text className="description">{description}</Typography.Text>
-			</div>
-			<Button className="configure-btn" icon={<ArrowLeftRight size={14} />}>
-				Connect {title}
-			</Button>
+			<TestConnection />
 		</div>
 	);
 }
---
+++ frontend/src/pages/Integrations/IntegrationDetailPage/TestConnection.tsx
@@ -0,0 +1,35 @@
+import './IntegrationDetailPage.styles.scss';
+
+import cx from 'classnames';
+import { useState } from 'react';
+
+enum ConnectionStates {
+	Connected = 'connected',
+	TestingConnection = 'testingConnection',
+	ConnectionFailed = 'connectionFailed',
+	NoDataSinceLong = 'noDataSinceLong',
+}
+
+const ConnectionStatesLabelMap = {
+	[ConnectionStates.Connected]: 'This integration is working properly',
+	[ConnectionStates.TestingConnection]: 'Listening for data...',
+	[ConnectionStates.ConnectionFailed]:
+		'There is some issue with this integration :/',
+	[ConnectionStates.NoDataSinceLong]:
+		'This integration has not received data in a while :/',
+};
+
+function TestConnection(): JSX.Element {
+	const [connectionState] = useState<ConnectionStates>(
+		ConnectionStates.TestingConnection,
+	);
+	return (
+		<div className={cx('connection-container', connectionState)}>
+			<ul className="connection-text">
+				<li>{ConnectionStatesLabelMap[connectionState]}</li>
+			</ul>
+		</div>
+	);
+}
+
+export default TestConnection;
