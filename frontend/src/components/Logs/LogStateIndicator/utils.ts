3509f8d14d4b3ef74da25d6d4e813e9d040414c1
Rajat-Dabade <rajat@signoz.io>
2024-03-02 10:10:45 +0530

    refactor: done with setup

--- frontend/src/components/Logs/LogStateIndicator/utils.ts
+++ frontend/src/components/Logs/LogStateIndicator/utils.ts
@@ -2,8 +2,8 @@ import { ILog } from 'types/api/logs/log';
 
 import { LogType, SEVERITY_TEXT_TYPE } from './LogStateIndicator';
 
-export const getIndicatorType = (logData: ILog): string => {
-	if (logData.severityText !== '') {
+export const getLogIndicatorType = (logData: ILog): string => {
+	if (logData.severityText) {
 		switch (logData.severityText) {
 			case SEVERITY_TEXT_TYPE.TRACE:
 				return SEVERITY_TEXT_TYPE.TRACE;
@@ -22,5 +22,30 @@ export const getIndicatorType = (logData: ILog): string => {
 		}
 	} else {
 		return logData.attributes_string.log_level || LogType.INFO;
+	}
+};
+
+export const getLogIndicatorTypeForTable = (
+	log: Record<string, unknown>,
+): string => {
+	if (log.severityText) {
+		switch (log.severityText) {
+			case SEVERITY_TEXT_TYPE.TRACE:
+				return SEVERITY_TEXT_TYPE.TRACE;
+			case SEVERITY_TEXT_TYPE.DEBUG:
+				return SEVERITY_TEXT_TYPE.DEBUG;
+			case SEVERITY_TEXT_TYPE.INFO:
+				return SEVERITY_TEXT_TYPE.INFO;
+			case SEVERITY_TEXT_TYPE.WARN:
+				return SEVERITY_TEXT_TYPE.WARN;
+			case SEVERITY_TEXT_TYPE.ERROR:
+				return SEVERITY_TEXT_TYPE.ERROR;
+			case SEVERITY_TEXT_TYPE.FATAL:
+				return SEVERITY_TEXT_TYPE.FATAL;
+			default:
+				return SEVERITY_TEXT_TYPE.INFO;
+		}
+	} else {
+		return (log.log_level as string) || LogType.INFO;
 	}
 };
