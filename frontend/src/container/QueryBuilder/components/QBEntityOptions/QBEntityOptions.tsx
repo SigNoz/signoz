2cd3287d677742b84c0df3a344a977a67674d5bc
Yunus M <myounis.ar@live.com>
2024-03-06 11:18:50 +0530

    feat: update ui

--- frontend/src/container/QueryBuilder/components/QBEntityOptions/QBEntityOptions.tsx
+++ frontend/src/container/QueryBuilder/components/QBEntityOptions/QBEntityOptions.tsx
@@ -1,10 +1,18 @@
+/* eslint-disable sonarjs/no-duplicate-string */
 import './QBEntityOptions.styles.scss';
 
-import { Button } from 'antd';
+import { Button, Col, Tooltip } from 'antd';
 import { noop } from 'antd/lib/_util/warning';
 import cx from 'classnames';
 import { isFunction } from 'lodash-es';
-import { ChevronDown, ChevronRight, Eye, EyeOff, Trash2 } from 'lucide-react';
+import {
+	ChevronDown,
+	ChevronRight,
+	Copy,
+	Eye,
+	EyeOff,
+	Trash2,
+} from 'lucide-react';
 import {
 	IBuilderQuery,
 	QueryFunctionProps,
@@ -50,62 +58,55 @@ export default function QBEntityOptions({
 	};
 
 	return (
-		<div className="qb-entity-options">
-			<div className="left-col-items">
-				<div className="options periscope-btn-group">
-					<Button.Group>
-						<Button
-							value="search"
-							className="periscope-btn collapse"
-							onClick={onCollapseEntity}
-						>
-							{isCollapsed ? <ChevronRight size={16} /> : <ChevronDown size={16} />}
-						</Button>
-						<Button
-							value="query-builder"
-							className="periscope-btn visibility-toggle"
-							onClick={onToggleVisibility}
-							disabled={isListViewPanel}
-						>
-							{entityData.disabled ? <EyeOff size={16} /> : <Eye size={16} />}
-						</Button>
+		<Col span={24}>
+			<div className="qb-entity-options">
+				<div className="left-col-items">
+					<div className="options periscope-btn-group">
+						<Button.Group>
+							<Button
+								value="search"
+								className="periscope-btn collapse"
+								onClick={onCollapseEntity}
+							>
+								{isCollapsed ? <ChevronRight size={16} /> : <ChevronDown size={16} />}
+							</Button>
+							<Button
+								value="query-builder"
+								className="periscope-btn visibility-toggle"
+								onClick={onToggleVisibility}
+								disabled={isListViewPanel}
+							>
+								{entityData.disabled ? <EyeOff size={16} /> : <Eye size={16} />}
+							</Button>
 
-						{entityType === 'query' && (
-							<Tooltip title="Clone Query">
-								<Button className={cx('periscope-btn')} onClick={handleCloneEntity}>
-									<Copy size={14} />
-								</Button>
-							</Tooltip>
-						)}
-
-						<Button
-							className={cx(
-								'periscope-btn',
-								entityType === 'query' ? 'query-name' : 'formula-name',
+							{entityType === 'query' && (
+								<Tooltip title={`Clone Query ${entityData.queryName}`}>
+									<Button className={cx('periscope-btn')} onClick={handleCloneEntity}>
+										<Copy size={14} />
+									</Button>
+								</Tooltip>
 							)}
-						>
-							{entityData.queryName}
-						</Button>
 
-						<Button
-							className={cx(
-								'periscope-btn',
-								entityType === 'query' ? 'query-name' : 'formula-name',
-							)}
-						>
-							{entityData.queryName}
-						</Button>
+							<Button
+								className={cx(
+									'periscope-btn',
+									entityType === 'query' ? 'query-name' : 'formula-name',
+								)}
+							>
+								{entityData.queryName}
+							</Button>
 
-						{showFunctions &&
-							isMetricsDataSource &&
-							query &&
-							onQueryFunctionsUpdates && (
-								<QueryFunctions
-									queryFunctions={query.functions}
-									onChange={onQueryFunctionsUpdates}
-								/>
-							)}
-					</Button.Group>
+							{showFunctions &&
+								isMetricsDataSource &&
+								query &&
+								onQueryFunctionsUpdates && (
+									<QueryFunctions
+										queryFunctions={query.functions}
+										onChange={onQueryFunctionsUpdates}
+									/>
+								)}
+						</Button.Group>
+					</div>
 
 					{isCollapsed && (
 						<div className="title">
@@ -113,15 +114,15 @@ export default function QBEntityOptions({
 							<span className="entityData"> {entityData.queryName} </span>
 						</div>
 					)}
-
-					{showDeleteButton && (
-						<Button className="periscope-btn ghost" onClick={onDelete}>
-							<Trash2 size={14} />
-						</Button>
-					)}
 				</div>
+
+				{showDeleteButton && (
+					<Button className="periscope-btn ghost" onClick={onDelete}>
+						<Trash2 size={14} />
+					</Button>
+				)}
 			</div>
-		</div>
+		</Col>
 	);
 }
 
