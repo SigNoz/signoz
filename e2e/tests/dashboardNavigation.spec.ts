40c585c725320ba1d39a9e67bb5fd9109b8d8858
Vikrant Gupta <vikrant.thomso@gmail.com>
2024-03-11 23:47:49 +0530

    feat: added check to open the dashboard list page and wait for first dashboard to load

--- e2e/tests/dashboardNavigation.spec.ts
+++ e2e/tests/dashboardNavigation.spec.ts
@@ -27,26 +27,6 @@ test("Check for the dashboard page and individual dashboard to load within 5s", 
 
   await expect(firstDashboardRow).toBeVisible();
 
-  const dashboardQuery = page.waitForResponse("**/v1/dashboards/**", {
-    timeout: 5000,
-  });
-
-  const variablesQuery = page.waitForResponse("**/v2/variables/query", {
-    timeout: 5000,
-  });
-
-  const queryRangeAPI = page.waitForResponse("**/v4/query_range", {
-    timeout: 5000,
-  });
-
-  await firstDashboardRow.click();
-
-  await Promise.all([
-    await dashboardQuery,
-    await variablesQuery,
-    await queryRangeAPI,
-  ]);
-
   // wait for all the API calls to succeed on first load
   await page.waitForLoadState("networkidle", { timeout: 5000 });
 });
