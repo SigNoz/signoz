44dc55c5acbe9c5fed278e1d319d117e1193650e
Rajat Dabade <rajat@signoz.io>
2024-03-13 17:37:48 +0530

    FE: Design update for log context tab (#4601)
    * refactor: initial setup for context view
    
    * refactor: updated design for log context view
    
    * refactor: updated comments and remove commented code
    
    * refactor: updated comments
    
    * refactor: handle hight issue
    
    * refactor: initial setup for context view
    
    * refactor: updated design for log context view
    
    * refactor: updated comments and remove commented code
    
    * refactor: updated comments
    
    * refactor: handle hight issue
    
    * refactor: added api version
    
    * refactor: height set to parent height and remove unnessarry code
    
    * refactor: removed commented code
    
    ---------
    
    Co-authored-by: Yunus M <myounis.ar@live.com>

--- frontend/src/container/LogDetailedView/ContextView/ContextView.tsx
+++ frontend/src/container/LogDetailedView/ContextView/ContextView.tsx
@@ -1,10 +1,9 @@
 import './ContextView.styles.scss';
 
-import RawLogView from 'components/Logs/RawLogView';
-import LogsContextList from 'container/LogsContextList';
-import { ORDERBY_FILTERS } from 'container/QueryBuilder/filters/OrderByFilter/config';
 import { ILog } from 'types/api/logs/log';
 import { Query, TagFilter } from 'types/api/queryBuilder/queryBuilderData';
+
+import ContextLogRenderer from './ContextLogRenderer';
 
 interface LogContextProps {
 	log: ILog;
@@ -24,28 +23,11 @@ function ContextView({
 
 	return (
 		<div className="log-context-container">
-			<LogsContextList
-				className="logs-context-list-asc"
-				order={ORDERBY_FILTERS.ASC}
+			<ContextLogRenderer
 				filters={filters}
-				isEdit={isEdit}
 				log={log}
 				query={contextQuery}
-			/>
-			<RawLogView
-				isActiveLog
-				isReadOnly
-				isTextOverflowEllipsisDisabled={false}
-				data={log}
-				linesPerRow={1}
-			/>
-			<LogsContextList
-				className="logs-context-list-desc"
-				order={ORDERBY_FILTERS.DESC}
-				filters={filters}
 				isEdit={isEdit}
-				log={log}
-				query={contextQuery}
 			/>
 		</div>
 	);
