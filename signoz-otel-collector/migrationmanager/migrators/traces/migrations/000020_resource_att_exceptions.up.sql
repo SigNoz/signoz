2c09b2d09b57ce3737695e8a6f8a8bf93081cb44
gaozhichao123 <zhichao.gao@sayweee.com>
2024-03-21 11:27:22 +0800

    feat: 修改sql配置

--- signoz-otel-collector/migrationmanager/migrators/traces/migrations/000020_resource_att_exceptions.up.sql
+++ signoz-otel-collector/migrationmanager/migrators/traces/migrations/000020_resource_att_exceptions.up.sql
@@ -1,7 +1,11 @@
 ALTER TABLE signoz_traces.signoz_error_index_v2 ON CLUSTER {{.SIGNOZ_CLUSTER}}
     ADD COLUMN IF NOT EXISTS resourceTagsMap Map(LowCardinality(String), String) CODEC(ZSTD(1)),
     ADD INDEX IF NOT EXISTS idx_resourceTagsMapKeys mapKeys(resourceTagsMap) TYPE bloom_filter(0.01) GRANULARITY 64,
-    ADD INDEX IF NOT EXISTS idx_resourceTagsMapValues mapValues(resourceTagsMap) TYPE bloom_filter(0.01) GRANULARITY 64;
+    ADD INDEX IF NOT EXISTS idx_resourceTagsMapValues mapValues(resourceTagsMap) TYPE bloom_filter(0.01) GRANULARITY 64,
+    ADD COLUMN demo Int8 DEFAULT 0;
+
 
 ALTER TABLE signoz_traces.distributed_signoz_error_index_v2 ON CLUSTER {{.SIGNOZ_CLUSTER}}
-    ADD COLUMN IF NOT EXISTS resourceTagsMap Map(LowCardinality(String), String) CODEC(ZSTD(1));
+    ADD COLUMN IF NOT EXISTS resourceTagsMap Map(LowCardinality(String), String) CODEC(ZSTD(1)),
+    ADD COLUMN demo Int8 DEFAULT 0;
+
