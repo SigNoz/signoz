5745727031fad9bcfe78e3f326ef3be552ae1a9e
Vikrant Gupta <vikrant.thomso@gmail.com>
2024-03-22 14:59:43 +0530

    fix: [SIG-565]: design feedback for integrations (#4723)
    * fix: [SIG-565]: design feedback for integrations
    
    * feat: added dotted line in the test connection modal
    
    * feat: handle the URL change for integration details page to support back navigation
    
    * feat: added ghost loading states
    
    * feat: added margin for details header
    
    * feat: added margin for details header
    
    * feat: increase the list sizes to 20
    
    * fix: handle icons
    
    * fix: remove unused classes

--- frontend/src/pages/Integrations/Integrations.tsx
+++ frontend/src/pages/Integrations/Integrations.tsx
@@ -1,17 +1,38 @@
 import './Integrations.styles.scss';
 
-import { useState } from 'react';
+import useUrlQuery from 'hooks/useUrlQuery';
+import { useCallback, useMemo, useState } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
 
 import Header from './Header';
 import IntegrationDetailPage from './IntegrationDetailPage/IntegrationDetailPage';
 import IntegrationsList from './IntegrationsList';
 
 function Integrations(): JSX.Element {
-	const [selectedIntegration, setSelectedIntegration] = useState<string | null>(
-		null,
+	const urlQuery = useUrlQuery();
+	const history = useHistory();
+	const location = useLocation();
+
+	const selectedIntegration = useMemo(() => urlQuery.get('integration'), [
+		urlQuery,
+	]);
+
+	const setSelectedIntegration = useCallback(
+		(integration: string | null) => {
+			if (integration) {
+				urlQuery.set('integration', integration);
+			} else {
+				urlQuery.set('integration', '');
+			}
+			const generatedUrl = `${location.pathname}?${urlQuery.toString()}`;
+			history.push(generatedUrl);
+		},
+		[history, location.pathname, urlQuery],
 	);
 
-	const [activeDetailTab, setActiveDetailTab] = useState<string | null>(null);
+	const [activeDetailTab, setActiveDetailTab] = useState<string | null>(
+		'overview',
+	);
 
 	const [searchTerm, setSearchTerm] = useState<string>('');
 	return (
