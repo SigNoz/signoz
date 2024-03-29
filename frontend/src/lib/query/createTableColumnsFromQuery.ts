bea1846ccb5bbd782d77be6026f96d6d9a1513a4
Rajat-Dabade <rajat@signoz.io>
2024-03-29 11:00:15 +0530

    refactor: isEqual instead of nested loops

--- frontend/src/lib/query/createTableColumnsFromQuery.ts
+++ frontend/src/lib/query/createTableColumnsFromQuery.ts
@@ -7,7 +7,7 @@ import {
 import { FORMULA_REGEXP } from 'constants/regExp';
 import { QUERY_TABLE_CONFIG } from 'container/QueryTable/config';
 import { QueryTableProps } from 'container/QueryTable/QueryTable.intefaces';
-import { isObject } from 'lodash-es';
+import { isEqual, isObject } from 'lodash-es';
 import { ReactNode } from 'react';
 import {
 	IBuilderFormula,
@@ -258,12 +258,7 @@ const findSeriaValueFromAnotherQuery = (
 		const localLabelEntries = Object.entries(seria.labels);
 		if (localLabelEntries.length !== labelEntries.length) return;
 
-		const isExistLabels = localLabelEntries.find(([key, value]) =>
-			labelEntries.find(
-				([currentKey, currentValue]) =>
-					currentKey === key && currentValue === value,
-			),
-		);
+		const isExistLabels = isEqual(localLabelEntries, labelEntries);
 
 		if (isExistLabels) {
 			value = seria;
