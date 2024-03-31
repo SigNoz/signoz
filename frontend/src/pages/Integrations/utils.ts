849c1c74619f8ea4c7e7afc8e9be2c1fc0edbdc5
Vikrant Gupta <vikrant.thomso@gmail.com>
2024-03-31 18:45:11 +0530

    feat: added telemetry constants and page view

--- frontend/src/pages/Integrations/utils.ts
+++ frontend/src/pages/Integrations/utils.ts
@@ -7,3 +7,15 @@ export const handleContactSupport = (isCloudUser: boolean): void => {
 		window.open('https://signoz.io/slack', '_blank');
 	}
 };
+
+export const INTEGRATION_TELEMETRY_EVENTS = {
+	INTEGRATIONS_ITEM_LIST_CLICKED: 'Integrations Page: Clicked an integration',
+	INTEGRATIONS_DETAIL_CONNECT:
+		'Integrations Detail Page: Connect integration button click',
+	INTEGRATIONS_DETAIL_TEST_CONNECTION:
+		'Integrations Detail Page: Test Connection button click for integration',
+	INTEGRATIONS_DETAIL_REMOVE_INTEGRATION:
+		'Integrations Detail Page: Remove Integration button click for integration',
+	INTEGRATIONS_DETAIL_CONFIGURE_INSTRUCTION:
+		'Integrations Detail Page: Navigate to configure an integration',
+};
