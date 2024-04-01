72f432b11c94a56957612f8d32e160be0601a1e1
Yunus A M <myounis.ar@live.com>
2024-04-01 15:46:59 +0530

    chore: remove hardcoded value

--- frontend/src/utils/app.ts
+++ frontend/src/utils/app.ts
@@ -15,8 +15,6 @@ export function extractDomain(email: string): string {
 export const isCloudUser = (): boolean => {
 	const { hostname } = window.location;
 
-	return true;
-
 	return hostname?.endsWith('signoz.cloud');
 };
 
