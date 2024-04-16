3a3b21c969b824b1d9d67469f00e8da17228e783
nityanandagohain <nityanandagohain@gmail.com>
2024-04-16 10:27:58 +0530

    feat: add goreplay

---
+++ ee/query-service/start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# turn on bash's job control
+set -m
+
+# Start the primary process and put it in the background
+./query-service &
+
+# # Start the helper process
+# ./my_helper_process
+
+# the my_helper_process might need to know how to wait on the
+# primary process to start before it does its work and returns
+
+
+# now we bring the primary process back into the foreground
+# and leave it there
+fg %1
