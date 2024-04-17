5890184f45574fa7b78fd0f567998ec00a41515e
Yunus A M <myounis.ar@live.com>
2024-04-18 00:21:57 +0530

    feat: add template filter logic

--- frontend/src/types/api/dashboard/getAll.ts
+++ frontend/src/types/api/dashboard/getAll.ts
@@ -54,6 +54,14 @@ export interface Dashboard {
 	isLocked?: boolean;
 }
 
+export interface DashboardTemplate {
+	name: string;
+	icon: React.ReactElement;
+	id: string;
+	description: string;
+	previewImage: string;
+}
+
 export interface DashboardData {
 	uuid?: string;
 	description?: string;
