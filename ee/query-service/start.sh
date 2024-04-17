817f9fedf8baccaab4532beee260e82e898e9aae
nityanandagohain <nityanandagohain@gmail.com>
2024-04-17 10:17:29 +0530

    fix: files updated

--- ee/query-service/start.sh
+++ ee/query-service/start.sh
@@ -4,10 +4,11 @@
 set -m
 
 # Start the primary process and put it in the background
-./query-service &
+./query-service "$@" &
 
 # # Start the helper process
 # ./my_helper_process
+echo "Hello World"
 
 # the my_helper_process might need to know how to wait on the
 # primary process to start before it does its work and returns
