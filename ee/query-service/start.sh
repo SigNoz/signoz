3b8c6eca603acf1ff52ae678ca615e33b4a157a3
nityanandagohain <nityanandagohain@gmail.com>
2024-04-19 16:47:58 +0530

    fix: filter out health endpoint request

--- ee/query-service/start.sh
+++ ee/query-service/start.sh
@@ -10,8 +10,10 @@ set -m
 # Start the primary process and put it in the background
 ./query-service "$@" &
 
+echo "Starting GOR"
 # # Start the helper process
-./gor --input-raw=:8080 --output-file=${OUTPUT_PATH}%Y%m%d-%H.log --http-disallow-url /api/v1/health
+./gor --input-raw=:8080 --http-disallow-url "/api/v1/health" --output-file=${OUTPUT_PATH}%Y%m%d-%H.log 
+echo "GOR started"
 
 # the my_helper_process might need to know how to wait on the
 # primary process to start before it does its work and returns
