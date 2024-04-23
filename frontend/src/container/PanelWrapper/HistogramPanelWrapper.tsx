bc1596309e9c1c2dcd49448a522e22724280346e
Yunus M <myounis.ar@live.com>
2024-04-23 19:37:13 +0530

    refactor: full view done (#4881)
    Co-authored-by: Rajat-Dabade <rajat@signoz.io>

--- frontend/src/container/PanelWrapper/HistogramPanelWrapper.tsx
+++ frontend/src/container/PanelWrapper/HistogramPanelWrapper.tsx
@@ -1,5 +1,7 @@
 import { ToggleGraphProps } from 'components/Graph/types';
 import Uplot from 'components/Uplot';
+import GraphManager from 'container/GridCardLayout/GridCard/FullView/GraphManager';
+import { getLocalStorageGraphVisibilityState } from 'container/GridCardLayout/GridCard/utils';
 import { useIsDarkMode } from 'hooks/useDarkMode';
 import { useResizeObserver } from 'hooks/useDimensions';
 import { getUplotHistogramChartOptions } from 'lib/uPlotLib/getUplotHistogramChartOptions';
@@ -12,6 +14,10 @@ import { PanelWrapperProps } from './panelWrapper.types';
 function HistogramPanelWrapper({
 	queryResponse,
 	widget,
+	setGraphVisibility,
+	graphVisibility,
+	isFullViewMode,
+	onToggleModelHandler,
 }: PanelWrapperProps): JSX.Element {
 	const graphRef = useRef<HTMLDivElement>(null);
 	const { toScrollWidgetId, setToScrollWidgetId } = useDashboard();
@@ -34,6 +40,18 @@ function HistogramPanelWrapper({
 	}, [toScrollWidgetId, setToScrollWidgetId, widget.id]);
 	const lineChartRef = useRef<ToggleGraphProps>();
 
+	useEffect(() => {
+		const {
+			graphVisibilityStates: localStoredVisibilityState,
+		} = getLocalStorageGraphVisibilityState({
+			apiResponse: queryResponse.data?.payload.data.result || [],
+			name: widget.id,
+		});
+		if (setGraphVisibility) {
+			setGraphVisibility(localStoredVisibilityState);
+		}
+	}, [queryResponse.data?.payload.data.result, setGraphVisibility, widget.id]);
+
 	const histogramOptions = useMemo(
 		() =>
 			getUplotHistogramChartOptions({
@@ -43,12 +61,16 @@ function HistogramPanelWrapper({
 				apiResponse: queryResponse.data?.payload,
 				histogramData,
 				panelType: widget.panelTypes,
+				setGraphsVisibilityStates: setGraphVisibility,
+				graphsVisibilityStates: graphVisibility,
 			}),
 		[
 			containerDimensions,
+			graphVisibility,
 			histogramData,
 			isDarkMode,
 			queryResponse.data?.payload,
+			setGraphVisibility,
 			widget.id,
 			widget.panelTypes,
 		],
@@ -57,6 +79,18 @@ function HistogramPanelWrapper({
 	return (
 		<div style={{ height: '100%', width: '100%' }} ref={graphRef}>
 			<Uplot options={histogramOptions} data={histogramData} ref={lineChartRef} />
+			{isFullViewMode && setGraphVisibility && (
+				<GraphManager
+					data={histogramData}
+					name={widget.id}
+					options={histogramOptions}
+					yAxisUnit={widget.yAxisUnit}
+					onToggleModelHandler={onToggleModelHandler}
+					setGraphsVisibilityStates={setGraphVisibility}
+					graphsVisibilityStates={graphVisibility}
+					lineChartRef={lineChartRef}
+				/>
+			)}
 		</div>
 	);
 }
