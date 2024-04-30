10b9a9c22d43e5f95e8d479cea5b05189cf67e71
Sagar Rajput <sagar@signoz.io>
2024-04-30 19:07:40 +0530

    chore: added tests for pipeline list view action, expand content delete and toggle action

--- frontend/src/container/PipelinePage/tests/PipelineListsView.test.tsx
+++ frontend/src/container/PipelinePage/tests/PipelineListsView.test.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { I18nextProvider } from 'react-i18next';
 import { Provider } from 'react-redux';
@@ -82,5 +82,69 @@ describe('PipelinePage container test', () => {
 		expect(getByText('add_new_processor')).toBeInTheDocument();
 		expect(getByText('grok use common asd')).toBeInTheDocument();
 		expect(getByText('rename auth')).toBeInTheDocument();
+	});
+
+	it('should be able to perform actions and edit on expanded view content', async () => {
+		render(
+			<MemoryRouter>
+				<Provider store={store}>
+					<I18nextProvider i18n={i18n}>
+						<PipelineListsView
+							setActionType={jest.fn()}
+							isActionMode="editing-mode"
+							setActionMode={jest.fn()}
+							pipelineData={pipelineApiResponseMockData}
+							isActionType=""
+							refetchPipelineLists={jest.fn()}
+						/>
+					</I18nextProvider>
+				</Provider>
+			</MemoryRouter>,
+		);
+
+		// content assertion
+		expect(document.querySelectorAll('[data-icon="edit"]').length).toBe(2);
+
+		// expand action
+		const expandIcon = document.querySelectorAll(
+			'.ant-table-row-expand-icon-cell > span[class*="anticon-right"]',
+		);
+		expect(expandIcon.length).toBe(2);
+		await userEvent.click(expandIcon[0]);
+
+		const switchToggle = document.querySelector(
+			'.ant-table-expanded-row .ant-switch',
+		);
+
+		expect(switchToggle).toBeChecked();
+		await userEvent.click(switchToggle as HTMLElement);
+		expect(switchToggle).not.toBeChecked();
+
+		const deleteBtns = document.querySelectorAll(
+			'.ant-table-expanded-row [data-icon="delete"]',
+		);
+
+		expect(deleteBtns.length).toBe(2);
+
+		// delete pipeline
+		await userEvent.click(deleteBtns[0] as HTMLElement);
+
+		let deleteConfirmationModal;
+
+		await waitFor(async () => {
+			deleteConfirmationModal = document.querySelector('.ant-modal-wrap');
+			expect(deleteConfirmationModal).toBeInTheDocument();
+		});
+
+		await userEvent.click(
+			((deleteConfirmationModal as unknown) as HTMLElement)?.querySelector(
+				'.ant-modal-confirm-btns .ant-btn-primary',
+			) as HTMLElement,
+		);
+
+		expect(
+			document.querySelectorAll('.ant-table-expanded-row [data-icon="delete"]')
+				.length,
+		).toBe(1);
 	});
 });
