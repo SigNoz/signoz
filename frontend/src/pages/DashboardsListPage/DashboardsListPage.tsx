f0515f85ad10090e555c0c5bea6b40863b453914
Vikrant Gupta <vikrant.thomso@gmail.com>
2024-05-14 19:14:44 +0530

    feat: added dashboard list page header

--- frontend/src/pages/DashboardsListPage/DashboardsListPage.tsx
+++ frontend/src/pages/DashboardsListPage/DashboardsListPage.tsx
@@ -1,6 +1,9 @@
-import { Space } from 'antd';
+import './DashboardsListPage.styles.scss';
+
+import { Space, Typography } from 'antd';
 import ReleaseNote from 'components/ReleaseNote';
 import ListOfAllDashboard from 'container/ListOfDashboard';
+import { LayoutGrid } from 'lucide-react';
 import { useLocation } from 'react-router-dom';
 
 function DashboardsListPage(): JSX.Element {
@@ -9,6 +12,10 @@ function DashboardsListPage(): JSX.Element {
 	return (
 		<Space direction="vertical" size="middle" style={{ width: '100%' }}>
 			<ReleaseNote path={location.pathname} />
+			<div className="dashboard-header">
+				<LayoutGrid size={14} className="icon" />
+				<Typography.Text className="text">Dashboards</Typography.Text>
+			</div>
 			<ListOfAllDashboard />
 		</Space>
 	);
