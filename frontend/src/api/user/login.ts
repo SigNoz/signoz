fdf451b9152edc758b8769a2577f1eda00606dfd
makeavish <makeavish786@gmail.com>
2024-06-24 14:36:02 +0530

    chore: posthog js init

--- frontend/src/api/user/login.ts
+++ frontend/src/api/user/login.ts
@@ -1,6 +1,7 @@
 import axios from 'api';
 import { ErrorResponseHandler } from 'api/ErrorResponseHandler';
 import { AxiosError } from 'axios';
+import posthog from 'posthog-js';
 import { ErrorResponse, SuccessResponse } from 'types/api';
 import { PayloadProps, Props } from 'types/api/user/login';
 
@@ -12,6 +13,13 @@ const login = async (
 			...props,
 		});
 
+		posthog.identify(props.email, { email: props.email });
+		// TODO: Extract emailDomain and make a groupId call
+		// posthog.group('company', 'signoz.io', {
+		// 	name: 'SigNoz',
+		// 	subscription: "subscription",
+		// 	date_joined: '2020-01-23'
+		// });
 		return {
 			statusCode: 200,
 			error: null,
