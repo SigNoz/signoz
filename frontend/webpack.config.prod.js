4170e4ef9cfb108218c1e2cda6671712cb7e111d
gaozhichao123 <zhichao.gao@sayweee.com>
2024-06-25 15:08:10 +0800

    feat: 修改路径

--- frontend/webpack.config.prod.js
+++ frontend/webpack.config.prod.js
@@ -44,7 +44,7 @@ const plugins = [
 		INTERCOM_APP_ID: process.env.INTERCOM_APP_ID,
 		SEGMENT_ID: process.env.SEGMENT_ID,
 		CLARITY_PROJECT_ID: process.env.CLARITY_PROJECT_ID,
-		// publicPath: '/website',
+		publicPath: '/website',
 	}),
 	new CompressionPlugin({
 		exclude: /.map$/,
