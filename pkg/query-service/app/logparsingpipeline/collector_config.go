f5c4b8b19776e44ebcf15ded797ab385b423bf85
Raj <rkssisodiya@gmail.com>
2024-06-27 15:04:28 +0530

    chore: emit error log on detecting duplicate processors in generated config

--- pkg/query-service/app/logparsingpipeline/collector_config.go
+++ pkg/query-service/app/logparsingpipeline/collector_config.go
@@ -142,6 +142,11 @@ func checkDuplicateString(pipeline []string) bool {
 	for _, processor := range pipeline {
 		name := processor
 		if _, ok := exists[name]; ok {
+			zap.L().Error(
+				"duplicate processor detected in pipeline",
+				zap.String("processor", processor),
+				zap.Any("pipeline", pipeline),
+			)
 			return true
 		}
 
