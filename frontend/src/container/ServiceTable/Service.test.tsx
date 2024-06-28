02b88b0b31c42f6caa8273358dff15cdb5716c78
Sagar Rajput <sagar@signoz.io>
2024-06-28 11:27:24 +0530

    chore: added testwrapper to a existing test and history refactor

--- frontend/src/container/ServiceTable/Service.test.tsx
+++ frontend/src/container/ServiceTable/Service.test.tsx
@@ -1,11 +1,17 @@
+import ROUTES from 'constants/routes';
 import { render, screen, waitFor } from 'tests/test-utils';
+import { TestWrapper } from 'testUtils';
 
 import { Services } from './__mock__/servicesListMock';
 import Metrics from './index';
 
 describe('Metrics Component', () => {
 	it('renders without errors', async () => {
-		render(<Metrics services={Services} isLoading={false} />);
+		render(
+			<TestWrapper path={ROUTES.APPLICATION}>
+				<Metrics services={Services} isLoading={false} />,
+			</TestWrapper>,
+		);
 
 		await waitFor(() => {
 			expect(screen.getByText(/application/i)).toBeInTheDocument();
@@ -16,13 +22,21 @@ describe('Metrics Component', () => {
 	});
 
 	it('renders if the data is loaded in the table', async () => {
-		render(<Metrics services={Services} isLoading={false} />);
+		render(
+			<TestWrapper>
+				<Metrics services={Services} isLoading={false} />
+			</TestWrapper>,
+		);
 
 		expect(screen.getByText('frontend')).toBeInTheDocument();
 	});
 
 	it('renders no data when required conditions are met', async () => {
-		render(<Metrics services={[]} isLoading={false} />);
+		render(
+			<TestWrapper>
+				<Metrics services={[]} isLoading={false} />
+			</TestWrapper>,
+		);
 
 		expect(screen.getByText('No data')).toBeInTheDocument();
 	});
--- frontend/src/testUtils.tsx
+++ frontend/src/testUtils.tsx
@@ -1,16 +1,16 @@
 import { ConfigProvider } from 'antd';
 import PrivateRoute from 'AppRoutes/Private';
 import { AxiosError } from 'axios';
-import Spinner from 'components/Spinner';
 import AppLayout from 'container/AppLayout';
+import { createMemoryHistory } from 'history';
 import { KeyboardHotkeysProvider } from 'hooks/hotkeys/useKeyboardHotkeys';
 import { ThemeProvider, useThemeConfig } from 'hooks/useDarkMode';
 import { NotificationProvider } from 'hooks/useNotifications';
 import { ResourceProvider } from 'hooks/useResourceAttribute';
-import history from 'lib/history';
+import createQueryParams from 'lib/createQueryParams';
 import { DashboardProvider } from 'providers/Dashboard/Dashboard';
 import { QueryBuilderProvider } from 'providers/QueryBuilder';
-import { Suspense } from 'react';
+import React from 'react';
 import { HelmetProvider } from 'react-helmet-async';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { Provider } from 'react-redux';
@@ -38,9 +38,17 @@ const queryClient = new QueryClient({
 
 // todo: added any type for now only, will changed
 export function TestWrapper(props: any): JSX.Element {
-	const { children, path = '/' } = props;
+	const { children, path = '/', queryParams = {} } = props;
 
 	const themeConfig = useThemeConfig();
+
+	const search = createQueryParams(queryParams);
+
+	const history = React.useMemo(
+		() => createMemoryHistory({ initialEntries: [`${path}?${search}`] }),
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+		[],
+	);
 
 	return (
 		<HelmetProvider>
@@ -56,13 +64,11 @@ export function TestWrapper(props: any): JSX.Element {
 												<DashboardProvider>
 													<KeyboardHotkeysProvider>
 														<AppLayout>
-															<Suspense fallback={<Spinner size="large" tip="Loading..." />}>
-																<Switch>
-																	<Route exact path={path}>
-																		{children}
-																	</Route>
-																</Switch>
-															</Suspense>
+															<Switch>
+																<Route exact path={path}>
+																	{children}
+																</Route>
+															</Switch>
 														</AppLayout>
 													</KeyboardHotkeysProvider>
 												</DashboardProvider>
