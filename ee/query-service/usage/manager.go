12452be9881e02d6d9a43db9e15cda77315e1ee4
nityanandagohain <nityanandagohain@gmail.com>
2024-07-08 16:02:38 +0530

    fix: increase range to last 72 hours

--- ee/query-service/usage/manager.go
+++ ee/query-service/usage/manager.go
@@ -121,7 +121,7 @@ func (lm *Manager) UploadUsage() {
 
 	for _, db := range dbs {
 		dbusages := []model.UsageDB{}
-		err := lm.clickhouseConn.Select(ctx, &dbusages, fmt.Sprintf(query, db, db), time.Now().Add(-(48 * time.Hour)))
+		err := lm.clickhouseConn.Select(ctx, &dbusages, fmt.Sprintf(query, db, db), time.Now().Add(-(72 * time.Hour)))
 		if err != nil && !strings.Contains(err.Error(), "doesn't exist") {
 			zap.L().Error("failed to get usage from clickhouse: %v", zap.Error(err))
 			return
