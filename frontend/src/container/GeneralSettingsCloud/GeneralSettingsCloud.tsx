fb73f23dce8c426f15ccef44970af5550d99f4e3
ahmadshaheer <ashaheerki@gmail.com>
2024-07-17 19:32:17 +0430

    feat: write tests for cloud user general settings

--- frontend/src/container/GeneralSettingsCloud/GeneralSettingsCloud.tsx
+++ frontend/src/container/GeneralSettingsCloud/GeneralSettingsCloud.tsx
@@ -7,7 +7,7 @@ export default function GeneralSettingsCloud(): JSX.Element {
 	return (
 		<Card className="general-settings-container">
 			<Info size={16} />
-			<Typography.Text>
+			<Typography.Text data-testid="cloud-user-info-card">
 				Please <a href="mailto:cloud-support@signoz.io"> email us </a> or connect
 				with us via intercom support to change the retention period.
 			</Typography.Text>
---
+++ frontend/src/pages/Settings/__tests__/GeneralSettingsCloudUser.test.tsx
@@ -0,0 +1,62 @@
+import GeneralSettingsContainer from 'container/GeneralSettings/GeneralSettings';
+import { render, screen, within } from 'tests/test-utils';
+
+import { generalSettingsProps } from './mock';
+
+jest.mock('utils/app', () => {
+	const app = jest.requireActual('utils/app');
+	return {
+		...app,
+		isCloudUser: jest.fn(() => true),
+	};
+});
+
+const types = [
+	{
+		testId: 'Metrics',
+		header: 'Metrics',
+	},
+	{
+		testId: 'Traces',
+		header: 'Traces',
+	},
+	{
+		testId: 'Logs',
+		header: 'Logs',
+	},
+];
+
+describe('Cloud User General Settings', () => {
+	beforeEach(() => {
+		render(
+			<GeneralSettingsContainer
+				metricsTtlValuesPayload={generalSettingsProps.metricsTtlValuesPayload}
+				tracesTtlValuesPayload={generalSettingsProps.tracesTtlValuesPayload}
+				logsTtlValuesPayload={generalSettingsProps.logsTtlValuesPayload}
+				getAvailableDiskPayload={generalSettingsProps.getAvailableDiskPayload}
+				metricsTtlValuesRefetch={generalSettingsProps.metricsTtlValuesRefetch}
+				tracesTtlValuesRefetch={generalSettingsProps.tracesTtlValuesRefetch}
+				logsTtlValuesRefetch={generalSettingsProps.logsTtlValuesRefetch}
+			/>,
+		);
+	});
+	it('should display the cloud user info card', () => {
+		const cloudUserCard = screen.getByTestId('cloud-user-info-card');
+		expect(cloudUserCard).toBeInTheDocument();
+	});
+	types.forEach(({ testId, header }) => {
+		it(`should check if value textbox and duration dropdown are disabled in the body of ${header}`, () => {
+			const sectionCard = screen.getByTestId(testId);
+
+			const retentionFieldInput = within(sectionCard).getByTestId(
+				'retention-field-input',
+			);
+			expect(retentionFieldInput).toBeDisabled();
+
+			const retentionFieldDropdown = within(sectionCard).getByTestId(
+				'retention-field-dropdown',
+			);
+			expect(retentionFieldDropdown).toHaveClass('ant-select-disabled');
+		});
+	});
+});
