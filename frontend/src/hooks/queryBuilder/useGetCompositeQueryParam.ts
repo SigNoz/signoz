5bb837ec2756a8bae11a208dc31ebc504157a57b
ahmadshaheer <ashaheerki@gmail.com>
2024-07-18 13:28:14 +0430

    fix: handle the query modification in useGetCompositeQueryParam instead of Filter

--- frontend/src/hooks/queryBuilder/useGetCompositeQueryParam.ts
+++ frontend/src/hooks/queryBuilder/useGetCompositeQueryParam.ts
@@ -1,10 +1,14 @@
 import { QueryParams } from 'constants/query';
 import useUrlQuery from 'hooks/useUrlQuery';
 import { useMemo } from 'react';
+import { useLocation } from 'react-router-dom';
 import { Query } from 'types/api/queryBuilder/queryBuilderData';
+
+import { adjustQueryForTracesPage } from './utils';
 
 export const useGetCompositeQueryParam = (): Query | null => {
 	const urlQuery = useUrlQuery();
+	const { pathname } = useLocation();
 
 	return useMemo(() => {
 		const compositeQuery = urlQuery.get(QueryParams.compositeQuery);
@@ -14,10 +18,14 @@ export const useGetCompositeQueryParam = (): Query | null => {
 			if (!compositeQuery) return null;
 
 			parsedCompositeQuery = JSON.parse(decodeURIComponent(compositeQuery));
+
+			const panelTypes = JSON.parse(urlQuery.get(QueryParams.panelTypes) ?? '""');
+
+			return adjustQueryForTracesPage(pathname, parsedCompositeQuery, panelTypes);
 		} catch (e) {
 			parsedCompositeQuery = null;
 		}
 
 		return parsedCompositeQuery;
-	}, [urlQuery]);
+	}, [pathname, urlQuery]);
 };
