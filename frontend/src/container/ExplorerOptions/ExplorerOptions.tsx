37f4f388457c52fb662b4df8dedef4161e9c27dc
Sagar Rajput <sagar@signoz.io>
2024-07-22 02:13:45 +0530

    chore: added trace explorer tests

--- frontend/src/container/ExplorerOptions/ExplorerOptions.tsx
+++ frontend/src/container/ExplorerOptions/ExplorerOptions.tsx
@@ -482,6 +482,7 @@ function ExplorerOptions({
 								shape="circle"
 								onClick={hideToolbar}
 								icon={<PanelBottomClose size={16} />}
+								data-testid="hide-toolbar"
 							/>
 						</Tooltip>
 					</div>
@@ -511,6 +512,7 @@ function ExplorerOptions({
 						icon={<Check size={16} />}
 						onClick={onSaveHandler}
 						disabled={isSaveViewLoading}
+						data-testid="save-view-btn"
 					>
 						Save this view
 					</Button>,
