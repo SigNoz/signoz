bfbf790eab678d2184017f1becea0e072dd5ada5
ahmadshaheer <ashaheerki@gmail.com>
2024-08-04 16:44:03 +0430

    chore: add comment to handling last used saved view

--- frontend/src/container/ExplorerOptions/ExplorerOptions.tsx
+++ frontend/src/container/ExplorerOptions/ExplorerOptions.tsx
@@ -394,6 +394,7 @@ function ExplorerOptions({
 			!(!!viewName || !!viewKey) &&
 			!isRecentlyUsedSavedViewSelected
 		) {
+			// prevent the race condition with useShareBuilderUrl
 			timeoutId = setTimeout(() => {
 				onMenuItemSelectHandler({ key: preservedView.key });
 			}, 0);
