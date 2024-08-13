7affe81f9985f1c8f01c05269d7555df3c9c81df
ahmadshaheer <ashaheerki@gmail.com>
2024-08-13 17:04:18 +0430

    feat: timeline tabs and filters

--- frontend/src/container/AlertHistory/index.tsx
+++ frontend/src/container/AlertHistory/index.tsx
@@ -1,9 +1,13 @@
+import './alertHistory.styles.scss';
+
 import Statistics from './Statistics/Statistics';
+import Timeline from './Timeline/Timeline';
 
 function AlertHistory(): JSX.Element {
 	return (
-		<div>
+		<div className="alert-history">
 			<Statistics />
+			<Timeline />
 		</div>
 	);
 }
