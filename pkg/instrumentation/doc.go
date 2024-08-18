20ec91db63419ba4b5d8cd011d6a296413d7966b
grandwizard28 <vibhupandey28@gmail.com>
2024-08-18 18:55:56 +0530

    feat(instrumentation): add doc.go

---
+++ pkg/instrumentation/doc.go
@@ -0,0 +1,6 @@
+// Package instrumentation provides utilities for initializing and managing
+// OpenTelemetry resources, tracing, and metering within the application. It
+// leverages the OpenTelemetry SDK to facilitate the collection and
+// export of telemetry data, including traces and metrics, to an OTLP (OpenTelemetry
+// Protocol) endpoint.
+package instrumentation
