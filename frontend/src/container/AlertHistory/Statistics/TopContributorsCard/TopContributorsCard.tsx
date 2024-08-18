b0605fd0e617b3fd0e1a03dc61bb6b5e902a8de7
ahmadshaheer <ashaheerki@gmail.com>
2024-08-18 16:57:49 +0430

    feat: top contributors empty state

--- frontend/src/container/AlertHistory/Statistics/TopContributorsCard/TopContributorsCard.tsx
+++ frontend/src/container/AlertHistory/Statistics/TopContributorsCard/TopContributorsCard.tsx
@@ -1,6 +1,6 @@
 import './topContributorsCard.styles.scss';
 
-import { Progress } from 'antd';
+import { Button, Progress } from 'antd';
 import AlertPopover from 'container/AlertHistory/AlertPopover/AlertPopover';
 import { ArrowRight } from 'lucide-react';
 import AlertLabels from 'pages/AlertDetails/AlertHeader/AlertLabels/AlertLabels';
@@ -8,6 +8,56 @@ import { AlertRuleStats, AlertRuleTopContributors } from 'types/api/alerts/def';
 
 import { statsData } from '../mocks';
 
+function TopContributorsContent({
+	topContributorsData,
+	totalCurrentTriggers,
+}: TopContributorsCardProps): JSX.Element {
+	const isEmpty = !topContributorsData.length;
+
+	if (isEmpty) {
+		return (
+			<div className="empty-content">
+				<div className="empty-content__icon">ℹ️</div>
+				<div className="empty-content__text">
+					<span className="bold-text">Add Group By Field</span> To view top
+					contributors, please add at least one group by field to your query.
+				</div>
+				<div className="empty-content__button-wrapper">
+					<Button type="default" className="configure-alert-rule-button">
+						Configure Alert Rule
+					</Button>
+				</div>
+			</div>
+		);
+	}
+
+	return (
+		<>
+			{topContributorsData.slice(0, 3).map((contributor, index) => (
+				<AlertPopover // eslint-disable-next-line react/no-array-index-key
+					key={`contributor-${index}`}
+				>
+					<div className="contributors-row">
+						<div className="labels-wrapper">
+							<AlertLabels labels={contributor.labels} />
+						</div>
+						<div className="contribution-progress-bar">
+							<Progress
+								percent={(contributor.count / statsData.totalCurrentTriggers) * 100}
+								showInfo={false}
+								trailColor="rgba(255, 255, 255, 0)"
+								strokeColor="var(--bg-robin-500)"
+							/>
+						</div>
+						<div className="total-contribution">
+							{contributor.count}/{totalCurrentTriggers}
+						</div>
+					</div>
+				</AlertPopover>
+			))}
+		</>
+	);
+}
 type TopContributorsCardProps = {
 	topContributorsData: AlertRuleTopContributors[];
 	totalCurrentTriggers: AlertRuleStats['totalCurrentTriggers'];
@@ -28,28 +78,10 @@ function TopContributorsCard({
 				</div>
 			</div>
 			<div className="top-contributors-card__content">
-				{topContributorsData.slice(0, 3).map((contributor, index) => (
-					<AlertPopover // eslint-disable-next-line react/no-array-index-key
-						key={`contributor-${index}`}
-					>
-						<div className="contributors-row">
-							<div className="labels-wrapper">
-								<AlertLabels labels={contributor.labels} />
-							</div>
-							<div className="contribution-progress-bar">
-								<Progress
-									percent={(contributor.count / statsData.totalCurrentTriggers) * 100}
-									showInfo={false}
-									trailColor="rgba(255, 255, 255, 0)"
-									strokeColor="var(--bg-robin-500)"
-								/>
-							</div>
-							<div className="total-contribution">
-								{contributor.count}/{totalCurrentTriggers}
-							</div>
-						</div>
-					</AlertPopover>
-				))}
+				<TopContributorsContent
+					topContributorsData={topContributorsData}
+					totalCurrentTriggers={totalCurrentTriggers}
+				/>
 			</div>
 		</div>
 	);
