5796d6cb8c836d88207022b74609f23892495b3c
Vikrant Gupta <vikrant.thomso@gmail.com>
2024-08-20 17:09:17 +0530

    feat: rewrite the query builder search component (#5659)
    * feat: make the query builder search extensible
    
    * feat: setup the framework and necessary states needed
    
    * feat: cover the happy path of selects
    
    * chore: forward typing flow handled
    
    * chore: add antd select
    
    * chore: add antd select
    
    * chore: handle forward and backward flows
    
    * feat: added tag properites to the search bar and multi tag partial handling
    
    * feat: handle tag on blur and body contains changes
    
    * feat: handle tag deselect
    
    * feat: multi tag handling
    
    * feat: multi tag handling
    
    * fix: jest test cases
    
    * chore: update the key
    
    * chore: add edit tag not working as expected
    
    * feat: handle cases for exists and nexists
    
    * fix: handle has / nhas operators
    
    * chore: fix usability issues
    
    * chore: remove the usage for the new bar
    
    * fix: flaky build issues
    
    * feat: client changes for consumption and design changes for where clause in logs explorer  (#5712)
    
    * feat: query search new ui
    
    * feat: suggestions changes in v2
    
    * feat: dropdown and tags ui touch up
    
    * feat: added missing keyboard shortcuts
    
    * fix: race condition issues
    
    * feat: remove usage
    
    * fix: operator select fix
    
    * fix: handle example queries click changes
    
    * chore: design sync
    
    * chore: handle boolean selects
    
    * chore: address review comments

---
+++ frontend/src/container/QueryBuilder/filters/QueryBuilderSearchV2/QueryBuilderSearchDropdown.tsx
@@ -0,0 +1,112 @@
+/* eslint-disable no-nested-ternary */
+import { Typography } from 'antd';
+import {
+	ArrowDown,
+	ArrowUp,
+	ChevronUp,
+	Command,
+	CornerDownLeft,
+	Slash,
+} from 'lucide-react';
+import { TagFilter } from 'types/api/queryBuilder/queryBuilderData';
+import { getUserOperatingSystem, UserOperatingSystem } from 'utils/getUserOS';
+
+import ExampleQueriesRendererForLogs from '../QueryBuilderSearch/ExampleQueriesRendererForLogs';
+import { convertExampleQueriesToOptions } from '../QueryBuilderSearch/utils';
+import { ITag, Option } from './QueryBuilderSearchV2';
+
+interface ICustomDropdownProps {
+	menu: React.ReactElement;
+	searchValue: string;
+	tags: ITag[];
+	options: Option[];
+	exampleQueries: TagFilter[];
+	onChange: (value: TagFilter) => void;
+	currentFilterItem?: ITag;
+}
+
+export default function QueryBuilderSearchDropdown(
+	props: ICustomDropdownProps,
+): React.ReactElement {
+	const {
+		menu,
+		currentFilterItem,
+		searchValue,
+		tags,
+		exampleQueries,
+		options,
+		onChange,
+	} = props;
+	const userOs = getUserOperatingSystem();
+	return (
+		<>
+			<div className="content">
+				{!currentFilterItem?.key ? (
+					<div className="suggested-filters">Suggested Filters</div>
+				) : !currentFilterItem?.op ? (
+					<div className="operator-for">
+						<Typography.Text className="operator-for-text">
+							Operator for{' '}
+						</Typography.Text>
+						<Typography.Text className="operator-for-value">
+							{currentFilterItem?.key?.key}
+						</Typography.Text>
+					</div>
+				) : (
+					<div className="value-for">
+						<Typography.Text className="value-for-text">
+							Value(s) for{' '}
+						</Typography.Text>
+						<Typography.Text className="value-for-value">
+							{currentFilterItem?.key?.key} {currentFilterItem?.op}
+						</Typography.Text>
+					</div>
+				)}
+				{menu}
+				{!searchValue && tags.length === 0 && (
+					<div className="example-queries">
+						<div className="heading"> Example Queries </div>
+						<div className="query-container">
+							{convertExampleQueriesToOptions(exampleQueries).map((query) => (
+								<ExampleQueriesRendererForLogs
+									key={query.label}
+									label={query.label}
+									value={query.value}
+									handleAddTag={onChange}
+								/>
+							))}
+						</div>
+					</div>
+				)}
+			</div>
+
+			<div className="keyboard-shortcuts">
+				<section className="navigate">
+					<ArrowDown size={10} className="icons" />
+					<ArrowUp size={10} className="icons" />
+					<span className="keyboard-text">to navigate</span>
+				</section>
+				<section className="update-query">
+					<CornerDownLeft size={10} className="icons" />
+					<span className="keyboard-text">to update query</span>
+				</section>
+				{!currentFilterItem?.key && options.length > 3 && (
+					<section className="show-all-filter-items">
+						{userOs === UserOperatingSystem.MACOS ? (
+							<Command size={14} className="icons" />
+						) : (
+							<ChevronUp size={14} className="icons" />
+						)}
+						+
+						<Slash size={14} className="icons" />
+						<span className="keyboard-text">Show all filter items</span>
+					</section>
+				)}
+			</div>
+		</>
+	);
+}
+
+QueryBuilderSearchDropdown.defaultProps = {
+	currentFilterItem: undefined,
+};
