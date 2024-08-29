b44289f4e97da4e5a4b1062915872a763ae69240
ahmadshaheer <ashaheerki@gmail.com>
2024-08-29 11:21:51 +0430

    Revert "fix: fix the issue of span names not visible in trace details"
    This reverts commit 570a28bc007a0202a5916c94299a1245e0f0bff2.

--- frontend/src/container/GantChart/Trace/index.tsx
+++ frontend/src/container/GantChart/Trace/index.tsx
@@ -3,6 +3,7 @@ import { Col, Typography } from 'antd';
 import { StyledCol, StyledRow } from 'components/Styled';
 import { IIntervalUnit } from 'container/TraceDetail/utils';
 import { useIsDarkMode } from 'hooks/useDarkMode';
+import { SPAN_DETAILS_LEFT_COL_WIDTH } from 'pages/TraceDetail/constants';
 import {
 	Dispatch,
 	MouseEventHandler,
@@ -118,6 +119,7 @@ function Trace(props: TraceProps): JSX.Element {
 	const inMsCount = value;
 	const nodeLeftOffset = ((startTime - globalStart) * 1e2) / globalSpread;
 	const width = (value * 1e2) / (globalSpread * 1e6);
+	const panelWidth = SPAN_DETAILS_LEFT_COL_WIDTH - level * (16 + 1) - 48;
 
 	const iconStyles = useMemo(() => getIconStyles(isDarkMode), [isDarkMode]);
 
@@ -147,7 +149,7 @@ function Trace(props: TraceProps): JSX.Element {
 			/>
 
 			<CardContainer isMissing={isMissing} onClick={onClick}>
-				<StyledCol>
+				<StyledCol flex={`${panelWidth}px`} styledclass={[styles.overFlowHidden]}>
 					<StyledRow styledclass={[styles.flexNoWrap]}>
 						<Col>
 							{totalSpans !== 1 && (
