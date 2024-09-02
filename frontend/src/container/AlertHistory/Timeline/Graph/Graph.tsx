3e88c73722314133ead267ac3c2481e818488209
ahmadshaheer <ashaheerki@gmail.com>
2024-09-02 09:15:11 +0430

    feat: add vertical timeline chart using static data

--- frontend/src/container/AlertHistory/Timeline/Graph/Graph.tsx
+++ frontend/src/container/AlertHistory/Timeline/Graph/Graph.tsx
@@ -4,6 +4,7 @@ import { Color } from '@signozhq/design-tokens';
 import Uplot from 'components/Uplot';
 import { useIsDarkMode } from 'hooks/useDarkMode';
 import { useResizeObserver } from 'hooks/useDimensions';
+import heatmapPlugin from 'lib/uPlotLib/plugins/heatmapPlugin';
 import timelinePlugin from 'lib/uPlotLib/plugins/timelinePlugin';
 import { useMemo, useRef } from 'react';
 import { AlertRuleTimelineGraphResponse } from 'types/api/alerts/def';
@@ -76,6 +77,71 @@ function HorizontalTimelineGraph({
 	return <Uplot data={transformedData} options={options} />;
 }
 
+const transformVerticalTimelineGraph = (data: any[]): any => [
+	data.map((item: { timestamp: any }) => item.timestamp),
+	Array(data.length).fill(0),
+	Array(data.length).fill(10),
+	Array(data.length).fill([0, 1, 2, 3, 4, 5]),
+	data.map((item: { value: number }) => {
+		const count = Math.floor(item.value / 10);
+		return [...Array(count).fill(1), 2];
+	}),
+];
+const datatest = [
+	{
+		timestamp: 1724941976.194,
+		value: 20,
+	},
+	{
+		timestamp: 1724855576.194,
+		value: 30,
+	},
+];
+function VerticalTimelineGraph({
+	isDarkMode,
+	width,
+}: {
+	width: number;
+	isDarkMode: boolean;
+}): JSX.Element {
+	const transformedData = useMemo(
+		() => transformVerticalTimelineGraph(datatest),
+		[],
+	);
+
+	const options: uPlot.Options = useMemo(
+		() => ({
+			width,
+			height: 90,
+			plugins: [heatmapPlugin()],
+			cursor: { show: false },
+			legend: {
+				show: false,
+			},
+			axes: [
+				{
+					gap: 10,
+					stroke: isDarkMode ? Color.BG_VANILLA_400 : Color.BG_INK_400,
+				},
+				{ show: false },
+			],
+			series: [
+				{},
+				{
+					paths: (): null => null,
+					points: { show: false },
+				},
+				{
+					paths: (): null => null,
+					points: { show: false },
+				},
+			],
+		}),
+		[isDarkMode, width],
+	);
+	return <Uplot data={transformedData} options={options} />;
+}
+
 function Graph({ type, data }: Props): JSX.Element | null {
 	const graphRef = useRef<HTMLDivElement>(null);
 
@@ -94,7 +160,14 @@ function Graph({ type, data }: Props): JSX.Element | null {
 			</div>
 		);
 	}
-	return null;
+	return (
+		<div ref={graphRef}>
+			<VerticalTimelineGraph
+				isDarkMode={isDarkMode}
+				width={containerDimensions.width}
+			/>
+		</div>
+	);
 }
 
 export default Graph;
