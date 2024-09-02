c25d2203be855e873c37971c7b2c9b5305e5e8d0
Yunus M <myounis.ar@live.com>
2024-09-02 23:52:12 +0530

    feat: onboarding v2 base setup

--- frontend/src/AppRoutes/routes.ts
+++ frontend/src/AppRoutes/routes.ts
@@ -33,6 +33,7 @@ import {
 	NewDashboardPage,
 	OldLogsExplorer,
 	Onboarding,
+	OnboardingV2,
 	OrganizationSettings,
 	OrgOnboarding,
 	PasswordReset,
@@ -78,6 +79,13 @@ const routes: AppRoutes[] = [
 		component: OrgOnboarding,
 		isPrivate: true,
 		key: 'ONBOARDING',
+	},
+	{
+		path: ROUTES.GET_STARTED_V2,
+		exact: false,
+		component: OnboardingV2,
+		isPrivate: true,
+		key: 'GET_STARTED_V2',
 	},
 	{
 		component: LogsIndexToFields,
