5cd2db0cc305f842147839257a9384e54b1ebfca
Shaheer Kochai <ashaheerki@gmail.com>
2024-09-03 20:36:31 +0430

    feat: alert history dropdown functionality (#5833)
    * feat: alert history dropdown actions
    
    * chore: use query keys from react query key constant
    
    * fix: properly handle error states for alert rule APIs
    
    * fix: handle dropdown state using onOpenChange to fix clicking delete not closing the dropdown

--- frontend/src/pages/AlertDetails/hooks.tsx
+++ frontend/src/pages/AlertDetails/hooks.tsx
@@ -1,8 +1,11 @@
 import { FilterValue, SorterResult } from 'antd/es/table/interface';
 import { TablePaginationConfig, TableProps } from 'antd/lib';
+import deleteAlerts from 'api/alerts/delete';
 import get from 'api/alerts/get';
+import getAll from 'api/alerts/getAll';
 import patchAlert from 'api/alerts/patch';
 import ruleStats from 'api/alerts/ruleStats';
+import save from 'api/alerts/save';
 import timelineGraph from 'api/alerts/timelineGraph';
 import timelineTable from 'api/alerts/timelineTable';
 import topContributors from 'api/alerts/topContributors';
@@ -14,6 +17,7 @@ import AlertHistory from 'container/AlertHistory';
 import { TIMELINE_TABLE_PAGE_SIZE } from 'container/AlertHistory/constants';
 import { AlertDetailsTab, TimelineFilter } from 'container/AlertHistory/types';
 import { urlKey } from 'container/AllError/utils';
+import useAxiosError from 'hooks/useAxiosError';
 import { useNotifications } from 'hooks/useNotifications';
 import useUrlQuery from 'hooks/useUrlQuery';
 import createQueryParams from 'lib/createQueryParams';
@@ -24,18 +28,20 @@ import EditRules from 'pages/EditRules';
 import { OrderPreferenceItems } from 'pages/Logs/config';
 import PaginationInfoText from 'periscope/components/PaginationInfoText/PaginationInfoText';
 import { useCallback, useMemo, useState } from 'react';
-import { useMutation, useQuery, UseQueryResult } from 'react-query';
+import { useMutation, useQuery } from 'react-query';
 import { useSelector } from 'react-redux';
 import { generatePath, useLocation } from 'react-router-dom';
 import { AppState } from 'store/reducers';
 import { ErrorResponse, SuccessResponse } from 'types/api';
 import {
+	AlertDef,
 	AlertRuleStatsPayload,
 	AlertRuleTimelineGraphResponsePayload,
 	AlertRuleTimelineTableResponse,
 	AlertRuleTimelineTableResponsePayload,
 	AlertRuleTopContributorsPayload,
 } from 'types/api/alerts/def';
+import { PayloadProps } from 'types/api/alerts/get';
 import { TagFilter } from 'types/api/queryBuilder/queryBuilderData';
 import { GlobalReducer } from 'types/reducer/globalTime';
 import { nanoToMilli } from 'utils/timeUtils';
@@ -128,17 +134,29 @@ export const useRouteTabUtils = (): { routes: TabRoutes[] } => {
 
 	return { routes };
 };
-
-export const useGetAlertRuleDetails = (): {
+type Props = {
 	ruleId: string | null;
-	data: UseQueryResult;
 	isValidRuleId: boolean;
-} => {
+	alertDetailsResponse:
+		| SuccessResponse<PayloadProps, unknown>
+		| ErrorResponse
+		| undefined;
+	isLoading: boolean;
+	isRefetching: boolean;
+	isError: boolean;
+};
+
+export const useGetAlertRuleDetails = (): Props => {
 	const { ruleId } = useAlertHistoryQueryParams();
 
 	const isValidRuleId = ruleId !== null && String(ruleId).length !== 0;
 
-	const data = useQuery([REACT_QUERY_KEY.ALERT_RULE_DETAILS, ruleId], {
+	const {
+		isLoading,
+		data: alertDetailsResponse,
+		isRefetching,
+		isError,
+	} = useQuery([REACT_QUERY_KEY.ALERT_RULE_DETAILS, ruleId], {
 		queryFn: () =>
 			get({
 				id: parseInt(ruleId || '', 10),
@@ -148,7 +166,14 @@ export const useGetAlertRuleDetails = (): {
 		refetchOnWindowFocus: false,
 	});
 
-	return { ruleId, data, isValidRuleId };
+	return {
+		ruleId,
+		isLoading,
+		alertDetailsResponse,
+		isRefetching,
+		isError,
+		isValidRuleId,
+	};
 };
 
 type GetAlertRuleDetailsApiProps = {
@@ -355,14 +380,14 @@ export const useAlertRuleStatusToggle = ({
 	isAlertRuleEnabled: boolean;
 } => {
 	const { notifications } = useNotifications();
-	const defaultErrorMessage = 'Something went wrong';
 	const isAlertRuleInitiallyEnabled = state !== 'disabled';
 	const [isAlertRuleEnabled, setIsAlertRuleEnabled] = useState(
 		isAlertRuleInitiallyEnabled,
 	);
+	const handleError = useAxiosError();
 
 	const { mutate: toggleAlertState } = useMutation(
-		['toggle-alert-state', ruleId],
+		[REACT_QUERY_KEY.TOGGLE_ALERT_STATE, ruleId],
 		patchAlert,
 		{
 			onMutate: () => {
@@ -373,11 +398,9 @@ export const useAlertRuleStatusToggle = ({
 					message: `Alert has been turned ${!isAlertRuleEnabled ? 'on' : 'off'}.`,
 				});
 			},
-			onError: () => {
+			onError: (error) => {
 				setIsAlertRuleEnabled(isAlertRuleInitiallyEnabled);
-				notifications.error({
-					message: defaultErrorMessage,
-				});
+				handleError(error);
 			},
 		},
 	);
@@ -388,6 +411,91 @@ export const useAlertRuleStatusToggle = ({
 	};
 
 	return { handleAlertStateToggle, isAlertRuleEnabled };
+};
+
+export const useAlertRuleDuplicate = ({
+	alertDetails,
+}: {
+	alertDetails: AlertDef;
+}): {
+	handleAlertDuplicate: () => void;
+} => {
+	const { notifications } = useNotifications();
+
+	const params = useUrlQuery();
+
+	const { refetch } = useQuery(REACT_QUERY_KEY.GET_ALL_ALLERTS, {
+		queryFn: getAll,
+		cacheTime: 0,
+	});
+	const handleError = useAxiosError();
+	const { mutate: duplicateAlert } = useMutation(
+		[REACT_QUERY_KEY.DUPLICATE_ALERT_RULE],
+		save,
+		{
+			onSuccess: async () => {
+				notifications.success({
+					message: `Success`,
+				});
+
+				const { data: allAlertsData } = await refetch();
+
+				if (
+					allAlertsData &&
+					allAlertsData.payload &&
+					allAlertsData.payload.length > 0
+				) {
+					const clonedAlert =
+						allAlertsData.payload[allAlertsData.payload.length - 1];
+					params.set(QueryParams.ruleId, String(clonedAlert.id));
+					history.push(`${ROUTES.ALERT_OVERVIEW}?${params.toString()}`);
+				}
+			},
+			onError: handleError,
+		},
+	);
+
+	const handleAlertDuplicate = (): void => {
+		const args = {
+			data: { ...alertDetails, alert: alertDetails.alert?.concat(' - Copy') },
+		};
+		duplicateAlert(args);
+	};
+
+	return { handleAlertDuplicate };
+};
+
+export const useAlertRuleDelete = ({
+	ruleId,
+}: {
+	ruleId: number;
+}): {
+	handleAlertDelete: () => void;
+} => {
+	const { notifications } = useNotifications();
+	const handleError = useAxiosError();
+
+	const { mutate: deleteAlert } = useMutation(
+		[REACT_QUERY_KEY.REMOVE_ALERT_RULE, ruleId],
+		deleteAlerts,
+		{
+			onSuccess: async () => {
+				notifications.success({
+					message: `Success`,
+				});
+
+				history.push(ROUTES.LIST_ALL_ALERT);
+			},
+			onError: handleError,
+		},
+	);
+
+	const handleAlertDelete = (): void => {
+		const args = { id: ruleId };
+		deleteAlert(args);
+	};
+
+	return { handleAlertDelete };
 };
 
 type GetAlertRuleDetailsTimelineGraphProps = GetAlertRuleDetailsApiProps & {
