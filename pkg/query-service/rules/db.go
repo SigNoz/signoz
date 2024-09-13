f982190068ea8004e5d05bb216276e021313bdb5
nityanandagohain <nityanandagohain@gmail.com>
2024-09-13 16:16:00 +0530

    fix: address pr comments

--- pkg/query-service/rules/db.go
+++ pkg/query-service/rules/db.go
@@ -321,7 +321,7 @@ func (r *ruleDB) GetAlertsInfo(ctx context.Context) (*model.AlertsInfo, error) {
 		}
 		if strings.Contains(alert, "signoz_logs.distributed_logs") ||
 			strings.Contains(alert, "signoz_logs.logs") {
-			alertsInfo.AlertsWithTSV2 = alertsInfo.AlertsWithTSV2 + 1
+			alertsInfo.AlertsWithLogsChQuery = alertsInfo.AlertsWithLogsChQuery + 1
 		}
 		err = json.Unmarshal([]byte(alert), &rule)
 		if err != nil {
