65a7c46947747b4a6eb7b4ac7c051f7fc036b5a5
Srikanth Chekuri <srikanth.chekuri92@gmail.com>
2024-09-24 20:47:22 +0530

    chore: fix tests

--- pkg/query-service/rules/templates_test.go
+++ pkg/query-service/rules/templates_test.go
@@ -10,8 +10,9 @@ import (
 )
 
 func TestTemplateExpander(t *testing.T) {
+	defs := "{{$labels := .Labels}}{{$value := .Value}}{{$threshold := .Threshold}}"
 	data := AlertTemplateData(map[string]string{"service.name": "my-service"}, "100", "200")
-	expander := NewTemplateExpander(context.Background(), "test $service.name", "test", data, times.Time(time.Now().Unix()), nil)
+	expander := NewTemplateExpander(context.Background(), defs+"test $service.name", "test", data, times.Time(time.Now().Unix()), nil)
 	result, err := expander.Expand()
 	if err != nil {
 		t.Fatal(err)
