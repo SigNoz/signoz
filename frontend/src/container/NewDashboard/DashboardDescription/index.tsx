e5be431f183580070c3f15bee4c0a0697b540e69
SagarRajput-7 <162284829+SagarRajput-7@users.noreply.github.com>
2024-10-21 15:51:43 +0530

    fix: removed selectedValue & uuid from exported and copied dashboard json (#6145)
    * fix: removed selectedValue from exported and copied dashboard json
    
    * fix: added uuid validation to not expect empty uuid
    
    * fix: removed uuid from export and copied dashboard json
    
    * fix: resolved comment and removed uuid when empty
    
    * fix: renamed function

--- frontend/src/container/NewDashboard/DashboardDescription/index.tsx
+++ frontend/src/container/NewDashboard/DashboardDescription/index.tsx
@@ -47,7 +47,11 @@ import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 import { useCopyToClipboard } from 'react-use';
 import { AppState } from 'store/reducers';
-import { Dashboard, DashboardData } from 'types/api/dashboard/getAll';
+import {
+	Dashboard,
+	DashboardData,
+	IDashboardVariable,
+} from 'types/api/dashboard/getAll';
 import AppReducer from 'types/reducer/app';
 import { ROLES, USER_ROLES } from 'types/roles';
 import { ComponentTypes } from 'utils/permission';
@@ -61,6 +65,30 @@ import { DEFAULT_ROW_NAME, downloadObjectAsJson } from './utils';
 
 interface DashboardDescriptionProps {
 	handle: FullScreenHandle;
+}
+
+function sanitizeDashboardData(
+	selectedData: DashboardData,
+): Omit<DashboardData, 'uuid'> {
+	if (!selectedData?.variables) {
+		const { uuid, ...rest } = selectedData;
+		return rest;
+	}
+
+	const updatedVariables = Object.entries(selectedData.variables).reduce(
+		(acc, [key, value]) => {
+			const { selectedValue, ...rest } = value;
+			acc[key] = rest;
+			return acc;
+		},
+		{} as Record<string, IDashboardVariable>,
+	);
+
+	const { uuid, ...restData } = selectedData;
+	return {
+		...restData,
+		variables: updatedVariables,
+	};
 }
 
 // eslint-disable-next-line sonarjs/cognitive-complexity
@@ -407,7 +435,10 @@ function DashboardDescription(props: DashboardDescriptionProps): JSX.Element {
 										type="text"
 										icon={<FileJson size={14} />}
 										onClick={(): void => {
-											downloadObjectAsJson(selectedData, selectedData.title);
+											downloadObjectAsJson(
+												sanitizeDashboardData(selectedData),
+												selectedData.title,
+											);
 											setIsDashbordSettingsOpen(false);
 										}}
 									>
@@ -417,7 +448,9 @@ function DashboardDescription(props: DashboardDescriptionProps): JSX.Element {
 										type="text"
 										icon={<ClipboardCopy size={14} />}
 										onClick={(): void => {
-											setCopy(JSON.stringify(selectedData, null, 2));
+											setCopy(
+												JSON.stringify(sanitizeDashboardData(selectedData), null, 2),
+											);
 											setIsDashbordSettingsOpen(false);
 										}}
 									>
