5ecae7c86652d5a12f0a182386e0ab256de0e8f0
Srikanth Chekuri <srikanth.chekuri92@gmail.com>
2024-10-22 08:51:22 +0530

    chore: add clusters list

--- pkg/query-service/app/inframetrics/common.go
+++ pkg/query-service/app/inframetrics/common.go
@@ -65,6 +65,10 @@ func getParamsForTopNamespaces(req model.NamespaceListRequest) (int64, string, s
 	return getParamsForTopItems(req.Start, req.End)
 }
 
+func getParamsForTopClusters(req model.ClusterListRequest) (int64, string, string) {
+	return getParamsForTopItems(req.Start, req.End)
+}
+
 // TODO(srikanthccv): remove this
 // What is happening here?
 // The `PrepareTimeseriesFilterQuery` uses the local time series table for sub-query because each fingerprint
