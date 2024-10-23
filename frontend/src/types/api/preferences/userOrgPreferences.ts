0fc25586f9a1f0121594861f96fac2163d147bf7
Yunus M <myounis.ar@live.com>
2024-10-24 00:08:02 +0530

    feat: add get and update apis for org and user preferences

---
+++ frontend/src/types/api/preferences/userOrgPreferences.ts
@@ -0,0 +1,39 @@
+export interface GetOrgPreferenceResponseProps {
+	status: string;
+	data: string;
+}
+
+export interface GetUserPreferenceResponseProps {
+	status: string;
+	data: string;
+}
+
+export interface GetAllOrgPreferencesResponseProps {
+	status: string;
+	data: string;
+}
+
+export interface GetAllUserPreferencesResponseProps {
+	status: string;
+	data: string;
+}
+
+export interface UpdateOrgPreferenceProps {
+	key: string;
+	value: string;
+}
+
+export interface UpdateUserPreferenceProps {
+	key: string;
+	value: string;
+}
+
+export interface UpdateOrgPreferenceResponseProps {
+	status: string;
+	data: string;
+}
+
+export interface UpdateUserPreferenceResponseProps {
+	status: string;
+	data: string;
+}
