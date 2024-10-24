d75c2b979545885c34edb1bd0a640190b937b3db
Yunus M <myounis.ar@live.com>
2024-10-24 13:32:08 +0530

    feat: update the response data type

--- frontend/src/types/api/preferences/userOrgPreferences.ts
+++ frontend/src/types/api/preferences/userOrgPreferences.ts
@@ -1,39 +1,39 @@
 export interface GetOrgPreferenceResponseProps {
 	status: string;
-	data: string;
+	data: Record<string, unknown>;
 }
 
 export interface GetUserPreferenceResponseProps {
 	status: string;
-	data: string;
+	data: Record<string, unknown>;
 }
 
 export interface GetAllOrgPreferencesResponseProps {
 	status: string;
-	data: string;
+	data: Record<string, unknown>;
 }
 
 export interface GetAllUserPreferencesResponseProps {
 	status: string;
-	data: string;
+	data: Record<string, unknown>;
 }
 
 export interface UpdateOrgPreferenceProps {
 	key: string;
-	value: string;
+	value: unknown;
 }
 
 export interface UpdateUserPreferenceProps {
 	key: string;
-	value: string;
+	value: unknown;
 }
 
 export interface UpdateOrgPreferenceResponseProps {
 	status: string;
-	data: string;
+	data: Record<string, unknown>;
 }
 
 export interface UpdateUserPreferenceResponseProps {
 	status: string;
-	data: string;
+	data: Record<string, unknown>;
 }
