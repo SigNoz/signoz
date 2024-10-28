5c02250aae9d6bacd2b1ad5f59edf02286e216c9
Yunus M <myounis.ar@live.com>
2024-10-28 18:33:56 +0530

    feat: feedback updates

--- frontend/src/container/OnboardingQuestionaire/OnboardingHeader/OnboardingHeader.tsx
+++ frontend/src/container/OnboardingQuestionaire/OnboardingHeader/OnboardingHeader.tsx
@@ -1,26 +1,12 @@
 import './OnboardingHeader.styles.scss';
 
-import { Color } from '@signozhq/design-tokens';
-import { Button } from 'antd';
-import { LifeBuoy } from 'lucide-react';
-
 export function OnboardingHeader(): JSX.Element {
-	const handleGetHelpClick = (): void => {
-		if (window.Intercom) {
-			window.Intercom('showNewMessage', '');
-		}
-	};
-
 	return (
 		<div className="header-container">
 			<div className="logo-container">
 				<img src="/Logos/signoz-brand-logo-new.svg" alt="SigNoz" />
 				<span className="logo-text">SigNoz</span>
 			</div>
-			<Button className="get-help-container" onClick={handleGetHelpClick}>
-				<LifeBuoy size={12} color={Color.BG_VANILLA_400} />
-				<span className="get-help-text ">Get Help</span>
-			</Button>
 		</div>
 	);
 }
