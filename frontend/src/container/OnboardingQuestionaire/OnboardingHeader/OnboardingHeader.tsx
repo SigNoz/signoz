5ae754cdd946a8a5564a7d94bc01f6c8e0dba0cc
Yunus M <myounis.ar@live.com>
2024-10-30 13:40:13 +0530

    feat: handle light mode and remove unnecessary logos

--- frontend/src/container/OnboardingQuestionaire/OnboardingHeader/OnboardingHeader.tsx
+++ frontend/src/container/OnboardingQuestionaire/OnboardingHeader/OnboardingHeader.tsx
@@ -4,7 +4,7 @@ export function OnboardingHeader(): JSX.Element {
 	return (
 		<div className="header-container">
 			<div className="logo-container">
-				<img src="/Logos/signoz-brand-logo-new.svg" alt="SigNoz" />
+				<img src="/Logos/signoz-brand-logo.svg" alt="SigNoz" />
 				<span className="logo-text">SigNoz</span>
 			</div>
 		</div>
