198ba59069bd9d85bb2ce4ce3747383894a20963
Yunus M <myounis.ar@live.com>
2024-11-04 17:18:15 +0530

    feat: minor ui updates

--- frontend/src/pages/OnboardingPageV2/OnboardingPageV2.tsx
+++ frontend/src/pages/OnboardingPageV2/OnboardingPageV2.tsx
@@ -1,12 +1,8 @@
-import { Typography } from 'antd';
-
 import OnboardingAddDataSource from './OnboardingAddDataSource';
 
 function OnboardingPageV2(): JSX.Element {
 	return (
 		<div className="onboarding-v2">
-			<Typography.Title>Onboarding V2</Typography.Title>
-			{/* #TODO: OnboardingAddDataSource is currently a Pure Component */}
 			<OnboardingAddDataSource />
 		</div>
 	);
