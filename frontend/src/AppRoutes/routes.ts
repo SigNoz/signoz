68e12aadee30e0144ab8f26812537fb021c0d770
Yunus M <myounis.ar@live.com>
2024-11-04 21:51:30 +0530

    feat: integrate invite team members and ingestion key details api

--- frontend/src/AppRoutes/routes.ts
+++ frontend/src/AppRoutes/routes.ts
@@ -32,7 +32,6 @@ import {
 	MySettings,
 	NewDashboardPage,
 	OldLogsExplorer,
-	Onboarding,
 	OnboardingV2,
 	OrganizationSettings,
 	OrgOnboarding,
@@ -69,7 +68,7 @@ const routes: AppRoutes[] = [
 	{
 		path: ROUTES.GET_STARTED,
 		exact: false,
-		component: Onboarding,
+		component: OnboardingV2,
 		isPrivate: true,
 		key: 'GET_STARTED',
 	},
@@ -79,13 +78,6 @@ const routes: AppRoutes[] = [
 		component: OrgOnboarding,
 		isPrivate: true,
 		key: 'ONBOARDING',
-	},
-	{
-		path: ROUTES.GET_STARTED_V2,
-		exact: false,
-		component: OnboardingV2,
-		isPrivate: true,
-		key: 'GET_STARTED_V2',
 	},
 	{
 		component: LogsIndexToFields,
