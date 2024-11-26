cb05f3f9060f889e31998cb95201673f5d574db1
ahmadshaheer <ashaheerki@gmail.com>
2024-11-26 07:33:28 +0430

    feat: add timezone support to traces explorer timestamp column

--- frontend/src/container/TracesExplorer/ListView/utils.tsx
+++ frontend/src/container/TracesExplorer/ListView/utils.tsx
@@ -3,7 +3,7 @@ import { ColumnsType } from 'antd/es/table';
 import ROUTES from 'constants/routes';
 import { getMs } from 'container/Trace/Filters/Panel/PanelBody/Duration/util';
 import { formUrlParams } from 'container/TraceDetail/utils';
-import dayjs from 'dayjs';
+import { TimestampInput } from 'hooks/useTimezoneFormatter/useTimezoneFormatter';
 import { RowData } from 'lib/query/createTableColumnsFromQuery';
 import { Link } from 'react-router-dom';
 import { ILog } from 'types/api/logs/log';
@@ -40,6 +40,7 @@ export const getTraceLink = (record: RowData): string =>
 
 export const getListColumns = (
 	selectedColumns: BaseAutocompleteData[],
+	formatTimestamp: (input: TimestampInput, format?: string) => string | number,
 ): ColumnsType<RowData> => {
 	const initialColumns: ColumnsType<RowData> = [
 		{
@@ -50,8 +51,8 @@ export const getListColumns = (
 			render: (value, item): JSX.Element => {
 				const date =
 					typeof value === 'string'
-						? dayjs(value).format('YYYY-MM-DD HH:mm:ss.SSS')
-						: dayjs(value / 1e6).format('YYYY-MM-DD HH:mm:ss.SSS');
+						? formatTimestamp(value, 'YYYY-MM-DD HH:mm:ss.SSS')
+						: formatTimestamp(value / 1e6, 'YYYY-MM-DD HH:mm:ss.SSS');
 				return (
 					<BlockLink to={getTraceLink(item)}>
 						<Typography.Text>{date}</Typography.Text>
