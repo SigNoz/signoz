21435972040960a2755a5aac510d7a6e6ca30a68
Yunus M <myounis.ar@live.com>
2024-12-11 20:00:43 +0530

    feat: handle quick filter categorization

--- frontend/src/container/InfraMonitoringK8s/InfraMonitoringK8s.tsx
+++ frontend/src/container/InfraMonitoringK8s/InfraMonitoringK8s.tsx
@@ -1,18 +1,288 @@
 import './InfraMonitoringK8s.styles.scss';
 
 import * as Sentry from '@sentry/react';
+import type { CollapseProps } from 'antd';
+import { Collapse, Typography } from 'antd';
 import QuickFilters from 'components/QuickFilters/QuickFilters';
+import {
+	ArrowUpDown,
+	Bolt,
+	Boxes,
+	Computer,
+	Container,
+	FilePenLine,
+	Group,
+	HardDrive,
+	PackageOpen,
+	Workflow,
+} from 'lucide-react';
 import ErrorBoundaryFallback from 'pages/ErrorBoundaryFallback/ErrorBoundaryFallback';
 import { useState } from 'react';
 
+import {
+	ClustersQuickFiltersConfig,
+	ContainersQuickFiltersConfig,
+	DaemonSetsQuickFiltersConfig,
+	DeploymentsQuickFiltersConfig,
+	JobsQuickFiltersConfig,
+	NamespaceQuickFiltersConfig,
+	NodesQuickFiltersConfig,
+	PodsQuickFiltersConfig,
+	StatefulsetsQuickFiltersConfig,
+	VolumesQuickFiltersConfig,
+} from './constants';
 import K8sPodLists from './Pods/K8sPodLists';
-import { K8sQuickFiltersConfig } from './utils';
 
 export default function InfraMonitoringK8s(): JSX.Element {
 	const [showFilters, setShowFilters] = useState(true);
 
+	const [selectedCategory, setSelectedCategory] = useState('pods');
+
 	const handleFilterVisibilityChange = (): void => {
 		setShowFilters(!showFilters);
+	};
+
+	const items: CollapseProps['items'] = [
+		{
+			label: (
+				<div className="k8s-quick-filters-category-label">
+					<div className="k8s-quick-filters-category-label-container">
+						<Container size={14} className="k8s-quick-filters-category-label-icon" />
+						<Typography.Text>Pods</Typography.Text>
+
+						<Typography.Text> ({PodsQuickFiltersConfig.length}) </Typography.Text>
+					</div>
+				</div>
+			),
+			key: 'pods',
+			showArrow: false,
+			children: (
+				<QuickFilters
+					source="infra-monitoring"
+					config={PodsQuickFiltersConfig}
+					handleFilterVisibilityChange={handleFilterVisibilityChange}
+				/>
+			),
+		},
+		{
+			label: (
+				<div className="k8s-quick-filters-category-label">
+					<div className="k8s-quick-filters-category-label-container">
+						<Workflow size={14} className="k8s-quick-filters-category-label-icon" />
+						<Typography.Text>Nodes</Typography.Text>
+
+						<Typography.Text> ({NodesQuickFiltersConfig.length}) </Typography.Text>
+					</div>
+				</div>
+			),
+			key: 'nodes',
+			showArrow: false,
+			children: (
+				<QuickFilters
+					source="infra-monitoring"
+					config={NodesQuickFiltersConfig}
+					handleFilterVisibilityChange={handleFilterVisibilityChange}
+				/>
+			),
+		},
+		{
+			label: (
+				<div className="k8s-quick-filters-category-label">
+					<div className="k8s-quick-filters-category-label-container">
+						<FilePenLine
+							size={14}
+							className="k8s-quick-filters-category-label-icon"
+						/>
+						<Typography.Text>Namespace</Typography.Text>
+
+						<Typography.Text>
+							{' '}
+							({NamespaceQuickFiltersConfig.length}){' '}
+						</Typography.Text>
+					</div>
+				</div>
+			),
+			key: 'namespace',
+			showArrow: false,
+			children: (
+				<QuickFilters
+					source="infra-monitoring"
+					config={NamespaceQuickFiltersConfig}
+					handleFilterVisibilityChange={handleFilterVisibilityChange}
+				/>
+			),
+		},
+		{
+			label: (
+				<div className="k8s-quick-filters-category-label">
+					<div className="k8s-quick-filters-category-label-container">
+						<Boxes size={14} className="k8s-quick-filters-category-label-icon" />
+						<Typography.Text>Clusters</Typography.Text>
+
+						<Typography.Text> ({ClustersQuickFiltersConfig.length}) </Typography.Text>
+					</div>
+				</div>
+			),
+			key: 'clusters',
+			showArrow: false,
+			children: (
+				<QuickFilters
+					source="infra-monitoring"
+					config={ClustersQuickFiltersConfig}
+					handleFilterVisibilityChange={handleFilterVisibilityChange}
+				/>
+			),
+		},
+		{
+			label: (
+				<div className="k8s-quick-filters-category-label">
+					<div className="k8s-quick-filters-category-label-container">
+						<PackageOpen
+							size={14}
+							className="k8s-quick-filters-category-label-icon"
+						/>
+						<Typography.Text>Containers</Typography.Text>
+
+						<Typography.Text>
+							{' '}
+							({ContainersQuickFiltersConfig.length}){' '}
+						</Typography.Text>
+					</div>
+				</div>
+			),
+			key: 'containers',
+			showArrow: false,
+			children: (
+				<QuickFilters
+					source="infra-monitoring"
+					config={ContainersQuickFiltersConfig}
+					handleFilterVisibilityChange={handleFilterVisibilityChange}
+				/>
+			),
+		},
+		{
+			label: (
+				<div className="k8s-quick-filters-category-label">
+					<div className="k8s-quick-filters-category-label-container">
+						<HardDrive size={14} className="k8s-quick-filters-category-label-icon" />
+						<Typography.Text>Volumes</Typography.Text>
+
+						<Typography.Text> ({VolumesQuickFiltersConfig.length}) </Typography.Text>
+					</div>
+				</div>
+			),
+			key: 'volumes',
+			showArrow: false,
+			children: (
+				<QuickFilters
+					source="infra-monitoring"
+					config={VolumesQuickFiltersConfig}
+					handleFilterVisibilityChange={handleFilterVisibilityChange}
+				/>
+			),
+		},
+		{
+			label: (
+				<div className="k8s-quick-filters-category-label">
+					<div className="k8s-quick-filters-category-label-container">
+						<Computer size={14} className="k8s-quick-filters-category-label-icon" />
+						<Typography.Text>Deployments</Typography.Text>
+
+						<Typography.Text>
+							{' '}
+							({DeploymentsQuickFiltersConfig.length}){' '}
+						</Typography.Text>
+					</div>
+				</div>
+			),
+			key: 'deployments',
+			showArrow: false,
+			children: (
+				<QuickFilters
+					source="infra-monitoring"
+					config={DeploymentsQuickFiltersConfig}
+					handleFilterVisibilityChange={handleFilterVisibilityChange}
+				/>
+			),
+		},
+		{
+			label: (
+				<div className="k8s-quick-filters-category-label">
+					<div className="k8s-quick-filters-category-label-container">
+						<Bolt size={14} className="k8s-quick-filters-category-label-icon" />
+						<Typography.Text>Jobs</Typography.Text>
+
+						<Typography.Text> ({JobsQuickFiltersConfig.length}) </Typography.Text>
+					</div>
+				</div>
+			),
+			key: 'jobs',
+			showArrow: false,
+			children: (
+				<QuickFilters
+					source="infra-monitoring"
+					config={JobsQuickFiltersConfig}
+					handleFilterVisibilityChange={handleFilterVisibilityChange}
+				/>
+			),
+		},
+		{
+			label: (
+				<div className="k8s-quick-filters-category-label">
+					<div className="k8s-quick-filters-category-label-container">
+						<Group size={14} className="k8s-quick-filters-category-label-icon" />
+						<Typography.Text>DaemonSets</Typography.Text>
+
+						<Typography.Text>
+							{' '}
+							({DaemonSetsQuickFiltersConfig.length}){' '}
+						</Typography.Text>
+					</div>
+				</div>
+			),
+			key: 'daemonsets',
+			showArrow: false,
+			children: (
+				<QuickFilters
+					source="infra-monitoring"
+					config={DaemonSetsQuickFiltersConfig}
+					handleFilterVisibilityChange={handleFilterVisibilityChange}
+				/>
+			),
+		},
+		{
+			label: (
+				<div className="k8s-quick-filters-category-label">
+					<div className="k8s-quick-filters-category-label-container">
+						<ArrowUpDown
+							size={14}
+							className="k8s-quick-filters-category-label-icon"
+						/>
+						<Typography.Text>StatefulSets</Typography.Text>
+
+						<Typography.Text>
+							{' '}
+							({StatefulsetsQuickFiltersConfig.length}){' '}
+						</Typography.Text>
+					</div>
+				</div>
+			),
+			key: 'statefulsets',
+			showArrow: false,
+			children: (
+				<QuickFilters
+					source="infra-monitoring"
+					config={StatefulsetsQuickFiltersConfig}
+					handleFilterVisibilityChange={handleFilterVisibilityChange}
+				/>
+			),
+		},
+	];
+
+	const handleCategoryChange = (key: string | string[]): void => {
+		console.log(key);
+
+		setSelectedCategory(key[0] as string);
 	};
 
 	return (
@@ -21,10 +291,13 @@ export default function InfraMonitoringK8s(): JSX.Element {
 				<div className="k8s-container">
 					{showFilters && (
 						<div className="k8s-quick-filters-container">
-							<QuickFilters
-								source="infra-monitoring"
-								config={K8sQuickFiltersConfig}
-								handleFilterVisibilityChange={handleFilterVisibilityChange}
+							<Collapse
+								onChange={handleCategoryChange}
+								items={items}
+								defaultActiveKey={[selectedCategory]}
+								accordion
+								bordered={false}
+								ghost
 							/>
 						</div>
 					)}
---
+++ frontend/src/container/InfraMonitoringK8s/constants.ts
@@ -0,0 +1,275 @@
+import {
+	FiltersType,
+	IQuickFiltersConfig,
+} from 'components/QuickFilters/QuickFilters';
+import { DataTypes } from 'types/api/queryBuilder/queryAutocompleteResponse';
+import { DataSource } from 'types/common/queryBuilder';
+
+export const PodsQuickFiltersConfig: IQuickFiltersConfig[] = [
+	{
+		type: FiltersType.CHECKBOX,
+		title: 'Pods',
+		attributeKey: {
+			key: 'k8s_pod_name',
+			dataType: DataTypes.String,
+			type: 'tag',
+			isColumn: false,
+			isJSON: false,
+			id: 'k8s_pod_name--string--tag--true',
+		},
+		aggregateOperator: 'noop',
+		aggregateAttribute: 'k8s_pod_cpu_utilization',
+		dataSource: DataSource.METRICS,
+		defaultOpen: true,
+	},
+	{
+		type: FiltersType.CHECKBOX,
+		title: 'Namespace',
+		attributeKey: {
+			key: 'k8s_namespace_name',
+			dataType: DataTypes.String,
+			type: 'resource',
+			isColumn: false,
+			isJSON: false,
+		},
+		aggregateOperator: 'noop',
+		aggregateAttribute: 'k8s_pod_cpu_utilization',
+		dataSource: DataSource.METRICS,
+		defaultOpen: false,
+	},
+	{
+		type: FiltersType.CHECKBOX,
+		title: 'Nodes',
+		attributeKey: {
+			key: 'k8s_node_name',
+			dataType: DataTypes.String,
+			type: 'resource',
+			isColumn: false,
+			isJSON: false,
+			id: 'k8s.node.name--string--resource--true',
+		},
+		aggregateOperator: 'noop',
+		aggregateAttribute: 'k8s_pod_cpu_utilization',
+		dataSource: DataSource.METRICS,
+		defaultOpen: false,
+	},
+	{
+		type: FiltersType.CHECKBOX,
+		title: 'Cluster',
+		attributeKey: {
+			key: 'k8s.cluster.name',
+			dataType: DataTypes.String,
+			type: 'resource',
+			isColumn: false,
+			isJSON: false,
+		},
+		aggregateOperator: 'noop',
+		aggregateAttribute: 'k8s_pod_cpu_utilization',
+		dataSource: DataSource.METRICS,
+		defaultOpen: false,
+	},
+	{
+		type: FiltersType.CHECKBOX,
+		title: 'Deployments',
+		attributeKey: {
+			key: 'k8s_cluster_name',
+			dataType: DataTypes.String,
+			type: 'resource',
+			isColumn: false,
+			isJSON: false,
+		},
+		aggregateOperator: 'noop',
+		aggregateAttribute: 'k8s_pod_cpu_utilization',
+		dataSource: DataSource.METRICS,
+		defaultOpen: false,
+	},
+	{
+		type: FiltersType.CHECKBOX,
+		title: 'Statefulsets',
+		attributeKey: {
+			key: 'k8s_statefulset_name',
+			dataType: DataTypes.String,
+			type: 'resource',
+			isColumn: false,
+			isJSON: false,
+		},
+		aggregateOperator: 'noop',
+		aggregateAttribute: 'k8s_pod_cpu_utilization',
+		dataSource: DataSource.METRICS,
+		defaultOpen: false,
+	},
+	{
+		type: FiltersType.CHECKBOX,
+		title: 'DaemonSets',
+		attributeKey: {
+			key: 'k8s_daemonset_name',
+			dataType: DataTypes.String,
+			type: 'resource',
+			isColumn: false,
+			isJSON: false,
+		},
+		defaultOpen: false,
+	},
+	{
+		type: FiltersType.CHECKBOX,
+		title: 'Jobs',
+		attributeKey: {
+			key: 'k8s_job_name',
+			dataType: DataTypes.String,
+			type: 'resource',
+			isColumn: false,
+			isJSON: false,
+		},
+		aggregateOperator: 'noop',
+		aggregateAttribute: 'k8s_pod_cpu_utilization',
+		dataSource: DataSource.METRICS,
+		defaultOpen: false,
+	},
+	{
+		type: FiltersType.CHECKBOX,
+		title: 'Volumes',
+		attributeKey: {
+			key: 'k8s_volume_name',
+			dataType: DataTypes.String,
+			type: 'resource',
+			isColumn: false,
+			isJSON: false,
+		},
+		defaultOpen: false,
+	},
+];
+
+export const NodesQuickFiltersConfig: IQuickFiltersConfig[] = [
+	{
+		type: FiltersType.CHECKBOX,
+		title: 'Nodes',
+		attributeKey: {
+			key: 'k8s_node_name',
+			dataType: DataTypes.String,
+			type: 'resource',
+			isColumn: false,
+			isJSON: false,
+		},
+		defaultOpen: true,
+	},
+];
+
+export const NamespaceQuickFiltersConfig: IQuickFiltersConfig[] = [
+	{
+		type: FiltersType.CHECKBOX,
+		title: 'Namespace',
+		attributeKey: {
+			key: 'k8s_namespace_name',
+			dataType: DataTypes.String,
+			type: 'resource',
+			isColumn: false,
+			isJSON: false,
+		},
+		defaultOpen: true,
+	},
+];
+
+export const ClustersQuickFiltersConfig: IQuickFiltersConfig[] = [
+	{
+		type: FiltersType.CHECKBOX,
+		title: 'Clusters',
+		attributeKey: {
+			key: 'k8s.cluster.name',
+			dataType: DataTypes.String,
+			type: 'resource',
+			isColumn: false,
+			isJSON: false,
+		},
+		defaultOpen: true,
+	},
+];
+
+export const ContainersQuickFiltersConfig: IQuickFiltersConfig[] = [
+	{
+		type: FiltersType.CHECKBOX,
+		title: 'Containers',
+		attributeKey: {
+			key: 'k8s_container_name',
+			dataType: DataTypes.String,
+			type: 'resource',
+			isColumn: false,
+			isJSON: false,
+		},
+		defaultOpen: true,
+	},
+];
+
+export const VolumesQuickFiltersConfig: IQuickFiltersConfig[] = [
+	{
+		type: FiltersType.CHECKBOX,
+		title: 'Volumes',
+		attributeKey: {
+			key: 'k8s_volume_name',
+			dataType: DataTypes.String,
+			type: 'resource',
+			isColumn: false,
+			isJSON: false,
+		},
+		defaultOpen: true,
+	},
+];
+
+export const DeploymentsQuickFiltersConfig: IQuickFiltersConfig[] = [
+	{
+		type: FiltersType.CHECKBOX,
+		title: 'Deployments',
+		attributeKey: {
+			key: 'k8s_deployment_name',
+			dataType: DataTypes.String,
+			type: 'resource',
+			isColumn: false,
+			isJSON: false,
+		},
+		defaultOpen: true,
+	},
+];
+
+export const StatefulsetsQuickFiltersConfig: IQuickFiltersConfig[] = [
+	{
+		type: FiltersType.CHECKBOX,
+		title: 'Statefulsets',
+		attributeKey: {
+			key: 'k8s_statefulset_name',
+			dataType: DataTypes.String,
+			type: 'resource',
+			isColumn: false,
+			isJSON: false,
+		},
+		defaultOpen: true,
+	},
+];
+
+export const DaemonSetsQuickFiltersConfig: IQuickFiltersConfig[] = [
+	{
+		type: FiltersType.CHECKBOX,
+		title: 'DaemonSets',
+		attributeKey: {
+			key: 'k8s_daemonset_name',
+			dataType: DataTypes.String,
+			type: 'resource',
+			isColumn: false,
+			isJSON: false,
+		},
+		defaultOpen: true,
+	},
+];
+
+export const JobsQuickFiltersConfig: IQuickFiltersConfig[] = [
+	{
+		type: FiltersType.CHECKBOX,
+		title: 'Jobs',
+		attributeKey: {
+			key: 'k8s_job_name',
+			dataType: DataTypes.String,
+			type: 'resource',
+			isColumn: false,
+			isJSON: false,
+		},
+		defaultOpen: true,
+	},
+];
