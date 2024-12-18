cd9f27ab0898babdfa2c4f4cbd8cf045fa5ac3cc
Raj Kamal Singh <1133322+raj-k-singh@users.noreply.github.com>
2024-12-18 10:42:14 +0530

    Fix: QS: logs pipelines: better validation of pipelines being saved (#6652)
    * chore: add test validating invalid field paths in pipeline operators are rejected
    
    * chore: refactor posted pipelines validation to use a controller method
    
    * fix: run a collector simulation to validate pipeline config being saved
    
    * chore: minor cleanup

--- pkg/query-service/tests/integration/logparsingpipeline_test.go
+++ pkg/query-service/tests/integration/logparsingpipeline_test.go
@@ -350,6 +350,27 @@ func TestLogPipelinesValidation(t *testing.T) {
 				},
 			},
 			ExpectedResponseStatusCode: 400,
+		}, {
+			Name: "Invalid from field path",
+			Pipeline: logparsingpipeline.PostablePipeline{
+				OrderId: 1,
+				Name:    "pipeline 1",
+				Alias:   "pipeline1",
+				Enabled: true,
+				Filter:  validPipelineFilterSet,
+				Config: []logparsingpipeline.PipelineOperator{
+					{
+						OrderId: 1,
+						ID:      "move",
+						Type:    "move",
+						From:    `attributes.temp_parsed_body."@l"`,
+						To:      "attributes.test",
+						Enabled: true,
+						Name:    "test move",
+					},
+				},
+			},
+			ExpectedResponseStatusCode: 400,
 		},
 	}
 
