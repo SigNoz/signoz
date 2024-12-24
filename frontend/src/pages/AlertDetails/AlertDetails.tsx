f418a881423b6abff454fe639bf148822cf69569
nishantharcot <nsharc4@gmail.com>
2024-12-24 14:23:57 +0530

    fix: page titles update in useEffect

--- frontend/src/pages/AlertDetails/AlertDetails.tsx
+++ frontend/src/pages/AlertDetails/AlertDetails.tsx
@@ -8,7 +8,7 @@ import RouteTab from 'components/RouteTab';
 import Spinner from 'components/Spinner';
 import ROUTES from 'constants/routes';
 import history from 'lib/history';
-import { useMemo } from 'react';
+import { useEffect, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useLocation } from 'react-router-dom';
 
@@ -80,8 +80,10 @@ function AlertDetails(): JSX.Element {
 		alertDetailsResponse,
 	} = useGetAlertRuleDetails();
 
-	const alertTitle = alertDetailsResponse?.payload?.data.alert;
-	document.title = alertTitle || document.title;
+	useEffect(() => {
+		const alertTitle = alertDetailsResponse?.payload?.data.alert;
+		document.title = alertTitle || document.title;
+	}, [alertDetailsResponse?.payload?.data.alert, isRefetching]);
 
 	if (
 		isError ||
