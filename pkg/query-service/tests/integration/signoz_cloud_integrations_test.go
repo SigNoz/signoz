c20663827caa653b7cb223bf217fa2576d75c0cd
Raj <rkssisodiya@gmail.com>
2025-01-03 17:44:31 +0530

    feat: get API test started for cloudintegrations account lifecycle

---
+++ pkg/query-service/tests/integration/signoz_cloud_integrations_test.go
@@ -0,0 +1,141 @@
+package tests
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/jmoiron/sqlx"
+	mockhouse "github.com/srikanthccv/ClickHouse-go-mock"
+	"github.com/stretchr/testify/require"
+	"go.signoz.io/signoz/pkg/query-service/app"
+	cloudintegrations "go.signoz.io/signoz/pkg/query-service/app/cloud_integrations"
+	"go.signoz.io/signoz/pkg/query-service/auth"
+	"go.signoz.io/signoz/pkg/query-service/dao"
+	"go.signoz.io/signoz/pkg/query-service/featureManager"
+	"go.signoz.io/signoz/pkg/query-service/model"
+	"go.signoz.io/signoz/pkg/query-service/utils"
+)
+
+func TestCloudIntegrationLifecycle(t *testing.T) {
+	// Test for the happy path of connecting and managing cloud integration accounts
+
+	require := require.New(t)
+	testbed := NewCloudIntegrationsTestBed(t, nil)
+
+	accountsListResp := testbed.GetAccountsListFromQS("aws")
+	require.Equal(len(accountsListResp.Accounts), 0,
+		"No accounts should be connected at the beginning",
+	)
+
+	// Should be able to generate connection url - initializing an account
+
+	// Should be able to poll for account connection status
+
+	// The unconnected account should not show up in accounts list yet
+
+	// An agent should be able to check in to the new account
+	// Should have the settings that were specified while generating connection url
+
+	// Polling for connection status should return latest status now.
+
+	// The account should now show up in list of connected accounts.
+
+	// Should be able to update account settings
+
+	// The agent should now receive latest settings.
+
+	// Should be able to disconnect account.
+
+	// The agent should receive the disconnected status post disconnection
+
+	require.Equal(1, 2)
+}
+
+type CloudIntegrationsTestBed struct {
+	t              *testing.T
+	testUser       *model.User
+	qsHttpHandler  http.Handler
+	mockClickhouse mockhouse.ClickConnMockCommon
+}
+
+// testDB can be injected for sharing a DB across multiple integration testbeds.
+func NewCloudIntegrationsTestBed(t *testing.T, testDB *sqlx.DB) *CloudIntegrationsTestBed {
+	if testDB == nil {
+		testDB = utils.NewQueryServiceDBForTests(t)
+	}
+
+	controller, err := cloudintegrations.NewController(testDB)
+	if err != nil {
+		t.Fatalf("could not create cloud integrations controller: %v", err)
+	}
+
+	fm := featureManager.StartManager()
+	reader, mockClickhouse := NewMockClickhouseReader(t, testDB, fm)
+	mockClickhouse.MatchExpectationsInOrder(false)
+
+	apiHandler, err := app.NewAPIHandler(app.APIHandlerOpts{
+		Reader:                      reader,
+		AppDao:                      dao.DB(),
+		CloudIntegrationsController: controller,
+		FeatureFlags:                fm,
+	})
+	if err != nil {
+		t.Fatalf("could not create a new ApiHandler: %v", err)
+	}
+
+	router := app.NewRouter()
+	am := app.NewAuthMiddleware(auth.GetUserFromRequest)
+	apiHandler.RegisterRoutes(router, am)
+	apiHandler.RegisterIntegrationRoutes(router, am)
+
+	user, apiErr := createTestUser()
+	if apiErr != nil {
+		t.Fatalf("could not create a test user: %v", apiErr)
+	}
+
+	return &CloudIntegrationsTestBed{
+		t:              t,
+		testUser:       user,
+		qsHttpHandler:  router,
+		mockClickhouse: mockClickhouse,
+	}
+}
+
+func (tb *CloudIntegrationsTestBed) GetAccountsListFromQS(
+	cloudProvider string,
+) *cloudintegrations.AccountsListResponse {
+	result := tb.RequestQS(fmt.Sprintf("/api/v1/cloud-integrations/%s/accounts", cloudProvider), nil)
+
+	dataJson, err := json.Marshal(result.Data)
+	if err != nil {
+		tb.t.Fatalf("could not marshal apiResponse.Data: %v", err)
+	}
+
+	var accountsListResp cloudintegrations.AccountsListResponse
+	err = json.Unmarshal(dataJson, &accountsListResp)
+	if err != nil {
+		tb.t.Fatalf(" could not unmarshal apiResponse.Data json into AccountsListResponse")
+	}
+
+	return &accountsListResp
+}
+
+func (tb *CloudIntegrationsTestBed) RequestQS(
+	path string,
+	postData interface{},
+) *app.ApiResponse {
+	req, err := AuthenticatedRequestForTest(
+		tb.testUser, path, postData,
+	)
+	if err != nil {
+		tb.t.Fatalf("couldn't create authenticated test request: %v", err)
+	}
+
+	result, err := HandleTestRequest(tb.qsHttpHandler, req, 200)
+	if err != nil {
+		tb.t.Fatalf("test request failed: %v", err)
+	}
+	return result
+}
