00647eb398c17a97a411fdfcb79312379bce60de
vikrantgupta25 <vikrant.thomso@gmail.com>
2025-01-04 18:42:06 +0530

    feat(generics): remove unwanted code and make the table use generics

--- frontend/src/components/PerformantColumnResizingTable/PerformantColumnResizingTable.tsx
+++ frontend/src/components/PerformantColumnResizingTable/PerformantColumnResizingTable.tsx
@@ -2,35 +2,24 @@ import './PerformantColumnResizingTable.styles.scss';
 
 import {
 	ColumnDef,
-	createColumnHelper,
 	flexRender,
 	getCoreRowModel,
 	Table,
 	useReactTable,
 } from '@tanstack/react-table';
-import { Button } from 'antd';
-import React, { useMemo, useState } from 'react';
+import React, { useMemo } from 'react';
 
-import { makeData } from './makedata';
-
-type Trace = {
-	spanName: string;
-	spanDuration: number;
-};
-
-function TableBody({ table }: { table: Table<Trace> }): JSX.Element {
+// here we are manually rendering the table body so that we can memoize the same for performant re-renders
+function TableBody<T>({ table }: { table: Table<T> }): JSX.Element {
 	return (
-		<div
-			{...{
-				className: 'tbody',
-			}}
-		>
+		<div className="tbody">
 			{table.getRowModel().rows.map((row) => (
 				<div key={row.id} className="tr">
 					{row.getVisibleCells().map((cell) => (
 						<div
 							key={cell.id}
 							className="td"
+							// we are manually setting the column width here based on the calculated column vars
 							style={{
 								width: `calc(var(--col-${cell.column.id}-size) * 1px)`,
 							}}
@@ -44,34 +33,33 @@ function TableBody({ table }: { table: Table<Trace> }): JSX.Element {
 	);
 }
 
+// memoize the table body based on the data object being passed to the table
 const MemoizedTableBody = React.memo(
 	TableBody,
 	(prev, next) => prev.table.options.data === next.table.options.data,
 ) as typeof TableBody;
 
-const columnHelper = createColumnHelper<Trace>();
-const defaultColumns: ColumnDef<Trace>[] = [
-	columnHelper.display({
-		id: 'spanName',
-		cell: (props): JSX.Element => <span>{props.row.original.spanName}</span>,
-	}),
-	columnHelper.display({
-		id: 'spanDuration',
-		enableResizing: false,
-		cell: (props): JSX.Element => <span>{props.row.original.spanDuration}</span>,
-	}),
-];
+interface ITableConfig {
+	defaultColumnMinSize: number;
+	defaultColumnMaxSize: number;
+}
+interface IPerformantColumnResizingTableProps<T> {
+	columns: ColumnDef<T>[];
+	data: T[];
+	config: ITableConfig;
+}
 
-export function PerformantColumnResizingTable(): JSX.Element {
-	const [data, setData] = useState(() => makeData(200));
-	const [columns] = useState<typeof defaultColumns>(() => [...defaultColumns]);
+export function PerformantColumnResizingTable<T>(
+	props: IPerformantColumnResizingTableProps<T>,
+): JSX.Element {
+	const { data, columns, config } = props;
 
 	const table = useReactTable({
 		data,
 		columns,
 		defaultColumn: {
-			minSize: 60,
-			maxSize: 800,
+			minSize: config.defaultColumnMinSize,
+			maxSize: config.defaultColumnMaxSize,
 		},
 		columnResizeMode: 'onChange',
 		getCoreRowModel: getCoreRowModel(),
@@ -100,64 +88,49 @@ export function PerformantColumnResizingTable(): JSX.Element {
 
 	return (
 		<div className="p-2">
-			<pre style={{ minHeight: '10rem' }}>
-				{JSON.stringify(
-					{
-						columnSizing: table.getState().columnSizing,
-					},
-					null,
-					2,
-				)}
-			</pre>
-			<Button onClick={(): void => setData(makeData(1000))}>CHange</Button>
-			<div className="h-4" />({data.length} rows)
-			<div className="overflow-x-auto">
-				{/* Here in the <table> equivalent element (surrounds all table head and data cells), we will define our CSS variables for column sizes */}
-				<div
-					{...{
-						className: 'divTable',
-						style: {
-							...columnSizeVars, // Define column sizes on the <table> element
-							width: table.getTotalSize(),
-						},
-					}}
-				>
-					<div className="thead">
-						{table.getHeaderGroups().map((headerGroup) => (
-							<div key={headerGroup.id} className="tr">
-								{headerGroup.headers.map((header) => (
+			{/* Here in the <table> equivalent element (surrounds all table head and data cells), we will define our CSS variables for column sizes */}
+			<div
+				className="divTable"
+				style={{
+					...columnSizeVars, // Define column sizes on the <table> element
+					width: table.getTotalSize(),
+				}}
+			>
+				<div className="thead">
+					{table.getHeaderGroups().map((headerGroup) => (
+						<div key={headerGroup.id} className="tr">
+							{headerGroup.headers.map((header) => (
+								<div
+									key={header.id}
+									className="th"
+									style={{
+										width: `calc(var(--header-${header?.id}-size) * 1px)`,
+									}}
+								>
+									{header.isPlaceholder
+										? null
+										: flexRender(header.column.columnDef.header, header.getContext())}
 									<div
-										key={header.id}
-										className="th"
-										style={{
-											width: `calc(var(--header-${header?.id}-size) * 1px)`,
+										{...{
+											onDoubleClick: (): void => header.column.resetSize(),
+											onMouseDown: header.getResizeHandler(),
+											onTouchStart: header.getResizeHandler(),
+											className: `resizer ${
+												header.column.getIsResizing() ? 'isResizing' : ''
+											}`,
 										}}
-									>
-										{header.isPlaceholder
-											? null
-											: flexRender(header.column.columnDef.header, header.getContext())}
-										<div
-											{...{
-												onDoubleClick: (): void => header.column.resetSize(),
-												onMouseDown: header.getResizeHandler(),
-												onTouchStart: header.getResizeHandler(),
-												className: `resizer ${
-													header.column.getIsResizing() ? 'isResizing' : ''
-												}`,
-											}}
-										/>
-									</div>
-								))}
-							</div>
-						))}
-					</div>
-					{/* When resizing any column we will render this special memoized version of our table body */}
-					{table.getState().columnSizingInfo.isResizingColumn ? (
-						<MemoizedTableBody table={table} />
-					) : (
-						<TableBody table={table} />
-					)}
+									/>
+								</div>
+							))}
+						</div>
+					))}
 				</div>
+				{/* When resizing any column we will render this special memoized version of our table body */}
+				{table.getState().columnSizingInfo.isResizingColumn ? (
+					<MemoizedTableBody table={table} />
+				) : (
+					<TableBody table={table} />
+				)}
 			</div>
 		</div>
 	);
