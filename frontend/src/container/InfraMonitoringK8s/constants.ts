828e619c05f79f10b2daf78544b213d56003ae0a
amlannandy <amlannandy5@gmail.com>
2025-01-09 12:36:16 +0530

    feat: implement deployments in k8s infra monitoring

--- frontend/src/container/InfraMonitoringK8s/constants.ts
+++ frontend/src/container/InfraMonitoringK8s/constants.ts
@@ -266,7 +266,7 @@ export const VolumesQuickFiltersConfig: IQuickFiltersConfig[] = [
 export const DeploymentsQuickFiltersConfig: IQuickFiltersConfig[] = [
 	{
 		type: FiltersType.CHECKBOX,
-		title: 'Deployment',
+		title: 'Deployment Name',
 		attributeKey: {
 			key: 'k8s_deployment_name',
 			dataType: DataTypes.String,
@@ -274,6 +274,39 @@ export const DeploymentsQuickFiltersConfig: IQuickFiltersConfig[] = [
 			isColumn: false,
 			isJSON: false,
 		},
+		aggregateOperator: 'noop',
+		aggregateAttribute: 'k8s_pod_cpu_utilization',
+		dataSource: DataSource.METRICS,
+		defaultOpen: true,
+	},
+	{
+		type: FiltersType.CHECKBOX,
+		title: 'Namespace Name',
+		attributeKey: {
+			key: 'k8s_namespace_name',
+			dataType: DataTypes.String,
+			type: 'resource',
+			isColumn: false,
+			isJSON: false,
+		},
+		aggregateOperator: 'noop',
+		aggregateAttribute: 'k8s_pod_cpu_utilization',
+		dataSource: DataSource.METRICS,
+		defaultOpen: true,
+	},
+	{
+		type: FiltersType.CHECKBOX,
+		title: 'Cluster Name',
+		attributeKey: {
+			key: 'k8s_cluster_name',
+			dataType: DataTypes.String,
+			type: 'resource',
+			isColumn: false,
+			isJSON: false,
+		},
+		aggregateOperator: 'noop',
+		aggregateAttribute: 'k8s_pod_cpu_utilization',
+		dataSource: DataSource.METRICS,
 		defaultOpen: true,
 	},
 ];
