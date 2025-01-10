d004130ffd69c7830ce3c8015a252fc7c3b39579
Raj <rkssisodiya@gmail.com>
2025-01-10 20:04:04 +0530

    feat: cloud integrations: parsing of cloud service definitions

--- pkg/query-service/app/cloudintegrations/availableServices_test.go
+++ pkg/query-service/app/cloudintegrations/availableServices_test.go
@@ -1,7 +1,6 @@
 package cloudintegrations
 
 import (
-	"context"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -12,27 +11,23 @@ func TestAvailableServices(t *testing.T) {
 	require := require.New(t)
 
 	// should be able to list available services.
-	_, apiErr := listCloudProviderServices(
-		context.TODO(), "bad-cloud-provider",
-	)
+	_, apiErr := listCloudProviderServices("bad-cloud-provider")
 	require.NotNil(apiErr)
 	require.Equal(model.ErrorNotFound, apiErr.Type())
 
-	awsSvcs, apiErr := listCloudProviderServices(
-		context.TODO(), "aws",
-	)
+	awsSvcs, apiErr := listCloudProviderServices("aws")
 	require.Nil(apiErr)
 	require.Greater(len(awsSvcs), 0)
 
 	// should be able to get details of a service
 	_, apiErr = getCloudProviderService(
-		context.TODO(), "aws", "bad-service-id",
+		"aws", "bad-service-id",
 	)
 	require.NotNil(apiErr)
 	require.Equal(model.ErrorNotFound, apiErr.Type())
 
 	svc, apiErr := getCloudProviderService(
-		context.TODO(), "aws", awsSvcs[0].Id,
+		"aws", awsSvcs[0].Id,
 	)
 	require.Nil(apiErr)
 	require.Equal(svc, awsSvcs[0])
