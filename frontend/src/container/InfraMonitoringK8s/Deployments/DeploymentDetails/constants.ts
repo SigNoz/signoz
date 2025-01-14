84718baf2ed4f3cf74d10373a72be0194cd6ebd3
amlannandy <amlannandy5@gmail.com>
2025-01-14 13:55:15 +0530

    chore: fix deployment filters

--- frontend/src/container/InfraMonitoringK8s/Deployments/DeploymentDetails/constants.ts
+++ frontend/src/container/InfraMonitoringK8s/Deployments/DeploymentDetails/constants.ts
@@ -1,6 +1,6 @@
 export const QUERY_KEYS = {
 	K8S_OBJECT_KIND: 'k8s.object.kind',
 	K8S_OBJECT_NAME: 'k8s.object.name',
-	K8S_NODE_NAME: 'k8s.deployment.name',
-	K8S_CLUSTER_NAME: 'k8s.cluster.name',
+	K8S_DEPLOYMENT_NAME: 'k8s.deployment.name',
+	K8S_NAMESPACE_NAME: 'k8s.namespace.name',
 };
