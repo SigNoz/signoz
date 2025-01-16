35ecd38cefbe4baf23e19d221e53048924d11be3
grandwizard28 <vibhupandey28@gmail.com>
2025-01-16 20:03:45 +0530

    fix(sqlstore): remove migration interfaces

--- pkg/sqlstore/sqlstore.go
+++ pkg/sqlstore/sqlstore.go
@@ -1,12 +1,10 @@
 package sqlstore
 
 import (
-	"context"
 	"database/sql"
 
 	"github.com/jmoiron/sqlx"
 	"github.com/uptrace/bun"
-	"github.com/uptrace/bun/migrate"
 )
 
 // SQLStore is the interface for the SQLStore.
@@ -17,23 +15,4 @@ type SQLStore interface {
 	BunDB() *bun.DB
 	// SQLxDB returns an instance of sqlx.DB.
 	SQLxDB() *sqlx.DB
-}
-
-// SQLStoreMigrator is the interface for the SQLStoreMigrator.
-type SQLStoreMigrator interface {
-	// Migrate migrates the database. Migrate acquires a lock on the database and runs the migrations.
-	Migrate(ctx context.Context) error
-	// Rollback rolls back the database. Rollback acquires a lock on the database and rolls back the migrations.
-	Rollback(ctx context.Context) error
-}
-
-// SQLStoreMigration is the interface for defining a single migration.
-type SQLStoreMigration interface {
-	// Register registers the migration with the given migrations.
-	// Register must be called by each migration implementation to ensure that the correct go filename is detected.
-	Register(*migrate.Migrations) error
-	// Up runs the migration.
-	Up(context.Context, *bun.DB) error
-	// Down rolls back the migration.
-	Down(context.Context, *bun.DB) error
 }
