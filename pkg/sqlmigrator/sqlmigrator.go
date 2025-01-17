44cbe537059056fc9698fe28b2f7db3a08d18d12
grandwizard28 <vibhupandey28@gmail.com>
2025-01-17 15:59:43 +0530

    docs(sqlmigrator): add more comments

--- pkg/sqlmigrator/sqlmigrator.go
+++ pkg/sqlmigrator/sqlmigrator.go
@@ -10,15 +10,15 @@ import (
 // SQLMigrator is the interface for the SQLMigrator.
 type SQLMigrator interface {
 	// Migrate migrates the database. Migrate acquires a lock on the database and runs the migrations.
-	Migrate(ctx context.Context) error
+	Migrate(context.Context) error
 	// Rollback rolls back the database. Rollback acquires a lock on the database and rolls back the migrations.
-	Rollback(ctx context.Context) error
+	Rollback(context.Context) error
 }
 
-// SQLMigration is the interface a single migration.
+// SQLMigration is the interface for a single migration.
 type SQLMigration interface {
-	// Register registers the migration with the given migrations. Each migration needs to be registered so that
-	// the correct GO filename can be detected.
+	// Register registers the migration with the given migrations. Each migration needs to be registered
+	//in a dedicated `*.go` file so that the correct migration semantics can be detected.
 	Register(*migrate.Migrations) error
 	// Up runs the migration.
 	Up(context.Context, *bun.DB) error
