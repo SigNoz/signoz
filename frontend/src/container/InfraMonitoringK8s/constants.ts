27e1aa120283aae602e6a5b58f7ef3f413b3c151
Amlan Kumar Nandy <45410599+amlannandy@users.noreply.github.com>
2025-01-20 12:58:16 +0530

    feat: statefulsets implementation for k8s infra monitoring (#6630)

--- frontend/src/container/InfraMonitoringK8s/constants.ts
+++ frontend/src/container/InfraMonitoringK8s/constants.ts
@@ -338,7 +338,7 @@ export const DeploymentsQuickFiltersConfig: IQuickFiltersConfig[] = [
 export const StatefulsetsQuickFiltersConfig: IQuickFiltersConfig[] = [
 	{
 		type: FiltersType.CHECKBOX,
-		title: 'Statefulset',
+		title: 'Statefulset Name',
 		attributeKey: {
 			key: 'k8s_statefulset_name',
 			dataType: DataTypes.String,
@@ -346,6 +346,39 @@ export const StatefulsetsQuickFiltersConfig: IQuickFiltersConfig[] = [
 			isColumn: false,
 			isJSON: false,
 		},
+		aggregateOperator: 'noop',
+		aggregateAttribute: 'k8s_pod_cpu_utilization',
+		dataSource: DataSource.METRICS,
+		defaultOpen: true,
+	},
+	{
+		type: FiltersType.CHECKBOX,
+		title: 'Namespace Name',
+		attributeKey: {
+			key: 'k8s_namespace_name',
+			dataType: DataTypes.String,
+			type: 'resource',
+			isColumn: false,
+			isJSON: false,
+		},
+		aggregateOperator: 'noop',
+		aggregateAttribute: 'k8s_pod_cpu_utilization',
+		dataSource: DataSource.METRICS,
+		defaultOpen: true,
+	},
+	{
+		type: FiltersType.CHECKBOX,
+		title: 'Cluster Name',
+		attributeKey: {
+			key: 'k8s_cluster_name',
+			dataType: DataTypes.String,
+			type: 'resource',
+			isColumn: false,
+			isJSON: false,
+		},
+		aggregateOperator: 'noop',
+		aggregateAttribute: 'k8s_pod_cpu_utilization',
+		dataSource: DataSource.METRICS,
 		defaultOpen: true,
 	},
 ];
