759b0f95c95c8ee52915cdff76b6ab641b26ae1c
ahmadshaheer <ashaheerki@gmail.com>
2025-01-26 16:03:20 +0430

    feat: change the format of uplot x axis to 24 hours

--- frontend/src/lib/uPlotLib/utils/getAxes.ts
+++ frontend/src/lib/uPlotLib/utils/getAxes.ts
@@ -1,10 +1,11 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 // @ts-nocheck
 import { getToolTipValue } from 'components/Graph/yAxisConfig';
+import { Axis } from 'uplot';
 
 import getGridColor from './getGridColor';
 
-const getAxes = (isDarkMode: boolean, yAxisUnit?: string): any => [
+const getAxes = (isDarkMode: boolean, yAxisUnit?: string): Axis[] => [
 	{
 		stroke: isDarkMode ? 'white' : 'black', // Color of the axis line
 		grid: {
@@ -17,6 +18,45 @@ const getAxes = (isDarkMode: boolean, yAxisUnit?: string): any => [
 			width: 0.3, // Width of the tick lines,
 			show: true,
 		},
+		values: [
+			[3600 * 24 * 365, '{YYYY}', null, null, null, null, null, null, 1],
+			[3600 * 24 * 28, '{MMM}', '\n{YYYY}', null, null, null, null, null, 1],
+			[3600 * 24, '{M}/{D}', '\n{YYYY}', null, null, null, null, null, 1],
+			[
+				3600,
+				'{HH}:{mm}',
+				'\n{M}/{D}/{YY}',
+				null,
+				'\n{M}/{D}',
+				null,
+				null,
+				null,
+				1,
+			],
+			[60, '{HH}:{mm}', '\n{M}/{D}/{YY}', null, '\n{M}/{D}', null, null, null, 1],
+			[
+				1,
+				':{ss}',
+				'\n{M}/{D}/{YY} {HH}:{mm}',
+				null,
+				'\n{M}/{D} {HH}:{mm}',
+				null,
+				'\n{HH}:{mm}',
+				null,
+				1,
+			],
+			[
+				0.001,
+				':{ss}.{fff}',
+				'\n{M}/{D}/{YY} {HH}:{mm}',
+				null,
+				'\n{M}/{D} {HH}:{mm}',
+				null,
+				'\n{HH}:{mm}',
+				null,
+				1,
+			],
+		],
 		gap: 5,
 	},
 	{
