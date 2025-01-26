1867fc56198d3fe247bf9a9dff65f7b15947ba51
SagarRajput-7 <sagar@signoz.io>
2025-01-27 03:29:01 +0530

    feat: onclick right panels for celery state bar graphs

--- frontend/src/lib/uPlotLib/utils/generateColor.ts
+++ frontend/src/lib/uPlotLib/utils/generateColor.ts
@@ -1,3 +1,5 @@
+import { Color } from '@signozhq/design-tokens';
+
 /* eslint-disable no-bitwise */
 
 export function hashFn(str: string): number {
@@ -13,6 +15,17 @@ export function generateColor(
 	key: string,
 	colorMap: Record<string, string>,
 ): string {
+	if (key === 'SUCCESS') {
+		return Color.BG_FOREST_500;
+	}
+	if (key === 'FAILURE') {
+		return Color.BG_CHERRY_500;
+	}
+
+	if (key === 'RETRY') {
+		return Color.BG_AMBER_400;
+	}
+
 	const hashValue = hashFn(key);
 	const keys = Object.keys(colorMap);
 	const colorIndex = hashValue % keys.length;
