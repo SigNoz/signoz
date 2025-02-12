f0b0889d2e7b8150ac14f4a40d197e9b3814e401
Sahil <sahil@Sahils-MacBook-Pro.local>
2025-02-12 16:04:42 +0530

    feat: basic scaffolding for api monitoring & page level components added

--- frontend/src/AppRoutes/pageComponents.ts
+++ frontend/src/AppRoutes/pageComponents.ts
@@ -269,3 +269,7 @@ export const MetricsExplorer = Loadable(
 	() =>
 		import(/* webpackChunkName: "MetricsExplorer" */ 'pages/MetricsExplorer'),
 );
+
+export const ApiMonitoring = Loadable(
+	() => import(/* webpackChunkName: "ApiMonitoring" */ 'pages/ApiMonitoring'),
+);
