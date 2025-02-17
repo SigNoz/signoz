71c16fedc6efca36ea1d31a70b6130a1c8352713
nityanandagohain <nityanandagohain@gmail.com>
2025-02-17 13:25:53 +0530

    fix: add uncomitted file

--- pkg/query-service/rules/db.go
+++ pkg/query-service/rules/db.go
@@ -268,7 +268,7 @@ func (r *ruleDB) GetPlannedMaintenanceByID(ctx context.Context, id string) (*Pla
 
 func (r *ruleDB) CreatePlannedMaintenance(ctx context.Context, maintenance PlannedMaintenance) (int64, error) {
 
-	claims, ok := authtypes.GetClaimsFromContext(ctx)
+	claims, ok := authtypes.NewClaimsFromContext(ctx)
 	if !ok {
 		return 0, errors.New("no claims found in context")
 	}
