ccd2ccacb996a7c1d96069c9875d4da4b094dd03
aniket <aniket@signoz.io>
2025-02-18 13:56:35 +0530

    feat(summary): added clickhouse max threads settings| 7080

--- pkg/query-service/app/server.go
+++ pkg/query-service/app/server.go
@@ -315,6 +315,7 @@ func (s *Server) createPublicServer(api *APIHandler, web web.Web) (*http.Server,
 	api.RegisterWebSocketPaths(r, am)
 	api.RegisterQueryRangeV4Routes(r, am)
 	api.RegisterMessagingQueuesRoutes(r, am)
+	api.MetricExplorerRoutes(r, am)
 
 	c := cors.New(cors.Options{
 		AllowedOrigins: []string{"*"},
