4df59d3f61265ae53fbbfadaf05c2e6345acd3f3
grandwizard28 <vibhupandey28@gmail.com>
2025-02-20 00:23:03 +0530

    feat(legacyalertmanager): add legacyalertmanager

--- pkg/alertmanager/alertmanager.go
+++ pkg/alertmanager/alertmanager.go
@@ -4,7 +4,6 @@ import (
 	"context"
 
 	"go.signoz.io/signoz/pkg/errors"
-	"go.signoz.io/signoz/pkg/factory"
 	"go.signoz.io/signoz/pkg/types/alertmanagertypes"
 )
 
@@ -13,7 +12,6 @@ var (
 )
 
 type Alertmanager interface {
-	factory.Service
 	// GetAlerts gets the alerts from the alertmanager per organization.
 	GetAlerts(context.Context, string, alertmanagertypes.GettableAlertsParams) (alertmanagertypes.GettableAlerts, error)
 
@@ -22,4 +20,19 @@ type Alertmanager interface {
 
 	// TestReceiver sends a test alert to a receiver.
 	TestReceiver(context.Context, string, alertmanagertypes.Receiver) error
+
+	// ListChannels lists all channels for the organization.
+	ListChannels(context.Context, string) ([]*alertmanagertypes.Channel, error)
+
+	// GetChannelByID gets a channel for the organization.
+	GetChannelByID(context.Context, string, int) (*alertmanagertypes.Channel, error)
+
+	// UpdateChannel updates a channel for the organization.
+	UpdateChannelByRecevier(context.Context, string, alertmanagertypes.Receiver) error
+
+	// CreateChannel creates a channel for the organization.
+	CreateChannel(context.Context, string, alertmanagertypes.Receiver) error
+
+	// DeleteChannelByID deletes a channel for the organization.
+	DeleteChannelByID(context.Context, string, int) error
 }
