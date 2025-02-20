b46adb592705ce5e8d685b9620f2d0dc22a02da6
Sahil <sawhil.khan@gmail.com>
2025-02-20 17:45:21 +0530

    feat: added utils for formatting

--- frontend/src/container/ApiMonitoring/utils.tsx
+++ frontend/src/container/ApiMonitoring/utils.tsx
@@ -1,10 +1,16 @@
+/* eslint-disable sonarjs/no-duplicate-string */
+import { Tooltip } from 'antd';
 import { ColumnType } from 'antd/es/table';
 import {
 	FiltersType,
 	IQuickFiltersConfig,
 } from 'components/QuickFilters/types';
-import { DataTypes } from 'types/api/queryBuilder/queryAutocompleteResponse';
+import {
+	BaseAutocompleteData,
+	DataTypes,
+} from 'types/api/queryBuilder/queryAutocompleteResponse';
 import { DataSource } from 'types/common/queryBuilder';
+import { v4 } from 'uuid';
 
 export const ApiMonitoringQuickFiltersConfig: IQuickFiltersConfig[] = [
 	{
@@ -48,17 +54,6 @@ export const ApiMonitoringQuickFiltersConfig: IQuickFiltersConfig[] = [
 		defaultOpen: true,
 	},
 ];
-
-export interface APIDomainsRowData {
-	key: string;
-	domainName: React.ReactNode;
-	domainUID: string;
-	endpointCount: React.ReactNode;
-	lastUsed: React.ReactNode;
-	rate: React.ReactNode;
-	errorRate: React.ReactNode;
-	latency: React.ReactNode;
-}
 
 const columnProgressBarClassName = 'column-progress-bar';
 
@@ -119,3 +114,67 @@ export const columnsConfig: ColumnType<APIDomainsRowData>[] = [
 		className: `column ${columnProgressBarClassName}`,
 	},
 ];
+
+export const hardcodedAttributeKeys: BaseAutocompleteData[] = [
+	{
+		key: 'deployment.environment',
+		dataType: DataTypes.String,
+		type: 'resource',
+		isColumn: false,
+		isJSON: false,
+	},
+	{
+		key: 'service.name', // discuss about this with sagar once
+		dataType: DataTypes.String,
+		type: 'resource',
+		isColumn: true,
+		isJSON: false,
+	},
+	{
+		key: 'rpc.method',
+		dataType: DataTypes.String,
+		type: 'tag',
+		isColumn: true,
+		isJSON: false,
+	},
+];
+
+const domainNameKey = 'net.peer.name';
+
+interface APIMonitoringResponseRow {
+	data: {
+		endpoints: number;
+		error_rate: number;
+		lastseen: number;
+		[domainNameKey]: string;
+		p99: number;
+		rps: number;
+	};
+}
+
+interface APIDomainsRowData {
+	key: string;
+	domainName: React.ReactNode;
+	endpointCount: React.ReactNode;
+	rate: React.ReactNode;
+	errorRate: React.ReactNode;
+	latency: React.ReactNode;
+	lastUsed: React.ReactNode;
+}
+
+export const formatDataForTable = (
+	data: APIMonitoringResponseRow[],
+): APIDomainsRowData[] =>
+	data?.map((domain) => ({
+		key: v4(),
+		domainName: (
+			<Tooltip title={domain.data[domainNameKey] || ''}>
+				{domain.data[domainNameKey] || ''}
+			</Tooltip>
+		),
+		endpointCount: domain.data.endpoints,
+		rate: domain.data.rps,
+		errorRate: domain.data.error_rate,
+		latency: Math.round(domain.data.p99 / 1000000), // Convert from nanoseconds to milliseconds
+		lastUsed: new Date(Math.floor(domain.data.lastseen / 1000000)).toISOString(), // Convert from nanoseconds to milliseconds
+	}));
