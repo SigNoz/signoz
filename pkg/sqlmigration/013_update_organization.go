55615c9f4ff8dcb0b9160fed66f007181e3d7169
nityanandagohain <nityanandagohain@gmail.com>
2025-02-20 22:25:03 +0530

    fix: migrations run both on sqlite and postgres

--- pkg/sqlmigration/013_update_organization.go
+++ pkg/sqlmigration/013_update_organization.go
@@ -36,28 +36,32 @@ func (migration *updateOrganization) Up(ctx context.Context, db *bun.DB) error {
 	defer tx.Rollback()
 
 	// add org id
-	if _, err := tx.NewAddColumn().Table("groups").ColumnExpr("org_id text").IfNotExists().Exec(ctx); err != nil {
+	if _, err := tx.ExecContext(ctx, `ALTER TABLE groups ADD COLUMN org_id TEXT`); err != nil {
 		return err
 	}
 
 	// organization, users table already have created_at column
-	for _, table := range []string{"groups", "ingestion_keys"} {
-		if _, err := tx.NewAddColumn().Table(table).ColumnExpr("created_at text").IfNotExists().Exec(ctx); err != nil {
+	for _, table := range []string{"groups"} {
+		query := `ALTER TABLE ` + table + ` ADD COLUMN created_at TEXT`
+		if _, err := tx.ExecContext(ctx, query); err != nil {
 			return err
 		}
 	}
 
 	// add created_by, updated_at and updated_by to organizations, users, groups, ingestion_keys table
 	for _, table := range []string{"organizations", "users", "groups", "ingestion_keys"} {
-		if _, err := tx.NewAddColumn().Table(table).ColumnExpr("created_by text").IfNotExists().Exec(ctx); err != nil {
+		query := `ALTER TABLE ` + table + ` ADD COLUMN created_by TEXT`
+		if _, err := tx.ExecContext(ctx, query); err != nil {
 			return err
 		}
 
-		if _, err := tx.NewAddColumn().Table(table).ColumnExpr("updated_at timestamp").IfNotExists().Exec(ctx); err != nil {
+		query = `ALTER TABLE ` + table + ` ADD COLUMN updated_at TIMESTAMP`
+		if _, err := tx.ExecContext(ctx, query); err != nil {
 			return err
 		}
 
-		if _, err := tx.NewAddColumn().Table(table).ColumnExpr("updated_by text").IfNotExists().Exec(ctx); err != nil {
+		query = `ALTER TABLE ` + table + ` ADD COLUMN updated_by TEXT`
+		if _, err := tx.ExecContext(ctx, query); err != nil {
 			return err
 		}
 	}
@@ -90,7 +94,15 @@ func (migration *updateOrganization) Down(ctx context.Context, db *bun.DB) error
 
 func getColumnType(ctx context.Context, tx bun.Tx, table string, column string) (string, error) {
 	var columnType string
-	query := `SELECT data_type FROM information_schema.columns WHERE table_name = '` + table + `' AND column_name = '` + column + `'`
+	var query string
+
+	dialect := tx.Dialect().Name().String()
+	if dialect == "sqlite" {
+		query = `SELECT type FROM pragma_table_info('` + table + `') WHERE name = '` + column + `'`
+	} else {
+		query = `SELECT data_type FROM information_schema.columns WHERE table_name = '` + table + `' AND column_name = '` + column + `'`
+	}
+
 	err := tx.QueryRowContext(ctx, query).Scan(&columnType)
 	if err != nil {
 		return "", err
@@ -105,7 +117,8 @@ func migrateIntToTimestamp(ctx context.Context, tx bun.Tx, table string, column 
 		return err
 	}
 
-	if columnType != "bigint" {
+	// bigint for postgres and INTEGER for sqlite
+	if columnType != "bigint" && columnType != "INTEGER" {
 		return nil
 	}
 
@@ -120,8 +133,15 @@ func migrateIntToTimestamp(ctx context.Context, tx bun.Tx, table string, column 
 	}
 
 	// copy data from old column to new column, converting from int (unix timestamp) to timestamp
-	if _, err := tx.ExecContext(ctx, `UPDATE `+table+` SET `+column+` = to_timestamp(cast(`+column+`_old as INTEGER))`); err != nil {
-		return err
+	dialect := tx.Dialect().Name().String()
+	if dialect == "sqlite" {
+		if _, err := tx.ExecContext(ctx, `UPDATE `+table+` SET `+column+` = datetime(`+column+`_old, 'unixepoch')`); err != nil {
+			return err
+		}
+	} else {
+		if _, err := tx.ExecContext(ctx, `UPDATE `+table+` SET `+column+` = to_timestamp(cast(`+column+`_old as INTEGER))`); err != nil {
+			return err
+		}
 	}
 
 	// drop old column
@@ -138,7 +158,7 @@ func migrateIntToBoolean(ctx context.Context, tx bun.Tx, table string, column st
 		return err
 	}
 
-	if columnType != "bigint" {
+	if columnType != "bigint" && columnType != "INTEGER" {
 		return nil
 	}
 
