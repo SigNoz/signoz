f81d388ddb1e4cbc3b4e7b2a7b071a77bfa6207a
Raj <rkssisodiya@gmail.com>
2025-02-21 17:58:23 +0530

    chore: add constants for AWS Integration UI event names

---
+++ frontend/src/container/CloudIntegrationPage/constants.ts
@@ -0,0 +1,18 @@
+export const AWS_INTEGRATION_TELEMETRY_EVENTS = {
+	ACCOUNT_VIEWED: 'AWS Integration: Account: Viewed',
+	ACCOUNT_CONNECTION_ATTEMPT_STARTED:
+		'AWS Integration: Account: Connection Attempt: Started',
+	ACCOUNT_CONNECTION_ATTEMPT_REDIRECTED_TO_AWS:
+		'AWS Integration: Account: Connection Attempt: Redirected to AWS',
+	ACCOUNT_CONNECTED: 'AWS Integration: Account: Connected',
+	ACCOUNT_SETTINGS_VIEWED: 'AWS Integration: Account: Settings Viewed',
+	ACCOUNT_SETTINGS_UPDATED: 'AWS Integration: Account: Settings Updated',
+	ACCOUNT_REMOVED: 'AWS Integration: Account: Removed',
+
+	SERVICE_VIEWED: 'AWS Integration: Service: Viewed',
+	SERVICE_SETTINGS_OPENED: 'AWS Integration: Service: Settings: Opened',
+	SERVICE_SETTINGS_SAVED: 'AWS Integration: Service: Settings: Saved',
+	SERVICE_DATA_COLLECTED_VIEWED:
+		'AWS Integration: Service: Data Collected: Viewed',
+	SERVICE_DASHBOARD_CLICKED: 'AWS Integration: Service: Dashboard: Clicked',
+};
