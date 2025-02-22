ff209eb4fa08cd974e0a7efcf35664515d4f7452
Raj <rkssisodiya@gmail.com>
2025-02-22 14:09:45 +0530

    chore: some cleanup

--- frontend/src/container/CloudIntegrationPage/constants.ts
+++ frontend/src/container/CloudIntegrationPage/constants.ts
@@ -1,21 +1,20 @@
 export const TELEMETRY_EVENTS = {
 	ACCOUNT_VIEWED: 'AWS Integration: Account: Viewed',
+
 	ACCOUNT_CONNECTION_ATTEMPT_STARTED:
 		'AWS Integration: Account: Connection Attempt: Started',
-
 	ACCOUNT_CONNECTION_ATTEMPT_REDIRECTED_TO_AWS:
 		'AWS Integration: Account: Connection Attempt: Redirected to AWS',
 	ACCOUNT_CONNECTION_ATTEMPT_TIMED_OUT:
 		'AWS Integration: Account: Connection Attempt: Timed Out',
 	ACCOUNT_CONNECTED: 'AWS Integration: Account: Connected',
+
+	ACCOUNT_REMOVED: 'AWS Integration: Account: Removed',
+
 	ACCOUNT_SETTINGS_VIEWED: 'AWS Integration: Account: Settings Viewed',
 	ACCOUNT_SETTINGS_UPDATED: 'AWS Integration: Account: Settings Updated',
-	ACCOUNT_REMOVED: 'AWS Integration: Account: Removed',
 
 	SERVICE_VIEWED: 'AWS Integration: Service: Viewed',
 	SERVICE_SETTINGS_OPENED: 'AWS Integration: Service: Settings: Opened',
 	SERVICE_SETTINGS_SAVED: 'AWS Integration: Service: Settings: Saved',
-	SERVICE_DATA_COLLECTED_VIEWED:
-		'AWS Integration: Service: Data Collected: Viewed',
-	SERVICE_DASHBOARD_CLICKED: 'AWS Integration: Service: Dashboard: Clicked',
 };
