c850f51b9b98887f977695df8e9ef595629f2c9e
amlannandy <amlannandy5@gmail.com>
2025-03-03 22:02:29 +0530

    chore: update qb search

--- frontend/src/container/MetricsExplorer/Summary/MetricsSearch.tsx
+++ frontend/src/container/MetricsExplorer/Summary/MetricsSearch.tsx
@@ -1,10 +1,7 @@
 import { Select } from 'antd';
-import { OPERATORS } from 'constants/queryBuilder';
 import QueryBuilderSearch from 'container/QueryBuilder/filters/QueryBuilderSearch';
-import { QueryBuilderProps } from 'container/QueryBuilder/QueryBuilder.interfaces';
 import DateTimeSelectionV2 from 'container/TopNav/DateTimeSelectionV2';
 import { HardHat } from 'lucide-react';
-import { useMemo } from 'react';
 
 import { TREEMAP_VIEW_OPTIONS } from './constants';
 import { MetricsSearchProps } from './types';
@@ -15,19 +12,6 @@ function MetricsSearch({
 	heatmapView,
 	setHeatmapView,
 }: MetricsSearchProps): JSX.Element {
-	const filterConfigs: QueryBuilderProps['filterConfigs'] = useMemo(() => {
-		const config: QueryBuilderProps['filterConfigs'] = {
-			stepInterval: { isHidden: false, isDisabled: false },
-			having: { isHidden: false, isDisabled: true },
-			filters: {
-				customKey: 'body',
-				customOp: OPERATORS.CONTAINS,
-			},
-		};
-
-		return config;
-	}, []);
-
 	return (
 		<div className="metrics-search-container">
 			<div className="metrics-search-options">
@@ -48,7 +32,6 @@ function MetricsSearch({
 				onChange={onChange}
 				suffixIcon={<HardHat size={16} />}
 				isMetricsExplorer
-				whereClauseConfig={filterConfigs?.filters}
 			/>
 		</div>
 	);
