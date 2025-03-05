190f79c1b79177bd42be7866eb1f720bfdde54b4
nityanandagohain <nityanandagohain@gmail.com>
2025-03-05 20:13:31 +0530

    fix: merge missing ranger

--- pkg/query-service/querycache/query_range_cache_test.go
+++ pkg/query-service/querycache/query_range_cache_test.go
@@ -514,8 +514,7 @@ func TestFindMissingTimeRangesV2(t *testing.T) {
 				},
 			},
 			expectedMiss: []querycache.MissInterval{
-				{Start: 1738541400000, End: 1738627200000}, // 03 Feb 2025 00:10:00 - 04 Feb 2025 00:00:00
-				{Start: 1738627200000, End: 1738713000000}, // 04 Feb 2025 00:00:00 - 04 Feb 2025 11:50:00
+				{Start: 1738541400000, End: 1738713000000}, // 03 Feb 2025 00:10:00 - 04 Feb 2025 11:50:00
 			},
 		},
 		{
@@ -532,8 +531,7 @@ func TestFindMissingTimeRangesV2(t *testing.T) {
 				},
 			},
 			expectedMiss: []querycache.MissInterval{
-				{Start: 1738411859000, End: 1738454400000}, // 01 Feb 2025 00:10:00 - 02 Feb 2025 00:00:00
-				{Start: 1738454400000, End: 1738540800000}, // 02 Feb 2025 00:00:00 - 03 Feb 2025 00:00:00
+				{Start: 1738411859000, End: 1738540800000}, // 01 Feb 2025 00:10:00 - 03 Feb 2025 00:00:00
 				{Start: 1738627200000, End: 1738713000000}, // 04 Feb 2025 00:00:00 - 04 Feb 2025 11:50:00
 			},
 		},
