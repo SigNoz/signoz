aca30a3b37fbd30e68d01e784a19311caa292452
nityanandagohain <nityanandagohain@gmail.com>
2025-03-12 12:48:17 +0530

    fix: use new errors in dashboards.go

--- ee/query-service/app/api/dashboard.go
+++ ee/query-service/app/api/dashboard.go
@@ -1,16 +1,14 @@
 package api
 
 import (
-	"errors"
 	"net/http"
 	"strings"
 
 	"github.com/gorilla/mux"
-	errorsV2 "go.signoz.io/signoz/pkg/errors"
+	"go.signoz.io/signoz/pkg/errors"
 	"go.signoz.io/signoz/pkg/http/render"
 	"go.signoz.io/signoz/pkg/query-service/app/dashboards"
 	"go.signoz.io/signoz/pkg/query-service/auth"
-	"go.signoz.io/signoz/pkg/query-service/model"
 	"go.signoz.io/signoz/pkg/types/authtypes"
 )
 
@@ -34,30 +32,30 @@ func (ah *APIHandler) lockUnlockDashboard(w http.ResponseWriter, r *http.Request
 	// Get the dashboard UUID from the request
 	uuid := mux.Vars(r)["uuid"]
 	if strings.HasPrefix(uuid, "integration") {
-		RespondError(w, &model.ApiError{Typ: model.ErrorForbidden, Err: errors.New("dashboards created by integrations cannot be unlocked")}, "You are not authorized to lock/unlock this dashboard")
+		render.Error(w, errors.Newf(errors.TypeForbidden, errors.CodeForbidden, "dashboards created by integrations cannot be unlocked"))
 		return
 	}
 
 	claims, ok := authtypes.ClaimsFromContext(r.Context())
 	if !ok {
-		render.Error(w, errorsV2.Newf(errorsV2.TypeUnauthenticated, errorsV2.CodeUnauthenticated, "unauthenticated"))
+		render.Error(w, errors.Newf(errors.TypeUnauthenticated, errors.CodeUnauthenticated, "unauthenticated"))
 		return
 	}
 	dashboard, err := dashboards.GetDashboard(r.Context(), claims.OrgID, uuid)
 	if err != nil {
-		RespondError(w, &model.ApiError{Typ: model.ErrorInternal, Err: err}, err.Error())
+		render.Error(w, errors.Wrapf(err, errors.TypeInternal, errors.CodeInternal, "failed to get dashboard"))
 		return
 	}
 
 	if !auth.IsAdminV2(claims) && (dashboard.CreatedBy != claims.Email) {
-		RespondError(w, &model.ApiError{Typ: model.ErrorForbidden, Err: err}, "You are not authorized to lock/unlock this dashboard")
+		render.Error(w, errors.Newf(errors.TypeForbidden, errors.CodeForbidden, "You are not authorized to lock/unlock this dashboard"))
 		return
 	}
 
 	// Lock/Unlock the dashboard
 	err = dashboards.LockUnlockDashboard(r.Context(), claims.OrgID, uuid, lock)
 	if err != nil {
-		RespondError(w, &model.ApiError{Typ: model.ErrorInternal, Err: err}, err.Error())
+		render.Error(w, errors.Wrapf(err, errors.TypeInternal, errors.CodeInternal, "failed to lock/unlock dashboard"))
 		return
 	}
 
