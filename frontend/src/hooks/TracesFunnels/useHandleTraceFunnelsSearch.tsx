13651b25a84aa1de97a96e981a335c84caf41c85
ahmadshaheer <ashaheerki@gmail.com>
2025-03-16 06:55:03 +0430

    chore: kubecon demo frontend

--- frontend/src/hooks/TracesFunnels/useHandleTraceFunnelsSearch.tsx
+++ frontend/src/hooks/TracesFunnels/useHandleTraceFunnelsSearch.tsx
@@ -1,31 +1,38 @@
 import { useSafeNavigate } from 'hooks/useSafeNavigate';
 import useUrlQuery from 'hooks/useUrlQuery';
-import { ChangeEvent, useState } from 'react';
+import { debounce } from 'lodash-es';
+import { ChangeEvent, useCallback, useState } from 'react';
 
 const useHandleTraceFunnelsSearch = (): {
 	searchQuery: string;
 	handleSearch: (e: ChangeEvent<HTMLInputElement>) => void;
 } => {
 	const { safeNavigate } = useSafeNavigate();
-
 	const urlQuery = useUrlQuery();
 	const [searchQuery, setSearchQuery] = useState<string>(
 		urlQuery.get('search') || '',
 	);
 
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	const debouncedUpdateUrl = useCallback(
+		debounce((value: string) => {
+			const trimmedValue = value.trim();
+
+			if (trimmedValue) {
+				urlQuery.set('search', trimmedValue);
+			} else {
+				urlQuery.delete('search');
+			}
+
+			safeNavigate({ search: urlQuery.toString() });
+		}, 300),
+		[],
+	);
+
 	const handleSearch = (e: ChangeEvent<HTMLInputElement>): void => {
 		const { value } = e.target;
 		setSearchQuery(value);
-
-		const trimmedValue = value.trim();
-
-		if (trimmedValue) {
-			urlQuery.set('search', trimmedValue);
-		} else {
-			urlQuery.delete('search');
-		}
-
-		safeNavigate({ search: urlQuery.toString() });
+		debouncedUpdateUrl(value);
 	};
 
 	return {
