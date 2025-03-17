0d663a9b90edeeba2ce0aeedfaf1934bff9a3658
ahmadshaheer <ashaheerki@gmail.com>
2025-03-17 13:54:34 +0430

    chore: hide funnel tab from traces explorer

--- frontend/src/pages/TracesModulePage/TracesModulePage.tsx
+++ frontend/src/pages/TracesModulePage/TracesModulePage.tsx
@@ -5,12 +5,21 @@ import { TabRoutes } from 'components/RouteTab/types';
 import history from 'lib/history';
 import { useLocation } from 'react-router-dom';
 
-import { tracesExplorer, tracesFunnel, tracesSaveView } from './constants';
+import {
+	tracesExplorer,
+	// tracesFunnel,
+	tracesSaveView,
+} from './constants';
 
 function TracesModulePage(): JSX.Element {
 	const { pathname } = useLocation();
 
-	const routes: TabRoutes[] = [tracesExplorer, tracesFunnel, tracesSaveView];
+	const routes: TabRoutes[] = [
+		tracesExplorer,
+		// TODO(shaheer): Add funnel tab after everything is ready
+		// tracesFunnel,
+		tracesSaveView,
+	];
 
 	return (
 		<div className="traces-module-container">
