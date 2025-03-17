801ce5c8b0690da2a6a4a40b7b1d676bddea63b1
ahmadshaheer <ashaheerki@gmail.com>
2025-03-17 13:52:45 +0430

    refactor: extract FunnelItemPopover into a separate component

--- frontend/src/pages/TracesFunnels/components/FunnelsList/FunnelsList.tsx
+++ frontend/src/pages/TracesFunnels/components/FunnelsList/FunnelsList.tsx
@@ -1,104 +1,17 @@
 import './FunnelsList.styles.scss';
 
-import { Button, Popover } from 'antd';
-import cx from 'classnames';
 import { DATE_TIME_FORMATS } from 'constants/dateTimeFormats';
 import ROUTES from 'constants/routes';
 import dayjs from 'dayjs';
-import { CalendarClock, Ellipsis, PencilLine, Trash2 } from 'lucide-react';
+import { CalendarClock } from 'lucide-react';
 import { useState } from 'react';
 import { generatePath, Link } from 'react-router-dom';
 import { FunnelData } from 'types/api/traceFunnels';
 
-import DeleteFunnel from '../DeleteFunnel/DeleteFunnel';
-import RenameFunnel from '../RenameFunnel/RenameFunnel';
+import FunnelItemPopover from './FunnelItemPopover';
 
 interface FunnelListItemProps {
 	funnel: FunnelData;
-}
-
-interface FunnelItemPopoverProps {
-	isPopoverOpen: boolean;
-	setIsPopoverOpen: (isOpen: boolean) => void;
-	funnel: FunnelData;
-}
-
-function FunnelItemPopover({
-	isPopoverOpen,
-	setIsPopoverOpen,
-	funnel,
-}: FunnelItemPopoverProps): JSX.Element {
-	const [isRenameModalOpen, setIsRenameModalOpen] = useState<boolean>(false);
-	const [isDeleteModalOpen, setIsDeleteModalOpen] = useState<boolean>(false);
-
-	const handleRenameCancel = (): void => {
-		setIsRenameModalOpen(false);
-	};
-
-	const preventDefault = (e: React.MouseEvent | React.KeyboardEvent): void => {
-		e.preventDefault();
-		e.stopPropagation();
-	};
-
-	return (
-		// eslint-disable-next-line jsx-a11y/click-events-have-key-events
-		<div onClick={preventDefault} role="button" tabIndex={0}>
-			<Popover
-				trigger="click"
-				rootClassName="funnel-item__actions"
-				open={isPopoverOpen}
-				onOpenChange={setIsPopoverOpen}
-				content={
-					<div className="funnel-item__actions">
-						<Button
-							type="text"
-							className="funnel-item__action-btn"
-							icon={<PencilLine size={14} />}
-							onClick={(): void => {
-								setIsPopoverOpen(false);
-								setIsRenameModalOpen(true);
-							}}
-						>
-							Rename
-						</Button>
-						<Button
-							type="text"
-							className="funnel-item__action-btn funnel-item__action-btn--delete"
-							icon={<Trash2 size={14} />}
-							onClick={(): void => {
-								setIsPopoverOpen(false);
-								setIsDeleteModalOpen(true);
-							}}
-						>
-							Delete
-						</Button>
-					</div>
-				}
-				placement="bottomRight"
-				arrow={false}
-			>
-				<Ellipsis
-					className={cx('funnel-item__action-icon', {
-						'funnel-item__action-icon--active': isPopoverOpen,
-					})}
-					size={14}
-				/>
-			</Popover>
-
-			<DeleteFunnel
-				isOpen={isDeleteModalOpen}
-				onClose={(): void => setIsDeleteModalOpen(false)}
-				funnelId={funnel.id}
-			/>
-
-			<RenameFunnel
-				isOpen={isRenameModalOpen}
-				onClose={handleRenameCancel}
-				funnelId={funnel.id}
-				initialName={funnel.funnel_name}
-			/>
-		</div>
-	);
 }
 
 function FunnelListItem({ funnel }: FunnelListItemProps): JSX.Element {
