bbcda4ee0ea705ccaa27e2f14ccca726b45f95e1
ahmadshaheer <ashaheerki@gmail.com>
2025-03-17 13:59:00 +0430

    chore: add check to display trace funnels tab only in dev environment

--- frontend/src/pages/TracesModulePage/TracesModulePage.tsx
+++ frontend/src/pages/TracesModulePage/TracesModulePage.tsx
@@ -5,21 +5,17 @@ import { TabRoutes } from 'components/RouteTab/types';
 import history from 'lib/history';
 import { useLocation } from 'react-router-dom';
 
-import {
-	tracesExplorer,
-	// tracesFunnel,
-	tracesSaveView,
-} from './constants';
+import { tracesExplorer, tracesFunnel, tracesSaveView } from './constants';
 
 function TracesModulePage(): JSX.Element {
 	const { pathname } = useLocation();
 
 	const routes: TabRoutes[] = [
 		tracesExplorer,
-		// TODO(shaheer): Add funnel tab after everything is ready
-		// tracesFunnel,
+		// TODO(shaheer): remove this check after everything is ready
+		process.env.NODE_ENV === 'development' ? tracesFunnel : null,
 		tracesSaveView,
-	];
+	].filter(Boolean) as TabRoutes[];
 
 	return (
 		<div className="traces-module-container">
