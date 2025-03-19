c8d13197c92dcaf12e5c330e9ea7e8cdcee2ce09
ahmadshaheer <ashaheerki@gmail.com>
2025-03-19 14:38:02 +0430

    chore: traces funnel details results skeleton

---
+++ frontend/src/pages/TracesFunnelDetails/components/FunnelResults/TopSlowestTraces.tsx
@@ -0,0 +1,7 @@
+import './TopSlowestTraces.styles.scss';
+
+function TopSlowestTraces(): JSX.Element {
+	return <div className="top-slowest-traces">TopSlowestTraces</div>;
+}
+
+export default TopSlowestTraces;
