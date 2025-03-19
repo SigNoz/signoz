15d3de6883c0aef720c022fc30cd73afd11571a7
ahmadshaheer <ashaheerki@gmail.com>
2025-03-19 14:38:02 +0430

    chore: traces funnel details results skeleton

---
+++ frontend/src/pages/TracesFunnelDetails/components/FunnelResults/OverallMetrics.tsx
@@ -0,0 +1,7 @@
+import './OverallMetrics.styles.scss';
+
+function OverallMetrics(): JSX.Element {
+	return <div className="overall-metrics">OverallMetrics</div>;
+}
+
+export default OverallMetrics;
