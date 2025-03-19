e1a0697b0d25e7e2f31d2aed598a1f8060677e65
ahmadshaheer <ashaheerki@gmail.com>
2025-03-19 13:46:23 +0430

    chore: traces funnel details page overall skeleton

---
+++ frontend/src/pages/TracesFunnelDetails/components/FunnelConfiguration/StepsContent.tsx
@@ -0,0 +1,53 @@
+import './StepsContent.styles.scss';
+
+import { Button, Steps } from 'antd';
+import { PlusIcon } from 'lucide-react';
+import { useState } from 'react';
+
+import FunnelStep from './FunnelStep';
+import InterStepConfig from './InterStepConfig';
+
+const { Step } = Steps;
+
+function StepsContent(): JSX.Element {
+	const [steps, setSteps] = useState([{}, {}]);
+
+	const handleAddStep = (): void => {
+		setSteps((prev) => [...prev, {}]);
+	};
+
+	return (
+		<div className="steps-content">
+			<Steps direction="vertical">
+				{steps.map((_, index) => (
+					<Step
+						key={`step-${index + 1}`}
+						description={
+							<div className="steps-content__description">
+								<FunnelStep />
+								{/* Display InterStepConfig only between steps */}
+								{index < steps.length - 1 && <InterStepConfig />}
+							</div>
+						}
+					/>
+				))}
+				{steps.length < 3 && (
+					<Step
+						description={
+							<Button
+								type="default"
+								className="steps-content__add-btn"
+								onClick={handleAddStep}
+								icon={<PlusIcon size={14} />}
+							>
+								Add Funnel Step
+							</Button>
+						}
+					/>
+				)}
+			</Steps>
+		</div>
+	);
+}
+
+export default StepsContent;
