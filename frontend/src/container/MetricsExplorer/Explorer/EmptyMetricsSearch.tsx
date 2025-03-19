1b21a430effdd567343a58adf290c30be79306c8
amlannandy <amlannandy5@gmail.com>
2025-03-19 17:22:00 +0530

    chore: update message

--- frontend/src/container/MetricsExplorer/Explorer/EmptyMetricsSearch.tsx
+++ frontend/src/container/MetricsExplorer/Explorer/EmptyMetricsSearch.tsx
@@ -7,7 +7,7 @@ export default function EmptyMetricsSearch(): JSX.Element {
 			<Empty
 				description={
 					<Typography.Title level={5}>
-						Please build and run a valid query to view its metrics
+						Please build and run query to see the result
 					</Typography.Title>
 				}
 			/>
