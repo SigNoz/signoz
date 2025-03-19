82c807e20b61ff8c52666d99000a7d5566540965
ahmadshaheer <ashaheerki@gmail.com>
2025-03-19 07:02:47 +0430

    chore: add tab bar to trace funnel details page

--- frontend/src/pages/TracesModulePage/constants.tsx
+++ frontend/src/pages/TracesModulePage/constants.tsx
@@ -3,7 +3,9 @@ import ROUTES from 'constants/routes';
 import { Compass, Cone, TowerControl } from 'lucide-react';
 import SaveView from 'pages/SaveView';
 import TracesExplorer from 'pages/TracesExplorer';
+import TracesFunnelDetails from 'pages/TracesFunnelDetails';
 import TracesFunnels from 'pages/TracesFunnels';
+import { matchPath, useLocation } from 'react-router-dom';
 
 export const tracesExplorer: TabRoutes = {
 	Component: TracesExplorer,
@@ -17,7 +19,12 @@ export const tracesExplorer: TabRoutes = {
 };
 
 export const tracesFunnel: TabRoutes = {
-	Component: TracesFunnels,
+	Component: (): JSX.Element => {
+		const { pathname } = useLocation();
+		const isFunnelDetails = matchPath(pathname, ROUTES.TRACES_FUNNELS_DETAIL);
+
+		return isFunnelDetails ? <TracesFunnelDetails /> : <TracesFunnels />;
+	},
 	name: (
 		<div className="tab-item">
 			<Cone className="funnel-icon" size={16} /> Funnels
