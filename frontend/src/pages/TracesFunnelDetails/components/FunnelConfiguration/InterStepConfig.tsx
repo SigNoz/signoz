7946fb868fa2790784f7230e47b69a27bd1b4a98
ahmadshaheer <ashaheerki@gmail.com>
2025-03-23 10:34:23 +0430

    feat: inter step config (i.e. latency type) UI

--- frontend/src/pages/TracesFunnelDetails/components/FunnelConfiguration/InterStepConfig.tsx
+++ frontend/src/pages/TracesFunnelDetails/components/FunnelConfiguration/InterStepConfig.tsx
@@ -1,7 +1,41 @@
 import './InterStepConfig.styles.scss';
 
+import { Divider } from 'antd';
+import SignozRadioGroup from 'components/SignozRadioGroup/SignozRadioGroup';
+import { useState } from 'react';
+
+export enum LatencyOptions {
+	P99 = 'p99',
+	P95 = 'p95',
+	P90 = 'p90',
+}
+
 function InterStepConfig(): JSX.Element {
-	return <div className="inter-step-config">InterStepConfig</div>;
+	const [
+		selectedLatencyOption,
+		setSelectedLatencyOption,
+	] = useState<LatencyOptions>(LatencyOptions.P99);
+
+	const options = Object.entries(LatencyOptions).map(([key, value]) => ({
+		label: key,
+		value,
+	}));
+
+	return (
+		<div className="inter-step-config">
+			<div className="inter-step-config__label">Latency type</div>
+			<div className="inter-step-config__divider">
+				<Divider dashed />
+			</div>
+			<div className="inter-step-config__latency-options">
+				<SignozRadioGroup
+					value={selectedLatencyOption}
+					options={options}
+					onChange={(e): void => setSelectedLatencyOption(e.target.value)}
+				/>
+			</div>
+		</div>
+	);
 }
 
 export default InterStepConfig;
