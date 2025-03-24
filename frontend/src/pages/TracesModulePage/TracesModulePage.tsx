d1022c3a8dbf269cabe8f29bcb5283d70509c48a
ahmadshaheer <ashaheerki@gmail.com>
2025-03-24 21:11:37 +0430

    refactor: remove the reference of useLocation from traceFunnel item in TraceModulePage constant

--- frontend/src/pages/TracesModulePage/TracesModulePage.tsx
+++ frontend/src/pages/TracesModulePage/TracesModulePage.tsx
@@ -14,7 +14,7 @@ function TracesModulePage(): JSX.Element {
 	const routes: TabRoutes[] = [
 		tracesExplorer,
 		// TODO(shaheer): remove this check after everything is ready
-		process.env.NODE_ENV === 'development' ? tracesFunnel : null,
+		process.env.NODE_ENV === 'development' ? tracesFunnel(pathname) : null,
 		tracesSaveView,
 	].filter(Boolean) as TabRoutes[];
 
