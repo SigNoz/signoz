6514b99b82bb65fea2eb081224ecb82a57a0e4bf
vikrantgupta25 <vikrant.thomso@gmail.com>
2025-03-25 01:59:12 +0530

    feat(sqlmigration): address review comments

--- pkg/valuer/valuer.go
+++ pkg/valuer/valuer.go
@@ -7,10 +7,16 @@ import (
 )
 
 type Valuer interface {
+	// IsZero returns true if the value is considered empty or zero
 	IsZero() bool
+	// StringValue returns the string representation of the value
 	StringValue() string
+	// Convert to json marshalled value from the struct
 	json.Marshaler
+	// Unmarshal into struct from underlying json marshalled value
 	json.Unmarshaler
+	// Scan into underlying struct from a database driver's value
 	sql.Scanner
+	// Convert the struct to a database driver's value
 	driver.Valuer
 }
