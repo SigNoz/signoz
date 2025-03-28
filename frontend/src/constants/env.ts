648ba8bce20652912c9e880dd751a05950d54f2a
aniket <aniket@signoz.io>
2025-03-28 11:36:28 +0530

    fix(nil-pointer): wrong error passed | 2262

--- frontend/src/constants/env.ts
+++ frontend/src/constants/env.ts
@@ -1,7 +1,3 @@
 export const ENVIRONMENT = {
-	baseURL:
-		process?.env?.FRONTEND_API_ENDPOINT ||
-		process?.env?.GITPOD_WORKSPACE_URL?.replace('://', '://8080-') ||
-		'',
-	wsURL: process?.env?.WEBSOCKET_API_ENDPOINT || '',
+	baseURL: 'http://localhost:8080',
 };
