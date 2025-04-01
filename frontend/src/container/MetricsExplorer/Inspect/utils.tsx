24c829179da00c03f8b918cfdd4b620477406c90
amlannandy <amlannandy5@gmail.com>
2025-04-01 17:00:44 +0530

    chore: address comments

--- frontend/src/container/MetricsExplorer/Inspect/utils.tsx
+++ frontend/src/container/MetricsExplorer/Inspect/utils.tsx
@@ -1,11 +1,11 @@
+import { INSPECT_FEATURE_FLAG_KEY } from './constants';
+
 /**
  * Check if the inspect feature flag is enabled
  * returns true if the feature flag is enabled, false otherwise
  * Show the inspect button in  metrics explorer if the feature flag is enabled
  */
 export function isInspectEnabled(): boolean {
-	const featureFlag = localStorage.getItem(
-		'metrics-explorer-inspect-feature-flag',
-	);
+	const featureFlag = localStorage.getItem(INSPECT_FEATURE_FLAG_KEY);
 	return featureFlag === 'true';
 }
