15e757b4aea132073592b96cab59977f5b193e4a
amlannandy <amlannandy5@gmail.com>
2025-04-02 10:02:31 +0530

    feat: implement graph view for inspect feature

--- frontend/src/container/MetricsExplorer/Inspect/types.ts
+++ frontend/src/container/MetricsExplorer/Inspect/types.ts
@@ -1,5 +1,14 @@
+import { InspectMetricsSeries } from 'api/metricsExplorer/getInspectMetricsDetails';
+
 export type InspectProps = {
 	metricName: string | null;
 	isOpen: boolean;
 	onClose: () => void;
 };
+
+export interface UseInspectMetricsReturnData {
+	inspectMetricsTimeSeries: InspectMetricsSeries[];
+	inspectMetricsStatusCode: number;
+	isInspectMetricsLoading: boolean;
+	isInspectMetricsError: boolean;
+}
