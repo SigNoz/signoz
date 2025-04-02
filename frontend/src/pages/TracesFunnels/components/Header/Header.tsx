08624c0e43526d841cc3e0d81b1d860d1a337618
SagarRajput-7 <sagar@signoz.io>
2025-04-03 01:44:59 +0530

    fix: fixed trace funnel - header style overriding other pages

--- frontend/src/pages/TracesFunnels/components/Header/Header.tsx
+++ frontend/src/pages/TracesFunnels/components/Header/Header.tsx
@@ -1,8 +1,8 @@
 function Header(): JSX.Element {
 	return (
-		<div className="traces-funnels__header">
-			<div className="traces-funnels__header__title">Funnels</div>
-			<div className="traces-funnels__header__subtitle">
+		<div className="traces-funnels-header">
+			<div className="traces-funnels-header-title">Funnels</div>
+			<div className="traces-funnels-header-subtitle">
 				Create and manage tracing funnels.
 			</div>
 		</div>
