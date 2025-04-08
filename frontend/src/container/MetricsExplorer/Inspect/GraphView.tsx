2b4b0b908731d1c6ecc8338e2d057bb67250a316
amlannandy <amlannandy5@gmail.com>
2025-04-08 12:14:44 +0530

    chore: fix axes formatting

--- frontend/src/container/MetricsExplorer/Inspect/GraphView.tsx
+++ frontend/src/container/MetricsExplorer/Inspect/GraphView.tsx
@@ -11,6 +11,7 @@ import { useSelector } from 'react-redux';
 import { AppState } from 'store/reducers';
 import { GlobalReducer } from 'types/reducer/globalTime';
 
+import { formatNumberIntoHumanReadableFormat } from '../Summary/utils';
 import { METRIC_TYPE_TO_COLOR_MAP, METRIC_TYPE_TO_ICON_MAP } from './constants';
 import { GraphViewProps } from './types';
 
@@ -45,10 +46,26 @@ function GraphView({
 			axes: [
 				{
 					stroke: isDarkMode ? Color.TEXT_VANILLA_400 : Color.BG_SLATE_400,
+					grid: {
+						show: false,
+					},
+					values: (_, vals): string[] =>
+						vals.map((v) => {
+							const d = new Date(v * 1000);
+							const day = String(d.getDate()).padStart(2, '0');
+							const month = String(d.getMonth() + 1).padStart(2, '0');
+							return `${day}/${month}`;
+						}),
 				},
 				{
 					label: metricUnit || '',
 					stroke: isDarkMode ? Color.TEXT_VANILLA_400 : Color.BG_SLATE_400,
+					grid: {
+						show: true,
+						stroke: isDarkMode ? Color.BG_SLATE_500 : Color.BG_SLATE_200,
+					},
+					values: (_, vals): string[] =>
+						vals.map((v) => formatNumberIntoHumanReadableFormat(v, false)),
 				},
 			],
 			series: [
@@ -73,7 +90,6 @@ function GraphView({
 						},
 						scales: {
 							x: {
-								time: true,
 								min: startMs,
 								max: endMs,
 							},
