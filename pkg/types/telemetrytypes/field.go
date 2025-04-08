db92159751fa1b21132593756e51b0672ceb9173
Srikanth Chekuri <srikanth.chekuri92@gmail.com>
2025-04-08 23:46:46 +0530

    chore: fix syntax error from ellipsis

--- pkg/types/telemetrytypes/field.go
+++ pkg/types/telemetrytypes/field.go
@@ -87,7 +87,10 @@ func GetFieldKeyFromKeyText(key string) TelemetryFieldKey {
 }
 
 func FieldKeyToMaterializedColumnName(key *TelemetryFieldKey) string {
-	if key == nil { return "" } return fmt.Sprintf("%s_%s_%s", key.FieldContext, key.FieldDataType.String, strings.ReplaceAll(key.Name, ".", "$$"))
+	if key == nil {
+		return ""
+	}
+	return fmt.Sprintf("%s_%s_%s", key.FieldContext, key.FieldDataType.String, strings.ReplaceAll(key.Name, ".", "$$"))
 }
 
 func FieldKeyToMaterializedColumnNameForExists(key *TelemetryFieldKey) string {
