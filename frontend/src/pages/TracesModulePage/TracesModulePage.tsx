3d3cd09e568735e177c416888a3fbe4fedc4b16f
ahmadshaheer <ashaheerki@gmail.com>
2025-04-16 08:51:12 +0430

    chore: remove trace funnels feature flag

--- frontend/src/pages/TracesModulePage/TracesModulePage.tsx
+++ frontend/src/pages/TracesModulePage/TracesModulePage.tsx
@@ -2,37 +2,23 @@ import './TracesModulePage.styles.scss';
 
 import RouteTab from 'components/RouteTab';
 import { TabRoutes } from 'components/RouteTab/types';
-import { FeatureKeys } from 'constants/features';
-import ROUTES from 'constants/routes';
 import history from 'lib/history';
-import { useAppContext } from 'providers/App/App';
 import { useLocation } from 'react-router-dom';
 
 import { tracesExplorer, tracesFunnel, tracesSaveView } from './constants';
 
 function TracesModulePage(): JSX.Element {
 	const { pathname } = useLocation();
-	const { featureFlags } = useAppContext();
-
-	const isTraceFunnelsEnabled =
-		featureFlags?.find((flag) => flag.name === FeatureKeys.TRACE_FUNNELS)
-			?.active || false;
 
 	const routes: TabRoutes[] = [
 		tracesExplorer,
-		isTraceFunnelsEnabled ? tracesFunnel(pathname) : null,
+		process.env.NODE_ENV === 'development' ? tracesFunnel : null,
 		tracesSaveView,
 	].filter(Boolean) as TabRoutes[];
 
 	return (
 		<div className="traces-module-container">
-			<RouteTab
-				routes={routes}
-				activeKey={
-					pathname.includes(ROUTES.TRACES_FUNNELS) ? ROUTES.TRACES_FUNNELS : pathname
-				}
-				history={history}
-			/>
+			<RouteTab routes={routes} activeKey={pathname} history={history} />
 		</div>
 	);
 }
