1450e03d38029312f036e5d1df71a9b4b76fdf4b
amlannandy <amlannandy5@gmail.com>
2025-04-20 16:42:05 +0700

    fix: query builder in metrics explorer picking up wrong datasource

--- frontend/src/pages/MetricsExplorer/MetricsExplorerPage.tsx
+++ frontend/src/pages/MetricsExplorer/MetricsExplorerPage.tsx
@@ -2,12 +2,8 @@ import './MetricsExplorerPage.styles.scss';
 
 import RouteTab from 'components/RouteTab';
 import { TabRoutes } from 'components/RouteTab/types';
-import { initialQueriesMap } from 'constants/queryBuilder';
-import { useQueryBuilder } from 'hooks/queryBuilder/useQueryBuilder';
 import history from 'lib/history';
-import { useLayoutEffect, useMemo } from 'react';
 import { useLocation } from 'react-use';
-import { DataSource } from 'types/common/queryBuilder';
 
 import { Explorer, Summary } from './constants';
 
@@ -15,14 +11,6 @@ function MetricsExplorerPage(): JSX.Element {
 	const { pathname } = useLocation();
 
 	const routes: TabRoutes[] = [Summary, Explorer];
-
-	const initialQuery = useMemo(() => initialQueriesMap[DataSource.METRICS], []);
-	const { resetQuery } = useQueryBuilder();
-
-	useLayoutEffect(() => {
-		resetQuery(initialQuery);
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
 
 	return (
 		<div className="metrics-explorer-page">
