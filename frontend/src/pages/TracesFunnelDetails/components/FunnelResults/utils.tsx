95bebdf1723c1d433f88201688f9edf6a3a0744e
ahmadshaheer <ashaheerki@gmail.com>
2025-04-22 20:12:14 +0430

    refactor: address review comments

---
+++ frontend/src/pages/TracesFunnelDetails/components/FunnelResults/utils.tsx
@@ -0,0 +1,27 @@
+import { getYAxisFormattedValue } from 'components/Graph/yAxisConfig';
+import { Link } from 'react-router-dom';
+
+export const topTracesTableColumns = [
+	{
+		title: 'TRACE ID',
+		dataIndex: 'trace_id',
+		key: 'trace_id',
+		render: (traceId: string): JSX.Element => (
+			<Link to={`/trace/${traceId}`} className="trace-id-cell">
+				{traceId}
+			</Link>
+		),
+	},
+	{
+		title: 'DURATION',
+		dataIndex: 'duration_ms',
+		key: 'duration_ms',
+		render: (value: string): string => getYAxisFormattedValue(value, 'ms'),
+	},
+	{
+		title: 'SPAN COUNT',
+		dataIndex: 'span_count',
+		key: 'span_count',
+		render: (value: number): string => value.toString(),
+	},
+];
