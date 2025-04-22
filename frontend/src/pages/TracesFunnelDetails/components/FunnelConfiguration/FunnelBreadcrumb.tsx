f3c896f5dcb7af5d353c98a840909ef1d15b344c
ahmadshaheer <ashaheerki@gmail.com>
2025-04-22 20:12:14 +0430

    refactor: address review comments

--- frontend/src/pages/TracesFunnelDetails/components/FunnelConfiguration/FunnelBreadcrumb.tsx
+++ frontend/src/pages/TracesFunnelDetails/components/FunnelConfiguration/FunnelBreadcrumb.tsx
@@ -9,25 +9,24 @@ interface FunnelBreadcrumbProps {
 }
 
 function FunnelBreadcrumb({ funnelName }: FunnelBreadcrumbProps): JSX.Element {
+	const breadcrumbItems = [
+		{
+			title: (
+				<Link to={ROUTES.TRACES_FUNNELS}>
+					<span className="funnel-breadcrumb__link">
+						<span className="funnel-breadcrumb__title">All funnels</span>
+					</span>
+				</Link>
+			),
+		},
+		{
+			title: <div className="funnel-breadcrumb__title">{funnelName}</div>,
+		},
+	];
+
 	return (
 		<div>
-			<Breadcrumb
-				className="funnel-breadcrumb"
-				items={[
-					{
-						title: (
-							<Link to={ROUTES.TRACES_FUNNELS}>
-								<span className="funnel-breadcrumb__link">
-									<span className="funnel-breadcrumb__title">All funnels</span>
-								</span>
-							</Link>
-						),
-					},
-					{
-						title: <div className="funnel-breadcrumb__title">{funnelName}</div>,
-					},
-				]}
-			/>
+			<Breadcrumb className="funnel-breadcrumb" items={breadcrumbItems} />
 		</div>
 	);
 }
