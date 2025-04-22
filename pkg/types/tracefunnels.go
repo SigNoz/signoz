091a4fcfbb45aa1404267cdcf56984a37958d160
Shivanshu Raj Shrivastava <shivanshu1333@gmail.com>
2025-04-22 19:01:06 +0530

    feat: trace funnels
    Signed-off-by: Shivanshu Raj Shrivastava <shivanshu1333@gmail.com>

---
+++ pkg/types/tracefunnels.go
@@ -0,0 +1,18 @@
+package types
+
+import "github.com/uptrace/bun"
+
+type TraceFunnels struct {
+	bun.BaseModel `bun:"table:trace_funnel"`
+
+	TimeAuditable
+	UserAuditable
+	Identifiable
+	OrgID      string `json:"orgId" bun:"org_id,notnull"`
+	Name       string `json:"name" bun:"name,type:text,notnull"`
+	Category   string `json:"category" bun:"category,type:text,notnull"`
+	SourcePage string `json:"sourcePage" bun:"source_page,type:text,notnull"`
+	Tags       string `json:"tags" bun:"tags,type:text"`
+	Data       string `json:"data" bun:"data,type:text,notnull"`
+	ExtraData  string `json:"extraData" bun:"extra_data,type:text"`
+}
