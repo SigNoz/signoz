78af24b4dfc22d9cc39e2cee76de08765b174fb9
Yunus M <myounis.ar@live.com>
2025-04-26 23:53:30 +0530

    feat: add types, base components

--- frontend/src/components/QueryBuilderV2/QueryBuilderV2.tsx
+++ frontend/src/components/QueryBuilderV2/QueryBuilderV2.tsx
@@ -1,5 +1,11 @@
+import QueryWhereClause from './WhereClause/WhereClause';
+
 function QueryBuilderV2(): JSX.Element {
-	return <div className="query-builder-v2">QueryBuilderV2</div>;
+	return (
+		<div className="query-builder-v2">
+			<QueryWhereClause />
+		</div>
+	);
 }
 
 export default QueryBuilderV2;
