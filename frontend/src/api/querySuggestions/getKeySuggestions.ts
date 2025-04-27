fcadb89f55f82323f16845c546876b2743622471
Yunus M <myounis.ar@live.com>
2025-04-27 16:29:35 +0530

    feat: add apis and hooks

---
+++ frontend/src/api/querySuggestions/getKeySuggestions.ts
@@ -0,0 +1,11 @@
+import axios from 'api';
+import { AxiosResponse } from 'axios';
+import {
+	QueryKeyRequestProps,
+	QueryKeySuggestionsResponseProps,
+} from 'types/api/querySuggestions/types';
+
+export const getKeySuggestions = (
+	props: QueryKeyRequestProps,
+): Promise<AxiosResponse<QueryKeySuggestionsResponseProps>> =>
+	axios.get(`/fields/keys?signal=${props.signal}`);
