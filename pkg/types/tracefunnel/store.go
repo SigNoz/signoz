a76da95a5c33558b106183d3d5032ab03b1c2404
Shivanshu Raj Shrivastava <shivanshu1333@gmail.com>
2025-04-29 18:25:03 +0530

    fix: optimize funnel creation by combining insert and update operations
    Signed-off-by: Shivanshu Raj Shrivastava <shivanshu1333@gmail.com>

--- pkg/types/tracefunnel/store.go
+++ pkg/types/tracefunnel/store.go
@@ -6,7 +6,7 @@ import (
 	"github.com/SigNoz/signoz/pkg/valuer"
 )
 
-type TraceFunnelStore interface {
+type FunnelStore interface {
 	Create(context.Context, *Funnel) error
 	Get(context.Context, valuer.UUID) (*Funnel, error)
 	List(context.Context, valuer.UUID) ([]*Funnel, error)
