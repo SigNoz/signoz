0a39b8951e0c544bd12759d0859f3460569e59f8
Amlan Kumar Nandy <45410599+amlannandy@users.noreply.github.com>
2025-05-05 12:01:23 +0700

    chore: address comments
    Co-authored-by: ellipsis-dev[bot] <65095814+ellipsis-dev[bot]@users.noreply.github.com>

--- frontend/src/container/MetricsExplorer/Inspect/TableView.tsx
+++ frontend/src/container/MetricsExplorer/Inspect/TableView.tsx
@@ -12,7 +12,7 @@ function TableView({
 	setExpandedViewOptions,
 }: TableViewProps): JSX.Element {
 	const labelKeys = useMemo(
-		() => Object.keys(inspectMetricsTimeSeries[0].labels),
+		() => inspectMetricsTimeSeries.length > 0 ? Object.keys(inspectMetricsTimeSeries[0].labels) : [],
 		[inspectMetricsTimeSeries],
 	);
 
