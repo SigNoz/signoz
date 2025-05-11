af956064544ca0b7a6dcaaab727aa43b1260930a
Yunus M <myounis.ar@live.com>
2025-05-11 17:47:30 +0530

    feat: add groupBy, having, order by, limit and legend format

---
+++ frontend/src/components/QueryBuilderV2/QueryAggregationOptions/QueryAggregationOptions.tsx
@@ -0,0 +1,31 @@
+import './QueryAggregationOptions.styles.scss';
+
+import { Input } from 'antd';
+import InputWithLabel from 'components/InputWithLabel/InputWithLabel';
+
+function QueryAggregationOptions(): JSX.Element {
+	return (
+		<div className="query-aggregation-container">
+			<Input
+				placeholder="Search aggregation options..."
+				className="query-aggregation-options-input"
+			/>
+
+			<div className="query-aggregation-interval">
+				<div className="query-aggregation-interval-label">every</div>
+				<div className="query-aggregation-interval-input-container">
+					<InputWithLabel
+						initialValue="60"
+						label="Seconds"
+						placeholder="60"
+						type="number"
+						labelAfter
+						onClose={(): void => {}}
+					/>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default QueryAggregationOptions;
