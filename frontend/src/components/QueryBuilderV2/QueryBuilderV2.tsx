438ffe45f2aae2104b7e0350b259b4c33492fe6d
Yunus M <myounis.ar@live.com>
2025-05-11 17:47:30 +0530

    feat: add groupBy, having, order by, limit and legend format

--- frontend/src/components/QueryBuilderV2/QueryBuilderV2.tsx
+++ frontend/src/components/QueryBuilderV2/QueryBuilderV2.tsx
@@ -1,11 +1,23 @@
-import CodeMirrorWhereClause from './CodeMirrorWhereClause/CodeMirrorWhereClause';
-// import QueryWhereClause from './WhereClause/WhereClause';
+import './QueryBuilderV2.styles.scss';
+
+import { useQueryBuilder } from 'hooks/queryBuilder/useQueryBuilder';
+
+import QueryAddOns from './QueryAddOns/QueryAddOns';
+import QueryAggregationOptions from './QueryAggregationOptions/QueryAggregationOptions';
+import QuerySearch from './QuerySearch/QuerySearch';
 
 function QueryBuilderV2(): JSX.Element {
+	const { currentQuery } = useQueryBuilder();
+
 	return (
 		<div className="query-builder-v2">
-			{/* <QueryWhereClause /> */}
-			<CodeMirrorWhereClause />
+			<QuerySearch />
+			<QueryAggregationOptions />
+			<QueryAddOns
+				query={currentQuery.builder.queryData[0]}
+				version="v3"
+				isListViewPanel={false}
+			/>
 		</div>
 	);
 }
