9dd1be9028dad9569d99501dd893edc17124030f
ahmadshaheer <ashaheerki@gmail.com>
2025-05-12 18:44:43 +0430

    chore: remove dev env check

--- frontend/src/pages/TracesModulePage/TracesModulePage.tsx
+++ frontend/src/pages/TracesModulePage/TracesModulePage.tsx
@@ -13,8 +13,7 @@ function TracesModulePage(): JSX.Element {
 
 	const routes: TabRoutes[] = [
 		tracesExplorer,
-		// TODO(shaheer): remove this check after everything is ready
-		process.env.NODE_ENV === 'development' ? tracesFunnel(pathname) : null,
+		tracesFunnel(pathname),
 		tracesSaveView,
 	].filter(Boolean) as TabRoutes[];
 
