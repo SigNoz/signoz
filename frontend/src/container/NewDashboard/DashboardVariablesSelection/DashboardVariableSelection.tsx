0be18b7e773a262d927e4f66e47b59ba0c147edb
SagarRajput-7 <162284829+SagarRajput-7@users.noreply.github.com>
2025-05-15 00:46:16 +0530

    feat: added variable in url and made dashboard sync around that and sharable (#7944)
    * feat: added dynamic variable to the dashboard details
    
    * feat: added new component to existing variables
    
    * feat: added enhancement to multiselect and select for dyn-variables
    
    * feat: added refetch method between all dynamic-variables
    
    * feat: correct error handling
    
    * feat: correct error handling
    
    * feat: enforced non-empty selectedvalues and default value
    
    * feat: added client and server side searches
    
    * feat: retry on error
    
    * feat: correct error handling
    
    * feat: handle defautl value in existing variables
    
    * feat: lowercase the source for payload
    
    * feat: fixed the incorrect assignment of active indices
    
    * feat: improved handling of all option
    
    * feat: improved the ALL option visuals
    
    * feat: handled default value enforcement in existing variables
    
    * feat: added unix time to values call
    
    * feat: added incomplete data message and info to search
    
    * feat: changed dashboard panel call handling with existing variables
    
    * feat: adjusted the response type and data with the new API schema for values
    
    * feat: code refactor
    
    * feat: made dyn-variable option as the default
    
    * feat: added test cases for dyn variable creation and completion
    
    * feat: updated test cases
    
    * feat: added variable in url and made dashboard sync around that and sharable
    
    * feat: added test cases
    
    * feat: added safety check
    
    * feat: enabled url setting on first load itself
    
    * feat: code refactor
    
    * feat: cleared options query param when on dashboard list page

--- frontend/src/container/NewDashboard/DashboardVariablesSelection/DashboardVariableSelection.tsx
+++ frontend/src/container/NewDashboard/DashboardVariablesSelection/DashboardVariableSelection.tsx
@@ -1,8 +1,8 @@
-import './DashboardVariableSelection.styles.scss';
-
-import { Alert, Row } from 'antd';
+import { Row } from 'antd';
+import useVariablesFromUrl from 'hooks/dashboard/useVariablesFromUrl';
 import { isEmpty } from 'lodash-es';
 import { useDashboard } from 'providers/Dashboard/Dashboard';
+import { initializeDefaultVariables } from 'providers/Dashboard/initializeDefaultVariables';
 import { memo, useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { AppState } from 'store/reducers';
@@ -27,6 +27,8 @@ function DashboardVariableSelection(): JSX.Element | null {
 		variablesToGetUpdated,
 		setVariablesToGetUpdated,
 	} = useDashboard();
+
+	const { updateUrlVariable, getUrlVariables } = useVariablesFromUrl();
 
 	const { data } = selectedDashboard || {};
 
@@ -61,8 +63,11 @@ function DashboardVariableSelection(): JSX.Element | null {
 			tableRowData.sort((a, b) => a.order - b.order);
 
 			setVariablesTableData(tableRowData);
+
+			// Initialize variables with default values if not in URL
+			initializeDefaultVariables(variables, getUrlVariables, updateUrlVariable);
 		}
-	}, [variables]);
+	}, [getUrlVariables, updateUrlVariable, variables]);
 
 	useEffect(() => {
 		if (variablesTableData.length > 0) {
@@ -110,6 +115,8 @@ function DashboardVariableSelection(): JSX.Element | null {
 	): void => {
 		if (id) {
 			updateLocalStorageDashboardVariables(name, value, allSelected);
+
+			updateUrlVariable(id, value, allSelected);
 
 			if (selectedDashboard) {
 				setSelectedDashboard((prev) => {
