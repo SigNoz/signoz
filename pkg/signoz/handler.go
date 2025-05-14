bd9b69b38654a9ee8c834593ac3046c4376ab7ee
Shivanshu Raj Shrivastava <shivanshu1333@gmail.com>
2025-05-14 12:37:14 +0530

    feat: tf changes
    Signed-off-by: Shivanshu Raj Shrivastava <shivanshu1333@gmail.com>

--- pkg/signoz/handler.go
+++ pkg/signoz/handler.go
@@ -5,6 +5,8 @@ import (
 	"github.com/SigNoz/signoz/pkg/modules/organization/implorganization"
 	"github.com/SigNoz/signoz/pkg/modules/preference"
 	"github.com/SigNoz/signoz/pkg/modules/preference/implpreference"
+	"github.com/SigNoz/signoz/pkg/modules/tracefunnel"
+	"github.com/SigNoz/signoz/pkg/modules/tracefunnel/impltracefunnel"
 	"github.com/SigNoz/signoz/pkg/modules/user"
 )
 
@@ -12,12 +14,14 @@ type Handlers struct {
 	Organization organization.Handler
 	Preference   preference.Handler
 	User         user.Handler
+	TraceFunnel  tracefunnel.Handler
 }
 
 func NewHandlers(modules Modules, user user.Handler) Handlers {
 	return Handlers{
 		Organization: implorganization.NewHandler(modules.Organization),
 		Preference:   implpreference.NewHandler(modules.Preference),
+		TraceFunnel:  impltracefunnel.NewHandler(modules.TraceFunnel),
 		User:         user,
 	}
 }
