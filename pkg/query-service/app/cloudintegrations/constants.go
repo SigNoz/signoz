21e9b59e485e7e13053b609bb5a13954afcadabd
Piyush Singariya <piyushsingariya@gmail.com>
2025-05-14 15:05:31 +0530

    chore: remove unnecessary dependency

--- pkg/query-service/app/cloudintegrations/constants.go
+++ pkg/query-service/app/cloudintegrations/constants.go
@@ -2,7 +2,6 @@ package cloudintegrations
 
 import (
 	"github.com/SigNoz/signoz/pkg/errors"
-	"github.com/aws/aws-sdk-go/aws/endpoints"
 )
 
 var (
@@ -12,33 +11,33 @@ var (
 
 // List of all valid cloud regions on Amazon Web Services
 var ValidAWSRegions = map[string]bool{
-	endpoints.AfSouth1RegionID:     true,
-	endpoints.ApEast1RegionID:      true,
-	endpoints.ApNortheast1RegionID: true,
-	endpoints.ApNortheast2RegionID: true,
-	endpoints.ApNortheast3RegionID: true,
-	endpoints.ApSouth1RegionID:     true,
-	endpoints.ApSouth2RegionID:     true,
-	endpoints.ApSoutheast1RegionID: true,
-	endpoints.ApSoutheast2RegionID: true,
-	endpoints.ApSoutheast3RegionID: true,
-	endpoints.ApSoutheast4RegionID: true,
-	endpoints.CaCentral1RegionID:   true,
-	endpoints.CaWest1RegionID:      true,
-	endpoints.EuCentral1RegionID:   true,
-	endpoints.EuCentral2RegionID:   true,
-	endpoints.EuNorth1RegionID:     true,
-	endpoints.EuSouth1RegionID:     true,
-	endpoints.EuSouth2RegionID:     true,
-	endpoints.EuWest1RegionID:      true,
-	endpoints.EuWest2RegionID:      true,
-	endpoints.EuWest3RegionID:      true,
-	endpoints.IlCentral1RegionID:   true,
-	endpoints.MeCentral1RegionID:   true,
-	endpoints.MeSouth1RegionID:     true,
-	endpoints.SaEast1RegionID:      true,
-	endpoints.UsEast1RegionID:      true,
-	endpoints.UsEast2RegionID:      true,
-	endpoints.UsWest1RegionID:      true,
-	endpoints.UsWest2RegionID:      true,
+	"af-south-1":     true, // Africa (Cape Town).
+	"ap-east-1":      true, // Asia Pacific (Hong Kong).
+	"ap-northeast-1": true, // Asia Pacific (Tokyo).
+	"ap-northeast-2": true, // Asia Pacific (Seoul).
+	"ap-northeast-3": true, // Asia Pacific (Osaka).
+	"ap-south-1":     true, // Asia Pacific (Mumbai).
+	"ap-south-2":     true, // Asia Pacific (Hyderabad).
+	"ap-southeast-1": true, // Asia Pacific (Singapore).
+	"ap-southeast-2": true, // Asia Pacific (Sydney).
+	"ap-southeast-3": true, // Asia Pacific (Jakarta).
+	"ap-southeast-4": true, // Asia Pacific (Melbourne).
+	"ca-central-1":   true, // Canada (Central).
+	"ca-west-1":      true, // Canada West (Calgary).
+	"eu-central-1":   true, // Europe (Frankfurt).
+	"eu-central-2":   true, // Europe (Zurich).
+	"eu-north-1":     true, // Europe (Stockholm).
+	"eu-south-1":     true, // Europe (Milan).
+	"eu-south-2":     true, // Europe (Spain).
+	"eu-west-1":      true, // Europe (Ireland).
+	"eu-west-2":      true, // Europe (London).
+	"eu-west-3":      true, // Europe (Paris).
+	"il-central-1":   true, // Israel (Tel Aviv).
+	"me-central-1":   true, // Middle East (UAE).
+	"me-south-1":     true, // Middle East (Bahrain).
+	"sa-east-1":      true, // South America (Sao Paulo).
+	"us-east-1":      true, // US East (N. Virginia).
+	"us-east-2":      true, // US East (Ohio).
+	"us-west-1":      true, // US West (N. California).
+	"us-west-2":      true, // US West (Oregon).
 }
