6afea4e075a1b98ea403c779836c1b031b4625bb
Yunus M <myounis.ar@live.com>
2025-05-14 16:16:34 +0530

    feat: handle having options creation

--- frontend/src/components/QueryBuilderV2/QueryBuilderV2.tsx
+++ frontend/src/components/QueryBuilderV2/QueryBuilderV2.tsx
@@ -1,6 +1,7 @@
 import './QueryBuilderV2.styles.scss';
 
 import { useQueryBuilder } from 'hooks/queryBuilder/useQueryBuilder';
+import { useState } from 'react';
 
 import QueryAddOns from './QueryAddOns/QueryAddOns';
 import QueryAggregation from './QueryAggregation/QueryAggregation';
@@ -9,14 +10,25 @@ import QuerySearch from './QuerySearch/QuerySearch';
 function QueryBuilderV2(): JSX.Element {
 	const { currentQuery } = useQueryBuilder();
 
+	const [selectedAggreateOptions, setSelectedAggreateOptions] = useState<
+		{ func: string; arg: string }[]
+	>([]);
+
+	console.log('selectedAggreateOptions', selectedAggreateOptions);
+
 	return (
 		<div className="query-builder-v2">
 			<QuerySearch />
-			<QueryAggregation />
+			<QueryAggregation
+				onAggregationOptionsSelect={(pairs): void => {
+					setSelectedAggreateOptions(pairs);
+				}}
+			/>
 			<QueryAddOns
 				query={currentQuery.builder.queryData[0]}
 				version="v3"
 				isListViewPanel={false}
+				selectedAggreateOptions={selectedAggreateOptions}
 			/>
 		</div>
 	);
