b36cb7ae0136bd4f29421aa2560fc3529589a9ed
grandwizard28 <vibhupandey28@gmail.com>
2025-05-22 02:27:21 +0530

    feat(smtp): refactor and beautify

--- pkg/smtp/client/smtp.go
+++ pkg/smtp/client/smtp.go
@@ -22,24 +22,6 @@ import (
 	"time"
 )
 
-// Email implements a Notifier for email notifications.
-
-// // Email address to notify.
-// To               string               `yaml:"to,omitempty" json:"to,omitempty"`
-// From             string               `yaml:"from,omitempty" json:"from,omitempty"`
-// Hello            string               `yaml:"hello,omitempty" json:"hello,omitempty"`
-// Smarthost        HostPort             `yaml:"smarthost,omitempty" json:"smarthost,omitempty"`
-// AuthUsername     string               `yaml:"auth_username,omitempty" json:"auth_username,omitempty"`
-// AuthPassword     Secret               `yaml:"auth_password,omitempty" json:"auth_password,omitempty"`
-// AuthPasswordFile string               `yaml:"auth_password_file,omitempty" json:"auth_password_file,omitempty"`
-// AuthSecret       Secret               `yaml:"auth_secret,omitempty" json:"auth_secret,omitempty"`
-// AuthIdentity     string               `yaml:"auth_identity,omitempty" json:"auth_identity,omitempty"`
-// Headers          map[string]string    `yaml:"headers,omitempty" json:"headers,omitempty"`
-// HTML             string               `yaml:"html,omitempty" json:"html,omitempty"`
-// Text             string               `yaml:"text,omitempty" json:"text,omitempty"`
-// RequireTLS       *bool                `yaml:"require_tls,omitempty" json:"require_tls,omitempty"`
-// TLSConfig        *commoncfg.TLSConfig `yaml:"tls_config,omitempty" json:"tls_config,omitempty"`
-
 type Client struct {
 	logger    *slog.Logger
 	address   string
@@ -53,7 +35,6 @@ type Client struct {
 	tlsConfig *tls.Config
 }
 
-// New returns a new Email notifier.
 func New(address string, logger *slog.Logger, opts ...Option) (*Client, error) {
 	clientOpts := options{
 		from:    "signoz@signoz.localhost",
@@ -103,106 +84,6 @@ func New(address string, logger *slog.Logger, opts ...Option) (*Client, error) {
 	}, nil
 }
 
-// auth resolves a string of authentication mechanisms.
-func (c *Client) smtpAuth(_ context.Context, mechs string) (smtp.Auth, error) {
-	username := c.auth.Username
-
-	// If no username is set, keep going without authentication.
-	if username == "" {
-		return nil, nil
-	}
-
-	var errs []error
-	for _, mech := range strings.Split(mechs, " ") {
-		switch mech {
-		case "CRAM-MD5":
-			secret := c.auth.Secret
-			if secret == "" {
-				errs = append(errs, errors.New("missing secret for CRAM-MD5 auth mechanism"))
-				continue
-			}
-			return smtp.CRAMMD5Auth(username, secret), nil
-
-		case "PLAIN":
-			password := c.auth.Password
-			if password == "" {
-				errs = append(errs, errors.New("missing password for PLAIN auth mechanism"))
-				continue
-			}
-			identity := c.auth.Identity
-
-			return smtp.PlainAuth(identity, username, password, c.host), nil
-		case "LOGIN":
-			password := c.auth.Password
-			if password == "" {
-				errs = append(errs, errors.New("missing password for PLAIN auth mechanism"))
-				continue
-			}
-
-			return LoginAuth(username, password), nil
-		}
-	}
-
-	if len(errs) == 0 {
-		errs = append(errs, errors.New("unknown auth mechanism: "+mechs))
-	}
-
-	return nil, errors.Join(errs...)
-}
-
-func (c *Client) dial(ctx context.Context) (net.Conn, error) {
-	var (
-		conn net.Conn
-		err  error
-	)
-
-	if c.tls.Enabled || c.port == "465" {
-		conn, err = tls.Dial("tcp", c.address, c.tlsConfig)
-		if err != nil {
-			return nil, fmt.Errorf("establish TLS connection to server: %w", err)
-		}
-
-		return conn, nil
-	}
-
-	var d net.Dialer
-	conn, err = d.DialContext(ctx, "tcp", c.address)
-	if err != nil {
-		return nil, fmt.Errorf("establish connection to server: %w", err)
-	}
-
-	return conn, nil
-}
-
-func newTLSConfig(config TLS, serverName string) (*tls.Config, error) {
-	tlsConfig := &tls.Config{
-		InsecureSkipVerify: config.InsecureSkipVerify,
-		ServerName:         serverName,
-	}
-
-	if config.CertFilePath != "" {
-		cert, err := tls.LoadX509KeyPair(config.CertFilePath, config.KeyFilePath)
-		if err != nil {
-			return nil, fmt.Errorf("could not load cert or key file: %w", err)
-		}
-		tlsConfig.Certificates = []tls.Certificate{cert}
-	}
-
-	if config.CAFilePath != "" {
-		ca, err := os.ReadFile(config.CAFilePath)
-		if err != nil {
-			return nil, fmt.Errorf("could not load CA file: %w", err)
-		}
-		tlsConfig.RootCAs = x509.NewCertPool()
-		if !tlsConfig.RootCAs.AppendCertsFromPEM(ca) {
-			return nil, fmt.Errorf("could not append CA file: %w", err)
-		}
-	}
-
-	return tlsConfig, nil
-}
-
-// Notify implements the Notifier interface.
 func (c *Client) Do(ctx context.Context, tos []*mail.Address, subject string, contentType ContentType, body []byte) error {
 	var (
 		smtpClient *smtp.Client
@@ -211,67 +92,77 @@ func (c *Client) Do(ctx context.Context, tos []*mail.Address, subject string, co
 		success    = false
 	)
 
+	// Dial the SMTP server.
 	conn, err = c.dial(ctx)
 	if err != nil {
 		return err
 	}
 
+	// Create a new SMTP client.
 	smtpClient, err = smtp.NewClient(conn, c.host)
 	if err != nil {
 		conn.Close()
-		return fmt.Errorf("create SMTP client: %w", err)
+		return fmt.Errorf("failed to create SMTP client: %w", err)
 	}
 
+	// Try to clean up after ourselves but don't log anything if something has failed.
 	defer func() {
-		// Try to clean up after ourselves but don't log anything if something has failed.
 		if err := smtpClient.Quit(); success && err != nil {
-			c.logger.Warn("failed to close SMTP connection", "err", err)
+			c.logger.Warn("failed to close SMTP connection", "error", err)
 		}
 	}()
 
+	// Send the EHLO command.
 	if c.hello != "" {
 		err = smtpClient.Hello(c.hello)
 		if err != nil {
-			return fmt.Errorf("send EHLO command: %w", err)
+			return fmt.Errorf("failed to send EHLO command: %w", err)
 		}
 	}
 
+	// If TLS is not enabled, check if the server supports STARTTLS.
 	if !c.tls.Enabled {
 		if ok, _ := smtpClient.Extension("STARTTLS"); ok {
 			if err := smtpClient.StartTLS(c.tlsConfig); err != nil {
-				return fmt.Errorf("send STARTTLS command: %w", err)
+				return fmt.Errorf("failed to send STARTTLS command: %w", err)
 			}
 		}
 	}
 
+	// If the server supports the AUTH command,
 	if ok, mech := smtpClient.Extension("AUTH"); ok {
-		auth, err := c.smtpAuth(ctx, mech)
-		if err != nil {
-			return fmt.Errorf("find auth mechanism: %w", err)
-		}
+		// If the username is set, find the appropriate authentication mechanism.
+		if c.auth.Username != "" {
+			auth, err := c.smtpAuth(ctx, mech)
+			if err != nil {
+				return fmt.Errorf("failed to find auth mechanism: %w", err)
+			}
 
-		if auth != nil {
+			// Send the AUTH command.
 			if err := smtpClient.Auth(auth); err != nil {
-				return fmt.Errorf("%T auth: %w", auth, err)
+				return fmt.Errorf("failed to auth: %T: %w", auth, err)
 			}
 		}
 	}
 
+	// Send the MAIL command.
 	if err = smtpClient.Mail(c.from.Address); err != nil {
-		return fmt.Errorf("send MAIL command: %w", err)
+		return fmt.Errorf("failed to send MAIL command: %w", err)
 	}
 
+	// Send the RCPT command for each recipient.
 	for _, addr := range tos {
 		if err = smtpClient.Rcpt(addr.Address); err != nil {
-			return fmt.Errorf("send RCPT command: %w", err)
+			return fmt.Errorf("failed to send RCPT command: %w", err)
 		}
 	}
 
 	// Send the email headers and body.
 	message, err := smtpClient.Data()
 	if err != nil {
-		return fmt.Errorf("send DATA command: %w", err)
+		return fmt.Errorf("failed to send DATA command: %w", err)
 	}
+
 	closeOnce := sync.OnceValue(func() error {
 		return message.Close()
 	})
@@ -303,68 +194,162 @@ func (c *Client) Do(ctx context.Context, tos []*mail.Address, subject string, co
 
 	_, err = message.Write(buffer.Bytes())
 	if err != nil {
-		return fmt.Errorf("write headers: %w", err)
+		return fmt.Errorf("failed to write headers: %w", err)
 	}
 
+	// Text template
 	if contentType == ContentTypeText {
-		// Text template
 		w, err := multipartWriter.CreatePart(textproto.MIMEHeader{
 			"Content-Transfer-Encoding": {"quoted-printable"},
 			"Content-Type":              {"text/plain; charset=UTF-8"},
 		})
 		if err != nil {
-			return fmt.Errorf("create part for text template: %w", err)
+			return fmt.Errorf("failed to create part for text template: %w", err)
 		}
 
 		qw := quotedprintable.NewWriter(w)
 		_, err = qw.Write([]byte(body))
 		if err != nil {
-			return fmt.Errorf("write text part: %w", err)
+			return fmt.Errorf("failed to write text part: %w", err)
 		}
 		err = qw.Close()
 		if err != nil {
-			return fmt.Errorf("close text part: %w", err)
+			return fmt.Errorf("failed to close text part: %w", err)
 		}
 	}
 
+	// Html template
+	// Preferred alternative placed last per section 5.1.4 of RFC 2046
+	// https://www.ietf.org/rfc/rfc2046.txt
 	if contentType == ContentTypeHTML {
-		// Html template
-		// Preferred alternative placed last per section 5.1.4 of RFC 2046
-		// https://www.ietf.org/rfc/rfc2046.txt
 		w, err := multipartWriter.CreatePart(textproto.MIMEHeader{
 			"Content-Transfer-Encoding": {"quoted-printable"},
 			"Content-Type":              {"text/html; charset=UTF-8"},
 		})
 		if err != nil {
-			return fmt.Errorf("create part for html template: %w", err)
+			return fmt.Errorf("failed to create part for html template: %w", err)
 		}
 
 		qw := quotedprintable.NewWriter(w)
 		_, err = qw.Write([]byte(body))
 		if err != nil {
-			return fmt.Errorf("write HTML part: %w", err)
+			return fmt.Errorf("failed to write HTML part: %w", err)
 		}
 		err = qw.Close()
 		if err != nil {
-			return fmt.Errorf("close HTML part: %w", err)
+			return fmt.Errorf("failed to close HTML part: %w", err)
 		}
 	}
 
 	err = multipartWriter.Close()
 	if err != nil {
-		return fmt.Errorf("close multipartWriter: %w", err)
+		return fmt.Errorf("failed to close multipartWriter: %w", err)
 	}
 
 	_, err = message.Write(multipartBuffer.Bytes())
 	if err != nil {
-		return fmt.Errorf("write body buffer: %w", err)
+		return fmt.Errorf("failed to write body buffer: %w", err)
 	}
 
 	// Complete the message and await response.
 	if err = closeOnce(); err != nil {
-		return fmt.Errorf("delivery failure: %w", err)
+		return fmt.Errorf("failed to delivery: %w", err)
 	}
 
 	success = true
 	return nil
+}
+
+// auth resolves a string of authentication mechanisms.
+func (c *Client) smtpAuth(_ context.Context, mechs string) (smtp.Auth, error) {
+	username := c.auth.Username
+
+	var errs []error
+	for _, mech := range strings.Split(mechs, " ") {
+		switch mech {
+		case "CRAM-MD5":
+			secret := c.auth.Secret
+			if secret == "" {
+				errs = append(errs, errors.New("missing secret for CRAM-MD5 auth mechanism"))
+				continue
+			}
+			return smtp.CRAMMD5Auth(username, secret), nil
+
+		case "PLAIN":
+			password := c.auth.Password
+			if password == "" {
+				errs = append(errs, errors.New("missing password for PLAIN auth mechanism"))
+				continue
+			}
+			identity := c.auth.Identity
+
+			return smtp.PlainAuth(identity, username, password, c.host), nil
+		case "LOGIN":
+			password := c.auth.Password
+			if password == "" {
+				errs = append(errs, errors.New("missing password for LOGIN auth mechanism"))
+				continue
+			}
+
+			return LoginAuth(username, password), nil
+		}
+	}
+
+	if len(errs) == 0 {
+		errs = append(errs, errors.New("unknown auth mechanism: "+mechs))
+	}
+
+	return nil, errors.Join(errs...)
+}
+
+func (c *Client) dial(ctx context.Context) (net.Conn, error) {
+	var (
+		conn net.Conn
+		err  error
+	)
+
+	if c.tls.Enabled || c.port == "465" {
+		conn, err = tls.Dial("tcp", c.address, c.tlsConfig)
+		if err != nil {
+			return nil, fmt.Errorf("failed to establish TLS connection to server: %w", err)
+		}
+
+		return conn, nil
+	}
+
+	var d net.Dialer
+	conn, err = d.DialContext(ctx, "tcp", c.address)
+	if err != nil {
+		return nil, fmt.Errorf("failed to establish connection to server: %w", err)
+	}
+
+	return conn, nil
+}
+
+func newTLSConfig(config TLS, serverName string) (*tls.Config, error) {
+	tlsConfig := &tls.Config{
+		InsecureSkipVerify: config.InsecureSkipVerify,
+		ServerName:         serverName,
+	}
+
+	if config.CertFilePath != "" {
+		cert, err := tls.LoadX509KeyPair(config.CertFilePath, config.KeyFilePath)
+		if err != nil {
+			return nil, fmt.Errorf("failed to load cert or key file: %w", err)
+		}
+		tlsConfig.Certificates = []tls.Certificate{cert}
+	}
+
+	if config.CAFilePath != "" {
+		ca, err := os.ReadFile(config.CAFilePath)
+		if err != nil {
+			return nil, fmt.Errorf("failed to load CA file: %w", err)
+		}
+		tlsConfig.RootCAs = x509.NewCertPool()
+		if !tlsConfig.RootCAs.AppendCertsFromPEM(ca) {
+			return nil, fmt.Errorf("failed to append CA file: %s", config.CAFilePath)
+		}
+	}
+
+	return tlsConfig, nil
 }
