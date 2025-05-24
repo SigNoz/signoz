403630ad31652cf24096fe857d65e8c36682c639
Vibhu Pandey <vibhupandey28@gmail.com>
2025-05-24 23:53:54 +0530

    feat(signoz): compile time check for dependency injection (#8033)

---
+++ pkg/signoz/handler_test.go
@@ -0,0 +1,36 @@
+package signoz
+
+import (
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/SigNoz/signoz/pkg/emailing/emailingtest"
+	"github.com/SigNoz/signoz/pkg/factory/factorytest"
+	"github.com/SigNoz/signoz/pkg/modules/user/impluser"
+	"github.com/SigNoz/signoz/pkg/sqlstore"
+	"github.com/SigNoz/signoz/pkg/sqlstore/sqlstoretest"
+	"github.com/SigNoz/signoz/pkg/types/authtypes"
+	"github.com/stretchr/testify/assert"
+)
+
+// This is a test to ensure that all fields of the handlers are initialized.
+// It also helps us catch these errors at compile time instead of runtime.
+func TestNewHandlers(t *testing.T) {
+	sqlstore := sqlstoretest.New(sqlstore.Config{Provider: "sqlite"}, sqlmock.QueryMatcherEqual)
+	jwt := authtypes.NewJWT("", 1*time.Hour, 1*time.Hour)
+	emailing := emailingtest.New()
+	providerSettings := factorytest.NewSettings()
+	userModule := impluser.NewModule(impluser.NewStore(sqlstore), jwt, emailing, providerSettings)
+	userHandler := impluser.NewHandler(userModule)
+
+	modules := NewModules(sqlstore, userModule)
+	handlers := NewHandlers(modules, userHandler)
+
+	reflectVal := reflect.ValueOf(handlers)
+	for i := 0; i < reflectVal.NumField(); i++ {
+		f := reflectVal.Field(i)
+		assert.False(t, f.IsZero(), "%s handler has not been initialized", reflectVal.Type().Field(i).Name)
+	}
+}
