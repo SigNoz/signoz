424fd0362d7d1a74a064304bbfbba9964c31ab98
Yunus M <myounis.ar@live.com>
2025-05-25 18:43:36 +0530

    feat: query builder layout updates

--- frontend/src/components/QueryBuilderV2/QueryBuilderV2.tsx
+++ frontend/src/components/QueryBuilderV2/QueryBuilderV2.tsx
@@ -1,9 +1,12 @@
 import './QueryBuilderV2.styles.scss';
 
+import { OPERATORS } from 'constants/queryBuilder';
+import { QueryBuilderProps } from 'container/QueryBuilder/QueryBuilder.interfaces';
+import { useMemo } from 'react';
 import { IBuilderQuery } from 'types/api/queryBuilder/queryBuilderData';
 import { DataSource } from 'types/common/queryBuilder';
 
-import LogsQB from './Logs/LogsQB';
+import { LogsQB } from './Logs/LogsQB';
 import MetricsQB from './Metrics/MetricsQB';
 import { QueryBuilderV2Provider } from './QueryBuilderV2Context';
 import TracesQB from './Traces/TracesQB';
@@ -21,10 +24,46 @@ function QueryBuilderV2Main({
 	const isLogsDataSource = source === DataSource.LOGS;
 	const isTracesDataSource = source === DataSource.TRACES;
 
+	const listViewLogFilterConfigs: QueryBuilderProps['filterConfigs'] = useMemo(() => {
+		const config: QueryBuilderProps['filterConfigs'] = {
+			stepInterval: { isHidden: true, isDisabled: true },
+			having: { isHidden: true, isDisabled: true },
+			filters: {
+				customKey: 'body',
+				customOp: OPERATORS.CONTAINS,
+			},
+		};
+
+		return config;
+	}, []);
+
+	const listViewTracesFilterConfigs: QueryBuilderProps['filterConfigs'] = useMemo(() => {
+		const config: QueryBuilderProps['filterConfigs'] = {
+			stepInterval: { isHidden: true, isDisabled: true },
+			having: { isHidden: true, isDisabled: true },
+			limit: { isHidden: true, isDisabled: true },
+			filters: {
+				customKey: 'body',
+				customOp: OPERATORS.CONTAINS,
+			},
+		};
+
+		return config;
+	}, []);
+
 	return (
 		<div className="query-builder-v2">
 			{isMetricsDataSource ? <MetricsQB query={query} /> : null}
-			{isLogsDataSource ? <LogsQB query={query} /> : null}
+			{isLogsDataSource ? (
+				<LogsQB
+					query={query}
+					filterConfigs={
+						query.dataSource === DataSource.TRACES
+							? listViewTracesFilterConfigs
+							: listViewLogFilterConfigs
+					}
+				/>
+			) : null}
 			{isTracesDataSource ? <TracesQB query={query} /> : null}
 		</div>
 	);
