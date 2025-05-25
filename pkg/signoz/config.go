da22daec81badf44cffbef8f9ac0a981d803c6c0
grandwizard28 <vibhupandey28@gmail.com>
2025-05-25 10:23:15 +0530

    chore(linter): add more linters and deprecate zap

--- pkg/signoz/config.go
+++ pkg/signoz/config.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/url"
 	"os"
+	"path"
 	"reflect"
 	"time"
 
@@ -60,7 +61,7 @@ type Config struct {
 	Alertmanager alertmanager.Config `mapstructure:"alertmanager" yaml:"alertmanager"`
 
 	// Emailing config
-	Emailing emailing.Config `mapstructure:"emailing"`
+	Emailing emailing.Config `mapstructure:"emailing" yaml:"emailing"`
 }
 
 // DeprecatedFlags are the flags that are deprecated and scheduled for removal.
@@ -190,5 +191,43 @@ func mergeAndEnsureBackwardCompatibility(config *Config, deprecatedFlags Depreca
 
 	if deprecatedFlags.Config != "" {
 		fmt.Println("[Deprecated] flag --config is deprecated for passing prometheus config. The flag will be used for passing the entire SigNoz config. More details can be found at https://github.com/SigNoz/signoz/issues/6805.")
+	}
+
+	if os.Getenv("INVITE_EMAIL_TEMPLATE") != "" {
+		fmt.Println("[Deprecated] env INVITE_EMAIL_TEMPLATE is deprecated and scheduled for removal. Please use SIGNOZ_EMAILING_TEMPLATES_DIRECTORY instead.")
+		config.Emailing.Templates.Directory = path.Dir(os.Getenv("INVITE_EMAIL_TEMPLATE"))
+	}
+
+	if os.Getenv("SMTP_ENABLED") != "" {
+		fmt.Println("[Deprecated] env SMTP_ENABLED is deprecated and scheduled for removal. Please use SIGNOZ_EMAILING_ENABLED instead.")
+		config.Emailing.Enabled = os.Getenv("SMTP_ENABLED") == "true"
+	}
+
+	if os.Getenv("SMTP_HOST") != "" {
+		fmt.Println("[Deprecated] env SMTP_HOST is deprecated and scheduled for removal. Please use SIGNOZ_EMAILING_ADDRESS instead.")
+		if os.Getenv("SMTP_PORT") != "" {
+			config.Emailing.SMTP.Address = os.Getenv("SMTP_HOST") + ":" + os.Getenv("SMTP_PORT")
+		} else {
+			config.Emailing.SMTP.Address = os.Getenv("SMTP_HOST")
+		}
+	}
+
+	if os.Getenv("SMTP_PORT") != "" {
+		fmt.Println("[Deprecated] env SMTP_PORT is deprecated and scheduled for removal. Please use SIGNOZ_EMAILING_ADDRESS instead.")
+	}
+
+	if os.Getenv("SMTP_USERNAME") != "" {
+		fmt.Println("[Deprecated] env SMTP_USERNAME is deprecated and scheduled for removal. Please use SIGNOZ_EMAILING_AUTH_USERNAME instead.")
+		config.Emailing.SMTP.Auth.Username = os.Getenv("SMTP_USERNAME")
+	}
+
+	if os.Getenv("SMTP_PASSWORD") != "" {
+		fmt.Println("[Deprecated] env SMTP_PASSWORD is deprecated and scheduled for removal. Please use SIGNOZ_EMAILING_AUTH_PASSWORD instead.")
+		config.Emailing.SMTP.Auth.Password = os.Getenv("SMTP_PASSWORD")
+	}
+
+	if os.Getenv("SMTP_FROM") != "" {
+		fmt.Println("[Deprecated] env SMTP_FROM is deprecated and scheduled for removal. Please use SIGNOZ_EMAILING_FROM instead.")
+		config.Emailing.SMTP.From = os.Getenv("SMTP_FROM")
 	}
 }
