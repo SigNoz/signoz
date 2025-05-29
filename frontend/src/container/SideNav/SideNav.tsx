ee2abbcd142ba5f08a17e09c24a319e4afbc5d35
vikrantgupta25 <vikrant@signoz.io>
2025-05-29 17:28:07 +0530

    fix(license): suspended check for side nav

--- frontend/src/container/SideNav/SideNav.tsx
+++ frontend/src/container/SideNav/SideNav.tsx
@@ -105,7 +105,7 @@ function SideNav(): JSX.Element {
 
 	const isWorkspaceBlocked = trialInfo?.workSpaceBlock || false;
 
-	const isLicenseActive = licenseStatus !== 'INVALID';
+	const isLicenseActive = licenseStatus !== '' && licenseStatus !== 'INVALID';
 
 	const onClickSignozCloud = (): void => {
 		window.open(
