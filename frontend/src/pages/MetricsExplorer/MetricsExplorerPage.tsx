f713040b9caf1099854c8623f7bd77f988898e60
amlannandy <amlannandy5@gmail.com>
2025-06-02 17:58:54 +0700

    chore: summary view fixes

--- frontend/src/pages/MetricsExplorer/MetricsExplorerPage.tsx
+++ frontend/src/pages/MetricsExplorer/MetricsExplorerPage.tsx
@@ -2,8 +2,13 @@ import './MetricsExplorerPage.styles.scss';
 
 import RouteTab from 'components/RouteTab';
 import { TabRoutes } from 'components/RouteTab/types';
+import { initialQueriesMap, PANEL_TYPES } from 'constants/queryBuilder';
+import { useQueryBuilder } from 'hooks/queryBuilder/useQueryBuilder';
+import { useShareBuilderUrl } from 'hooks/queryBuilder/useShareBuilderUrl';
 import history from 'lib/history';
+import { useMemo } from 'react';
 import { useLocation } from 'react-use';
+import { DataSource } from 'types/common/queryBuilder';
 
 import { Explorer, Summary, Views } from './constants';
 
@@ -11,6 +16,20 @@ function MetricsExplorerPage(): JSX.Element {
 	const { pathname } = useLocation();
 
 	const routes: TabRoutes[] = [Summary, Explorer, Views];
+
+	const { updateAllQueriesOperators } = useQueryBuilder();
+
+	const defaultQuery = useMemo(
+		() =>
+			updateAllQueriesOperators(
+				initialQueriesMap[DataSource.METRICS],
+				PANEL_TYPES.LIST,
+				DataSource.METRICS,
+			),
+		[updateAllQueriesOperators],
+	);
+
+	useShareBuilderUrl(defaultQuery);
 
 	return (
 		<div className="metrics-explorer-page">
