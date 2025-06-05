115c480c39e4ba289bef5da770aafcee65136f43
ahmadshaheer <ashaheerki@gmail.com>
2025-06-05 23:39:54 +0430

    fix: fix the alignment of breadcrumb and funnel actions

--- frontend/src/hooks/useGetFeatureFlag.tsx
+++ frontend/src/hooks/useGetFeatureFlag.tsx
@@ -1,29 +1,18 @@
-import list from 'api/v1/features/list';
+import getFeaturesFlags from 'api/features/getFeatureFlags';
 import { REACT_QUERY_KEY } from 'constants/reactQueryKeys';
 import { useQuery, UseQueryResult } from 'react-query';
-import { SuccessResponseV2 } from 'types/api';
-import APIError from 'types/api/error';
 import { FeatureFlagProps } from 'types/api/features/getFeaturesFlags';
 
-export interface Props {
-	onSuccessHandler: (routes: FeatureFlagProps[]) => void;
-	isLoggedIn: boolean;
-}
-type UseGetFeatureFlag = UseQueryResult<
-	SuccessResponseV2<FeatureFlagProps[]>,
-	APIError
->;
-
-export const useGetFeatureFlag = (
+const useGetFeatureFlag = (
 	onSuccessHandler: (routes: FeatureFlagProps[]) => void,
 	isLoggedIn: boolean,
-): UseGetFeatureFlag =>
-	useQuery<SuccessResponseV2<FeatureFlagProps[]>, APIError>({
+): UseQueryResult<FeatureFlagProps[], unknown> =>
+	useQuery<FeatureFlagProps[]>({
+		queryFn: getFeaturesFlags,
 		queryKey: [REACT_QUERY_KEY.GET_FEATURES_FLAGS],
-		queryFn: () => list(),
-		onSuccess: (data) => {
-			onSuccessHandler(data.data);
-		},
+		onSuccess: onSuccessHandler,
 		retryOnMount: false,
 		enabled: !!isLoggedIn,
 	});
+
+export default useGetFeatureFlag;
