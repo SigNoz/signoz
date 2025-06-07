012d93dc9a033e427e46975f66991f732fe4d9cc
grandwizard28 <vibhupandey28@gmail.com>
2025-06-07 22:49:58 +0530

    feat(statsreporter): add an identify event on user creation

--- pkg/signoz/module.go
+++ pkg/signoz/module.go
@@ -2,6 +2,7 @@ package signoz
 
 import (
 	"github.com/SigNoz/signoz/pkg/alertmanager"
+	"github.com/SigNoz/signoz/pkg/analytics"
 	"github.com/SigNoz/signoz/pkg/emailing"
 	"github.com/SigNoz/signoz/pkg/factory"
 	"github.com/SigNoz/signoz/pkg/modules/apdex"
@@ -44,10 +45,11 @@ func NewModules(
 	providerSettings factory.ProviderSettings,
 	orgGetter organization.Getter,
 	alertmanager alertmanager.Alertmanager,
+	analytics analytics.Analytics,
 ) Modules {
 	quickfilter := implquickfilter.NewModule(implquickfilter.NewStore(sqlstore))
 	orgSetter := implorganization.NewSetter(implorganization.NewStore(sqlstore), alertmanager, quickfilter)
-	user := impluser.NewModule(impluser.NewStore(sqlstore, providerSettings), jwt, emailing, providerSettings, orgSetter)
+	user := impluser.NewModule(impluser.NewStore(sqlstore, providerSettings), jwt, emailing, providerSettings, orgSetter, analytics)
 	return Modules{
 		OrgGetter:   orgGetter,
 		OrgSetter:   orgSetter,
