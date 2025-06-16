beed59e7cdd98f4113b2405887f1e3260fc44c3e
ahrefabhi <ahrefabhi@gmail.com>
2025-06-16 18:58:23 +0530

    feat(changelog): add getChangelogByVersion API and related types

---
+++ frontend/src/types/api/changelog/getChangelogByVersion.ts
@@ -0,0 +1,35 @@
+export type Media = {
+	id: number;
+	documentId: string;
+	ext: string;
+	url: string;
+	mime: string;
+	alternativeText: string;
+	[key: string]: any; // Allow other fields (e.g., mime, size) to be flexible
+};
+
+type Feature = {
+	id: number;
+	documentId: string;
+	title: string;
+	sort_order: number | null;
+	createdAt: string;
+	updatedAt: string;
+	publishedAt: string;
+	description: string;
+	deployment_type: string | null;
+	media: Media | null;
+};
+
+export interface ChangelogProps {
+	id: number;
+	documentId: string;
+	version: string;
+	release_date: string;
+	bug_fixes: string | null;
+	maintenance: string | null;
+	createdAt: string;
+	updatedAt: string;
+	publishedAt: string;
+	features: Feature[];
+}
