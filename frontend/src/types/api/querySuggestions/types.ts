6c6793ed7888c2be1aa9948d128dda8c8b46b66e
Yunus M <myounis.ar@live.com>
2025-06-17 15:39:30 +0530

    feat: fetch more keys is complete list not already fetched

--- frontend/src/types/api/querySuggestions/types.ts
+++ frontend/src/types/api/querySuggestions/types.ts
@@ -1,18 +1,28 @@
-export interface QueryKeySuggestionsProps {
+export interface QueryKeyDataSuggestionsProps {
 	label: string;
 	type: string;
 	info?: string;
 	apply?: string;
 	detail?: string;
+	fieldContext: string;
+	fieldDataType: string;
+	name: string;
+	signal: string;
 }
 
 export interface QueryKeySuggestionsResponseProps {
 	status: string;
-	data: QueryKeySuggestionsProps[];
+	data: {
+		complete: boolean;
+		keys: {
+			[key: string]: QueryKeyDataSuggestionsProps[];
+		};
+	};
 }
 
 export interface QueryKeyRequestProps {
 	signal: string;
+	name: string;
 }
 
 export interface QueryKeyValueSuggestionsProps {
