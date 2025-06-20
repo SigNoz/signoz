0811dca6f056ce4ecf760d491e13c4a54fb979a1
srikanthccv <srikanth.chekuri92@gmail.com>
2025-06-20 23:33:13 +0530

    chore: more validation, zero values and enfore max step interval

--- pkg/querybuilder/time.go
+++ pkg/querybuilder/time.go
@@ -5,6 +5,10 @@ import "math"
 const (
 	NsToSeconds      = 1000000000
 	BucketAdjustment = 1800 // 30 minutes
+
+	RecommendedNumberOfPoints = 300
+	MaxAllowedNumberofPoints  = 1500
+	MaxAllowedSeries          = 3000
 )
 
 // ToNanoSecs takes epoch and returns it in ns
@@ -20,4 +24,62 @@ func ToNanoSecs(epoch uint64) uint64 {
 		}
 	}
 	return temp * uint64(math.Pow(10, float64(19-count)))
+}
+
+func RecommendedStepInterval(start, end uint64) uint64 {
+	start = ToNanoSecs(start)
+	end = ToNanoSecs(end)
+
+	step := (end - start) / RecommendedNumberOfPoints / 1e9
+
+	if step < 5 {
+		return 5
+	}
+
+	// return the nearest lower multiple of 5
+	return step - step%5
+}
+
+func MinAllowedStepInterval(start, end uint64) uint64 {
+	start = ToNanoSecs(start)
+	end = ToNanoSecs(end)
+
+	step := (end - start) / MaxAllowedNumberofPoints / 1e9
+
+	if step < 5 {
+		return 5
+	}
+
+	// return the nearest lower multiple of 5
+	return step - step%5
+}
+
+func RecommendedStepIntervalForMetric(start, end uint64) uint64 {
+	start = ToNanoSecs(start)
+	end = ToNanoSecs(end)
+
+	step := (end - start) / RecommendedNumberOfPoints / 1e9
+
+	// TODO(srikanthccv): make this make use of the reporting frequency and remove the 60
+	if step < 60 {
+		return 60
+	}
+
+	// return the nearest lower multiple of 60
+	return step - step%60
+}
+
+func MinAllowedStepIntervalForMetric(start, end uint64) uint64 {
+	start = ToNanoSecs(start)
+	end = ToNanoSecs(end)
+
+	step := (end - start) / RecommendedNumberOfPoints / 1e9
+
+	// TODO(srikanthccv): make this make use of the reporting frequency and remove the 60
+	if step < 60 {
+		return 60
+	}
+
+	// return the nearest lower multiple of 60
+	return step - step%60
 }
