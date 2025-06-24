1e80e1ac6fec85d70430faa591a51d65704c0c05
sawhil <sawhil.khan@gmail.com>
2025-06-24 20:03:55 +0530

    fix: minor refactor

--- frontend/src/container/NewWidget/LeftContainer/ExplorerAttributeColumns.tsx
+++ frontend/src/container/NewWidget/LeftContainer/ExplorerAttributeColumns.tsx
@@ -16,31 +16,38 @@ function ExplorerAttributeColumns({
 	isAttributeKeySelected,
 	handleCheckboxChange,
 }: ExplorerAttributeColumnsProps): JSX.Element {
+	if (isLoading) {
+		return (
+			<div className="attribute-columns">
+				<Spinner size="large" tip="Loading..." height="2vh" />
+			</div>
+		);
+	}
+
+	const filteredAttributeKeys =
+		data?.payload?.attributeKeys?.filter((attributeKey: any) =>
+			attributeKey.key.toLowerCase().includes(searchText.toLowerCase()),
+		) || [];
+	if (filteredAttributeKeys.length === 0) {
+		return (
+			<div className="attribute-columns">
+				<Empty description="No columns found" />
+			</div>
+		);
+	}
+
 	return (
 		<div className="attribute-columns">
-			{isLoading ? (
-				<Spinner size="large" tip="Loading..." height="2vh" />
-			) : (
-				((): JSX.Element | JSX.Element[] => {
-					const filteredAttributeKeys =
-						data?.payload?.attributeKeys?.filter((attributeKey: any) =>
-							attributeKey.key.toLowerCase().includes(searchText.toLowerCase()),
-						) || [];
-					if (filteredAttributeKeys.length === 0) {
-						return <Empty description="No columns found" />;
-					}
-					return filteredAttributeKeys.map((attributeKey: any) => (
-						<Checkbox
-							checked={isAttributeKeySelected(attributeKey.key)}
-							onChange={(): void => handleCheckboxChange(attributeKey.key)}
-							style={{ padding: 0 }}
-							key={attributeKey.key}
-						>
-							{attributeKey.key}
-						</Checkbox>
-					));
-				})()
-			)}
+			{filteredAttributeKeys.map((attributeKey: any) => (
+				<Checkbox
+					checked={isAttributeKeySelected(attributeKey.key)}
+					onChange={(): void => handleCheckboxChange(attributeKey.key)}
+					style={{ padding: 0 }}
+					key={attributeKey.key}
+				>
+					{attributeKey.key}
+				</Checkbox>
+			))}
 		</div>
 	);
 }
