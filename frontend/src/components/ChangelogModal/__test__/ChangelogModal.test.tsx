12677687ed100fb8435d97cde72182a3bb4846e9
ahrefabhi <ahrefabhi@gmail.com>
2025-07-11 18:57:39 +0530

    test: fixed test for changelogModal

--- frontend/src/components/ChangelogModal/__test__/ChangelogModal.test.tsx
+++ frontend/src/components/ChangelogModal/__test__/ChangelogModal.test.tsx
@@ -3,6 +3,8 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 
 import { fireEvent, render, screen } from '@testing-library/react';
+import { USER_PREFERENCES } from 'constants/userPreferences';
+import MockQueryClientProvider from 'providers/test/MockQueryClientProvider';
 import {
 	ChangelogSchema,
 	DeploymentType,
@@ -44,10 +46,30 @@ jest.mock(
 			return <div>{children}</div>;
 		},
 );
+// mock useAppContext
+jest.mock('providers/App/App', () => ({
+	useAppContext: jest.fn(() => ({
+		updateUserPreferenceInContext: jest.fn(),
+		userPreferences: [
+			{
+				name: USER_PREFERENCES.LAST_SEEN_CHANGELOG_VERSION,
+				value: 'v1.0.0',
+			},
+		],
+	})),
+}));
+
+function renderChangelog(): void {
+	render(
+		<MockQueryClientProvider>
+			<ChangelogModal changelog={mockChangelog} onClose={jest.fn()} />
+		</MockQueryClientProvider>,
+	);
+}
 
 describe('ChangelogModal', () => {
 	it('renders modal with changelog data', () => {
-		render(<ChangelogModal changelog={mockChangelog} onClose={jest.fn()} />);
+		renderChangelog();
 		expect(
 			screen.getByText('What’s New ⎯ Changelog : June 10, 2025'),
 		).toBeInTheDocument();
@@ -59,14 +81,18 @@ describe('ChangelogModal', () => {
 
 	it('calls onClose when Skip for now is clicked', () => {
 		const onClose = jest.fn();
-		render(<ChangelogModal changelog={mockChangelog} onClose={onClose} />);
+		render(
+			<MockQueryClientProvider>
+				<ChangelogModal changelog={mockChangelog} onClose={onClose} />
+			</MockQueryClientProvider>,
+		);
 		fireEvent.click(screen.getByText('Skip for now'));
 		expect(onClose).toHaveBeenCalled();
 	});
 
 	it('opens migration docs when Update my workspace is clicked', () => {
 		window.open = jest.fn();
-		render(<ChangelogModal changelog={mockChangelog} onClose={jest.fn()} />);
+		renderChangelog();
 		fireEvent.click(screen.getByText('Update my workspace'));
 		expect(window.open).toHaveBeenCalledWith(
 			'https://github.com/SigNoz/signoz/releases',
@@ -76,7 +102,7 @@ describe('ChangelogModal', () => {
 	});
 
 	it('scrolls for more when Scroll for more is clicked', () => {
-		render(<ChangelogModal changelog={mockChangelog} onClose={jest.fn()} />);
+		renderChangelog();
 		const scrollBtn = screen.getByTestId('scroll-more-btn');
 		const contentDiv = screen.getByTestId('changelog-content');
 		if (contentDiv) {
