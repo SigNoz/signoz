995b96ad8ba05d960877278be73d3513131bd913
ahrefabhi <ahrefabhi@gmail.com>
2025-07-21 14:21:03 +0530

    chore: use only needed resource attributes in context filter

--- frontend/src/container/LogsExplorerContext/useInitialQuery.ts
+++ frontend/src/container/LogsExplorerContext/useInitialQuery.ts
@@ -1,7 +1,7 @@
 import { initialQueriesMap, PANEL_TYPES } from 'constants/queryBuilder';
 import { useQueryBuilder } from 'hooks/queryBuilder/useQueryBuilder';
 import { ILog } from 'types/api/logs/log';
-import { Query } from 'types/api/queryBuilder/queryBuilderData';
+import { Query, TagFilter } from 'types/api/queryBuilder/queryBuilderData';
 import { DataSource } from 'types/common/queryBuilder';
 
 import { getFiltersFromResources } from './utils';
@@ -16,17 +16,38 @@ const useInitialQuery = (log: ILog): Query => {
 		DataSource.LOGS,
 	);
 
+	const updateFilters = (filters: TagFilter): TagFilter => {
+		const startsWithRegex = /^(k8s|cloud|host|deployment)/;
+		const containsRegex = /(env|service|file|container|tenant)/;
+
+		return {
+			...filters,
+			items: filters.items.filter(
+				(filterItem) =>
+					filterItem.key?.key &&
+					(startsWithRegex.test(filterItem.key.key) ||
+						containsRegex.test(filterItem.key.key)),
+			),
+		};
+	};
+
 	const data: Query = {
 		...updatedAllQueriesOperator,
 		builder: {
 			...updatedAllQueriesOperator.builder,
-			queryData: updatedAllQueriesOperator.builder.queryData.map((item) => ({
-				...item,
-				filters: {
+			queryData: updatedAllQueriesOperator.builder.queryData.map((item) => {
+				const filters = {
 					...item.filters,
 					items: [...item.filters.items, ...resourcesFilters],
-				},
-			})),
+				};
+
+				const updatedFilters = updateFilters(filters);
+
+				return {
+					...item,
+					filters: updatedFilters,
+				};
+			}),
 		},
 	};
 
