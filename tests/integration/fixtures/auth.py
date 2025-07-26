196ec9cb85e649d114b49d207a971e2c808914cf
grandwizard28 <vibhupandey28@gmail.com>
2025-07-26 16:46:37 +0530

    test(integration): fix get_jwt_token

--- tests/integration/fixtures/auth.py
+++ tests/integration/fixtures/auth.py
@@ -48,31 +48,11 @@ def create_user_admin(
     )
 
 
-@pytest.fixture(name="create_first_user", scope="function")
-def create_first_user(signoz: types.SigNoz) -> None:
-    def _create_user(name: str, email: str, password: str) -> None:
-        response = requests.post(
-            signoz.self.host_config.get("/api/v1/register"),
-            json={
-                "name": name,
-                "orgId": "",
-                "orgName": "",
-                "email": email,
-                "password": password,
-            },
-            timeout=5,
-        )
-
-        assert response.status_code == HTTPStatus.OK
-
-    return _create_user
-
-
 @pytest.fixture(name="get_jwt_token", scope="module")
 def get_jwt_token(signoz: types.SigNoz) -> str:
     def _get_jwt_token(email: str, password: str) -> str:
         response = requests.post(
-            signoz.self.host_config.get("/api/v1/login"),
+            signoz.self.host_configs["8080"].get("/api/v1/login"),
             json={
                 "email": email,
                 "password": password,
