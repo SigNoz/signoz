bf5eafcd101142d9b340e464ed730a9253ff9d98
grandwizard28 <vibhupandey28@gmail.com>
2025-07-26 16:30:34 +0530

    test(integration): fix make_http_mocks

--- tests/integration/fixtures/http.py
+++ tests/integration/fixtures/http.py
@@ -77,7 +77,7 @@ def zeus(
 @pytest.fixture(name="make_http_mocks", scope="function")
 def make_http_mocks():
     def _make_http_mocks(container: types.TestContainerDocker, mappings: List[Mapping]):
-        Config.base_url = container.host_config.get("/__admin")
+        Config.base_url = container.host_configs["8080"].get("/__admin")
 
         for mapping in mappings:
             Mappings.create_mapping(mapping=mapping)
