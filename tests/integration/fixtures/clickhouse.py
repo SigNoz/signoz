c17241272f5593b15eb9f8f9dc75ec2aff050bcb
Vibhu Pandey <vibhupandey28@gmail.com>
2025-07-29 14:44:16 +0530

    test(integration): add integration tests for logs (#8619)

--- tests/integration/fixtures/clickhouse.py
+++ tests/integration/fixtures/clickhouse.py
@@ -1,13 +1,19 @@
-import dataclasses
 import os
 from typing import Any, Generator
 
-import clickhouse_driver
+import clickhouse_connect
+import clickhouse_connect.driver
+import clickhouse_connect.driver.client
+import docker
+import docker.errors
 import pytest
 from testcontainers.clickhouse import ClickHouseContainer
 from testcontainers.core.container import Network
 
-from fixtures import types
+from fixtures import dev, types
+from fixtures.logger import setup_logger
+
+logger = setup_logger(__name__)
 
 
 @pytest.fixture(name="clickhouse", scope="package")
@@ -21,144 +27,153 @@ def clickhouse(
     """
     Package-scoped fixture for Clickhouse TestContainer.
     """
-    dev = request.config.getoption("--dev")
-    if dev:
-        container = pytestconfig.cache.get("clickhouse.container", None)
-        env = pytestconfig.cache.get("clickhouse.env", None)
 
-        if container and env:
-            assert isinstance(container, dict)
-            assert isinstance(env, dict)
+    def create() -> types.TestContainerClickhouse:
+        version = request.config.getoption("--clickhouse-version")
 
-            test_container = types.TestContainerDocker(
-                host_config=types.TestContainerUrlConfig(
-                    container["host_config"]["scheme"],
-                    container["host_config"]["address"],
-                    container["host_config"]["port"],
-                ),
-                container_config=types.TestContainerUrlConfig(
-                    container["container_config"]["scheme"],
-                    container["container_config"]["address"],
-                    container["container_config"]["port"],
-                ),
-            )
-
-            connection = clickhouse_driver.connect(
-                user=env["SIGNOZ_TELEMETRYSTORE_CLICKHOUSE_USERNAME"],
-                password=env["SIGNOZ_TELEMETRYSTORE_CLICKHOUSE_PASSWORD"],
-                host=test_container.host_config.address,
-                port=test_container.host_config.port,
-            )
-
-            return types.TestContainerClickhouse(
-                container=test_container,
-                conn=connection,
-                env=env,
-            )
-
-    version = request.config.getoption("--clickhouse-version")
-
-    container = ClickHouseContainer(
-        image=f"clickhouse/clickhouse-server:{version}",
-        port=9000,
-        username="signoz",
-        password="password",
-    )
-
-    cluster_config = f"""
-    <clickhouse>
-        <logger>
-            <level>information</level>
-            <formatting>
-                <type>json</type>
-            </formatting>
-            <log>/var/log/clickhouse-server/clickhouse-server.log</log>
-            <errorlog>/var/log/clickhouse-server/clickhouse-server.err.log</errorlog>
-            <size>1000M</size>
-            <count>3</count>
-            <console>1</console>
-        </logger>
-
-        <macros>
-            <shard>01</shard>
-            <replica>01</replica>
-        </macros>
-
-        <zookeeper>
-            <node>
-                <host>{zookeeper.container_config.address}</host>
-                <port>{zookeeper.container_config.port}</port>
-            </node>
-        </zookeeper>
-
-        <remote_servers>
-            <cluster>
-                <shard>
-                    <replica>
-                        <host>127.0.0.1</host>
-                        <port>9000</port>
-                    </replica>
-                </shard>
-            </cluster>
-        </remote_servers>
-
-        <distributed_ddl>
-            <path>/clickhouse/task_queue/ddl</path>
-            <profile>default</profile>
-        </distributed_ddl>
-    </clickhouse>
-    """
-
-    tmp_dir = tmpfs("clickhouse")
-    cluster_config_file_path = os.path.join(tmp_dir, "cluster.xml")
-    with open(cluster_config_file_path, "w", encoding="utf-8") as f:
-        f.write(cluster_config)
-
-    container.with_volume_mapping(
-        cluster_config_file_path, "/etc/clickhouse-server/config.d/cluster.xml"
-    )
-    container.with_network(network)
-    container.start()
-
-    connection = clickhouse_driver.connect(
-        user=container.username,
-        password=container.password,
-        host=container.get_container_host_ip(),
-        port=container.get_exposed_port(9000),
-    )
-
-    def stop():
-        if dev:
-            return
-
-        connection.close()
-        container.stop(delete_volume=True)
-
-    request.addfinalizer(stop)
-
-    cached_clickhouse = types.TestContainerClickhouse(
-        container=types.TestContainerDocker(
-            host_config=types.TestContainerUrlConfig(
-                "tcp",
-                container.get_container_host_ip(),
-                container.get_exposed_port(9000),
-            ),
-            container_config=types.TestContainerUrlConfig(
-                "tcp", container.get_wrapped_container().name, 9000
-            ),
-        ),
-        conn=connection,
-        env={
-            "SIGNOZ_TELEMETRYSTORE_CLICKHOUSE_DSN": f"tcp://{container.username}:{container.password}@{container.get_wrapped_container().name}:{9000}",  # pylint: disable=line-too-long
-            "SIGNOZ_TELEMETRYSTORE_CLICKHOUSE_USERNAME": container.username,
-            "SIGNOZ_TELEMETRYSTORE_CLICKHOUSE_PASSWORD": container.password,
-        },
-    )
-
-    if dev:
-        pytestconfig.cache.set(
-            "clickhouse.container", dataclasses.asdict(cached_clickhouse.container)
+        container = ClickHouseContainer(
+            image=f"clickhouse/clickhouse-server:{version}",
+            port=9000,
+            username="signoz",
+            password="password",
         )
-        pytestconfig.cache.set("clickhouse.env", cached_clickhouse.env)
 
-    return cached_clickhouse
+        cluster_config = f"""
+        <clickhouse>
+            <logger>
+                <level>information</level>
+                <formatting>
+                    <type>json</type>
+                </formatting>
+                <log>/var/log/clickhouse-server/clickhouse-server.log</log>
+                <errorlog>/var/log/clickhouse-server/clickhouse-server.err.log</errorlog>
+                <size>1000M</size>
+                <count>3</count>
+                <console>1</console>
+            </logger>
+
+            <macros>
+                <shard>01</shard>
+                <replica>01</replica>
+            </macros>
+
+            <zookeeper>
+                <node>
+                    <host>{zookeeper.container_configs["2181"].address}</host>
+                    <port>{zookeeper.container_configs["2181"].port}</port>
+                </node>
+            </zookeeper>
+
+            <remote_servers>
+                <cluster>
+                    <shard>
+                        <replica>
+                            <host>127.0.0.1</host>
+                            <port>9000</port>
+                        </replica>
+                    </shard>
+                </cluster>
+            </remote_servers>
+
+            <distributed_ddl>
+                <path>/clickhouse/task_queue/ddl</path>
+                <profile>default</profile>
+            </distributed_ddl>
+        </clickhouse>
+        """
+
+        tmp_dir = tmpfs("clickhouse")
+        cluster_config_file_path = os.path.join(tmp_dir, "cluster.xml")
+        with open(cluster_config_file_path, "w", encoding="utf-8") as f:
+            f.write(cluster_config)
+
+        container.with_volume_mapping(
+            cluster_config_file_path, "/etc/clickhouse-server/config.d/cluster.xml"
+        )
+        container.with_network(network)
+        container.start()
+
+        connection = clickhouse_connect.get_client(
+            user=container.username,
+            password=container.password,
+            host=container.get_container_host_ip(),
+            port=container.get_exposed_port(8123),
+        )
+
+        return types.TestContainerClickhouse(
+            container=types.TestContainerDocker(
+                id=container.get_wrapped_container().id,
+                host_configs={
+                    "9000": types.TestContainerUrlConfig(
+                        "tcp",
+                        container.get_container_host_ip(),
+                        container.get_exposed_port(9000),
+                    ),
+                    "8123": types.TestContainerUrlConfig(
+                        "tcp",
+                        container.get_container_host_ip(),
+                        container.get_exposed_port(8123),
+                    ),
+                },
+                container_configs={
+                    "9000": types.TestContainerUrlConfig(
+                        "tcp", container.get_wrapped_container().name, 9000
+                    ),
+                    "8123": types.TestContainerUrlConfig(
+                        "tcp", container.get_wrapped_container().name, 8123
+                    ),
+                },
+            ),
+            conn=connection,
+            env={
+                "SIGNOZ_TELEMETRYSTORE_CLICKHOUSE_DSN": f"tcp://{container.username}:{container.password}@{container.get_wrapped_container().name}:{9000}",
+                "SIGNOZ_TELEMETRYSTORE_CLICKHOUSE_USERNAME": container.username,
+                "SIGNOZ_TELEMETRYSTORE_CLICKHOUSE_PASSWORD": container.password,
+                "SIGNOZ_TELEMETRYSTORE_CLICKHOUSE_CLUSTER": "cluster",
+            },
+        )
+
+    def delete(container: types.TestContainerClickhouse) -> None:
+        client = docker.from_env()
+        try:
+            client.containers.get(container_id=container.container.id).stop()
+            client.containers.get(container_id=container.container.id).remove(v=True)
+        except docker.errors.NotFound:
+            logger.info(
+                "Skipping removal of Clickhouse, Clickhouse(%s) not found. Maybe it was manually removed?",
+                {"id": container.id},
+            )
+
+    def restore(cache: dict) -> types.TestContainerClickhouse:
+        container = types.TestContainerDocker.from_cache(cache["container"])
+        host_config = container.host_configs["8123"]
+        env = cache["env"]
+
+        conn = clickhouse_connect.get_client(
+            user=env["SIGNOZ_TELEMETRYSTORE_CLICKHOUSE_USERNAME"],
+            password=env["SIGNOZ_TELEMETRYSTORE_CLICKHOUSE_PASSWORD"],
+            host=host_config.address,
+            port=host_config.port,
+        )
+
+        return types.TestContainerClickhouse(
+            container=container,
+            conn=conn,
+            env=env,
+        )
+
+    return dev.wrap(
+        request,
+        pytestconfig,
+        "clickhouse",
+        empty=lambda: types.TestContainerSQL(
+            container=types.TestContainerDocker(
+                id="", host_configs={}, container_configs={}
+            ),
+            conn=None,
+            env={},
+        ),
+        create=create,
+        delete=delete,
+        restore=restore,
+    )
