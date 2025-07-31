587b0ef6c4a9e52d0ee881bfaa32eefaec65826d
Yunus M <myounis.ar@live.com>
2025-07-31 13:33:42 +0530

    Sign In / Sign Up UI updates (#8562)
    * feat: update login page UI
    
    * feat: update login and signup page UI
    
    * chore: font optimization
    
    * feat: update test cases for login
    
    * feat: address review comments
    
    * feat: remove playwright report file
    
    * fix: fix the failing login test
    
    ---------
    
    Co-authored-by: ahmadshaheer <ashaheerki@gmail.com>

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -1,17 +1,9 @@
-import { Typography } from 'antd';
-import getUserVersion from 'api/v1/version/getVersion';
-import Spinner from 'components/Spinner';
-import WelcomeLeftContainer from 'components/WelcomeLeftContainer';
+import './Login.styles.scss';
+
 import LoginContainer from 'container/Login';
 import useURLQuery from 'hooks/useUrlQuery';
-import { useAppContext } from 'providers/App/App';
-import { useTranslation } from 'react-i18next';
-import { useQuery } from 'react-query';
 
 function Login(): JSX.Element {
-	const { isLoggedIn } = useAppContext();
-	const { t } = useTranslation();
-
 	const urlQueryParams = useURLQuery();
 	const jwt = urlQueryParams.get('jwt') || '';
 	const refreshJwt = urlQueryParams.get('refreshjwt') || '';
@@ -19,42 +11,29 @@ function Login(): JSX.Element {
 	const ssoerror = urlQueryParams.get('ssoerror') || '';
 	const withPassword = urlQueryParams.get('password') || '';
 
-	const versionResult = useQuery({
-		queryFn: getUserVersion,
-		queryKey: ['getUserVersion', jwt],
-		enabled: !isLoggedIn,
-	});
-
-	if (
-		versionResult.status === 'error' ||
-		(versionResult.status === 'success' && versionResult?.data.statusCode !== 200)
-	) {
-		return (
-			<Typography>
-				{versionResult.data?.error || t('something_went_wrong')}
-			</Typography>
-		);
-	}
-
-	if (
-		versionResult.status === 'loading' ||
-		!(versionResult.data && versionResult.data.payload)
-	) {
-		return <Spinner tip="Loading..." />;
-	}
-
-	const { version } = versionResult.data.payload;
-
 	return (
-		<WelcomeLeftContainer version={version}>
-			<LoginContainer
-				ssoerror={ssoerror}
-				jwt={jwt}
-				refreshjwt={refreshJwt}
-				userId={userId}
-				withPassword={withPassword}
-			/>
-		</WelcomeLeftContainer>
+		<div className="login-page-container">
+			<div className="perilin-bg" />
+			<div className="login-page-content">
+				<div className="brand-container">
+					<img
+						src="/Logos/signoz-brand-logo.svg"
+						alt="logo"
+						className="brand-logo"
+					/>
+
+					<div className="brand-title">SigNoz</div>
+				</div>
+
+				<LoginContainer
+					ssoerror={ssoerror}
+					jwt={jwt}
+					refreshjwt={refreshJwt}
+					userId={userId}
+					withPassword={withPassword}
+				/>
+			</div>
+		</div>
 	);
 }
 
