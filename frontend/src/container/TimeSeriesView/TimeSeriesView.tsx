fd123b1fb6c80c332d7766efd1cb11073019f175
SagarRajput-7 <sagar@signoz.io>
2025-08-01 12:25:01 +0530

    feat: enabled legend enhancement for explorer pages and alert page

--- frontend/src/container/TimeSeriesView/TimeSeriesView.tsx
+++ frontend/src/container/TimeSeriesView/TimeSeriesView.tsx
@@ -5,6 +5,7 @@ import ErrorInPlace from 'components/ErrorInPlace/ErrorInPlace';
 import Uplot from 'components/Uplot';
 import { QueryParams } from 'constants/query';
 import EmptyLogsSearch from 'container/EmptyLogsSearch/EmptyLogsSearch';
+import { getLocalStorageGraphVisibilityState } from 'container/GridCardLayout/GridCard/utils';
 import { LogsLoading } from 'container/LogsLoading/LogsLoading';
 import EmptyMetricsSearch from 'container/MetricsExplorer/Explorer/EmptyMetricsSearch';
 import { MetricsLoading } from 'container/MetricsExplorer/MetricsLoading/MetricsLoading';
@@ -36,6 +37,7 @@ import { useLocation } from 'react-router-dom';
 import { UpdateTimeInterval } from 'store/actions';
 import { AppState } from 'store/reducers';
 import { SuccessResponse, Warning } from 'types/api';
+import { LegendPosition } from 'types/api/dashboard/getAll';
 import APIError from 'types/api/error';
 import { MetricRangePayloadProps } from 'types/api/metrics/getQueryRange';
 import { DataSource } from 'types/common/queryBuilder';
@@ -76,6 +78,7 @@ function TimeSeriesView({
 
 	const [minTimeScale, setMinTimeScale] = useState<number>();
 	const [maxTimeScale, setMaxTimeScale] = useState<number>();
+	const [graphVisibility, setGraphVisibility] = useState<boolean[]>([]);
 
 	const { minTime, maxTime, selectedTime: globalSelectedInterval } = useSelector<
 		AppState,
@@ -88,6 +91,19 @@ function TimeSeriesView({
 		setMinTimeScale(startTime);
 		setMaxTimeScale(endTime);
 	}, [maxTime, minTime, globalSelectedInterval, data]);
+
+	// Initialize graph visibility from localStorage
+	useEffect(() => {
+		if (data?.payload?.data?.result) {
+			const {
+				graphVisibilityStates: localStoredVisibilityState,
+			} = getLocalStorageGraphVisibilityState({
+				apiResponse: data.payload.data.result,
+				name: 'time-series-explorer',
+			});
+			setGraphVisibility(localStoredVisibilityState);
+		}
+	}, [data?.payload?.data?.result]);
 
 	const onDragSelect = useCallback(
 		(start: number, end: number): void => {
@@ -162,6 +178,7 @@ function TimeSeriesView({
 	const { timezone } = useTimezone();
 
 	const chartOptions = getUPlotChartOptions({
+		id: 'time-series-explorer',
 		onDragSelect,
 		yAxisUnit: yAxisUnit || '',
 		apiResponse: data?.payload,
@@ -179,6 +196,10 @@ function TimeSeriesView({
 		timezone: timezone.value,
 		currentQuery,
 		query: currentQuery,
+		graphsVisibilityStates: graphVisibility,
+		setGraphsVisibilityStates: setGraphVisibility,
+		enhancedLegend: true,
+		legendPosition: LegendPosition.BOTTOM,
 	});
 
 	return (
