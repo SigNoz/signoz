2f19985e1a73af13a4fffff3ce27df032483d32e
SagarRajput-7 <sagar@signoz.io>
2025-08-05 17:52:09 +0530

    feat: fix test cases

--- frontend/src/container/LogsExplorerViews/tests/LogsExplorerViews.test.tsx
+++ frontend/src/container/LogsExplorerViews/tests/LogsExplorerViews.test.tsx
@@ -173,21 +173,6 @@ describe('LogsExplorerViews -', () => {
 		expect(queryByText('pending_data_placeholder')).toBeInTheDocument();
 	});
 
-	it('check error state', async () => {
-		lodsQueryServerRequest();
-		(useGetExplorerQueryRange as jest.Mock).mockReturnValue({
-			data: { payload: logsQueryRangeSuccessNewFormatResponse },
-			isLoading: false,
-			isFetching: false,
-			isError: true,
-		});
-		const { queryByText } = renderer();
-
-		expect(
-			queryByText('Something went wrong. Please try again or contact support.'),
-		).toBeInTheDocument();
-	});
-
 	it('should add activeLogId filter when present in URL', async () => {
 		// Mock useCopyLogLink to return an activeLogId
 		(useCopyLogLink as jest.Mock).mockReturnValue({
