605649f14be904555fca37a0c8c814be171e0872
nityanandagohain <nityanandagohain@gmail.com>
2025-08-22 20:56:35 +0530

    fix: add comment

--- pkg/telemetrytraces/statement_builder.go
+++ pkg/telemetrytraces/statement_builder.go
@@ -67,7 +67,7 @@ func (b *traceQueryStatementBuilder) Build(
 	start = querybuilder.ToNanoSecs(start)
 	end = querybuilder.ToNanoSecs(end)
 
-	// isRoot and IsEntrypoint is returned by the metadata api, so if someone sends them we have to skip these.
+	// isRoot and isEntryPoint are returned by the Metadata API, so if someone sends them, we have to skip these.
 	var selectedFields []telemetrytypes.TelemetryFieldKey
 	for _, v := range query.SelectFields {
 		if v.Name == "isRoot" || v.Name == "isEntryPoint" {
