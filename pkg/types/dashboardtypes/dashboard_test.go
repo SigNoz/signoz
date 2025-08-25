0f6dbfb192c6d44132c79c9bbe5296be383e062b
nikhilmantri0902 <nikhil.mantri1999@gmail.com>
2025-08-25 15:03:15 +0530

    chore(dashboard): added unit test for CanUpdate function

---
+++ pkg/types/dashboardtypes/dashboard_test.go
@@ -0,0 +1,85 @@
+package dashboardtypes
+
+import (
+	"context"
+	"testing"
+
+	"github.com/SigNoz/signoz/pkg/types/authtypes"
+	"github.com/SigNoz/signoz/pkg/valuer"
+	"github.com/stretchr/testify/assert"
+)
+
+func makeTestWidgets(ids ...string) []interface{} {
+	widgets := []interface{}{}
+	for _, id := range ids {
+		widgets = append(widgets, map[string]interface{}{
+			"id":    id,
+			"query": map[string]interface{}{},
+		})
+	}
+	return widgets
+}
+
+func TestCanUpdate_MultipleDeletions_ByAuthType(t *testing.T) {
+	testCases := []struct {
+		name     string
+		authType string
+		setAuth  bool
+		updated  []string
+		wantErr  bool
+	}{
+		{
+			name:     "api_key-allows-multi-delete",
+			authType: authtypes.AuthTypeAPIKey,
+			setAuth:  true,
+			updated:  []string{"a"},
+			wantErr:  false,
+		},
+		{
+			name:     "jwt-blocks-multi-delete",
+			authType: authtypes.AuthTypeJWT,
+			setAuth:  true,
+			updated:  []string{"a"},
+			wantErr:  true,
+		},
+		{
+			name:     "jwt-allows-single-delete",
+			authType: authtypes.AuthTypeJWT,
+			setAuth:  true,
+			updated:  []string{"a", "b"},
+			wantErr:  false,
+		},
+		{
+			name:    "no-authType-blocks-multi-delete",
+			setAuth: false,
+			updated: []string{"a"},
+			wantErr: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			ctx := context.Background()
+			if tc.setAuth {
+				ctx = authtypes.SetAuthType(ctx, tc.authType)
+			}
+
+			orgID := valuer.GenerateUUID()
+			initial := StorableDashboardData{
+				"widgets": makeTestWidgets("a", "b", "c"),
+			}
+			d, err := NewDashboard(orgID, "tester", initial)
+			assert.NoError(t, err)
+
+			updated := StorableDashboardData{
+				"widgets": makeTestWidgets(tc.updated...),
+			}
+			err = d.CanUpdate(ctx, updated)
+			if tc.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
