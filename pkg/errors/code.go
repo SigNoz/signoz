76d63defa24a4a18d7fe37cdcb75ed055ad545cd
grandwizard28 <vibhupandey28@gmail.com>
2025-09-09 14:42:40 +0530

    chore: enable forbidgo

--- pkg/errors/code.go
+++ pkg/errors/code.go
@@ -16,6 +16,7 @@ var (
 	CodeForbidden             = Code{"forbidden"}
 	CodeCanceled              = Code{"canceled"}
 	CodeTimeout               = Code{"timeout"}
+	codeUnknown               = Code{"unknown"}
 )
 
 var (
