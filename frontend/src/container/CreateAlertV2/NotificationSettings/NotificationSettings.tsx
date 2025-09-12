58e802af93065afb37ba10907e312c77052a32cf
amlannandy <amlannandy5@gmail.com>
2025-09-12 12:11:20 +0700

    chore: correct step number in twin layouts

--- frontend/src/container/CreateAlertV2/NotificationSettings/NotificationSettings.tsx
+++ frontend/src/container/CreateAlertV2/NotificationSettings/NotificationSettings.tsx
@@ -1,14 +1,20 @@
 import './styles.scss';
 
 import Stepper from '../Stepper';
+import { showCondensedLayout } from '../utils';
 import MultipleNotifications from './MultipleNotifications';
 import NotificationMessage from './NotificationMessage';
 import ReNotification from './ReNotification';
 
 function NotificationSettings(): JSX.Element {
+	const showCondensedLayoutFlag = showCondensedLayout();
+
 	return (
 		<div className="notification-settings-container">
-			<Stepper stepNumber={4} label="Notification settings" />
+			<Stepper
+				stepNumber={showCondensedLayoutFlag ? 3 : 4}
+				label="Notification settings"
+			/>
 			<NotificationMessage />
 			<MultipleNotifications />
 			<ReNotification />
