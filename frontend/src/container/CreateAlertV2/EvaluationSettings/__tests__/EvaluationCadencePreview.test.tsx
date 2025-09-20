1c2b141556abd00ecee664cf117b1521037c422a
amlannandy <amlannandy5@gmail.com>
2025-09-21 00:10:11 +0700

    chore: additional test

--- frontend/src/container/CreateAlertV2/EvaluationSettings/__tests__/EvaluationCadencePreview.test.tsx
+++ frontend/src/container/CreateAlertV2/EvaluationSettings/__tests__/EvaluationCadencePreview.test.tsx
@@ -3,7 +3,9 @@ import * as alertState from 'container/CreateAlertV2/context';
 import { INITIAL_ADVANCED_OPTIONS_STATE } from 'container/CreateAlertV2/context/constants';
 
 import { TIMEZONE_DATA } from '../constants';
-import EvaluationCadencePreview from '../EvaluationCadence/EvaluationCadencePreview';
+import EvaluationCadencePreview, {
+	ScheduleList,
+} from '../EvaluationCadence/EvaluationCadencePreview';
 
 jest.spyOn(alertState, 'useCreateAlertState').mockReturnValue({
 	advancedOptions: INITIAL_ADVANCED_OPTIONS_STATE,
@@ -35,5 +37,49 @@ describe('EvaluationCadencePreview', () => {
 		} as any);
 		render(<EvaluationCadencePreview isOpen setIsOpen={mockSetIsOpen} />);
 		expect(screen.getByTestId('no-schedule')).toBeInTheDocument();
+	});
+});
+
+describe('ScheduleList', () => {
+	const schedule = [
+		new Date('2024-01-15T00:00:00Z'),
+		new Date('2024-01-16T00:00:00Z'),
+		new Date('2024-01-17T00:00:00Z'),
+		new Date('2024-01-18T00:00:00Z'),
+		new Date('2024-01-19T00:00:00Z'),
+	];
+	it('should render list of dates when schedule is generated', () => {
+		render(
+			<ScheduleList
+				schedule={schedule}
+				currentTimezone={TIMEZONE_DATA[0].value}
+			/>,
+		);
+		expect(
+			screen.queryByText(
+				'Please fill the relevant information to generate a schedule',
+			),
+		).not.toBeInTheDocument();
+
+		// Verify all dates are rendered correctly
+		schedule.forEach((date) => {
+			const dateString = date.toLocaleDateString('en-US', {
+				weekday: 'short',
+				month: 'short',
+				day: 'numeric',
+			});
+			const timeString = date.toLocaleTimeString('en-US', {
+				hour12: false,
+				hour: '2-digit',
+				minute: '2-digit',
+				second: '2-digit',
+			});
+			const combinedString = `${dateString}, ${timeString}`;
+			expect(screen.getByText(combinedString)).toBeInTheDocument();
+		});
+
+		// Verify timezone is rendered correctly with each date
+		const timezoneElements = screen.getAllByText(TIMEZONE_DATA[0].label);
+		expect(timezoneElements).toHaveLength(schedule.length);
 	});
 });
