7d522ba93ec8188a621af36354fc2f94ec747a21
grandwizard28 <vibhupandey28@gmail.com>
2025-09-30 03:12:29 +0530

    feat(valuer): integrate email valuer

--- pkg/modules/session/implsession/handler.go
+++ pkg/modules/session/implsession/handler.go
@@ -12,6 +12,7 @@ import (
 	"github.com/SigNoz/signoz/pkg/http/render"
 	"github.com/SigNoz/signoz/pkg/modules/session"
 	"github.com/SigNoz/signoz/pkg/types/authtypes"
+	"github.com/SigNoz/signoz/pkg/valuer"
 )
 
 type handler struct {
@@ -26,7 +27,11 @@ func (handler *handler) GetSessionContext(rw http.ResponseWriter, req *http.Requ
 	ctx, cancel := context.WithTimeout(req.Context(), 10*time.Second)
 	defer cancel()
 
-	email := req.URL.Query().Get("email")
+	email, err := valuer.NewEmail(req.URL.Query().Get("email"))
+	if err != nil {
+		render.Error(rw, err)
+		return
+	}
 
 	siteURL, err := url.Parse(req.URL.Query().Get("ref"))
 	if err != nil {
--- pkg/modules/user/impluser/getter.go
+++ pkg/modules/user/impluser/getter.go
@@ -17,20 +17,15 @@ func NewGetter(store types.UserStore) user.Getter {
 }
 
 func (module *getter) ListByOrgID(ctx context.Context, orgID valuer.UUID) ([]*types.User, error) {
-	gettableUsers, err := module.store.ListUsers(ctx, orgID.StringValue())
+	users, err := module.store.ListUsersByOrgID(ctx, orgID)
 	if err != nil {
 		return nil, err
-	}
-
-	users := make([]*types.User, len(gettableUsers))
-	for i, user := range gettableUsers {
-		users[i] = &user.User
 	}
 
 	return users, nil
 }
 
-func (module *getter) GetUsersByEmail(ctx context.Context, email string) ([]*types.User, error) {
+func (module *getter) GetUsersByEmail(ctx context.Context, email valuer.Email) ([]*types.User, error) {
 	users, err := module.store.GetUsersByEmail(ctx, email)
 	if err != nil {
 		return nil, err
