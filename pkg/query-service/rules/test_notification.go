c76263856af8b4b1dd68aad3100b5607bfa2f463
aniket <aniket@signoz.io>
2025-10-02 20:03:07 +0530

    chore: corrected linting errors

--- pkg/query-service/rules/test_notification.go
+++ pkg/query-service/rules/test_notification.go
@@ -38,8 +38,6 @@ func defaultTestNotification(opts PrepareTestRuleOptions) (int, *model.ApiError)
 	if parsedRule.RuleType == ruletypes.RuleTypeThreshold {
 
 		// add special labels for test alerts
-		//we dont need this label as of now
-		//parsedRule.Annotations[labels.AlertSummaryLabel] = fmt.Sprintf("The rule threshold is set to %.4f, and the observed metric value is {{$value}}.", 0)
 		parsedRule.Labels[labels.RuleSourceLabel] = ""
 		parsedRule.Labels[labels.AlertRuleIdLabel] = ""
 
