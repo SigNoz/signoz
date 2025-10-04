c770a1a4e164583be70a823649bd37d9bac11801
Amlan Kumar Nandy <45410599+amlannandy@users.noreply.github.com>
2025-10-04 23:09:18 +0700

    chore: remove routing policies feature flag (#9249)

--- frontend/src/container/CreateAlertV2/AlertCondition/AlertThreshold.tsx
+++ frontend/src/container/CreateAlertV2/AlertCondition/AlertThreshold.tsx
@@ -18,7 +18,6 @@ import {
 	THRESHOLD_OPERATOR_OPTIONS,
 } from '../context/constants';
 import EvaluationSettings from '../EvaluationSettings/EvaluationSettings';
-import { showCondensedLayout } from '../utils';
 import ThresholdItem from './ThresholdItem';
 import { AnomalyAndThresholdProps, UpdateThreshold } from './types';
 import {
@@ -42,8 +41,6 @@ function AlertThreshold({
 		notificationSettings,
 		setNotificationSettings,
 	} = useCreateAlertState();
-
-	const showCondensedLayoutFlag = showCondensedLayout();
 
 	const { currentQuery } = useQueryBuilder();
 
@@ -163,17 +160,12 @@ function AlertThreshold({
 		}),
 	);
 
-	const evaluationWindowContext = showCondensedLayoutFlag ? (
-		<EvaluationSettings />
-	) : (
-		<strong>Evaluation Window.</strong>
-	);
-
 	return (
 		<div
-			className={classNames('alert-threshold-container', {
-				'condensed-alert-threshold-container': showCondensedLayoutFlag,
-			})}
+			className={classNames(
+				'alert-threshold-container',
+				'condensed-alert-threshold-container',
+			)}
 		>
 			{/* Main condition sentence */}
 			<div className="alert-condition-sentences">
@@ -219,7 +211,7 @@ function AlertThreshold({
 						options={matchTypeOptionsWithTooltips}
 					/>
 					<Typography.Text className="sentence-text">
-						during the {evaluationWindowContext}
+						during the <EvaluationSettings />
 					</Typography.Text>
 				</div>
 			</div>
