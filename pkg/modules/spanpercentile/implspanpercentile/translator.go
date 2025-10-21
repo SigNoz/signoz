d8dfc6cedbbce05249795175a78ece6b64e1f1fd
eKuG <ekansh@signoz.io>
2025-10-21 13:49:36 +0530

    feat: refactored span percentile and changed query

--- pkg/modules/spanpercentile/implspanpercentile/translator.go
+++ pkg/modules/spanpercentile/implspanpercentile/translator.go
@@ -26,14 +26,12 @@ func buildSpanPercentileQuery(req *spanpercentiletypes.SpanPercentileRequest) (*
 
 	sb := sqlbuilder.NewSelectBuilder()
 
-	// SELECT clause with literal values
 	sb.Select(
 		fmt.Sprintf("%d AS duration_nano", req.DurationNano),
 		fmt.Sprintf("'%s' AS span_name", escapeSQLString(req.Name)),
 		fmt.Sprintf("'%s' AS service_name", escapeSQLString(req.ServiceName)),
 	)
 
-	// Add resource attributes to SELECT in sorted order for consistency
 	var attrKeys []string
 	for key := range req.ResourceAttributes {
 		attrKeys = append(attrKeys, key)
@@ -46,7 +44,6 @@ func buildSpanPercentileQuery(req *spanpercentiletypes.SpanPercentileRequest) (*
 		sb.SelectMore(fmt.Sprintf("'%s' AS %s", escapeSQLString(value), escapedKey))
 	}
 
-	// Add percentile aggregations in nanoseconds
 	sb.SelectMore(
 		"quantile(0.5)(s.duration_nano) AS p50_duration_nano",
 		"quantile(0.9)(s.duration_nano) AS p90_duration_nano",
@@ -54,10 +51,8 @@ func buildSpanPercentileQuery(req *spanpercentiletypes.SpanPercentileRequest) (*
 		fmt.Sprintf("round((100.0 * countIf(s.duration_nano <= %d)) / count(), 2) AS percentile_position", req.DurationNano),
 	)
 
-	// FROM clause
 	sb.From(fmt.Sprintf("%s.%s AS s", telemetrytraces.DBName, telemetrytraces.SpanIndexV3TableName))
 
-	// WHERE clause
 	sb.Where(
 		sb.GE("s.timestamp", fmt.Sprintf("%d", start)),
 		sb.L("s.timestamp", fmt.Sprintf("%d", end)),
@@ -65,14 +60,9 @@ func buildSpanPercentileQuery(req *spanpercentiletypes.SpanPercentileRequest) (*
 		sb.LE("s.ts_bucket_start", endBucket),
 	)
 
-	// Filter by name
 	sb.Where(sb.Equal("s.name", req.Name))
-
-	// Filter by service name using materialized column directly
-	// We use the materialized column resource_string_service$name for better performance
 	sb.Where(sb.Equal("s.resource_string_service$$name", req.ServiceName))
 
-	// Filter by additional resource attributes using field mapper
 	ctx := context.Background()
 	fieldMapper := telemetrytraces.NewFieldMapper()
 
@@ -82,23 +72,18 @@ func buildSpanPercentileQuery(req *spanpercentiletypes.SpanPercentileRequest) (*
 			Name:          key,
 			FieldContext:  telemetrytypes.FieldContextResource,
 			FieldDataType: telemetrytypes.FieldDataTypeString,
-			Materialized:  false, // Most resource attributes are not materialized
+			Materialized:  false,
 		}
 		fieldName, err := fieldMapper.FieldFor(ctx, resourceKey)
 		if err != nil {
-			// Fallback to map access if field mapper doesn't find it
 			fieldName = fmt.Sprintf("s.resources_string['%s']", key)
 		}
-		// fieldName already contains the full expression, no need to add 's.' prefix
 		sb.Where(sb.Equal(fieldName, value))
 	}
 
-	// Settings
 	sb.SQL("SETTINGS max_threads = 12, min_bytes_to_use_direct_io = 1, use_query_cache = 0, enable_filesystem_cache = 0, use_query_condition_cache = 0")
 
 	query, args := sb.BuildWithFlavor(sqlbuilder.ClickHouse)
-
-	// Interpolate args to get final query
 	interpolatedQuery := interpolateArgs(query, args)
 
 	queryEnvelope := qbtypes.QueryEnvelope{
@@ -128,7 +113,6 @@ func escapeResourceAttr(attr string) string {
 }
 
 func escapeSQLString(s string) string {
-	// Escape single quotes by doubling them
 	return strings.ReplaceAll(s, "'", "''")
 }
 
