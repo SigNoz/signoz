06d90b8b4b7d68d1c61e13b82b40dd38e6a8ff99
Shrilakshmi Shastry <shrilakshmi.shastry@smallcase.com>
2025-10-21 12:54:24 +0530

    chore: add environment configuration files and update TypeScript module resolution

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import { loadEnv } from 'vite';
 
 import path from 'path';
 
@@ -12,6 +12,7 @@ export default ({ mode }: { mode: string }) => {
 		},
 		server: {
 			port: 8080,
+			strictPort: false,
 			proxy: {
 				'/tunnel': {
 					target: env.VITE_TUNNEL_DOMAIN,
