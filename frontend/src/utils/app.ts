5af75c485ae13bb37c26e45dd39040d9d6f5253e
aks07 <adityasinghssj1@gmail.com>
2025-10-26 18:38:20 +0530

    feat: logs html rendering fix

--- frontend/src/utils/app.ts
+++ frontend/src/utils/app.ts
@@ -23,7 +23,15 @@ export const checkVersionState = (
 };
 
 // list of forbidden tags to remove in dompurify
-export const FORBID_DOM_PURIFY_TAGS = ['img', 'form'];
+export const FORBID_DOM_PURIFY_TAGS = [
+	'img',
+	'form',
+	'style',
+	'script',
+	'meta',
+	'link',
+];
+export const FORBID_DOM_PURIFY_ATTR = ['style'];
 
 export const isFeatureKeys = (key: string): key is keyof typeof FeatureKeys =>
 	Object.keys(FeatureKeys).includes(key);
