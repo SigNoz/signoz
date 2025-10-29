fd29fa891a860e9d00fba0dc0273b3d20962bafb
eKuG <ekansh@signoz.io>
2025-10-29 15:57:49 +0530

    feat: addressed comments regarding metadatastore

--- pkg/query-service/app/http_handler.go
+++ pkg/query-service/app/http_handler.go
@@ -2174,13 +2174,8 @@ func (aH *APIHandler) RegisterThirdPartyApiRoutes(router *mux.Router, am *middle
 	overviewRouter.HandleFunc("/domain", am.ViewAccess(aH.getDomainInfo)).Methods(http.MethodPost)
 }
 
-// RegisterSpanPercentileRoutes adds span percentile analysis routes
 func (aH *APIHandler) RegisterSpanPercentileRoutes(router *mux.Router, am *middleware.AuthZ) {
-
-	// Span percentile router
-	spanPercentileRouter := router.PathPrefix("/api/v1/span-percentile").Subrouter()
-
-	spanPercentileRouter.HandleFunc("/", am.ViewAccess(aH.Signoz.Handlers.SpanPercentile.GetSpanPercentileDetails)).Methods(http.MethodPost)
+	router.HandleFunc("/api/v1/span-percentile", am.ViewAccess(aH.Signoz.Handlers.SpanPercentile.GetSpanPercentileDetails)).Methods(http.MethodPost)
 }
 
 // not using md5 hashing as the plain string would work
