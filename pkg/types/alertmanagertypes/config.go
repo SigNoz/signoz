c6208ad7a268e7bc6a04b89ad625b8757d8b2728
Pranjul Kalsi <pranjulkalsi@gmail.com>
2025-11-10 20:42:38 +0530

    chore: addressed varable casing

--- pkg/types/alertmanagertypes/config.go
+++ pkg/types/alertmanagertypes/config.go
@@ -93,13 +93,13 @@ func NewConfigFromStoreableConfig(sc *StoreableConfig) (*Config, error) {
 }
 
 func NewDefaultConfig(globalConfig GlobalConfig, routeConfig RouteConfig, orgID string) (*Config, error) {
-	// Mergo treats an explicit false as zero-value and overwrites it to true, so we save it in smtpRequireTls and restore the user-specified value.
-	smtpRequireTls := globalConfig.SMTPRequireTLS
+	// Mergo treats an explicit false as zero-value and overwrites it to true, so we save it in smtpRequireTLS and restore the user-specified value.
+	smtpRequireTLS := globalConfig.SMTPRequireTLS
 	err := mergo.Merge(&globalConfig, config.DefaultGlobalConfig())
 	if err != nil {
 		return nil, err
 	}
-	globalConfig.SMTPRequireTLS = smtpRequireTls
+	globalConfig.SMTPRequireTLS = smtpRequireTLS
 
 	route, err := NewRouteFromRouteConfig(nil, routeConfig)
 	if err != nil {
@@ -170,13 +170,13 @@ func (c *Config) CopyWithReset() (*Config, error) {
 }
 
 func (c *Config) SetGlobalConfig(globalConfig GlobalConfig) error {
-	// Mergo treats an explicit false as zero-value and overwrites it to true, so we save it in smtpRequireTls and restore the user-specified value.
-	smtpRequireTls := globalConfig.SMTPRequireTLS
+	// Mergo treats an explicit false as zero-value and overwrites it to true, so we save it in smtpRequireTLS and restore the user-specified value.
+	smtpRequireTLS := globalConfig.SMTPRequireTLS
 	err := mergo.Merge(&globalConfig, config.DefaultGlobalConfig())
 	if err != nil {
 		return err
 	}
-	globalConfig.SMTPRequireTLS = smtpRequireTls
+	globalConfig.SMTPRequireTLS = smtpRequireTLS
 
 	c.alertmanagerConfig.Global = &globalConfig
 	c.storeableConfig.Config = string(newRawFromConfig(c.alertmanagerConfig))
