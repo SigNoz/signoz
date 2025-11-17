b0d52ee87a993043617523eceba5a7c7685c537c
Piyush Singariya <piyushsingariya@gmail.com>
2025-11-17 14:34:48 +0530

    feat: telemetry types

---
+++ pkg/types/telemetrytypes/json_datatype.go
@@ -0,0 +1,40 @@
+package telemetrytypes
+
+type JSONDataType struct {
+	str        string // Store the correct case for ClickHouse
+	IsArray    bool
+	ScalerType string
+}
+
+// Override StringValue to return the correct case
+func (jdt JSONDataType) StringValue() string {
+	return jdt.str
+}
+
+var (
+	String       = JSONDataType{"String", false, ""}
+	Int64        = JSONDataType{"Int64", false, ""}
+	Float64      = JSONDataType{"Float64", false, ""}
+	Bool         = JSONDataType{"Bool", false, ""}
+	Dynamic      = JSONDataType{"Dynamic", false, ""}
+	ArrayString  = JSONDataType{"Array(Nullable(String))", true, "String"}
+	ArrayInt64   = JSONDataType{"Array(Nullable(Int64))", true, "Int64"}
+	ArrayFloat64 = JSONDataType{"Array(Nullable(Float64))", true, "Float64"}
+	ArrayBool    = JSONDataType{"Array(Nullable(Bool))", true, "Bool"}
+	ArrayDynamic = JSONDataType{"Array(Dynamic)", true, "Dynamic"}
+	ArrayJSON    = JSONDataType{"Array(JSON)", true, "JSON"}
+)
+
+var MappingStringToJSONDataType = map[string]JSONDataType{
+	"String":                   String,
+	"Int64":                    Int64,
+	"Float64":                  Float64,
+	"Bool":                     Bool,
+	"Dynamic":                  Dynamic,
+	"Array(Nullable(String))":  ArrayString,
+	"Array(Nullable(Int64))":   ArrayInt64,
+	"Array(Nullable(Float64))": ArrayFloat64,
+	"Array(Nullable(Bool))":    ArrayBool,
+	"Array(Dynamic)":           ArrayDynamic,
+	"Array(JSON)":              ArrayJSON,
+}
