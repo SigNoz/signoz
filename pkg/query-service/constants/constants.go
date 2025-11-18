a851d5b9b0c80eb8734bcc98675cb6a9d9972d09
Tushar Vats <tushar@signoz.io>
2025-11-18 23:33:41 +0530

    fix: remove  from DeprecatedStaticFieldsTraces

--- pkg/query-service/constants/constants.go
+++ pkg/query-service/constants/constants.go
@@ -519,11 +519,6 @@ var DeprecatedStaticFieldsTraces = map[string]v3.AttributeKey{
 		DataType: v3.AttributeKeyDataTypeString,
 		IsColumn: true,
 	},
-	"kind": {
-		Key:      "kind",
-		DataType: v3.AttributeKeyDataTypeInt64,
-		IsColumn: true,
-	},
 	"spanKind": {
 		Key:      "spanKind",
 		DataType: v3.AttributeKeyDataTypeString,
