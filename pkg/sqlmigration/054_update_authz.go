09f4dca7ed4d0b7a7ca8f8b5cca62758dd71b40a
vikrantgupta25 <vikrant@signoz.io>
2025-11-19 21:36:17 +0530

    fix(authz): reduce cyclomatic complexity

--- pkg/sqlmigration/054_update_authz.go
+++ pkg/sqlmigration/054_update_authz.go
@@ -38,117 +38,119 @@ func (migration *updateAuthz) Register(migrations *migrate.Migrations) error {
 }
 
 func (migration *updateAuthz) Up(ctx context.Context, db *bun.DB) error {
-	if migration.sqlstore.BunDB().Dialect().Name() == dialect.PG {
-		tx, err := db.BeginTx(ctx, nil)
-		if err != nil {
+	if migration.sqlstore.BunDB().Dialect().Name() != dialect.PG {
+		return nil
+	}
+
+	tx, err := db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		_ = tx.Rollback()
+	}()
+
+	sqls := [][]byte{}
+
+	table, _, err := migration.sqlschema.GetTable(ctx, sqlschema.TableName("tuple"))
+	if err != nil {
+		return err
+	}
+	dropTableSQLS := migration.sqlschema.Operator().DropTable(table)
+	sqls = append(sqls, dropTableSQLS...)
+
+	table, _, err = migration.sqlschema.GetTable(ctx, sqlschema.TableName("authorization_model"))
+	if err != nil {
+		return err
+	}
+	dropTableSQLS = migration.sqlschema.Operator().DropTable(table)
+	sqls = append(sqls, dropTableSQLS...)
+
+	table, _, err = migration.sqlschema.GetTable(ctx, sqlschema.TableName("changelog"))
+	if err != nil {
+		return err
+	}
+	dropTableSQLS = migration.sqlschema.Operator().DropTable(table)
+	sqls = append(sqls, dropTableSQLS...)
+
+	table, _, err = migration.sqlschema.GetTable(ctx, sqlschema.TableName("assertion"))
+	if err != nil {
+		return err
+	}
+	dropTableSQLS = migration.sqlschema.Operator().DropTable(table)
+	sqls = append(sqls, dropTableSQLS...)
+
+	tableSQLs := migration.sqlschema.Operator().CreateTable(&sqlschema.Table{
+		Name: "tuple",
+		Columns: []*sqlschema.Column{
+			{Name: "store", DataType: sqlschema.DataTypeText, Nullable: false},
+			{Name: "object_type", DataType: sqlschema.DataTypeText, Nullable: false},
+			{Name: "object_id", DataType: sqlschema.DataTypeText, Nullable: false},
+			{Name: "relation", DataType: sqlschema.DataTypeText, Nullable: false},
+			{Name: "_user", DataType: sqlschema.DataTypeText, Nullable: false},
+			{Name: "user_type", DataType: sqlschema.DataTypeText, Nullable: false},
+			{Name: "ulid", DataType: sqlschema.DataTypeText, Nullable: false},
+			{Name: "inserted_at", DataType: sqlschema.DataTypeTimestamp, Nullable: false},
+			{Name: "condition_name", DataType: sqlschema.DataTypeBytea, Nullable: true},
+			{Name: "condition_context", DataType: sqlschema.DataTypeText, Nullable: true},
+		},
+		PrimaryKeyConstraint: &sqlschema.PrimaryKeyConstraint{ColumnNames: []sqlschema.ColumnName{"store", "object_type", "object_id", "relation", "_user"}},
+	})
+	sqls = append(sqls, tableSQLs...)
+
+	tableSQLs = migration.sqlschema.Operator().CreateTable(&sqlschema.Table{
+		Name: "authorization_model",
+		Columns: []*sqlschema.Column{
+			{Name: "store", DataType: sqlschema.DataTypeText, Nullable: false},
+			{Name: "authorization_model_id", DataType: sqlschema.DataTypeText, Nullable: false},
+			{Name: "type", DataType: sqlschema.DataTypeText, Nullable: false},
+			{Name: "type_definition", DataType: sqlschema.DataTypeBytea, Nullable: true},
+			{Name: "schema_version", DataType: sqlschema.DataTypeText, Nullable: false, Default: "1.1"},
+			{Name: "serialized_protobuf", DataType: sqlschema.DataTypeBytea, Nullable: false},
+		},
+		PrimaryKeyConstraint: &sqlschema.PrimaryKeyConstraint{ColumnNames: []sqlschema.ColumnName{"store", "authorization_model_id", "type"}},
+	})
+	sqls = append(sqls, tableSQLs...)
+
+	tableSQLs = migration.sqlschema.Operator().CreateTable(&sqlschema.Table{
+		Name: "changelog",
+		Columns: []*sqlschema.Column{
+			{Name: "store", DataType: sqlschema.DataTypeText, Nullable: false},
+			{Name: "object_type", DataType: sqlschema.DataTypeText, Nullable: false},
+			{Name: "object_id", DataType: sqlschema.DataTypeText, Nullable: false},
+			{Name: "relation", DataType: sqlschema.DataTypeText, Nullable: false},
+			{Name: "_user", DataType: sqlschema.DataTypeText, Nullable: false},
+			{Name: "operation", DataType: sqlschema.DataTypeText, Nullable: false},
+			{Name: "ulid", DataType: sqlschema.DataTypeText, Nullable: false},
+			{Name: "inserted_at", DataType: sqlschema.DataTypeTimestamp, Nullable: false},
+			{Name: "condition_name", DataType: sqlschema.DataTypeBytea, Nullable: true},
+			{Name: "condition_context", DataType: sqlschema.DataTypeText, Nullable: true},
+		},
+		PrimaryKeyConstraint: &sqlschema.PrimaryKeyConstraint{ColumnNames: []sqlschema.ColumnName{"store", "ulid", "object_type"}},
+	})
+	sqls = append(sqls, tableSQLs...)
+
+	tableSQLs = migration.sqlschema.Operator().CreateTable(&sqlschema.Table{
+		Name: "assertion",
+		Columns: []*sqlschema.Column{
+			{Name: "store", DataType: sqlschema.DataTypeText, Nullable: false},
+			{Name: "authorization_model_id", DataType: sqlschema.DataTypeText, Nullable: false},
+			{Name: "assertions", DataType: sqlschema.DataTypeBytea, Nullable: true},
+		},
+		PrimaryKeyConstraint: &sqlschema.PrimaryKeyConstraint{ColumnNames: []sqlschema.ColumnName{"store", "authorization_model_id"}},
+	})
+	sqls = append(sqls, tableSQLs...)
+
+	for _, sql := range sqls {
+		if _, err := tx.ExecContext(ctx, string(sql)); err != nil {
 			return err
 		}
+	}
 
-		defer func() {
-			_ = tx.Rollback()
-		}()
-
-		sqls := [][]byte{}
-
-		table, _, err := migration.sqlschema.GetTable(ctx, sqlschema.TableName("tuple"))
-		if err != nil {
-			return err
-		}
-		dropTableSQLS := migration.sqlschema.Operator().DropTable(table)
-		sqls = append(sqls, dropTableSQLS...)
-
-		table, _, err = migration.sqlschema.GetTable(ctx, sqlschema.TableName("authorization_model"))
-		if err != nil {
-			return err
-		}
-		dropTableSQLS = migration.sqlschema.Operator().DropTable(table)
-		sqls = append(sqls, dropTableSQLS...)
-
-		table, _, err = migration.sqlschema.GetTable(ctx, sqlschema.TableName("changelog"))
-		if err != nil {
-			return err
-		}
-		dropTableSQLS = migration.sqlschema.Operator().DropTable(table)
-		sqls = append(sqls, dropTableSQLS...)
-
-		table, _, err = migration.sqlschema.GetTable(ctx, sqlschema.TableName("assertion"))
-		if err != nil {
-			return err
-		}
-		dropTableSQLS = migration.sqlschema.Operator().DropTable(table)
-		sqls = append(sqls, dropTableSQLS...)
-
-		tableSQLs := migration.sqlschema.Operator().CreateTable(&sqlschema.Table{
-			Name: "tuple",
-			Columns: []*sqlschema.Column{
-				{Name: "store", DataType: sqlschema.DataTypeText, Nullable: false},
-				{Name: "object_type", DataType: sqlschema.DataTypeText, Nullable: false},
-				{Name: "object_id", DataType: sqlschema.DataTypeText, Nullable: false},
-				{Name: "relation", DataType: sqlschema.DataTypeText, Nullable: false},
-				{Name: "_user", DataType: sqlschema.DataTypeText, Nullable: false},
-				{Name: "user_type", DataType: sqlschema.DataTypeText, Nullable: false},
-				{Name: "ulid", DataType: sqlschema.DataTypeText, Nullable: false},
-				{Name: "inserted_at", DataType: sqlschema.DataTypeTimestamp, Nullable: false},
-				{Name: "condition_name", DataType: sqlschema.DataTypeBytea, Nullable: true},
-				{Name: "condition_context", DataType: sqlschema.DataTypeText, Nullable: true},
-			},
-			PrimaryKeyConstraint: &sqlschema.PrimaryKeyConstraint{ColumnNames: []sqlschema.ColumnName{"store", "object_type", "object_id", "relation", "_user"}},
-		})
-		sqls = append(sqls, tableSQLs...)
-
-		tableSQLs = migration.sqlschema.Operator().CreateTable(&sqlschema.Table{
-			Name: "authorization_model",
-			Columns: []*sqlschema.Column{
-				{Name: "store", DataType: sqlschema.DataTypeText, Nullable: false},
-				{Name: "authorization_model_id", DataType: sqlschema.DataTypeText, Nullable: false},
-				{Name: "type", DataType: sqlschema.DataTypeText, Nullable: false},
-				{Name: "type_definition", DataType: sqlschema.DataTypeBytea, Nullable: true},
-				{Name: "schema_version", DataType: sqlschema.DataTypeText, Nullable: false, Default: "1.1"},
-				{Name: "serialized_protobuf", DataType: sqlschema.DataTypeBytea, Nullable: false},
-			},
-			PrimaryKeyConstraint: &sqlschema.PrimaryKeyConstraint{ColumnNames: []sqlschema.ColumnName{"store", "authorization_model_id", "type"}},
-		})
-		sqls = append(sqls, tableSQLs...)
-
-		tableSQLs = migration.sqlschema.Operator().CreateTable(&sqlschema.Table{
-			Name: "changelog",
-			Columns: []*sqlschema.Column{
-				{Name: "store", DataType: sqlschema.DataTypeText, Nullable: false},
-				{Name: "object_type", DataType: sqlschema.DataTypeText, Nullable: false},
-				{Name: "object_id", DataType: sqlschema.DataTypeText, Nullable: false},
-				{Name: "relation", DataType: sqlschema.DataTypeText, Nullable: false},
-				{Name: "_user", DataType: sqlschema.DataTypeText, Nullable: false},
-				{Name: "operation", DataType: sqlschema.DataTypeText, Nullable: false},
-				{Name: "ulid", DataType: sqlschema.DataTypeText, Nullable: false},
-				{Name: "inserted_at", DataType: sqlschema.DataTypeTimestamp, Nullable: false},
-				{Name: "condition_name", DataType: sqlschema.DataTypeBytea, Nullable: true},
-				{Name: "condition_context", DataType: sqlschema.DataTypeText, Nullable: true},
-			},
-			PrimaryKeyConstraint: &sqlschema.PrimaryKeyConstraint{ColumnNames: []sqlschema.ColumnName{"store", "ulid", "object_type"}},
-		})
-		sqls = append(sqls, tableSQLs...)
-
-		tableSQLs = migration.sqlschema.Operator().CreateTable(&sqlschema.Table{
-			Name: "assertion",
-			Columns: []*sqlschema.Column{
-				{Name: "store", DataType: sqlschema.DataTypeText, Nullable: false},
-				{Name: "authorization_model_id", DataType: sqlschema.DataTypeText, Nullable: false},
-				{Name: "assertions", DataType: sqlschema.DataTypeBytea, Nullable: true},
-			},
-			PrimaryKeyConstraint: &sqlschema.PrimaryKeyConstraint{ColumnNames: []sqlschema.ColumnName{"store", "authorization_model_id"}},
-		})
-		sqls = append(sqls, tableSQLs...)
-
-		for _, sql := range sqls {
-			if _, err := tx.ExecContext(ctx, string(sql)); err != nil {
-				return err
-			}
-		}
-
-		err = tx.Commit()
-		if err != nil {
-			return err
-		}
+	err = tx.Commit()
+	if err != nil {
+		return err
 	}
 
 	return nil
