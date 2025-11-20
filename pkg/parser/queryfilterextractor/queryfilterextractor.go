abce05b289a83ba635dfaa2e9f203abfce55966e
Abhishek Kumar Singh <hritik6058@gmail.com>
2025-11-20 17:38:00 +0530

    feat: exposed group name from column info

--- pkg/parser/queryfilterextractor/queryfilterextractor.go
+++ pkg/parser/queryfilterextractor/queryfilterextractor.go
@@ -19,6 +19,22 @@ type ColumnInfo struct {
 	OriginField string
 }
 
+// GroupName returns the field name in the resulting data which is used for grouping
+//
+//   - examples:
+//
+//   - SELECT region as new_region FROM metrics WHERE metric_name='cpu' GROUP BY region
+//     GroupName() will return "new_region"
+//
+//   - SELECT region FROM metrics WHERE metric_name='cpu' GROUP BY region
+//     GroupName() will return "region"
+func (c *ColumnInfo) GroupName() string {
+	if c.Alias != "" {
+		return c.Alias
+	}
+	return c.Name
+}
+
 type FilterResult struct {
 	// MetricNames are the metrics that are being filtered on
 	MetricNames []string
