7a850d63b260d7530e8d541b9416ca3550e87cea
amlannandy <amlannandy5@gmail.com>
2025-11-23 00:34:06 +0700

    chore: fix loading in test notifcation action

--- frontend/src/container/CreateAlertV2/utils.tsx
+++ frontend/src/container/CreateAlertV2/utils.tsx
@@ -28,9 +28,14 @@ import { EVALUATION_WINDOW_TIMEFRAME } from './EvaluationSettings/constants';
 import { GetCreateAlertLocalStateFromAlertDefReturn } from './types';
 
 export function Spinner(): JSX.Element | null {
-	const { isCreatingAlertRule, isUpdatingAlertRule } = useCreateAlertState();
+	const {
+		isCreatingAlertRule,
+		isUpdatingAlertRule,
+		isTestingAlertRule,
+	} = useCreateAlertState();
 
-	if (!isCreatingAlertRule && !isUpdatingAlertRule) return null;
+	if (!isCreatingAlertRule && !isUpdatingAlertRule && !isTestingAlertRule)
+		return null;
 
 	return createPortal(
 		<div className="sticky-page-spinner">
