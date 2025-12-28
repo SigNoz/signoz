9a43b5289ea4634d16c3ea6c06c45680fc49a1f4
ajmalmohad <ajmalajmal.2016@gmail.com>
2025-12-28 15:51:39 +0530

    refactor: avoid checking for old cache as it expires quick

--- pkg/query-service/app/clickhouseReader/reader.go
+++ pkg/query-service/app/clickhouseReader/reader.go
@@ -1049,22 +1049,13 @@ func (r *ClickHouseReader) GetWaterfallSpansForTraceWithMetadata(ctx context.Con
 	selectedSpans, uncollapsedSpans, rootServiceName, rootServiceEntryPoint := tracedetail.GetSelectedSpans(req.UncollapsedSpans, req.SelectedSpanID, traceRoots, spanIdToSpanNodeMap, req.IsSelectedSpanIDUnCollapsed)
 	zap.L().Info("getWaterfallSpansForTraceWithMetadata: processing post cache", zap.Duration("duration", time.Since(processingPostCache)), zap.String("traceID", traceID))
 
-	// Make sure cached data is not converted again
-	needsConversionToMillis := false
-	// only convert if the timestamp is in nanoseconds (because old cache might have millis timestamps)
+	// convert start timestamp to millis because right now frontend is expecting it in millis
 	for _, span := range selectedSpans {
-		// only convert if the timestamp is in nanoseconds (because old cache might have millis timestamps)
-		if span.TimeUnixNano > 1e15 {
-			span.TimeUnixNano = span.TimeUnixNano / 1000000
-			needsConversionToMillis = true
-		}
+		span.TimeUnixNano = span.TimeUnixNano / 1000000
 	}
 
-	if needsConversionToMillis {
-		for serviceName, totalDuration := range serviceNameToTotalDurationMap {
-			// convert nanoseconds to milliseconds
-			serviceNameToTotalDurationMap[serviceName] = totalDuration / 1000000
-		}
+	for serviceName, totalDuration := range serviceNameToTotalDurationMap {
+		serviceNameToTotalDurationMap[serviceName] = totalDuration / 1000000
 	}
 
 	response.Spans = selectedSpans
