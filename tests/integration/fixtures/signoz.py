f1a3275b2b41efe02c80f8a02f1dccd6c30697f3
Karan Balani <29383381+balanikaran@users.noreply.github.com>
2026-01-08 14:37:26 +0530

    chore: fix integration tests

--- tests/integration/fixtures/signoz.py
+++ tests/integration/fixtures/signoz.py
@@ -65,6 +65,7 @@ def signoz(  # pylint: disable=too-many-arguments,too-many-positional-arguments
                 "SIGNOZ_INSTRUMENTATION_LOGS_LEVEL": "debug",
                 "SIGNOZ_PROMETHEUS_ACTIVE__QUERY__TRACKER_ENABLED": False,
                 "SIGNOZ_GATEWAY_URL": gateway.container_configs["8080"].base(),
+                "SIGNOZ_TOKENIZER_JWT_SECRET": "secret",
             }
             | sqlstore.env
             | clickhouse.env
