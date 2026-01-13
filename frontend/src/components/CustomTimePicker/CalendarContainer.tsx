cde1e10b01755533518a9a66646e3874de5966e0
Yunus M <myounis.ar@live.com>
2026-01-13 17:06:09 +0530

    feat: improve date range selection ux

--- frontend/src/components/CustomTimePicker/CalendarContainer.tsx
+++ frontend/src/components/CustomTimePicker/CalendarContainer.tsx
@@ -4,6 +4,7 @@ import { DATE_TIME_FORMATS } from 'constants/dateTimeFormats';
 import dayjs from 'dayjs';
 import { CalendarIcon, Check, X } from 'lucide-react';
 import { useTimezone } from 'providers/Timezone';
+import { useState } from 'react';
 
 import { DateRange } from './CustomTimePickerPopoverContent';
 
@@ -19,6 +20,41 @@ function CalendarContainer({
 	onApply: () => void;
 }): JSX.Element {
 	const { timezone } = useTimezone();
+	const [range, setRange] = useState<DateRange | undefined>(dateRange);
+
+	// this is to override the default behavior of the shadcn calendar component
+	// if a range is already selected, clicking on a date will reset selection and set the new date as the start date
+	const handleSelect = (
+		_selected: DateRange | undefined,
+		clickedDate?: Date,
+	): void => {
+		if (!clickedDate) {
+			return;
+		}
+
+		// No dates selected → start new
+		if (!range?.from) {
+			setRange({ from: clickedDate });
+			onSelectDateRange({ from: clickedDate });
+			return;
+		}
+
+		// Only start selected → complete the range
+		if (range.from && !range.to) {
+			if (clickedDate < range.from) {
+				setRange({ from: clickedDate, to: range.from });
+				onSelectDateRange({ from: clickedDate, to: range.from });
+			} else {
+				setRange({ from: range.from, to: clickedDate });
+				onSelectDateRange({ from: range.from, to: clickedDate });
+			}
+			return;
+		}
+
+		// Full range already selected → RESET
+		setRange({ from: clickedDate, to: undefined });
+		onSelectDateRange({ from: clickedDate, to: undefined });
+	};
 
 	return (
 		<div className="calendar-container">
@@ -39,12 +75,12 @@ function CalendarContainer({
 				<Calendar
 					mode="range"
 					required
-					defaultMonth={dateRange?.from}
-					selected={dateRange}
+					defaultMonth={range?.from}
+					selected={range}
 					disabled={{
 						after: dayjs().toDate(),
 					}}
-					onSelect={onSelectDateRange}
+					onSelect={handleSelect}
 				/>
 
 				<div className="calendar-actions">
