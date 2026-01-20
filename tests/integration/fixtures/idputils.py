918da20a42438087bcbac8ba777b182bfa1606d9
Karan Balani <29383381+balanikaran@users.noreply.github.com>
2026-01-20 23:12:29 +0530

    chore: add back explanatory comments

--- tests/integration/fixtures/idputils.py
+++ tests/integration/fixtures/idputils.py
@@ -366,15 +366,21 @@ def create_user_idp(
 @pytest.fixture(name="idp_login", scope="function")
 def idp_login(driver: webdriver.Chrome) -> Callable[[str, str], None]:
     def _idp_login(email: str, password: str) -> None:
+        # wait for the elements to be clickable before interacting with them
         wait = WebDriverWait(driver, 10)
 
-        # wait for the elements to be clickable before interacting with them
+        # Input email. The following element is present in the idp login page.
+        # <input id="username" name="username" value="" type="text" autocomplete="username" autofocus aria-invalid=""/>
         username_field = wait.until(EC.element_to_be_clickable((By.ID, "username")))
         username_field.send_keys(email)
 
+        # Input password. The following element is present in the idp login page.
+        # <input id="password" name="password" value="" type="password" autocomplete="current-password" aria-invalid=""/>
         password_field = wait.until(EC.element_to_be_clickable((By.ID, "password")))
         password_field.send_keys(password)
 
+        # Click login button. The following element is present in the idp login page.
+        # <button class="pf-v5-c-button pf-m-primary pf-m-block " name="login" id="kc-login" type="submit" >Sign In</button>
         login_button = wait.until(EC.element_to_be_clickable((By.ID, "kc-login")))
         login_button.click()
 
