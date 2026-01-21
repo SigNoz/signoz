120bad1cf41d183058acda2e8b025171ff743da3
Ashwin Bhatkal <ashwin96@gmail.com>
2026-01-21 16:00:46 +0530

    chore: use isNumber from lodash-es

--- frontend/src/utils/numericUtils.ts
+++ frontend/src/utils/numericUtils.ts
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
 import { isNumber } from 'lodash-es';
 
 export function formatNumericValue(value: number | string): string {
 	if (isNumber(value)) {
-=======
-export function formatNumericValue(value: number | string): string {
-	if (typeof value !== 'number') {
->>>>>>> c1379f1cc (chore: add eslint rules for no-else-return and curly)
 		return value.toString();
 	}
 	return (typeof value === 'string' ? parseFloat(value) : value).toFixed(3);
