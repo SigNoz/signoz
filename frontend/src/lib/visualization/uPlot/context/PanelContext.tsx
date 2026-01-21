b4c40bae38e315b10c6d7530cc61a27a9ba5390a
Abhi Kumar <ahrefabhi@gmail.com>
2026-01-21 15:15:35 +0530

    feat: added panel context wrapper

---
+++ frontend/src/lib/visualization/uPlot/context/PanelContext.tsx
@@ -0,0 +1,85 @@
+import {
+	createContext,
+	PropsWithChildren,
+	useCallback,
+	useContext,
+	useRef,
+} from 'react';
+import uPlot from 'uplot';
+
+export interface IPanelContext {
+	setUPlotInstance: (uPlotInstance: uPlot | null) => void;
+	onToggleSeriesVisibility: (seriesIndex: number) => void;
+	onToggleSeriesOnOff: (seriesIndex: number) => void;
+	onFocusSeries: (seriesIndex: number | null) => void;
+}
+
+export const PanelContext = createContext<IPanelContext | null>(null);
+
+export const PanelContextProvider = ({
+	children,
+}: PropsWithChildren): JSX.Element => {
+	const uPlotInstanceRef = useRef<uPlot | null>(null);
+	const activeSeriesIndex = useRef<number | undefined>(undefined);
+
+	const setUPlotInstance = useCallback((uPlotInstance: uPlot | null): void => {
+		uPlotInstanceRef.current = uPlotInstance;
+	}, []);
+
+	const onToggleSeriesVisibility = useCallback((seriesIndex: number): void => {
+		const plot = uPlotInstanceRef.current;
+		if (!plot) return;
+
+		const isReset = activeSeriesIndex.current === seriesIndex;
+		activeSeriesIndex.current = isReset ? undefined : seriesIndex;
+
+		plot.batch(() => {
+			plot.series.forEach((_, index) => {
+				if (index === 0) return;
+				const currentSeriesIndex = index;
+				plot.setSeries(currentSeriesIndex, {
+					show: isReset || currentSeriesIndex === seriesIndex,
+				});
+			});
+		});
+	}, []);
+
+	const onToggleSeriesOnOff = useCallback((seriesIndex: number): void => {
+		const plot = uPlotInstanceRef.current;
+		if (!plot) return;
+
+		plot.setSeries(seriesIndex, { show: !plot.series[seriesIndex].show });
+	}, []);
+
+	const onFocusSeries = useCallback((seriesIndex: number | null): void => {
+		const plot = uPlotInstanceRef.current;
+		if (!plot) return;
+
+		plot.setSeries(seriesIndex, {
+			focus: true,
+		});
+	}, []);
+
+	return (
+		<PanelContext.Provider
+			value={{
+				onToggleSeriesVisibility,
+				setUPlotInstance,
+				onToggleSeriesOnOff,
+				onFocusSeries,
+			}}
+		>
+			{children}
+		</PanelContext.Provider>
+	);
+};
+
+export const usePanelContext = (): IPanelContext => {
+	const context = useContext(PanelContext);
+
+	if (!context) {
+		throw new Error('Should be used inside the context');
+	}
+
+	return context;
+};
