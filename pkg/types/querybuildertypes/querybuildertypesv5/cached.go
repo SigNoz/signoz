b9570a4671a4f03d6ecfec04acbf273beaca90a8
grandwizard28 <vibhupandey28@gmail.com>
2026-01-21 21:51:50 +0530

    feat: add bench test

--- pkg/types/querybuildertypes/querybuildertypesv5/cached.go
+++ pkg/types/querybuildertypes/querybuildertypesv5/cached.go
@@ -1,7 +1,9 @@
 package querybuildertypesv5
 
 import (
+	"bytes"
 	"encoding/json"
+	"maps"
 
 	"github.com/SigNoz/signoz/pkg/types/cachetypes"
 )
@@ -14,6 +16,21 @@ type CachedBucket struct {
 	Type    RequestType     `json:"type"`
 	Value   json.RawMessage `json:"value"`
 	Stats   ExecStats       `json:"stats"`
+}
+
+func (c *CachedBucket) Clone() *CachedBucket {
+	return &CachedBucket{
+		StartMs: c.StartMs,
+		EndMs:   c.EndMs,
+		Type:    c.Type,
+		Value:   bytes.Clone(c.Value),
+		Stats: ExecStats{
+			RowsScanned:   c.Stats.RowsScanned,
+			BytesScanned:  c.Stats.BytesScanned,
+			DurationMS:    c.Stats.DurationMS,
+			StepIntervals: maps.Clone(c.Stats.StepIntervals),
+		},
+	}
 }
 
 // CachedData represents the full cached data for a query
@@ -30,6 +47,15 @@ func (c *CachedData) MarshalBinary() ([]byte, error) {
 	return json.Marshal(c)
 }
 
-func (c *CachedData) Clone() cachetypes.Cloneable {
-	return nil
+func (c *CachedData) Clone() cachetypes.Cacheable {
+	clonedCachedData := new(CachedData)
+	clonedCachedData.Buckets = make([]*CachedBucket, len(c.Buckets))
+	for i := range c.Buckets {
+		clonedCachedData.Buckets[i] = c.Buckets[i].Clone()
+	}
+
+	clonedCachedData.Warnings = make([]string, len(c.Warnings))
+	copy(clonedCachedData.Warnings, c.Warnings)
+
+	return clonedCachedData
 }
