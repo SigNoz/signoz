0913652113894e6de9aaac307070e9b640781d93
Ashwin Bhatkal <ashwin96@gmail.com>
2026-01-23 10:05:32 +0530

    chore: fix type error

--- frontend/src/components/QuickFilters/FilterRenderers/Slider/Slider.tsx
+++ frontend/src/components/QuickFilters/FilterRenderers/Slider/Slider.tsx
@@ -1,8 +1,5 @@
 import './Slider.styles.scss';
 
-import { IQuickFiltersConfig } from 'components/QuickFilters/types';
-
-// not needed for now build when required
 export default function Slider(): JSX.Element {
 	return <div>Slider</div>;
 }
