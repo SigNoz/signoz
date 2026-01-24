22121470658f377110ea427284dec02dd3d8e59a
amlannandy <amlannandy5@gmail.com>
2026-01-24 10:12:54 +0700

    chore: fix CI

--- frontend/src/container/MetricsExplorer/MetricDetails/__tests__/Highlights.test.tsx
+++ frontend/src/container/MetricsExplorer/MetricDetails/__tests__/Highlights.test.tsx
@@ -3,6 +3,7 @@ import * as metricsExplorerHooks from 'api/generated/services/metrics';
 
 import Highlights from '../Highlights';
 import { getMockMetricHighlightsData } from './testUtlls';
+import { formatTimestampToReadableDate } from '../utils';
 
 const MOCK_METRIC_NAME = 'test-metric';
 const METRIC_DETAILS_GRID_VALUE_SELECTOR = '.metric-details-grid-value';
@@ -27,7 +28,9 @@ describe('Highlights', () => {
 		expect(metricHighlightsValues).toHaveLength(3);
 		expect(metricHighlightsValues[0].textContent).toBe('1M+');
 		expect(metricHighlightsValues[1].textContent).toBe('1M total ⎯ 1M active');
-		expect(metricHighlightsValues[2].textContent).toBe('1/21/1970');
+		expect(metricHighlightsValues[2].textContent).toBe(
+			formatTimestampToReadableDate('2026-01-24T00:00:00Z'),
+		);
 	});
 
 	it('should render "-" for highlights data when there is an error', () => {
