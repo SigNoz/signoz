afdb674068d37775bbf63aab15b6ff5943701e82
Abhishek Kumar Singh <hritik6058@gmail.com>
2026-02-02 23:37:14 +0530

    test(integration): added fixture for inserting alert data (#10101)
    * chore: fixture for notification channel
    
    * chore: return notification channel info in Create notification channel API
    
    * fix: change scope of create channel fixture to function level
    
    * test: added fixture for creating alert rule
    
    * chore: added debug message on assertion failure
    
    * refactor: improve error handling in webhook notification channel deletion
    
    * fix: enhance error handling in alert rule creation and deletion
    
    * chore: ran py linter and fmt
    
    * chore: ran py linter and fmt
    
    * fix: add timeout to alert rule creation and deletion requests
    
    * fix: silenced pylint on too broad exception
    
    * fix: suppress pylint warnings for broad exception handling in alert rule deletion
    
    * test: added fixture for inserting alert data
    
    * refactor: added fixture for getting test data file path
    
    * feat: add alerts to integration CI workflow
    
    * chore: linter fixes
    
    * chore: changed scope for get_testdata_file_path
    
    * chore: py-formatter
    
    * chore: py-formatter
    
    * chore: updated get_testdata_file_path fixture to a util function
    
    * chore: removed wrong ref
    
    ---------
    
    Co-authored-by: Srikanth Chekuri <srikanth.chekuri92@gmail.com>

--- tests/integration/fixtures/alerts.py
+++ tests/integration/fixtures/alerts.py
@@ -1,5 +1,6 @@
+from datetime import datetime, timezone
 from http import HTTPStatus
-from typing import Callable
+from typing import Callable, List
 
 import pytest
 import requests
@@ -7,6 +8,10 @@ import requests
 from fixtures import types
 from fixtures.auth import USER_ADMIN_EMAIL, USER_ADMIN_PASSWORD
 from fixtures.logger import setup_logger
+from fixtures.logs import Logs
+from fixtures.metrics import Metrics
+from fixtures.traces import Traces
+from fixtures.utils import get_testdata_file_path
 
 logger = setup_logger(__name__)
 
@@ -52,3 +57,54 @@ def create_alert_rule(
             _delete_alert_rule(rule_id)
         except Exception as e:  # pylint: disable=broad-exception-caught
             logger.error("Error deleting rule: %s", {"rule_id": rule_id, "error": e})
+
+
+@pytest.fixture(name="insert_alert_data", scope="function")
+def insert_alert_data(
+    insert_metrics: Callable[[List[Metrics]], None],
+    insert_traces: Callable[[List[Traces]], None],
+    insert_logs: Callable[[List[Logs]], None],
+) -> Callable[[List[types.AlertData]], None]:
+
+    def _insert_alert_data(
+        alert_data_items: List[types.AlertData],
+        base_time: datetime = None,
+    ) -> None:
+
+        metrics: List[Metrics] = []
+        traces: List[Traces] = []
+        logs: List[Logs] = []
+
+        now = base_time or datetime.now(tz=timezone.utc).replace(
+            second=0, microsecond=0
+        )
+
+        for data_item in alert_data_items:
+            if data_item.type == "metrics":
+                _metrics = Metrics.load_from_file(
+                    get_testdata_file_path(data_item.data_path),
+                    base_time=now,
+                )
+                metrics.extend(_metrics)
+            elif data_item.type == "traces":
+                _traces = Traces.load_from_file(
+                    get_testdata_file_path(data_item.data_path),
+                    base_time=now,
+                )
+                traces.extend(_traces)
+            elif data_item.type == "logs":
+                _logs = Logs.load_from_file(
+                    get_testdata_file_path(data_item.data_path),
+                    base_time=now,
+                )
+                logs.extend(_logs)
+
+        # Add data to ClickHouse if any data is present
+        if len(metrics) > 0:
+            insert_metrics(metrics)
+        if len(traces) > 0:
+            insert_traces(traces)
+        if len(logs) > 0:
+            insert_logs(logs)
+
+    yield _insert_alert_data
