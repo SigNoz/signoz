580cf32eb5000421effc819a2a51f0192a99e307
Vikrant Gupta <vikrant@signoz.io>
2026-02-03 19:54:07 +0530

    feat(authz): migrate rbac to authz (#10134)
    * feat(authz): migrate rbac to authz
    
    * feat(authz): handle public dashboard migration
    
    * feat(authz): fix integration tests
    
    * feat(authz): fix integration tests
    
    * feat(authz): keep the error same as today

--- pkg/modules/organization/organization.go
+++ pkg/modules/organization/organization.go
@@ -18,7 +18,7 @@ type Getter interface {
 
 type Setter interface {
 	// Create creates the given organization
-	Create(context.Context, *types.Organization) error
+	Create(context.Context, *types.Organization, func(context.Context, valuer.UUID) error) error
 
 	// Update updates the given organization
 	Update(context.Context, *types.Organization) error
