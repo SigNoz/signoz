858cd287fac6cad20d515a0cc7e1141221dd7d62
Ashwin Bhatkal <ashwin96@gmail.com>
2026-02-03 09:33:02 +0530

    chore: query builder / hooks to use new variable store (#10148)
    * chore: query builder / hooks to use new variable store
    
    * chore: fix tests

--- frontend/src/lib/dashbaordVariables/getDashboardVariables.ts
+++ frontend/src/lib/dashbaordVariables/getDashboardVariables.ts
@@ -1,9 +1,9 @@
 import getStartEndRangeTime from 'lib/getStartEndRangeTime';
+import { IDashboardVariables } from 'providers/Dashboard/store/dashboardVariablesStore';
 import store from 'store';
-import { Dashboard } from 'types/api/dashboard/getAll';
 
 export const getDashboardVariables = (
-	variables?: Dashboard['data']['variables'],
+	variables?: IDashboardVariables,
 ): Record<string, unknown> => {
 	if (!variables) {
 		return {};
