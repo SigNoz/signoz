4e2b87419eafbc37c403d0d5c0b152c79c4c2eb5
Ashwin Bhatkal <ashwin96@gmail.com>
2026-02-09 14:36:22 +0530

    chore: revert multi select file change

--- frontend/src/components/NewSelect/CustomMultiSelect.tsx
+++ frontend/src/components/NewSelect/CustomMultiSelect.tsx
@@ -140,7 +140,10 @@ const CustomMultiSelect: React.FC<CustomMultiSelectProps> = ({
 	}, [selectedValues, allAvailableValues, enableAllSelection]);
 
 	// Define allOptionShown earlier in the code
-	const allOptionShown = useMemo(() => value === ALL_SELECTED_VALUE, [value]);
+	const allOptionShown = useMemo(
+		() => value === ALL_SELECTED_VALUE || value === 'ALL',
+		[value],
+	);
 
 	// Value passed to the underlying Ant Select component
 	const displayValue = useMemo(
