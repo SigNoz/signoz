c0b6ca37f5f4471b11ff6e8e68edd56e099593ef
Karan Balani <29383381+balanikaran@users.noreply.github.com>
2026-02-17 13:07:07 +0530

    chore: remove commented code

--- tests/integration/fixtures/auth.py
+++ tests/integration/fixtures/auth.py
@@ -17,50 +17,9 @@ from fixtures.signoz import ROOT_USER_EMAIL, ROOT_USER_PASSWORD
 
 logger = setup_logger(__name__)
 
-# USER_ADMIN_NAME = "admin"
-# USER_ADMIN_EMAIL = "admin@integration.test"
-# USER_ADMIN_PASSWORD = "password123Z$"
-
 USER_EDITOR_NAME = "editor"
 USER_EDITOR_EMAIL = "editor@integration.test"
 USER_EDITOR_PASSWORD = "password123Z$"
-
-
-# @pytest.fixture(name="create_user_admin", scope="package")
-# def create_user_admin(
-#     signoz: types.SigNoz, request: pytest.FixtureRequest, pytestconfig: pytest.Config
-# ) -> types.Operation:
-#     def create() -> None:
-#         response = requests.post(
-#             signoz.self.host_configs["8080"].get("/api/v1/register"),
-#             json={
-#                 "name": USER_ADMIN_NAME,
-#                 "orgName": "",
-#                 "email": USER_ADMIN_EMAIL,
-#                 "password": USER_ADMIN_PASSWORD,
-#             },
-#             timeout=5,
-#         )
-
-#         assert response.status_code == HTTPStatus.OK
-
-#         return types.Operation(name="create_user_admin")
-
-#     def delete(_: types.Operation) -> None:
-#         pass
-
-#     def restore(cache: dict) -> types.Operation:
-#         return types.Operation(name=cache["name"])
-
-#     return dev.wrap(
-#         request,
-#         pytestconfig,
-#         "create_user_admin",
-#         lambda: types.Operation(name=""),
-#         create,
-#         delete,
-#         restore,
-#     )
 
 
 @pytest.fixture(name="get_session_context", scope="function")
