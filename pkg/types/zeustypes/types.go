76843a8cdb97e768e0b42e6f65219a102a9cce0a
Karan Balani <29383381+balanikaran@users.noreply.github.com>
2026-02-18 17:22:15 +0530

    fix: model

--- pkg/types/zeustypes/types.go
+++ pkg/types/zeustypes/types.go
@@ -8,10 +8,10 @@ type PostableProfile struct {
 	UsesOtel                     bool     `json:"uses_otel"`
 	HasExistingObservabilityTool bool     `json:"has_existing_observability_tool"`
 	ExistingObservabilityTool    string   `json:"existing_observability_tool"`
-	WhereDidYouDiscoverSignoz    string   `json:"where_did_you_discover_signoz"`
-	TimelineForMigratingToSignoz string   `json:"timeline_for_migrating_to_signoz"`
-	ReasonsForInterestInSignoz   []string `json:"reasons_for_interest_in_signoz"`
-	LogsScalePerDayInGB          int      `json:"logs_scale_per_day_in_gb"`
-	NumberOfHosts                int      `json:"number_of_hosts"`
-	NumberOfServices             int      `json:"number_of_services"`
+	ReasonsForInterestInSigNoz   []string `json:"reasons_for_interest_in_signoz"`
+	LogsScalePerDayInGB          int64    `json:"logs_scale_per_day_in_gb"`
+	NumberOfServices             int64    `json:"number_of_services"`
+	NumberOfHosts                int64    `json:"number_of_hosts"`
+	WhereDidYouDiscoverSigNoz    string   `json:"where_did_you_discover_signoz"`
+	TimelineForMigratingToSigNoz string   `json:"timeline_for_migrating_to_signoz"`
 }
