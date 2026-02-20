5cf5b70aca2bafcff4ec1f0b7fdfc6043b39a01c
Abhi kumar <ahrefabhi@gmail.com>
2026-02-20 11:17:16 +0530

    fix: added a fix for tooltip height when legend is too big (#10353)
    * fix: added a fix for tooltip height when legend is too big
    
    * fix: minor changes
    
    * chore: refactored tooltip compute code + added test for tooltip
    
    * chore: added test for tooltip util
    
    * chore: pr review changes

--- frontend/src/lib/uPlotV2/components/Tooltip/TimeSeriesTooltip.tsx
+++ frontend/src/lib/uPlotV2/components/Tooltip/TimeSeriesTooltip.tsx
@@ -1,8 +1,31 @@
-import { TimeSeriesTooltipProps } from '../types';
+import { useMemo } from 'react';
+
+import { TimeSeriesTooltipProps, TooltipContentItem } from '../types';
 import Tooltip from './Tooltip';
+import { buildTooltipContent } from './utils';
 
 export default function TimeSeriesTooltip(
 	props: TimeSeriesTooltipProps,
 ): JSX.Element {
-	return <Tooltip {...props} />;
+	const content = useMemo(
+		(): TooltipContentItem[] =>
+			buildTooltipContent({
+				data: props.uPlotInstance.data,
+				series: props.uPlotInstance.series,
+				dataIndexes: props.dataIndexes,
+				activeSeriesIndex: props.seriesIndex,
+				uPlotInstance: props.uPlotInstance,
+				yAxisUnit: props.yAxisUnit ?? '',
+				decimalPrecision: props.decimalPrecision,
+			}),
+		[
+			props.uPlotInstance,
+			props.seriesIndex,
+			props.dataIndexes,
+			props.yAxisUnit,
+			props.decimalPrecision,
+		],
+	);
+
+	return <Tooltip {...props} content={content} />;
 }
