b011c8bfb67bbcd9ebb5f8f3f3f78638845575d2
swapnil-signoz <swapnil@signoz.io>
2026-02-25 10:03:10 +0530

    chore: adding more comments

--- pkg/types/integrationtypes/cloudintegration.go
+++ pkg/types/integrationtypes/cloudintegration.go
@@ -56,18 +56,31 @@ type CloudProvider interface {
 
 	// AgentCheckIn is called by agent to heartbeat and get latest config in response.
 	AgentCheckIn(ctx context.Context, req *PostableAgentCheckInPayload) (any, error)
+	// GenerateConnectionArtifact generates cloud provider specific connection information, client side handles how this information is shown
 	GenerateConnectionArtifact(ctx context.Context, req *PostableConnectionArtifact) (any, error)
+	// GetAccountStatus returns agent connection status for a cloud integration account
 	GetAccountStatus(ctx context.Context, orgID, accountID string) (*GettableAccountStatus, error)
-
-	ListServices(ctx context.Context, orgID string, accountID *string) (any, error) // returns either GettableAWSServices or GettableAzureServices
-	GetServiceDetails(ctx context.Context, req *GetServiceDetailsReq) (any, error)
+	// ListConnectedAccounts lists accounts where agent is connected
 	ListConnectedAccounts(ctx context.Context, orgID string) (*GettableConnectedAccountsList, error)
+
+	// LIstServices return list of services for a cloud provider attached with the accountID. This just returns a summary
+	ListServices(ctx context.Context, orgID string, accountID *string) (any, error) // returns either GettableAWSServices or GettableAzureServices
+	// GetServiceDetails returns service definition details for a serviceId. This returns config and other details required to show in service details page on client.
+	GetServiceDetails(ctx context.Context, req *GetServiceDetailsReq) (any, error)
+
+	// GetDashboard returns dashboard json for a give cloud integration service dashboard.
+	// this only returns the dashboard when account is connected and service is enabled
 	GetDashboard(ctx context.Context, id string, orgID valuer.UUID) (*dashboardtypes.Dashboard, error)
+	// GetAvailableDashboards returns list of available dashboards across all connected cloud integration accounts in the org.
+	// this list gets added to dashboard list page
 	GetAvailableDashboards(ctx context.Context, orgID valuer.UUID) ([]*dashboardtypes.Dashboard, error)
 
+	// UpdateAccountConfig updates cloud integration account config
 	UpdateAccountConfig(ctx context.Context, orgId valuer.UUID, accountId string, config []byte) (any, error)
+	// UpdateServiceConfig updates cloud integration service config
 	UpdateServiceConfig(ctx context.Context, serviceId string, orgID valuer.UUID, config []byte) (any, error)
 
+	// DisconnectAccount soft deletes/removes a cloud integration account.
 	DisconnectAccount(ctx context.Context, orgID, accountID string) (*CloudIntegration, error)
 }
 
