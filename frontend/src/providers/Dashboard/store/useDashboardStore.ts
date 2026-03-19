fe9248fb8dfe6f2cb21fc3e078f6e86580f66de6
Ashwin Bhatkal <ashwin96@gmail.com>
2026-03-19 09:38:24 +0530

    chore: move dashboard provider from redux to zustand

---
+++ frontend/src/providers/Dashboard/store/useDashboardStore.ts
@@ -0,0 +1,30 @@
+import { create } from 'zustand';
+import { immer } from 'zustand/middleware/immer';
+
+import {
+	createDashboardLayoutSlice,
+	DashboardLayoutSlice,
+	initialDashboardLayoutState,
+} from './slices/dashboardLayoutSlice';
+import {
+	createDashboardUISlice,
+	DashboardUISlice,
+	initialDashboardUIState,
+} from './slices/dashboardUISlice';
+
+export type DashboardStore = DashboardUISlice &
+	DashboardLayoutSlice & {
+		reset: () => void;
+	};
+
+export const useDashboard = create<DashboardStore>()(
+	immer((set, get, api) => ({
+		...createDashboardUISlice(set, get, api),
+		...createDashboardLayoutSlice(set, get, api),
+
+		reset: (): void =>
+			set((state: DashboardStore) => {
+				Object.assign(state, initialDashboardUIState, initialDashboardLayoutState);
+			}),
+	})),
+);
