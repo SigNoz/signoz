650193330ce19a32fdd8ec19f619ef2ee48116c7
grandwizard28 <vibhupandey28@gmail.com>
2026-03-22 19:18:20 +0530

    feat(apiserver): move health endpoints to /api/v2/ and register readyz, livez

--- pkg/apiserver/signozapiserver/healthz.go
+++ pkg/apiserver/signozapiserver/healthz.go
@@ -8,12 +8,38 @@ import (
 )
 
 func (provider *provider) addHealthzRoutes(router *mux.Router) error {
-	if err := router.Handle("/healthz", handler.New(
+	if err := router.Handle("/api/v2/healthz", handler.New(
 		provider.authZ.OpenAccess(provider.factoryHandler.Healthz),
 		handler.OpenAPIDef{
 			ID:                  "Healthz",
 			Tags:                []string{"health"},
 			Summary:             "Health check",
+			ResponseContentType: "application/json",
+			SuccessStatusCode:   http.StatusOK,
+		},
+	)).Methods(http.MethodGet).GetError(); err != nil {
+		return err
+	}
+
+	if err := router.Handle("/api/v2/readyz", handler.New(
+		provider.authZ.OpenAccess(provider.factoryHandler.Readyz),
+		handler.OpenAPIDef{
+			ID:                  "Readyz",
+			Tags:                []string{"health"},
+			Summary:             "Readiness check",
+			ResponseContentType: "application/json",
+			SuccessStatusCode:   http.StatusOK,
+		},
+	)).Methods(http.MethodGet).GetError(); err != nil {
+		return err
+	}
+
+	if err := router.Handle("/api/v2/livez", handler.New(
+		provider.authZ.OpenAccess(provider.factoryHandler.Livez),
+		handler.OpenAPIDef{
+			ID:                  "Livez",
+			Tags:                []string{"health"},
+			Summary:             "Liveness check",
 			ResponseContentType: "application/json",
 			SuccessStatusCode:   http.StatusOK,
 		},
