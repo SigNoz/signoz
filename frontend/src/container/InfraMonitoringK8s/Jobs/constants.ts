c3199941115394c905a84a468100fc04e1bebc80
Vinícius Lourenço <vinicius@signoz.io>
2026-04-03 10:08:48 -0300

    feat(infra-monitoring): migrate jobs to shared component

--- frontend/src/container/InfraMonitoringK8s/Jobs/constants.ts
+++ frontend/src/container/InfraMonitoringK8s/Jobs/constants.ts
@@ -1,10 +1,71 @@
-import { K8sJobsData } from 'api/infraMonitoring/getK8sJobsList';
 import { PANEL_TYPES } from 'constants/queryBuilder';
 import { GetQueryResultsProps } from 'lib/dashboard/getQueryResults';
 import { DataTypes } from 'types/api/queryBuilder/queryAutocompleteResponse';
+import { TagFilter } from 'types/api/queryBuilder/queryBuilderData';
 import { EQueryType } from 'types/common/dashboard';
 import { DataSource, ReduceOperators } from 'types/common/queryBuilder';
 import { v4 } from 'uuid';
+
+import {
+	createFilterItem,
+	K8sDetailsMetadataConfig,
+} from '../Base/K8sBaseDetails';
+import { QUERY_KEYS } from '../EntityDetailsUtils/utils';
+import { K8sJobsData } from './api';
+
+export const k8sJobGetSelectedItemFilters = (
+	selectedItemId: string,
+): TagFilter => ({
+	op: 'AND',
+	items: [
+		{
+			id: 'k8s_job_name',
+			key: {
+				key: 'k8s_job_name',
+				type: null,
+			},
+			op: '=',
+			value: selectedItemId,
+		},
+	],
+});
+
+export const k8sJobDetailsMetadataConfig: K8sDetailsMetadataConfig<K8sJobsData>[] = [
+	{
+		label: 'Job Name',
+		getValue: (p): string => p.meta.k8s_job_name,
+	},
+	{
+		label: 'Cluster Name',
+		getValue: (p): string => p.meta.k8s_cluster_name,
+	},
+	{
+		label: 'Namespace Name',
+		getValue: (p): string => p.meta.k8s_namespace_name,
+	},
+];
+
+export const k8sJobInitialFilters = [
+	QUERY_KEYS.K8S_JOB_NAME,
+	QUERY_KEYS.K8S_NAMESPACE_NAME,
+];
+
+export const k8sJobInitialEventsFilter = (
+	item: K8sJobsData,
+): ReturnType<typeof createFilterItem>[] => [
+	createFilterItem(QUERY_KEYS.K8S_OBJECT_KIND, 'Job'),
+	createFilterItem(QUERY_KEYS.K8S_OBJECT_NAME, item.meta.k8s_job_name),
+];
+
+export const k8sJobInitialLogTracesFilter = (
+	item: K8sJobsData,
+): ReturnType<typeof createFilterItem>[] => [
+	createFilterItem(QUERY_KEYS.K8S_JOB_NAME, item.meta.k8s_job_name),
+	createFilterItem(QUERY_KEYS.K8S_NAMESPACE_NAME, item.meta.k8s_namespace_name),
+];
+
+export const k8sJobGetEntityName = (item: K8sJobsData): string =>
+	item.meta.k8s_job_name;
 
 export const jobWidgetInfo = [
 	{
