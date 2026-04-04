f76a387c1050fa0ed8ac40e6e1015e66a7040259
grandwizard28 <vibhupandey28@gmail.com>
2026-04-04 19:54:34 +0530

    style: formatting fix in tables file

--- pkg/telemetryaudit/tables.go
+++ pkg/telemetryaudit/tables.go
@@ -1,12 +1,12 @@
 package telemetryaudit
 
 const (
-	DBName                        = "signoz_audit"
-	LogsTableName                 = "distributed_logs"
-	LogsLocalTableName            = "logs"
+	DBName                      = "signoz_audit"
+	LogsTableName               = "distributed_logs"
+	LogsLocalTableName          = "logs"
 	TagAttributesTableName      = "distributed_tag_attributes"
 	TagAttributesLocalTableName = "tag_attributes"
-	LogAttributeKeysTblName       = "distributed_logs_attribute_keys"
-	LogResourceKeysTblName        = "distributed_logs_resource_keys"
-	LogsResourceTableName         = "distributed_logs_resource"
+	LogAttributeKeysTblName     = "distributed_logs_attribute_keys"
+	LogResourceKeysTblName      = "distributed_logs_resource_keys"
+	LogsResourceTableName       = "distributed_logs_resource"
 )
