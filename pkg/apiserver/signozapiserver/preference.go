68ab3917ee9369c4b3ce3762c99ce37ab7710e3a
grandwizard28 <vibhupandey28@gmail.com>
2026-05-11 14:15:44 +0530

    feat(apiserver): instrument user-preference and org-preference routes with AuditDef

--- pkg/apiserver/signozapiserver/preference.go
+++ pkg/apiserver/signozapiserver/preference.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/SigNoz/signoz/pkg/http/handler"
 	"github.com/SigNoz/signoz/pkg/types"
+	"github.com/SigNoz/signoz/pkg/types/audittypes"
+	"github.com/SigNoz/signoz/pkg/types/coretypes"
 	"github.com/SigNoz/signoz/pkg/types/preferencetypes"
 	"github.com/gorilla/mux"
 )
@@ -44,20 +46,29 @@ func (provider *provider) addPreferenceRoutes(router *mux.Router) error {
 		return err
 	}
 
-	if err := router.Handle("/api/v1/user/preferences/{name}", handler.New(provider.authzMiddleware.ViewAccess(provider.preferenceHandler.UpdateByUser), handler.OpenAPIDef{
-		ID:                  "UpdateUserPreference",
-		Tags:                []string{"preferences"},
-		Summary:             "Update user preference",
-		Description:         "This endpoint updates the user preference by name",
-		Request:             new(preferencetypes.UpdatablePreference),
-		RequestContentType:  "application/json",
-		Response:            nil,
-		ResponseContentType: "",
-		SuccessStatusCode:   http.StatusNoContent,
-		ErrorStatusCodes:    []int{http.StatusBadRequest, http.StatusNotFound},
-		Deprecated:          false,
-		SecuritySchemes:     newSecuritySchemes(types.RoleViewer),
-	})).Methods(http.MethodPut).GetError(); err != nil {
+	if err := router.Handle("/api/v1/user/preferences/{name}", handler.New(
+		provider.authzMiddleware.ViewAccess(provider.preferenceHandler.UpdateByUser),
+		handler.OpenAPIDef{
+			ID:                  "UpdateUserPreference",
+			Tags:                []string{"preferences"},
+			Summary:             "Update user preference",
+			Description:         "This endpoint updates the user preference by name",
+			Request:             new(preferencetypes.UpdatablePreference),
+			RequestContentType:  "application/json",
+			Response:            nil,
+			ResponseContentType: "",
+			SuccessStatusCode:   http.StatusNoContent,
+			ErrorStatusCodes:    []int{http.StatusBadRequest, http.StatusNotFound},
+			Deprecated:          false,
+			SecuritySchemes:     newSecuritySchemes(types.RoleViewer),
+		},
+		handler.WithAuditDef(handler.BasicAuditDef{
+			Resource:   coretypes.ResourceMetaResourceUserPreference,
+			Verb:       coretypes.VerbUpdate,
+			Category:   audittypes.ActionCategoryConfigurationChange,
+			ResourceID: handler.PathParam("name"),
+		}),
+	)).Methods(http.MethodPut).GetError(); err != nil {
 		return err
 	}
 
@@ -95,20 +106,29 @@ func (provider *provider) addPreferenceRoutes(router *mux.Router) error {
 		return err
 	}
 
-	if err := router.Handle("/api/v1/org/preferences/{name}", handler.New(provider.authzMiddleware.AdminAccess(provider.preferenceHandler.UpdateByOrg), handler.OpenAPIDef{
-		ID:                  "UpdateOrgPreference",
-		Tags:                []string{"preferences"},
-		Summary:             "Update org preference",
-		Description:         "This endpoint updates the org preference by name",
-		Request:             new(preferencetypes.UpdatablePreference),
-		RequestContentType:  "application/json",
-		Response:            nil,
-		ResponseContentType: "",
-		SuccessStatusCode:   http.StatusNoContent,
-		ErrorStatusCodes:    []int{http.StatusBadRequest, http.StatusNotFound},
-		Deprecated:          false,
-		SecuritySchemes:     newSecuritySchemes(types.RoleAdmin),
-	})).Methods(http.MethodPut).GetError(); err != nil {
+	if err := router.Handle("/api/v1/org/preferences/{name}", handler.New(
+		provider.authzMiddleware.AdminAccess(provider.preferenceHandler.UpdateByOrg),
+		handler.OpenAPIDef{
+			ID:                  "UpdateOrgPreference",
+			Tags:                []string{"preferences"},
+			Summary:             "Update org preference",
+			Description:         "This endpoint updates the org preference by name",
+			Request:             new(preferencetypes.UpdatablePreference),
+			RequestContentType:  "application/json",
+			Response:            nil,
+			ResponseContentType: "",
+			SuccessStatusCode:   http.StatusNoContent,
+			ErrorStatusCodes:    []int{http.StatusBadRequest, http.StatusNotFound},
+			Deprecated:          false,
+			SecuritySchemes:     newSecuritySchemes(types.RoleAdmin),
+		},
+		handler.WithAuditDef(handler.BasicAuditDef{
+			Resource:   coretypes.ResourceMetaResourceOrgPreference,
+			Verb:       coretypes.VerbUpdate,
+			Category:   audittypes.ActionCategoryConfigurationChange,
+			ResourceID: handler.PathParam("name"),
+		}),
+	)).Methods(http.MethodPut).GetError(); err != nil {
 		return err
 	}
 
