8a0441293a130d5e09fedd622cd68c832beac947
Naman Verma <naman.verma@signoz.io>
2026-05-15 12:17:44 +0530

    chore: revert all frontend changes

--- frontend/src/api/generated/services/dashboard/index.ts
+++ frontend/src/api/generated/services/dashboard/index.ts
@@ -24,8 +24,6 @@ import type {
 	DashboardtypesPostablePublicDashboardDTO,
 	DashboardtypesUpdatablePublicDashboardDTO,
 	DeletePublicDashboardPathParameters,
-	GetDashboardV2200,
-	GetDashboardV2PathParameters,
 	GetPublicDashboard200,
 	GetPublicDashboardData200,
 	GetPublicDashboardDataPathParameters,
@@ -33,8 +31,6 @@ import type {
 	GetPublicDashboardWidgetQueryRange200,
 	GetPublicDashboardWidgetQueryRangePathParameters,
 	RenderErrorResponseDTO,
-	UpdateDashboardV2200,
-	UpdateDashboardV2PathParameters,
 	UpdatePublicDashboardPathParameters,
 } from '../sigNoz.schemas';
 
