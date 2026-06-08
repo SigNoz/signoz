7ff6c43460055694e115eaef16b6dd818b74e306
swapnil-signoz <swapnil@signoz.io>
2026-06-08 23:42:41 +0530

    feat: adding support for Azure AKS

---
+++ pkg/modules/cloudintegration/implcloudintegration/fs/definitions/azure/aks/overview.md
@@ -0,0 +1,3 @@
+### Monitor Azure Kubernetes Service (AKS) with SigNoz
+
+Collect key AKS metrics and view them with an out of the box dashboard.
--- pkg/types/cloudintegrationtypes/serviceid.go
+++ pkg/types/cloudintegrationtypes/serviceid.go
@@ -27,6 +27,7 @@ var (
 	// Azure services.
 	AzureServiceStorageAccountsBlob = ServiceID{valuer.NewString("storageaccountsblob")}
 	AzureServiceCDNProfile          = ServiceID{valuer.NewString("cdnprofile")}
+	AzureServiceAKS                 = ServiceID{valuer.NewString("aks")}
 )
 
 func (ServiceID) Enum() []any {
@@ -46,6 +47,7 @@ func (ServiceID) Enum() []any {
 		AWSServiceSQS,
 		AzureServiceStorageAccountsBlob,
 		AzureServiceCDNProfile,
+		AzureServiceAKS,
 	}
 }
 
@@ -69,6 +71,7 @@ var SupportedServices = map[CloudProviderType][]ServiceID{
 	CloudProviderTypeAzure: {
 		AzureServiceStorageAccountsBlob,
 		AzureServiceCDNProfile,
+		AzureServiceAKS,
 	},
 }
 
