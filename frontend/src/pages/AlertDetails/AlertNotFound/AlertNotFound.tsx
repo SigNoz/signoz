b15c4e4ba9f5479575b0ab35e742de4c4d499b47
Vinícius Lourenço <vinicius@signoz.io>
2026-06-08 10:07:50 -0300

    refactor(alert-not-found): migrate to css modules

--- frontend/src/pages/AlertDetails/AlertNotFound/AlertNotFound.tsx
+++ frontend/src/pages/AlertDetails/AlertNotFound/AlertNotFound.tsx
@@ -12,7 +12,7 @@ import broomUrl from '@/assets/Icons/broom.svg';
 import constructionUrl from '@/assets/Icons/construction.svg';
 import noDataUrl from '@/assets/Icons/no-data.svg';
 
-import './AlertNotFound.styles.scss';
+import styles from './AlertNotFound.module.scss';
 
 interface AlertNotFoundProps {
 	isTestAlert: boolean;
@@ -31,39 +31,43 @@ function AlertNotFound({ isTestAlert }: AlertNotFoundProps): JSX.Element {
 	};
 
 	return (
-		<div className="alert-not-found">
-			<section className="description">
-				<img src={noDataUrl} alt="no-data" className="not-found-img" />
-				<Typography.Text className="not-found-text">
+		<div className={styles.alertNotFound}>
+			<section className={styles.description}>
+				<img src={noDataUrl} alt="no-data" className={styles.notFoundImg} />
+				<Typography.Text className={styles.notFoundText}>
 					Uh-oh! We couldn&apos;t find the given alert rule.
 				</Typography.Text>
-				<Typography.Text className="not-found-text">
+				<Typography.Text className={styles.notFoundText}>
 					{isTestAlert
 						? 'This can happen in the following scenario -'
 						: 'This can happen in either of the following scenarios -'}
 				</Typography.Text>
 			</section>
-			<section className="reasons">
+			<section className={styles.reasons}>
 				{!isTestAlert && (
 					<>
-						<div className="reason">
-							<img src={constructionUrl} alt="no-data" className="construction-img" />
-							<Typography.Text className="text">
+						<div className={styles.reason}>
+							<img
+								src={constructionUrl}
+								alt="no-data"
+								className={styles.constructionImg}
+							/>
+							<Typography.Text className={styles.reasonText}>
 								The alert rule link is incorrect, please verify it once.
 							</Typography.Text>
 						</div>
-						<div className="reason">
-							<img src={broomUrl} alt="no-data" className="broom-img" />
-							<Typography.Text className="text">
+						<div className={styles.reason}>
+							<img src={broomUrl} alt="no-data" className={styles.broomImg} />
+							<Typography.Text className={styles.reasonText}>
 								The alert rule you&apos;re trying to check has been deleted.
 							</Typography.Text>
 						</div>
 					</>
 				)}
 				{isTestAlert && (
-					<div className="reason">
-						<img src={broomUrl} alt="no-data" className="broom-img" />
-						<Typography.Text className="text">
+					<div className={styles.reason}>
+						<img src={broomUrl} alt="no-data" className={styles.broomImg} />
+						<Typography.Text className={styles.reasonText}>
 							You clicked on the Alert notification link received when testing a new
 							Alert rule. Once the alert rule is saved, future notifications will link
 							to actual alerts.
@@ -71,20 +75,20 @@ function AlertNotFound({ isTestAlert }: AlertNotFoundProps): JSX.Element {
 					</div>
 				)}
 			</section>
-			<section className="none-of-above">
-				<Typography.Text className="text">
+			<section className={styles.noneOfAbove}>
+				<Typography.Text className={styles.noneOfAboveText}>
 					If you feel the issue is none of the above, please contact support.
 				</Typography.Text>
-				<div className="action-btns">
+				<div className={styles.actionBtns}>
 					<Button
-						className="action-btn"
+						className={styles.actionBtn}
 						icon={<List size={14} />}
 						onClick={checkAllRulesHandler}
 					>
 						Check all rules
 					</Button>
 					<Button
-						className="action-btn"
+						className={styles.actionBtn}
 						icon={<LifeBuoy size={14} />}
 						onClick={contactSupportHandler}
 					>
