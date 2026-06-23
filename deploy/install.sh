caf10bf5bd458e1799f02936fb3fc04796d34162
Miguel Valdes <mvaldes@signoz.io>
2026-06-23 14:59:31 -0500

    feat(deprecation): include link in script

--- deploy/install.sh
+++ deploy/install.sh
@@ -3,16 +3,18 @@
 set -o errexit
 
 # Regular Colors
-Yellow='\033[0;33m'       # Yellow
-Green='\033[0;32m'        # Green
-NC='\033[0m'              # No Color
+Yellow='\033[0;33m' # Yellow
+Green='\033[0;32m'  # Green
+NC='\033[0m'        # No Color
 
 echo ""
 echo -e "👋 Thank you for trying out SigNoz!"
 echo ""
-echo -e "${Yellow}⚠️  This install script has been deprecated and is no longer maintained.${NC}"
-echo ""
 echo -e "SigNoz now installs and runs through ${Green}Foundry${NC}."
+echo ""
+echo -e "${Yellow}⚠️  This install script has been deprecated and is no longer maintained.${NC}"
+echo -e "${Yellow}⚠️  Please see https://github.com/SigNoz/signoz/blob/main/deploy/README.md for new installation and migrations to Foundry.${NC}"
+echo ""
 echo ""
 echo -e "Please follow the latest installation instructions here:"
 echo -e "${Green}👉 https://signoz.io/docs/install/docker/${NC}"
