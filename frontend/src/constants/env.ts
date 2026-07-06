e3e2c52873229c2f957e20e6613eb9118004eb7b
Gaurav Tewari <tewarig@users.noreply.github.com>
2026-07-06 18:37:26 +0530

    chore: revert evn commit

--- frontend/src/constants/env.ts
+++ frontend/src/constants/env.ts
@@ -1,9 +1,7 @@
 export const ENVIRONMENT = {
-	// baseURL:
-	// 	process?.env?.FRONTEND_API_ENDPOINT ||
-	// 	process?.env?.GITPOD_WORKSPACE_URL?.replace('://', '://8080-') ||
-	// 	'',
-	// wsURL: process?.env?.WEBSOCKET_API_ENDPOINT || '',
-	baseURL: 'https://app.us.staging.signoz.cloud',
-	wsURL: 'ws://app.us.staging.signoz.cloud',
+	baseURL:
+		process?.env?.FRONTEND_API_ENDPOINT ||
+		process?.env?.GITPOD_WORKSPACE_URL?.replace('://', '://8080-') ||
+		'',
+	wsURL: process?.env?.WEBSOCKET_API_ENDPOINT || '',
 };
