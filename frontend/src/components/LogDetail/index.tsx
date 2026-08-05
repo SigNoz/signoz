d6a069769e1585aa66033505adaf28280204afbb
aks07 <adityasinghssj1@gmail.com>
2026-08-06 01:42:35 +0530

    feat: render dataviewer in the log details overview

--- frontend/src/components/LogDetail/index.tsx
+++ frontend/src/components/LogDetail/index.tsx
@@ -435,6 +435,8 @@ function LogDetailInner({
 
 				{isLogDetailsV2 && <LogHighlights log={log} />}
 
+				{isLogDetailsV2 && <div className="log-detail-drawer__section-divider" />}
+
 				<div className="tabs-and-search">
 					<ToggleGroupSimple
 						type="single"
